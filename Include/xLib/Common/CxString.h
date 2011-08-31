025c27eaef6be8b48431a69faaa9c53b0754d4c2
skynowa <skynowa@gmail.com>
2011-08-31 20:40:31 +0300

    ADD: CxString::vsnprintf_safe

--- Include/xLib/Common/CxString.h
+++ Include/xLib/Common/CxString.h
@@ -164,6 +164,9 @@ class CxString :
             ///< constructor
         virtual             ~CxString          ();
             ///< destructor
+
+       static  INT           vsnprintf_safe    (TCHAR *pszBuff, const size_t  cuiBuffSize, const TCHAR *pcszFormat, va_list alList);
+            ///< call _vsnprintf, but always NUL-terminate
 };
 //---------------------------------------------------------------------------
 #include <Common/CxString.inl>
