6111d7623a9cb0ae5492aa4161a822e073d8cddc
skynowa <skynowa@gmail.com>
2011-10-29 22:14:39 +0300

    FIX: CxStackTrace

--- Source/Debug/CxStackTrace.cpp
+++ Source/Debug/CxStackTrace.cpp
@@ -41,30 +41,6 @@ CxStackTrace::~CxStackTrace() {
 *
 *****************************************************************************/
 
-void printStack( void ) {
-     unsigned int   i;
-     void         * stack[ 100 ];
-     unsigned short frames;
-     SYMBOL_INFO  * symbol;
-     HANDLE         process;
-
-     process = GetCurrentProcess();
-
-     SymInitialize( process, NULL, TRUE );
-
-     frames               = CaptureStackBackTrace( 0, 100, stack, NULL );
-     symbol               = ( SYMBOL_INFO * )calloc( sizeof( SYMBOL_INFO ) + 256 * sizeof( char ), 1 );
-     symbol->MaxNameLen   = 255;
-     symbol->SizeOfStruct = sizeof( SYMBOL_INFO );
-
-     for( i = 0; i < frames; i++ )  {
-         SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
-
-         printf( "%i: %s - 0x%0X\n", frames - i - 1, symbol->Name, symbol->Address );
-     }
-
-     free( symbol );
-}
 //---------------------------------------------------------------------------
 BOOL
 CxStackTrace::bGet(
