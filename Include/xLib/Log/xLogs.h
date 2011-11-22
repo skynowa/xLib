1f890cec7054496967f05036396dc23fcbaa34a9
sergey <sergey@exmediasoft.com>
2011-11-22 18:25:22 +0200

    REVIEW: Log

--- Include/xLib/Log/xLogs.h
+++ Include/xLib/Log/xLogs.h
@@ -8,12 +8,9 @@
 #define xLib_Logs_xLogH
 //---------------------------------------------------------------------------
 #include <xLib/Log/CxConsoleLog.h>
-#include <xLib/Log/CxTraceLog.h>
 #include <xLib/Log/CxFileLog.h>
 #include <xLib/Log/CxSystemLog.h>
-
-#if xOS_ENV_WIN
-    #include <xLib/Log/Win/CxWndLog.h>
-#endif
+#include <xLib/Log/CxTraceLog.h>
+#include <xLib/Log/CxWndLog.h>
 //---------------------------------------------------------------------------
 #endif    //xLib_Logs_xLogH
