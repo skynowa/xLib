eb7ab83c3d656950d2a80055671954d601fbd7db
sergey <sergey@exmediasoft.com>
2011-11-23 18:18:01 +0200

    JOB: CxSystemLog

--- Source/Log/CxSystemLog.cpp
+++ Source/Log/CxSystemLog.cpp
@@ -9,6 +9,12 @@
 #include <xLib/Common/CxDateTime.h>
 #include <xLib/Filesystem/CxPath.h>
 
+#if defined(xOS_ENV_WIN)
+    #include <syslog.h>
+#elif defined(xOS_ENV_UNIX)
+
+#endif
+
 
 xNAMESPACE_BEGIN(NxLib)
 
@@ -18,12 +24,82 @@ xNAMESPACE_BEGIN(NxLib)
 *****************************************************************************/
 
 //---------------------------------------------------------------------------
-CxSystemLog::CxSystemLog() {
+CxSystemLog::CxSystemLog(
+    const std::tstring_t &csLogName
+) :
+    _m_bIsEnable(true)
+{
+
+#if defined(xOS_ENV_WIN)
+    //HANDLE OpenEventLog(__in  LPCTSTR lpUNCServerName, __in  LPCTSTR lpSourceName);
+#elif defined(xOS_ENV_UNIX)
+    //(void)::openlog(csLogName.c_str(), int __option, int __facility);
+
+    //int setlogmask (int __mask) __THROW;
+#endif
 
 }
 //---------------------------------------------------------------------------
+/*virtual*/
 CxSystemLog::~CxSystemLog() {
+#if defined(xOS_ENV_WIN)
+    //BOOL CloseEventLog(__inout  HANDLE hEventLog);
+#elif defined(xOS_ENV_UNIX)
+    (void)::closelog();
+#endif
+}
+//---------------------------------------------------------------------------
+bool
+CxTraceLog::bSetEnabled(
+    const bool cbFlag
+)
+{
+    /*DEBUG*/// cbFlag - n/a
 
+    _m_bIsEnable = cbFlag;
+
+    return true;
+}
+//---------------------------------------------------------------------------
+bool
+CxSystemLog::bWrite(
+    const ELevel   lvLevel,
+    const tchar_t *pcszFormat, ...
+)
+{
+    /*DEBUG*/xASSERT_RET(NULL != pcszFormat, false);
+
+    xCHECK_RET(false == _m_bIsEnable, true);
+
+
+    //-------------------------------------
+    //comment
+    std::tstring_t sMessage;
+    va_list        alArgs;
+
+    xVA_START(alArgs, pcszFormat);
+    sMessage = CxString::sFormatV(pcszFormat, alArgs);
+    xVA_END(alArgs);
+
+    //-------------------------------------
+    //data
+    std::tstring_t sData;
+
+    sData = CxString::sFormat(
+                xT("[%s] %s"),
+                CxDateTime::dtGetCurrent().sFormat(CxDateTime::ftTime).c_str(),
+                sMessage.c_str()
+    );
+
+    //-------------------------------------
+    //write
+#if xOS_ENV_WIN
+
+#elif xOS_ENV_UNIX
+    (void)::syslog(lvLevel, sData.c_str());
+#endif
+
+    return true;
 }
 //---------------------------------------------------------------------------
 
