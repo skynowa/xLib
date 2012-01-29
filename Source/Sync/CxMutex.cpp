d96c54c3d1ba19dd660912125802bf182bd8d787
skynowa <skynowa@gmail.com>
2012-01-29 21:50:54 +0200

    REVIEW: CxMutex

--- Source/Sync/CxMutex.cpp
+++ Source/Sync/CxMutex.cpp
@@ -28,8 +28,7 @@ CxMutex::~CxMutex() {
 #if defined(xOS_ENV_WIN)
 
 #elif defined(xOS_ENV_UNIX)
-    int iRes = ::pthread_mutex_destroy(&_m_hHandle);
-    /*DEBUG*/xASSERT_MSG_DO(0 == iRes, CxLastError::sFormat(iRes), return);
+
 #endif
 }
 //---------------------------------------------------------------------------
@@ -46,37 +45,15 @@ CxMutex::bCreate(
 )
 {
     /*DEBUG*/xASSERT_RET(false == _m_hHandle.bIsValid(), false);
-    /*DEBUG*///csName       - n/a
+    /*DEBUG*///csName - n/a
 
 #if defined(xOS_ENV_WIN)
-    HANDLE hRes = NULL;
-
-    hRes = ::CreateMutex(NULL, FALSE, CxMacros::pcszAsCString(csName));
+    HANDLE hRes = ::CreateMutex(NULL, FALSE, CxMacros::pcszAsCString(csName));
     /*DEBUG*/xASSERT_RET(NULL != hRes, false);
 
     _m_hHandle.bSet(hRes);
 #elif defined(xOS_ENV_UNIX)
-    int iRes = - 1;
 
-    pthread_mutexattr_t maAttr;    // n/a {{0}}
-
-    iRes = ::pthread_mutexattr_init(&maAttr);
-    /*DEBUG*/xASSERT_MSG_RET(0 == iRes, CxLastError::sFormat(iRes), false);
-
-    //TODO: PTHREAD_PROCESS_PRIVATE or PTHREAD_PROCESS_SHARED
-    iRes = ::pthread_mutexattr_setpshared(&maAttr, PTHREAD_PROCESS_PRIVATE);
-    /*DEBUG*/xASSERT_MSG_RET(0 == iRes, CxLastError::sFormat(iRes), false);
-    
-    iRes = ::pthread_mutexattr_settype(&maAttr, PTHREAD_MUTEX_RECURSIVE);
-    /*DEBUG*/xASSERT_MSG_RET(0 == iRes, CxLastError::sFormat(iRes), false);
-
-    {
-        iRes = ::pthread_mutex_init(&_m_hHandle, &maAttr);
-        /*DEBUG*/xASSERT_MSG_RET(0 == iRes, CxLastError::sFormat(iRes), false);
-    }
-
-    iRes = ::pthread_mutexattr_destroy(&maAttr);
-    /*DEBUG*/xASSERT_MSG_RET(0 == iRes, CxLastError::sFormat(iRes), false);
 #endif
 
     return true;
@@ -91,14 +68,11 @@ CxMutex::bLock(
     /*DEBUG*///culTimeout - n/a
 
 #if defined(xOS_ENV_WIN)
-    DWORD ulRes = WAIT_FAILED;
-
-    ulRes = ::WaitForSingleObject(_m_hHandle.hGet(), culTimeout);
+    DWORD ulRes = ::WaitForSingleObject(_m_hHandle.hGet(), culTimeout);
     /*DEBUG*/xASSERT_RET(WAIT_OBJECT_0  == ulRes, false);
     /*DEBUG*/xASSERT_RET(WAIT_ABANDONED != ulRes, false);
 #elif defined(xOS_ENV_UNIX)
-    int iRes = ::pthread_mutex_lock(&_m_hHandle);
-    /*DEBUG*/xASSERT_MSG_RET(0 == iRes, CxLastError::sFormat(iRes), false);
+
 #endif
 
     return true;
@@ -112,8 +86,7 @@ CxMutex::bUnlock() const {
     BOOL bRes = ::ReleaseMutex(_m_hHandle.hGet());
     /*DEBUG*/xASSERT_RET(FALSE != bRes, false);
 #elif defined(xOS_ENV_UNIX)
-    int iRes = ::pthread_mutex_unlock(&_m_hHandle);
-    /*DEBUG*/xASSERT_MSG_RET(0 == iRes, CxLastError::sFormat(iRes), false);
+
 #endif
 
     return true;
