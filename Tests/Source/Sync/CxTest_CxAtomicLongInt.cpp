10e76bf06f2cf615fb7a8856b3e2be8025733a1c
skynowa <skynowa@gmail.com>
2012-03-06 18:25:02 +0200

    JOB: CxAtomicLongInt

--- Tests/Source/Sync/CxTest_CxAtomicLongInt.cpp
+++ Tests/Source/Sync/CxTest_CxAtomicLongInt.cpp
@@ -22,36 +22,205 @@ CxTest_CxAtomicLongInt::bUnit(
     const ulonglong_t cullCaseLoops
 )
 {
-#if xTEMP_DISABLED
-    long_t iRes = 0;
+    //--------------------------------------------------
+    //operator += (const CxAtomicLongInt &cRight);
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+        CxAtomicLongInt alLong2;
 
-    CxAtomicLongInt alAL_1;
-    CxAtomicLongInt alAL_2;
+        alLong1 = 7L;
+        alLong2 = 10L;
 
-    alAL_1 = 1;
-    alAL_2 = 2;
+        alLong1 += alLong2;
+        xTEST_EQ(17L, alLong1.liGetValue());
+    }
 
-    alAL_1 += alAL_2;
-    /*DEBUG*/xTEST_EQ(alAL_1, 3L);
+    //--------------------------------------------------
+    //operator += (const long_t cliRight);
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
 
-    alAL_1 -= alAL_2;
-    /*DEBUG*/xTEST_EQ(alAL_1, 1L);
+        alLong1 = 7L;
 
-    alAL_1  = alAL_2;
-    /*DEBUG*/xTEST_EQ(alAL_1, 2L);
+        alLong1 += 10L;
+        xTEST_EQ(17L, alLong1.liGetValue());
+    }
 
-    alAL_1 += 10;
-    /*DEBUG*/xTEST_EQ(alAL_1, 12L);
+    //--------------------------------------------------
+    //operator -= (const CxAtomicLongInt &cRight);
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+        CxAtomicLongInt alLong2;
 
-    alAL_1 -= 10;
-    /*DEBUG*/xTEST_EQ(alAL_1, 2L);
+        alLong1 = 7L;
+        alLong2 = 10L;
 
-    alAL_1  = 10;
-    /*DEBUG*/xTEST_EQ(alAL_1, 10L);
+        alLong1 -= alLong2;
+        xTEST_EQ(- 3L, alLong1.liGetValue());
+    }
 
+    //--------------------------------------------------
+    //operator -= (const long_t cliRight);
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
 
-    iRes = alAL_1;
-#endif
+        alLong1 = 7L;
+
+        alLong1 -= 10L;
+        xTEST_EQ(- 3L, alLong1.liGetValue());
+    }
+
+    //--------------------------------------------------
+    //operator = (const CxAtomicLongInt &cRight);
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+        CxAtomicLongInt alLong2;
+
+        alLong1 = 7L;
+        alLong2 = 10L;
+
+        alLong1 = alLong2;
+        xTEST_EQ(10L, alLong1.liGetValue());
+    }
+
+    //--------------------------------------------------
+    //operator = (const long_t cliRight)
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+
+        alLong1 = 7L;
+
+        alLong1 = 10L;
+        xTEST_EQ(10L, alLong1.liGetValue());
+    }
+
+    //--------------------------------------------------
+    //operator == (const CxAtomicLongInt &cRight)
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+        CxAtomicLongInt alLong2;
+
+        alLong1 = 7L;
+        alLong2 = 7L;
+
+        m_bRes = ( alLong1 == alLong2 );
+        xTEST_EQ(true, m_bRes);
+    }
+
+    //--------------------------------------------------
+    //operator == (const long_t cliRight)
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+
+        alLong1 = 7L;
+
+        m_bRes = ( alLong1 == 7L );
+        xTEST_EQ(true, m_bRes);
+    }
+
+    //--------------------------------------------------
+    //operator != (const CxAtomicLongInt &cRight)
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+        CxAtomicLongInt alLong2;
+
+        alLong1 = 7L;
+        alLong2 = 7L;
+
+        m_bRes = ( alLong1 != alLong2 );
+        xTEST_EQ(false, m_bRes);
+    }
+
+    //--------------------------------------------------
+    //operator != (const long_t cliRight)
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+
+        alLong1 = 7L;
+
+        m_bRes = ( alLong1 != 7L );
+        xTEST_EQ(false, m_bRes);
+    }
+
+    //--------------------------------------------------
+    //operator ++ (const int ciPos)
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+
+        alLong1 = 7L;
+
+        alLong1 ++;
+        xTEST_EQ(8L, alLong1.liGetValue());
+    }
+
+    //--------------------------------------------------
+    //operator -- (const int ciPos)
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+
+        alLong1 = 7L;
+
+        alLong1 --;
+        xTEST_EQ(6L, alLong1.liGetValue());
+    }
+
+    //--------------------------------------------------
+    //liGetValue
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+        CxAtomicLongInt alLong2;
+
+        alLong1 = 7L;
+        alLong2 = 10L;
+
+        xTEST_EQ(7L,  alLong1.liGetValue());
+        xTEST_EQ(10L, alLong2.liGetValue());
+    }
+
+    //--------------------------------------------------
+    //common
+    xTEST_CASE(cullCaseLoops)
+    {
+        CxAtomicLongInt alLong1;
+        CxAtomicLongInt alLong2;
+
+        alLong1 = 1L;
+        alLong2 = 2L;
+
+        alLong1 += alLong2;
+        xTEST_EQ(alLong1.liGetValue(), 3L);
+
+        alLong1 -= alLong2;
+        xTEST_EQ(alLong1.liGetValue(), 1L);
+
+        alLong1  = alLong2;
+        xTEST_EQ(alLong1.liGetValue(), 2L);
+
+        alLong1 += 10L;
+        xTEST_EQ(alLong1.liGetValue(), 12L);
+
+        alLong1 -= 10L;
+        xTEST_EQ(alLong1.liGetValue(), 2L);
+
+        alLong1  = 10L;
+        xTEST_EQ(alLong1.liGetValue(), 10L);
+
+        m_liRes = alLong1.liGetValue();
+        xTEST_EQ(10L, m_liRes);
+    }
 
     return true;
 }
