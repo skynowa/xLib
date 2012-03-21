d2c67d1680f1f0bf983706f0b16f0feea30ac016
skynowa <skynowa@gmail.com>
2012-03-21 23:36:33 +0200

    ADD: CxProcessInfo

---
+++ Tests/Include/Test/Common/CxTest_CxProcessInfo.h
@@ -0,0 +1,29 @@
+/**
+ * \file   CxTest_CxProcessInfo.h
+ * \brief  test CxProcessInfo
+ */
+
+
+#ifndef CxTest_Common_CxProcessInfoH
+#define CxTest_Common_CxProcessInfoH
+//---------------------------------------------------------------------------
+#include <xLib/Common/xCommon.h>
+#include <xLib/Debug/CxTest.h>
+#include <xLib/Common/CxProcessInfo.h>
+//---------------------------------------------------------------------------
+class CxTest_CxProcessInfo :
+    public CxTest
+{
+	public:
+		              CxTest_CxProcessInfo();
+            ///< constructor
+	    virtual      ~CxTest_CxProcessInfo();
+	        ///< destructor
+
+	    virtual bool  bUnit(const ulonglong_t cullBlockLoops);
+	        ///< unit test
+
+	private:
+};
+//---------------------------------------------------------------------------
+#endif //CxTest_Common_CxProcessInfoH
