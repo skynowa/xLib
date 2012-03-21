70a195d1f74d07e4b1a36ac4bcf803e36bf3f7a2
skynowa <skynowa@gmail.com>
2012-03-21 23:36:33 +0200

    ADD: CxProcessInfo

---
+++ Include/xLib/Common/CxProcessInfo.h
@@ -0,0 +1,25 @@
+/**
+ * \file   CxProcessInfo.h
+ * \brief
+ */
+
+
+#ifndef xLib_CxProcessInfoH
+#define xLib_CxProcessInfoH
+//---------------------------------------------------------------------------
+#include <xLib/Common/xCommon.h>
+//---------------------------------------------------------------------------
+class CxProcessInfo :
+    public CxNonCopyable
+{
+	public:
+				 CxProcessInfo();
+            ///< constructor
+		virtual ~CxProcessInfo();
+		    ///< destructor
+
+    private:
+
+};
+//---------------------------------------------------------------------------
+#endif //xLib_CxProcessInfoH
