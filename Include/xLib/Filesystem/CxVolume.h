357538258b8261493dbb9c676c03681cb67cef29
skynowa <skynowa@gmail.com>
2012-03-27 23:36:09 +0300

    ADD: CxVolume::sGetLabel(), CxVolume::bSetLabel() (Windows-part)

--- Include/xLib/Filesystem/CxVolume.h
+++ Include/xLib/Filesystem/CxVolume.h
@@ -16,20 +16,24 @@ class CxVolume :
     /// volume
 {
     public:
-        static bool  bIsValid     (const std::tstring_t &csVolumePath);
+        static bool           bIsValid (const std::tstring_t &csVolumePath);
             ///< is valid path
-        static bool  bIsReady     (const std::tstring_t &csVolumePath);
+        static bool           bIsReady (const std::tstring_t &csVolumePath);
             ///< is ready
-        static bool  bIsEmpty     (const std::tstring_t &csVolumePath);
+        static bool           bIsEmpty (const std::tstring_t &csVolumePath);
             ///< is empty
-        static bool  bGetFreeSpace(const std::tstring_t &csDirPath, ulonglong_t *pullAvailable, ulonglong_t *pullTotal, ulonglong_t *pullFree);
+        static bool           bGetSpace(const std::tstring_t &csDirPath, ulonglong_t *pullAvailable, ulonglong_t *pullTotal, ulonglong_t *pullFree);
             ///< get free space
-        static bool  bMount       (const std::tstring_t &csSourcePath, const std::tstring_t &csDestPath);
+        static bool           bMount   (const std::tstring_t &csSourcePath, const std::tstring_t &csDestPath);
             ///< mount
-        static bool  bUnMount     (const std::tstring_t &csSourcePath, const bool cbIsForce);
+        static bool           bUnMount (const std::tstring_t &csSourcePath, const bool cbIsForce);
             ///< unmount
-        static bool  bGetPaths    (std::vector<std::tstring_t> *pvsVolumePaths);
+        static bool           bGetPaths(std::vector<std::tstring_t> *pvsVolumePaths);
             ///< get paths
+        static std::tstring_t sGetLabel(const std::tstring_t &csVolumePath);
+            ///< get label
+        static bool           bSetLabel(const std::tstring_t &csVolumePath, const std::tstring_t &csVolumeName);
+            ///< set label
 
     #if   xOS_ENV_WIN
         enum EType
@@ -44,19 +48,19 @@ class CxVolume :
             dtRam       = DRIVE_RAMDISK
         };
 
-        static EType dtGetType    (const std::tstring_t &csVolumePath);
-        static bool  bGetInfo     (const std::tstring_t &csVolumePath,
-                                   std::tstring_t       *psVolumeName,
-                                   ulong_t              *pulVolumeSerialNumber,
-                                   ulong_t              *pulMaximumComponentLength,
-                                   ulong_t              *pulFileSystemFlags,
-                                   std::tstring_t       *psFileSystemName);
+        static EType          dtGetType(const std::tstring_t &csVolumePath);
+        static bool           bGetInfo (const std::tstring_t &csVolumePath,
+                                        std::tstring_t       *psVolumeName,
+                                        ulong_t              *pulVolumeSerialNumber,
+                                        ulong_t              *pulMaximumComponentLength,
+                                        ulong_t              *pulFileSystemFlags,
+                                        std::tstring_t       *psFileSystemName);
     #endif
 
     private:
-                     CxVolume     ();
+                              CxVolume ();
             ///< constructor
-        virtual     ~CxVolume     ();
+        virtual              ~CxVolume ();
             ///< destructor
 };
 
