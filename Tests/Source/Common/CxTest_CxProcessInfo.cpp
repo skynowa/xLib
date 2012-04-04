c6bbe7f6288e8643aa9c0a29a03d280a33b2dc07
skynowa <skynowa@gmail.com>
2012-04-04 23:32:24 +0300

    ADD: CxProcessInfo::sGetExeName (FreeBSD)

--- Tests/Source/Common/CxTest_CxProcessInfo.cpp
+++ Tests/Source/Common/CxTest_CxProcessInfo.cpp
@@ -8,6 +8,7 @@
 
 #include <xLib/Sync/CxCurrentProcess.h>
 #include <xLib/Filesystem/CxPath.h>
+#include <xLib/Filesystem/CxFile.h>
 
 
 //---------------------------------------------------------------------------
@@ -26,10 +27,11 @@ CxTest_CxProcessInfo::bUnit(
 )
 {
     //--------------------------------------------------
-    //ulGetCpuUsage
+    // ulGetCpuUsage
     xTEST_CASE(cullCaseLoops)
     {
         m_ulRes = CxProcessInfo::ulGetCpuUsage(CxCurrentProcess::ulGetId());
+        // TODO: CxProcessInfo::ulGetCpuUsage
         #if xTEST_IGNORE
             xTRACEV(xT("\tCxProcessInfo::ulGetCpuUsage(): %lu"), m_ulRes);
         #endif
@@ -37,31 +39,43 @@ CxTest_CxProcessInfo::bUnit(
     }
 
     //--------------------------------------------------
-    //ulGetRamUsage
+    // ulGetRamUsage
     xTEST_CASE(cullCaseLoops)
     {
         m_ulRes = CxProcessInfo::ulGetRamUsage(CxCurrentProcess::ulGetId());
+        // TODO: CxProcessInfo::ulGetRamUsage
         #if xTEST_IGNORE
             xTRACEV(xT("\tCxProcessInfo::ulGetRamUsage(): %lu"), m_ulRes);
         #endif
     }
 
     //--------------------------------------------------
-    //ulGetIOBytes
+    // ulGetIOBytes
     xTEST_CASE(cullCaseLoops)
     {
         m_ulRes = CxProcessInfo::ulGetIOBytes(CxCurrentProcess::ulGetId());
-        #if xTEST_IGNORE || 1
+        // TODO: CxProcessInfo::ulGetIOBytes
+        #if xTEST_IGNORE
             xTRACEV(xT("\tCxProcessInfo::ulGetIOBytes(): %lu"), m_ulRes);
         #endif
     }
 
     //--------------------------------------------------
-    //sGetExeName
+    // sGetExeName
     xTEST_CASE(cullCaseLoops)
     {
         m_sRes = CxProcessInfo::sGetExeName( CxCurrentProcess::ulGetId() );
+        xTEST_EQ(true,   CxFile::bIsExists(m_sRes));
         xTEST_EQ(m_sRes, CxPath::sGetExe());
+    }
+
+    //--------------------------------------------------
+    // ulGetParentId
+    xTEST_CASE(cullCaseLoops)
+    {
+        m_ulRes = CxProcessInfo::ulGetParentId( CxCurrentProcess::ulGetId() );
+        // TODO: CxProcessInfo::ulGetParentId
+        // xTEST_DIFF(0UL, m_ulRes);
     }
 
     return true;
