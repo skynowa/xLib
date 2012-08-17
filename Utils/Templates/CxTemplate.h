ec0d0623f7908301ea5f89de2a4ab398242dbcd5
skynowa <skynowa@gmail.com>
2012-08-17 17:22:01 +0300

    MOVE: Templates -> Utils

---
+++ Utils/Templates/CxTemplate.h
@@ -0,0 +1,25 @@
+/**
+ * \file   [CxTemplate].h
+ * \brief
+ */
+
+
+#ifndef xLib_[CxTemplate]H
+#define xLib_[CxTemplate]H
+//---------------------------------------------------------------------------
+#include <xLib/Common/xCommon.h>
+//---------------------------------------------------------------------------
+class [CxTemplate] :
+    public CxNonCopyable
+{
+	public:
+				 [CxTemplate]();
+            ///< constructor
+		virtual ~[CxTemplate]();
+		    ///< destructor
+
+    private:
+
+};
+//---------------------------------------------------------------------------
+#endif //xLib_[CxTemplate]H
