a22aabae20750d9c207469fd8e9409d52e42463a
skynowa <skynowa@gmail.com>
2012-08-26 14:28:04 +0100

    FIX: scripts (FreeBSD)

--- Tests_Unix.sh
+++ Tests_Unix.sh
@@ -24,10 +24,10 @@ echo -e
 echo -e $COL_GREEN"Prepare...($OSTYPE)"$COL_NORM
 echo -e
 
-if   [ "$OSTYPE" = "linux-gnu" ]; then
+if   [ `uname` = "Linux" ]; then
     MAKE=make
     TARGET_DIR="./Build/Tests/G++_linux/Release"
-elif [ "$OSTYPE" = "FreeBSD" ]; then
+elif [ `uname` = "FreeBSD" ]; then
     MAKE=gmake
     TARGET_DIR="./Build/Tests/G++_freebsd/Release"
 else
