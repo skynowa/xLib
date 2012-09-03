d08dd7f06d4310554237f506cd11b176c6fd32d9
skynowa <skynowa@gmail.com>
2012-09-04 01:27:52 +0300

    ADD: build scripts (Windows)

---
+++ LibsBuild_Win.bat
@@ -0,0 +1,10 @@
+﻿@rem
+@rem \file  LibsBuild_Win.bat
+@rem \brief build libs for Windows
+@rem
+
+
+@rem execute
+call "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat"
+
+msbuild /p:Configuration=Debug "%xLib%\Build\Libs\VC++2012\StaticLib_VC++2012.sln"
---
+++ TestsBuild_Win.bat
@@ -0,0 +1,10 @@
+﻿@rem
+@rem \file  TestsBuild_Win.bat
+@rem \brief build tests for Windows
+@rem
+
+
+@rem execute
+call "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat"
+
+msbuild /p:Configuration=Debug "%xLib%\Build\Tests\VC++2012\Test_VC++2012.sln"
