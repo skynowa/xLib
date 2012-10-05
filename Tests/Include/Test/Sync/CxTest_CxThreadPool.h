dab1ef51d09695ae21dda8f7a2ac71f0731142cd
skynowa <skynowa@gmail.com>
2012-10-05 20:14:54 +0300

    FIX: CxThreadPool - return bool -> return void

---
+++ Tests/Include/Test/Sync/CxTest_CxThreadPool.h
@@ -0,0 +1,42 @@
+/**
+ * \file   CxTest_CxThreadPool.h
+ * \brief  test CxThreadPool
+ */
+
+
+#ifndef CxTest_CxThreadPoolH
+#define CxTest_CxThreadPoolH
+//---------------------------------------------------------------------------
+#include <xLib/Common/xCommon.h>
+#include <xLib/Test/CxTest.h>
+#include <xLib/Sync/CxThreadPool.h>
+//---------------------------------------------------------------------------
+class CxTest_CxThreadPool :
+    public CxTest
+{
+    public:
+                      CxTest_CxThreadPool();
+            ///< constructor
+        virtual      ~CxTest_CxThreadPool();
+            ///< destructor
+
+        virtual bool  bUnit(const ulonglong_t cullCaseLoops);
+            ///< unit test
+
+    private:
+};
+//---------------------------------------------------------------------------
+class CPoolThread :
+    public CxThread
+{
+    public:
+        size_t         m_uiIndex;
+
+                       CPoolThread(const bool &cbAutoDelete);
+        virtual       ~CPoolThread();
+
+    protected:
+        virtual uint_t uiOnRun    (void *pData);
+};
+//---------------------------------------------------------------------------
+#endif //CxTest_CxThreadPoolH
