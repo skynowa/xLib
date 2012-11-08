b6381f6bf6d3d2c38333fdf8a83d53ab6a6e7554
skynowa <skynowa@gmail.com>
2012-11-08 12:43:28 +0200

    ADD: CxProcess::ulIdByName (FreeBSD)

--- Project/Source/Sync/CxProcess.cpp
+++ Project/Source/Sync/CxProcess.cpp
@@ -322,6 +322,61 @@ CxProcess::ulIdByName(
         ulRv = iPid;
     #elif xOS_FREEBSD
         // TODO: CxProcess::ulIdByName
+
+        struct kinfo_proc *sProcesses = NULL, *sNewProcesses;
+        pid_t  iCurrentPid;
+        int    aiNames[4];
+        size_t iNamesLength;
+        int    i, iRetCode, iNumProcs;
+        size_t iSize;
+
+        iSize = 0;
+        aiNames[0] = CTL_KERN;
+        aiNames[1] = KERN_PROC;
+        aiNames[2] = KERN_PROC_ALL;
+        aiNames[3] = 0;
+        iNamesLength = 3;
+
+        iRetCode = sysctl(aiNames, iNamesLength, NULL, &iSize, NULL, 0);
+
+        /*
+         * Allocate memory and populate info in the  processes structure
+         */
+
+        do {
+                iSize += iSize / 10;
+                sNewProcesses = realloc(sProcesses, iSize);
+
+                if (sNewProcesses == 0) {
+                        if (sProcesses)
+                                free(sProcesses);
+                                errx(1, "could not reallocate memory");
+                }
+                sProcesses = sNewProcesses;
+                iRetCode = sysctl(aiNames, iNamesLength, sProcesses, &iSize, NULL, 0);
+        } while (iRetCode == -1 && errno == ENOMEM);
+
+        iNumProcs = iSize / sizeof(struct kinfo_proc);
+
+        /*
+        * Search for the given process name and return its pid.
+        */
+
+        for (i = 0; i < iNumProcs; i++) {
+                iCurrentPid = sProcesses[i].kp_proc.p_pid;
+                if( strncmp(csProcessName, sProcesses[i].kp_proc.p_comm, MAXCOMLEN) == 0 ) {
+                        free(sProcesses);
+                        return iCurrentPid;
+                }
+        }
+
+        /*
+         * Clean up and return -1 because the given proc name was not found
+         */
+
+        free(sProcesses);
+
+        return (-1);
     #endif
 #endif
 
