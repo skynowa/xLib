8c128e0ebdc073bca00a2f2989a3da863226720d
skynowa <skynowa@gmail.com>
2012-12-11 22:04:26 +0200

    FIX: CxFinder

--- Project/Source/Filesystem/CxFinder.cpp
+++ Project/Source/Filesystem/CxFinder.cpp
@@ -25,14 +25,14 @@ xNAMESPACE_BEGIN(NxLib)
 //---------------------------------------------------------------------------
 CxFinder::CxFinder(
     const std::tstring_t &a_csRootDirPath,
-    const std::tstring_t &a_csFileFilter
+    const std::tstring_t &a_csFilter
 ) :
     _m_enEnrty      (),
     _m_csRootDirPath(a_csRootDirPath),
-    _m_csFilter     (a_csFileFilter)
+    _m_csFilter     (a_csFilter)
 {
     xTEST_EQ(false, a_csRootDirPath.empty());
-    xTEST_EQ(false, a_csFileFilter.empty());
+    xTEST_EQ(false, a_csFilter.empty());
 }
 //---------------------------------------------------------------------------
 /* virtual */
@@ -55,7 +55,7 @@ CxFinder::sFilter() const {
 }
 //--------------------------------------------------------------------------
 std::tstring_t
-CxFinder::sFileName() const {
+CxFinder::sEntryName() const {
     std::tstring_t sRv;
 
 #if   xOS_ENV_WIN
@@ -63,6 +63,8 @@ CxFinder::sFileName() const {
 #elif xOS_ENV_UNIX
     sRv.assign( _m_enEnrty.pdrData->d_name );
 #endif
+
+    xTEST_EQ(false, sRv.empty());
 
     return sRv;
 }
@@ -106,7 +108,7 @@ CxFinder::bIsValid() const {
 }
 //---------------------------------------------------------------------------
 bool
-CxFinder::bFirst() {
+CxFinder::bMoveFirst() {
 #if   xOS_ENV_WIN
     _m_enEnrty.hHandle = ::FindFirstFile(
                             (sRootDirPath() + CxConst::xSLASH + sFilter()).c_str(),
@@ -116,28 +118,29 @@ CxFinder::bFirst() {
     _m_enEnrty.pHandle = ::opendir(sRootDirPath().c_str());
     xTEST_PTR(_m_enEnrty.pHandle);
 
-    _m_enEnrty.pdrData = ::readdir(_m_enEnrty.pHandle);
-    xCHECK_RET(NULL == _m_enEnrty.pdrData, false);
+    bool bRv = bMoveNext();
+    xCHECK_RET(false == bRv, false);
 #endif
 
     return true;
 }
 //---------------------------------------------------------------------------
 bool
-CxFinder::bNext() {
+CxFinder::bMoveNext() {
 #if   xOS_ENV_WIN
     BOOL blRv = ::FindNextFile(_m_enEnrty.hHandle, &_m_enEnrty.fdData);
     xCHECK_RET(FALSE == blRv, false);
 #elif xOS_ENV_UNIX
-    _m_enEnrty.pdrData = ::readdir(_m_enEnrty.pHandle);
-    xCHECK_RET(NULL == _m_enEnrty.pdrData, false);
+    xFOREVER {
+        _m_enEnrty.pdrData = ::readdir(_m_enEnrty.pHandle);
+        xCHECK_RET(NULL == _m_enEnrty.pdrData, false);
 
-    // filter by pattern
-    {
-        int iRv = ::fnmatch(sFilter().c_str(), sFileName().c_str(), 0);
+        // filter by pattern
+        int iRv = ::fnmatch(sFilter().c_str(), sEntryName().c_str(), 0);
         xTEST_EQ(true, (0 == iRv) || (FNM_NOMATCH == iRv));
 
-        //  xCHECK_DO(0 != iRv, bNext());
+        xCHECK_DO(FNM_NOMATCH == iRv, continue);
+        xCHECK_DO(0           == iRv, break);
     }
 #endif
 
@@ -171,112 +174,5 @@ CxFinder::vClose() {
     }
 }
 //--------------------------------------------------------------------------
-
-
-
-//--------------------------------------------------------------------------
-/* static */
-void
-vFindDirs(
-    const std::tstring_t &a_csDirPath,          ///< directory path
-    const std::tstring_t &a_csPattern,          ///< pattern
-    const bool           &a_cbIsRecursively,    ///< recursively scan
-    std::vec_tstring_t   *a_pvsDirPathes        ///< output directory paths (must be empty)
-)
-{
-    xTEST_EQ(false, a_csDirPath.empty());
-    xTEST_NA(a_cbIsRecursively);
-    xTEST_PTR(a_pvsDirPathes);
-
-    // TODO: CxDir::vFindDirs
-    #if xTODO
-        (*a_pvsDirPathes).clear();
-    #endif
-
-#if   xOS_ENV_WIN
-    std::tstring_t  sRootDirPath = CxPath::sToNative( CxPath::sSlashAppend(a_csDirPath) + a_csPattern, false );
-    WIN32_FIND_DATA fdData       = {0};
-
-    HANDLE hFile = ::FindFirstFile(sRootDirPath.c_str(), &fdData);
-    xTEST_DIFF(xNATIVE_HANDLE_INVALID, hFile);
-
-    do {
-        // dirs
-        if (CxFileAttribute::faDirectory == (fdData.dwFileAttributes & CxFileAttribute::faDirectory)) {
-            std::tstring_t sFileName = fdData.cFileName;
-
-            // skip "." ".."
-            xCHECK_DO(CxConst::xDOT  == sFileName, continue);
-            xCHECK_DO(CxConst::x2DOT == sFileName, continue);
-
-            std::tstring_t sDirPath = CxPath::sSlashAppend(a_csDirPath) + sFileName;
-
-            (*a_pvsDirPathes).push_back(sDirPath);
-
-            // is search in subdirs ?
-            if (true == a_cbIsRecursively) {
-                vFindDirs(sDirPath, a_csPattern, a_cbIsRecursively, a_pvsDirPathes);  // recursion
-            }
-        }
-        // files, etc
-        else {
-            xNA;
-        }
-    }
-    while ( FALSE != ::FindNextFile(hFile, &fdData) );
-
-    BOOL blRes = ::FindClose(hFile);
-    xTEST_DIFF(FALSE, blRes);
-#elif xOS_ENV_UNIX
-    DIR *pDir = ::opendir(a_csDirPath.c_str());
-    xTEST_PTR(pDir);
-
-    dirent *pdrEntry = ::readdir(pDir);
-    xTEST_PTR(pdrEntry);
-
-    do {
-        // dirs
-        if (DT_DIR == pdrEntry->d_type) {
-            std::tstring_t sFileName = pdrEntry->d_name;
-
-            // skip "." ".."
-            xCHECK_DO(CxConst::xDOT  == sFileName, continue);
-            xCHECK_DO(CxConst::x2DOT == sFileName, continue);
-
-            // filter by pattern
-            {
-                int iRv = ::fnmatch(a_csPattern.c_str(), sFileName.c_str(), 0);
-                xTEST_EQ(true, (0 == iRv) || (FNM_NOMATCH == iRv));
-
-                xCHECK_DO(0 != iRv, continue);
-            }
-
-            std::tstring_t sDirPath = CxPath::sSlashAppend(a_csDirPath) + sFileName;
-
-            (*a_pvsDirPathes).push_back(sDirPath);
-
-            // is search in subdirs ?
-            if (true == a_cbIsRecursively) {
-                vFindDirs(sDirPath, a_csPattern, a_cbIsRecursively, a_pvsDirPathes);  // recursion
-            }
-        }
-        // files, etc
-        else {
-            xNA;
-        }
-    }
-    while ( NULL != (pdrEntry = ::readdir(pDir)) );
-
-    int iRv = ::closedir(pDir); pDir = NULL;
-    xTEST_DIFF(- 1, iRv);
-#endif
-}
-//---------------------------------------------------------------------------
-
-
-/****************************************************************************
-*    private
-*
-*****************************************************************************/
 
 xNAMESPACE_END(NxLib)
