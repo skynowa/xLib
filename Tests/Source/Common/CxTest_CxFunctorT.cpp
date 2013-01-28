fbffa861ab12c888f13ca6e8d1cf8fcd31312c1b
skynowa <skynowa@gmail.com>
2013-01-28 15:07:17 +0200

    REVIEW: code style (remove method prefix)

--- Tests/Source/Common/CxTest_CxFunctorT.cpp
+++ Tests/Source/Common/CxTest_CxFunctorT.cpp
@@ -104,7 +104,7 @@ CxTest_CxFunctorT::vUnit(
         delete pbF;
     }
 
-    xTEST_CASE("CxFunctorT::Execute", cullCaseLoops)
+    xTEST_CASE("CxFunctorT::execute", cullCaseLoops)
     {
         A a;
         B b;
@@ -112,7 +112,7 @@ CxTest_CxFunctorT::vUnit(
         CxFunctorT<B, bool, A> *pbF = new CxFunctorT<B, bool, A>(&b, &B::methodB);
         xTEST_PTR(pbF);
 
-        m_bRv = pbF->Execute(a);
+        m_bRv = pbF->execute(a);
         xTEST_EQ(true, m_bRv);
 
         delete pbF;
@@ -128,22 +128,22 @@ CxTest_CxFunctorT::vUnit(
         xTEST_EQ(true, m_bRv);
     }
 
-    xTEST_CASE("CxFunctorT::Execute", cullCaseLoops)
+    xTEST_CASE("CxFunctorT::execute", cullCaseLoops)
     {
         A a;
         B b;
 
         CxFunctorT<B, bool, A> bF(&b, &B::methodB);
 
-        m_bRv = bF.Execute(a);
+        m_bRv = bF.execute(a);
         xTEST_EQ(true, m_bRv);
     }
 
-    xTEST_CASE("CxFunctorT::Execute", cullCaseLoops)
+    xTEST_CASE("CxFunctorT::execute", cullCaseLoops)
     {
         CxFunctorT<CParam, std::tstring_t, void *> bF(&objParam, &CParam::sSetName);
 
-        m_sRv = bF.Execute(0);
+        m_sRv = bF.execute(0);
         xTEST_EQ(std::tstring_t(xT("Class_C")), m_sRv);
     }
 }
