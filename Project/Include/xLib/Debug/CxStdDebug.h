9086218e40fd4f1431ad33e9efc8aeefa3af9dc4
skynowa <skynowa@gmail.com>
2013-09-11 14:46:28 +0300

    FIX: CxStdDebug

--- Project/Include/xLib/Debug/CxStdDebug.h
+++ Project/Include/xLib/Debug/CxStdDebug.h
@@ -24,34 +24,65 @@
 //------------------------------------------------------------------------------
 #define xSTD_TRACE_POINT \
     { \
-        printf("    ::::: #%d in %s:%d, func: %s, error: %ld :::::\n", \
-               __COUNTER__, __FILE__, __LINE__, __FUNCTION__, errno); \
+        ::printf("\t::::: #%d in %s:%d, func: %s, error: %s :::::\n", \
+            __COUNTER__, __FILE__, __LINE__, __FUNCTION__, ::strerror(errno)); \
     }
+    ///< trace point
+
+#define xSTD_TRACE_VAR(v) \
+    #v ": " << v
+    ///< variable for tracing
 
 #define xSTD_TEST_DO(expr, do_expr) \
-    if ( !(expr) )  { \
-        fprintf(stderr, \
-                "\n------------------ xSTD_TEST --------------------\n" \
+    if ( !(expr) ) { \
+        ::fprintf(stdout, \
+                "\n-------------------- TEST ----------------------\n" \
                 " Expression: %s\n" \
                 " File:       %s\n" \
                 " Function:   %s\n" \
                 " Line:       %d\n" \
-                " Last error: %d\n" \
+                " Last error: %s\n" \
+                " Date time:  %s\n" \
                 "--------------------------------------------------\n", \
                 #expr, \
                 __FILE__, \
                 __FUNCTION__, \
                 __LINE__, \
-                errno); \
+                ::strerror(errno), \
+                currentDateTime().c_str()); \
         { \
             do_expr; \
         } \
     }
+    ///< test macros
 
 #define xSTD_TEST(expr) \
-    xSTD_TEST_DO(expr, exit(EXIT_FAILURE))
+    xSTD_TEST_DO(expr, {})
+    ///< test macros
 
 #define xSTD_TEST_RET(expr, ret_expr) \
-    xSTD_TEST_DO(expr, return (ret_expr))
+    xSTD_TEST_DO(expr, return ret_expr)
+    ///< test macros
+
+#define xSTD_TEST_NA(expr) ;
+    ///< test macros
+//------------------------------------------------------------------------------
+/**
+ * current date/time, format is YYYY-MM-DD.HH:mm:ss
+ */
+inline std::string
+currentDateTime()
+{
+    char       srv[80 + 1] = {};
+    time_t     now         = ::time(nullptr);
+    struct tm *tms         = ::localtime(&now);
+
+    size_t urv = ::strftime(srv, sizeof(srv), "%Y-%m-%d.%X", tms);
+    if (urv == 0) {
+        return std::string();
+    }
+
+    return srv;
+}
 //------------------------------------------------------------------------------
 #endif // xLib_Debug_CxStdDebugH
