4544af7be88c2ad775db192fe4ba50fec9120a50
skynowa <skynowa@gmail.com>
2013-10-23 22:07:46 +0300

    FIX: xFunctors - fix code style

--- Project/Include/xLib/Core/xFunctors.h
+++ Project/Include/xLib/Core/xFunctors.h
@@ -16,15 +16,16 @@ struct SCompareNoCase
     bool
     operator () (
         const std::tstring_t::value_type &a_value1,
-        const std::tstring_t::value_type &a_value2
-    )  const
+        const std::tstring_t::value_type &a_value2)  const
     {
         return CxChar::toUpper(a_value1) == CxChar::toUpper(a_value2);
     }
 
     // TODO: bool_t
     bool
-    operator() (std::ctstring_t &a_value1, std::ctstring_t &a_value2) const
+    operator() (
+        std::ctstring_t &a_value1,
+        std::ctstring_t &a_value2) const
     {
         return CxString::compareNoCase(a_value1, a_value2);
     }
