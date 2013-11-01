f6a9fc7256e59042ef26cf168e9df7673105e9c3
skynowa <skynowa@gmail.com>
2013-11-01 15:18:44 +0200

    xStdStream - add

---
+++ Project/Include/xLib/Core/xStdStream.h
@@ -0,0 +1,37 @@
+/**
+ * \file  xStdStream.h
+ * \brief overload operators << for std::basic_ostream
+ */
+
+
+#pragma once
+
+#include <xLib/Core/xCore.h>
+#include <xLib/Test/xTest.h>
+#include <xLib/Debug/xDebug.h>
+//------------------------------------------------------------------------------
+xNAMESPACE_BEGIN(std)
+
+template<class Traits>
+inline basic_ostream<tchar_t, Traits> &
+operator << (basic_ostream<tchar_t, Traits> &os, std::custring_t &value);
+    ///< overload operators << for std::ustring_t
+
+template<class Traits, class T>
+inline basic_ostream<tchar_t, Traits> &
+operator << (basic_ostream<tchar_t, Traits> &os, const vector<T> &value);
+    ///< overload operators << for std::vector
+
+template<class Traits, class T1, class T2>
+inline basic_ostream<tchar_t, Traits> &
+operator << (basic_ostream<tchar_t, Traits> &os, const map<T1, T2> &value);
+    ///< overload operators << for std::map
+
+template<class Traits, class T1, class T2>
+inline basic_ostream<tchar_t, Traits> &
+operator << (basic_ostream<tchar_t, Traits> &os, const multimap<T1, T2> &value);
+    ///< overload operators << for std::multimap
+
+xNAMESPACE_END(std)
+//------------------------------------------------------------------------------
+#include "xStdStream.inl"
