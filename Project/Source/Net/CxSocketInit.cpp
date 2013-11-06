e5cda66ab12f6828926263c2f8170d9ef5549b83
skynowa <skynowa@gmail.com>
2013-11-06 22:37:21 +0200

    CxSocketInit - fix

--- Project/Source/Net/CxSocketInit.cpp
+++ Project/Source/Net/CxSocketInit.cpp
@@ -8,6 +8,12 @@
 
 #include <xLib/Test/xTest.h>
 #include <xLib/Debug/xDebug.h>
+#include <xLib/Debug/CxLastError.h>
+#include <xLib/Debug/CxStdError.h>
+#include <xLib/Debug/CxStackTrace.h>
+#include <xLib/Debug/CxErrorReport.h>
+#include <xLib/Debug/CxDebugger.h>
+#include <xLib/Log/CxTracer.h>
 
 #if xOS_ENV_WIN
     #if xCOMPILER_MS
