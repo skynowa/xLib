0583e1276ec4298eb88ac2e3e2986c7a157d61bb
skynowa <skynowa@gmail.com>
2013-11-06 10:31:48 +0200

    RepoSync.sh - add

---
+++ Tools/RepoSync.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# \file  RepoSync.sh
+# \brief sync repository
+#
+
+cd ..
+
+# github.com
+git checkout master
+git push --tags https://skynowa@github.com/skynowa/xLib.git master:master
+
+git checkout develop
+git push --tags https://skynowa@github.com/skynowa/xLib.git develop:develop
+
+# sourceforge.net
+git checkout master
+git push --tags https://skynowa@git.code.sf.net/p/x-lib/code master:master
+
+git checkout develop
+git push --tags https://skynowa@git.code.sf.net/p/x-lib/code develop:develop
