4743ff797bb347a8769aa6bd1fcd78c2a137d1ca
skynowa <skynowa@gmail.com>
2013-11-07 10:44:46 +0200

    CxType - review

--- Project/Include/xLib/Core/CxType.h
+++ Project/Include/xLib/Core/CxType.h
@@ -16,15 +16,15 @@ class CxType :
 {
 public:
     template<class T>
-    static std::tstring_t name(const T &object) xWARN_UNUSED_RV;
+    static std::tstring_t name(const T &objT) xWARN_UNUSED_RV;
         ///< get name
 
     template<class T>
-    static std::tstring_t rawName(const T &object) xWARN_UNUSED_RV;
+    static std::tstring_t rawName(const T &objT) xWARN_UNUSED_RV;
         ///< get raw name
 
     template<class T1, class T2>
-    static bool_t         isEquals(const T1 &object1, const T2 &object2) xWARN_UNUSED_RV;
+    static bool_t         isEquals(const T1 &obj1T, const T2 &obj2T) xWARN_UNUSED_RV;
         ///< is equals
 private:
                           CxType();
