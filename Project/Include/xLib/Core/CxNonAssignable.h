4669553e238d65cd3626c81774a33d19b8e26109
skynowa <skynowa@gmail.com>
2013-11-12 21:34:08 +0200

    Fix header includes

--- Project/Include/xLib/Core/CxNonAssignable.h
+++ Project/Include/xLib/Core/CxNonAssignable.h
@@ -9,8 +9,6 @@
 #ifndef xLib_CxNonAssignableH
 #define xLib_CxNonAssignableH
 //-------------------------------------------------------------------------------------------------
-#include <xLib/Core/xCore.h>
-//-------------------------------------------------------------------------------------------------
 xNAMESPACE_BEGIN(NxLib)
 
 class CxNonAssignable
