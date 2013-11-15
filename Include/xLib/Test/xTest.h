a23a9b733c598095e8ba26c63b5d5de60edba1ec
skynowa <skynowa@gmail.com>
2013-11-15 22:04:10 +0200

    xTest - fix code style

--- Include/xLib/Test/xTest.h
+++ Include/xLib/Test/xTest.h
@@ -9,82 +9,199 @@
 #ifndef xLib_xTestH
 #define xLib_xTestH
 //-------------------------------------------------------------------------------------------------
-#define _xTEST_MSG_EQ(reportType, val1, val2, msg)      { if ( !((val1) == (val2)) ) { ulong_t _lastError = CxLastError::get(); CxErrorReport report(reportType, xT(#val1),   xT(#val2), (val1),        (val2),         xT("=="), _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg));  CxDebugger().reportMake(report); } }
-#define _xTEST_MSG_DIFF(reportType, val1, val2, msg)    { if ( !((val1) != (val2)) ) { ulong_t _lastError = CxLastError::get(); CxErrorReport report(reportType, xT(#val1),   xT(#val2), (val1),        (val2),         xT("!="), _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg));  CxDebugger().reportMake(report); } }
-#define _xTEST_MSG_LESS(reportType, val1, val2, msg)    { if ( !((val1) <  (val2)) ) { ulong_t _lastError = CxLastError::get(); CxErrorReport report(reportType, xT(#val1),   xT(#val2), (val1),        (val2),         xT("<"),  _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg));  CxDebugger().reportMake(report); } }
-#define _xTEST_MSG_GR(reportType, val1, val2, msg)      { if ( !((val1) >  (val2)) ) { ulong_t _lastError = CxLastError::get(); CxErrorReport report(reportType, xT(#val1),   xT(#val2), (val1),        (val2),         xT(">"),  _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg));  CxDebugger().reportMake(report); } }
-#define _xTEST_MSG_LESS_EQ(reportType, val1, val2, msg) { if ( !((val1) <= (val2)) ) { ulong_t _lastError = CxLastError::get(); CxErrorReport report(reportType, xT(#val1),   xT(#val2), (val1),        (val2),         xT("<="), _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg));  CxDebugger().reportMake(report); } }
-#define _xTEST_MSG_GR_EQ(reportType, val1, val2, msg)   { if ( !((val1) >= (val2)) ) { ulong_t _lastError = CxLastError::get(); CxErrorReport report(reportType, xT(#val1),   xT(#val2), (val1),        (val2),         xT(">="), _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg));  CxDebugger().reportMake(report); } }
-#define _xTEST_MSG_PTR(reportType, ptr, msg)            { if ( NULL     == (ptr)   ) { ulong_t _lastError = CxLastError::get(); CxErrorReport report(reportType, xT("NULL"),  xT(#ptr),  (intptr_t)ptr, (intptr_t)NULL, xT("!="), _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg));  CxDebugger().reportMake(report); } }
-#define _xTEST_MSG_PTR_FAIL(reportType, ptr, msg)       { if ( NULL     != (ptr)   ) { ulong_t _lastError = CxLastError::get(); CxErrorReport report(reportType, xT("NULL"),  xT(#ptr),  (intptr_t)ptr, (intptr_t)NULL, xT("=="), _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg));  CxDebugger().reportMake(report); } }
-#define _xTEST_MSG_FAIL(reportType, msg)                { if ( true                ) { ulong_t _lastError = CxLastError::get(); CxErrorReport report(reportType, xT("false"), xT(""),    xT(""),        xT(""),         xT(""),   _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg));  CxDebugger().reportMake(report); } }
+#define _xTEST_MSG_EQ(reportType, val1, val2, msg) \
+    if ( !((val1) == (val2)) ) { \
+        ulong_t _lastError = CxLastError::get(); \
+        CxErrorReport report(reportType, xT(#val1), xT(#val2), (val1), (val2), xT("=="), \
+            _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg)); \
+        CxDebugger().reportMake(report); \
+    }
+#define _xTEST_MSG_DIFF(reportType, val1, val2, msg) \
+    if ( !((val1) != (val2)) ) { \
+        ulong_t _lastError = CxLastError::get(); \
+        CxErrorReport report(reportType, xT(#val1), xT(#val2), (val1), (val2), xT("!="), \
+            _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg)); \
+        CxDebugger().reportMake(report); \
+    }
+#define _xTEST_MSG_LESS(reportType, val1, val2, msg) \
+    if ( !((val1) < (val2)) ) { \
+        ulong_t _lastError = CxLastError::get(); \
+        CxErrorReport report(reportType, xT(#val1), xT(#val2), (val1), (val2), xT("<"), \
+            _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg)); \
+        CxDebugger().reportMake(report); \
+    }
+#define _xTEST_MSG_GR(reportType, val1, val2, msg) \
+    if ( !((val1) > (val2)) ) { \
+        ulong_t _lastError = CxLastError::get(); \
+        CxErrorReport report(reportType, xT(#val1), xT(#val2), (val1), (val2), xT(">"), \
+            _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg)); \
+        CxDebugger().reportMake(report); \
+    }
+#define _xTEST_MSG_LESS_EQ(reportType, val1, val2, msg) \
+    if ( !((val1) <= (val2)) ) { \
+        ulong_t _lastError = CxLastError::get(); \
+        CxErrorReport report(reportType, xT(#val1), xT(#val2), (val1), (val2), xT("<="), \
+            _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg)); \
+        CxDebugger().reportMake(report); \
+    }
+#define _xTEST_MSG_GR_EQ(reportType, val1, val2, msg) \
+    if ( !((val1) >= (val2)) ) { \
+        ulong_t _lastError = CxLastError::get(); \
+        CxErrorReport report(reportType, xT(#val1), xT(#val2), (val1), (val2), xT(">="), \
+            _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(), (msg)); \
+        CxDebugger().reportMake(report); \
+    }
+#define _xTEST_MSG_PTR(reportType, ptr, msg) \
+    if ( NULL == (ptr) ) { \
+        ulong_t _lastError = CxLastError::get(); \
+        CxErrorReport report(reportType, xT("NULL"), xT(#ptr), intptr_t(ptr), intptr_t(NULL), \
+            xT("!="), _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, \
+            CxStackTrace().toString(), (msg)); \
+        CxDebugger().reportMake(report); \
+    }
+#define _xTEST_MSG_PTR_FAIL(reportType, ptr, msg) \
+    if ( NULL != (ptr) ) { \
+        ulong_t _lastError = CxLastError::get(); \
+        CxErrorReport report(reportType, xT("NULL"), xT(#ptr), intptr_t(ptr), intptr_t(NULL), \
+            xT("=="), _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, \
+            CxStackTrace().toString(), (msg)); \
+        CxDebugger().reportMake(report); \
+    }
+#define _xTEST_MSG_FAIL(reportType, msg) \
+    if ( true ) { \
+        ulong_t _lastError = CxLastError::get(); \
+        CxErrorReport report(reportType, xT("false"), xT(""), xT(""), xT(""), xT(""), \
+            _lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, \
+            CxStackTrace().toString(), (msg)); \
+        CxDebugger().reportMake(report); \
+    }
 
 #if xDEBUG_MODE_STDOUT_PLAIN
-    #define xTEST_EQ(val1, val2)                        _xTEST_MSG_EQ      (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
-    #define xTEST_DIFF(val1, val2)                      _xTEST_MSG_DIFF    (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
-    #define xTEST_LESS(val1, val2)                      _xTEST_MSG_LESS    (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
-    #define xTEST_GR(val1, val2)                        _xTEST_MSG_GR      (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
-    #define xTEST_LESS_EQ(val1, val2)                   _xTEST_MSG_LESS_EQ (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
-    #define xTEST_GR_EQ(val1, val2)                     _xTEST_MSG_GR_EQ   (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
-    #define xTEST_PTR(ptr)                              _xTEST_MSG_PTR     (CxErrorReport::rtStdoutPlain, ptr, xT(""))
-    #define xTEST_PTR_FAIL(ptr)                         _xTEST_MSG_PTR_FAIL(CxErrorReport::rtStdoutPlain, ptr, xT(""))
-    #define xTEST_FAIL                                  _xTEST_MSG_FAIL    (CxErrorReport::rtStdoutPlain, xT(""))
-    #define xTEST(expr)                                 _xTEST_MSG_EQ      (CxErrorReport::rtStdoutPlain, true, expr, xT(""))
+    #define xTEST_EQ(val1, val2) \
+        _xTEST_MSG_EQ      (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
+    #define xTEST_DIFF(val1, val2) \
+        _xTEST_MSG_DIFF    (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
+    #define xTEST_LESS(val1, val2) \
+        _xTEST_MSG_LESS    (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
+    #define xTEST_GR(val1, val2) \
+        _xTEST_MSG_GR      (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
+    #define xTEST_LESS_EQ(val1, val2) \
+        _xTEST_MSG_LESS_EQ (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
+    #define xTEST_GR_EQ(val1, val2) \
+        _xTEST_MSG_GR_EQ   (CxErrorReport::rtStdoutPlain, val1, val2, xT(""))
+    #define xTEST_PTR(ptr) \
+        _xTEST_MSG_PTR     (CxErrorReport::rtStdoutPlain, ptr, xT(""))
+    #define xTEST_PTR_FAIL(ptr) \
+        _xTEST_MSG_PTR_FAIL(CxErrorReport::rtStdoutPlain, ptr, xT(""))
+    #define xTEST_FAIL \
+        _xTEST_MSG_FAIL    (CxErrorReport::rtStdoutPlain, xT(""))
+    #define xTEST(expr) \
+        _xTEST_MSG_EQ      (CxErrorReport::rtStdoutPlain, true, expr, xT(""))
 
-    #define xTEST_MSG_EQ(val1, val2, msg)               _xTEST_MSG_EQ      (CxErrorReport::rtStdoutPlain, val1, val2, msg)
-    #define xTEST_MSG_DIFF(val1, val2, msg)             _xTEST_MSG_DIFF    (CxErrorReport::rtStdoutPlain, val1, val2, msg)
-    #define xTEST_MSG_LESS(val1, val2, msg)             _xTEST_MSG_LESS    (CxErrorReport::rtStdoutPlain, val1, val2, msg)
-    #define xTEST_MSG_GR(val1, val2, msg)               _xTEST_MSG_GR      (CxErrorReport::rtStdoutPlain, val1, val2, msg)
-    #define xTEST_MSG_LESS_EQ(val1, val2, msg)          _xTEST_MSG_LESS_EQ (CxErrorReport::rtStdoutPlain, val1, val2, msg)
-    #define xTEST_MSG_GR_EQ(val1, val2, msg)            _xTEST_MSG_GR_EQ   (CxErrorReport::rtStdoutPlain, val1, val2, msg)
-    #define xTEST_MSG_PTR(ptr, msg)                     _xTEST_MSG_PTR     (CxErrorReport::rtStdoutPlain, ptr, msg)
-    #define xTEST_MSG_PTR_FAIL(ptr, msg)                _xTEST_MSG_PTR_FAIL(CxErrorReport::rtStdoutPlain, ptr, msg)
-    #define xTEST_MSG_FAIL(msg)                         _xTEST_MSG_FAIL    (CxErrorReport::rtStdoutPlain, msg)
-    #define xTEST_MSG(expr, msg)                        _xTEST_MSG_EQ      (CxErrorReport::rtStdoutPlain, true, expr, msg)
+    #define xTEST_MSG_EQ(val1, val2, msg) \
+        _xTEST_MSG_EQ      (CxErrorReport::rtStdoutPlain, val1, val2, msg)
+    #define xTEST_MSG_DIFF(val1, val2, msg) \
+        _xTEST_MSG_DIFF    (CxErrorReport::rtStdoutPlain, val1, val2, msg)
+    #define xTEST_MSG_LESS(val1, val2, msg) \
+        _xTEST_MSG_LESS    (CxErrorReport::rtStdoutPlain, val1, val2, msg)
+    #define xTEST_MSG_GR(val1, val2, msg) \
+        _xTEST_MSG_GR      (CxErrorReport::rtStdoutPlain, val1, val2, msg)
+    #define xTEST_MSG_LESS_EQ(val1, val2, msg) \
+        _xTEST_MSG_LESS_EQ (CxErrorReport::rtStdoutPlain, val1, val2, msg)
+    #define xTEST_MSG_GR_EQ(val1, val2, msg) \
+        _xTEST_MSG_GR_EQ   (CxErrorReport::rtStdoutPlain, val1, val2, msg)
+    #define xTEST_MSG_PTR(ptr, msg) \
+        _xTEST_MSG_PTR     (CxErrorReport::rtStdoutPlain, ptr, msg)
+    #define xTEST_MSG_PTR_FAIL(ptr, msg) \
+        _xTEST_MSG_PTR_FAIL(CxErrorReport::rtStdoutPlain, ptr, msg)
+    #define xTEST_MSG_FAIL(msg) \
+        _xTEST_MSG_FAIL    (CxErrorReport::rtStdoutPlain, msg)
+    #define xTEST_MSG(expr, msg) \
+        _xTEST_MSG_EQ      (CxErrorReport::rtStdoutPlain, true, expr, msg)
 #elif xDEBUG_MODE_MSGBOX_PLAIN
-    #define xTEST_EQ(val1, val2)                        _xTEST_MSG_EQ      (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
-    #define xTEST_DIFF(val1, val2)                      _xTEST_MSG_DIFF    (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
-    #define xTEST_LESS(val1, val2)                      _xTEST_MSG_LESS    (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
-    #define xTEST_GR(val1, val2)                        _xTEST_MSG_GR      (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
-    #define xTEST_LESS_EQ(val1, val2)                   _xTEST_MSG_LESS_EQ (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
-    #define xTEST_GR_EQ(val1, val2)                     _xTEST_MSG_GR_EQ   (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
-    #define xTEST_PTR(ptr)                              _xTEST_MSG_PTR     (CxErrorReport::rtMsgboxPlain, ptr, xT(""))
-    #define xTEST_PTR_FAIL(ptr)                         _xTEST_MSG_PTR_FAIL(CxErrorReport::rtMsgboxPlain, ptr, xT(""))
-    #define xTEST_FAIL                                  _xTEST_MSG_FAIL    (CxErrorReport::rtMsgboxPlain, xT(""))
-    #define xTEST(expr)                                 _xTEST_MSG_EQ      (CxErrorReport::rtMsgboxPlain, true, expr, xT(""))
+    #define xTEST_EQ(val1, val2) \
+        _xTEST_MSG_EQ      (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
+    #define xTEST_DIFF(val1, val2) \
+        _xTEST_MSG_DIFF    (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
+    #define xTEST_LESS(val1, val2) \
+        _xTEST_MSG_LESS    (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
+    #define xTEST_GR(val1, val2) \
+        _xTEST_MSG_GR      (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
+    #define xTEST_LESS_EQ(val1, val2) \
+        _xTEST_MSG_LESS_EQ (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
+    #define xTEST_GR_EQ(val1, val2) \
+        _xTEST_MSG_GR_EQ   (CxErrorReport::rtMsgboxPlain, val1, val2, xT(""))
+    #define xTEST_PTR(ptr) \
+        _xTEST_MSG_PTR     (CxErrorReport::rtMsgboxPlain, ptr, xT(""))
+    #define xTEST_PTR_FAIL(ptr) \
+        _xTEST_MSG_PTR_FAIL(CxErrorReport::rtMsgboxPlain, ptr, xT(""))
+    #define xTEST_FAIL \
+        _xTEST_MSG_FAIL    (CxErrorReport::rtMsgboxPlain, xT(""))
+    #define xTEST(expr) \
+        _xTEST_MSG_EQ      (CxErrorReport::rtMsgboxPlain, true, expr, xT(""))
 
-    #define xTEST_MSG_EQ(val1, val2, msg)               _xTEST_MSG_EQ      (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
-    #define xTEST_MSG_DIFF(val1, val2, msg)             _xTEST_MSG_DIFF    (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
-    #define xTEST_MSG_LESS(val1, val2, msg)             _xTEST_MSG_LESS    (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
-    #define xTEST_MSG_GR(val1, val2, msg)               _xTEST_MSG_GR      (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
-    #define xTEST_MSG_LESS_EQ(val1, val2, msg)          _xTEST_MSG_LESS_EQ (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
-    #define xTEST_MSG_GR_EQ(val1, val2, msg)            _xTEST_MSG_GR_EQ   (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
-    #define xTEST_MSG_PTR(ptr, msg)                     _xTEST_MSG_PTR     (CxErrorReport::rtMsgboxPlain, ptr, msg)
-    #define xTEST_MSG_PTR_FAIL(ptr, msg)                _xTEST_MSG_PTR_FAIL(CxErrorReport::rtMsgboxPlain, ptr, msg)
-    #define xTEST_MSG_FAIL(msg)                         _xTEST_MSG_FAIL    (CxErrorReport::rtMsgboxPlain, msg)
-    #define xTEST_MSG(expr, msg)                        _xTEST_MSG_EQ      (CxErrorReport::rtMsgboxPlain, true, expr, msg)
+    #define xTEST_MSG_EQ(val1, val2, msg) \
+        _xTEST_MSG_EQ      (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
+    #define xTEST_MSG_DIFF(val1, val2, msg) \
+        _xTEST_MSG_DIFF    (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
+    #define xTEST_MSG_LESS(val1, val2, msg) \
+        _xTEST_MSG_LESS    (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
+    #define xTEST_MSG_GR(val1, val2, msg) \
+        _xTEST_MSG_GR      (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
+    #define xTEST_MSG_LESS_EQ(val1, val2, msg) \
+        _xTEST_MSG_LESS_EQ (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
+    #define xTEST_MSG_GR_EQ(val1, val2, msg) \
+        _xTEST_MSG_GR_EQ   (CxErrorReport::rtMsgboxPlain, val1, val2, msg)
+    #define xTEST_MSG_PTR(ptr, msg) \
+        _xTEST_MSG_PTR     (CxErrorReport::rtMsgboxPlain, ptr, msg)
+    #define xTEST_MSG_PTR_FAIL(ptr, msg) \
+        _xTEST_MSG_PTR_FAIL(CxErrorReport::rtMsgboxPlain, ptr, msg)
+    #define xTEST_MSG_FAIL(msg) \
+        _xTEST_MSG_FAIL    (CxErrorReport::rtMsgboxPlain, msg)
+    #define xTEST_MSG(expr, msg) \
+        _xTEST_MSG_EQ      (CxErrorReport::rtMsgboxPlain, true, expr, msg)
 #else
-    #define xTEST_EQ(val1, val2)                        { xNA }
-    #define xTEST_DIFF(val1, val2)                      { xNA }
-    #define xTEST_LESS(val1, val2)                      { xNA }
-    #define xTEST_GR(val1, val2)                        { xNA }
-    #define xTEST_LESS_EQ(val1, val2)                   { xNA }
-    #define xTEST_GR_EQ(val1, val2)                     { xNA }
-    #define xTEST_PTR(ptr)                              { xNA }
-    #define xTEST_PTR_FAIL(ptr)                         { xNA }
-    #define xTEST_FAIL                                  { xNA }
-    #define xTEST(expr)                                 { xNA }
+    #define xTEST_EQ(val1, val2) \
+        { xNA }
+    #define xTEST_DIFF(val1, val2) \
+        { xNA }
+    #define xTEST_LESS(val1, val2) \
+        { xNA }
+    #define xTEST_GR(val1, val2) \
+        { xNA }
+    #define xTEST_LESS_EQ(val1, val2) \
+        { xNA }
+    #define xTEST_GR_EQ(val1, val2) \
+        { xNA }
+    #define xTEST_PTR(ptr) \
+        { xNA }
+    #define xTEST_PTR_FAIL(ptr) \
+        { xNA }
+    #define xTEST_FAIL \
+        { xNA }
+    #define xTEST(expr) \
+        { xNA }
 
-    #define xTEST_MSG_EQ(val1, val2, msg)               { xNA }
-    #define xTEST_MSG_DIFF(val1, val2, msg)             { xNA }
-    #define xTEST_MSG_LESS(val1, val2, msg)             { xNA }
-    #define xTEST_MSG_GR(val1, val2, msg)               { xNA }
-    #define xTEST_MSG_LESS_EQ(val1, val2, msg)          { xNA }
-    #define xTEST_MSG_GR_EQ(val1, val2, msg)            { xNA }
-    #define xTEST_MSG_PTR(ptr, msg)                     { xNA }
-    #define xTEST_MSG_PTR_FAIL(ptr, msg)                { xNA }
-    #define xTEST_MSG_FAIL(msg)                         { xNA }
-    #define xTEST_MSG(expr, msg)                        { xNA }
+    #define xTEST_MSG_EQ(val1, val2, msg) \
+        { xNA }
+    #define xTEST_MSG_DIFF(val1, val2, msg) \
+        { xNA }
+    #define xTEST_MSG_LESS(val1, val2, msg) \
+        { xNA }
+    #define xTEST_MSG_GR(val1, val2, msg) \
+        { xNA }
+    #define xTEST_MSG_LESS_EQ(val1, val2, msg) \
+        { xNA }
+    #define xTEST_MSG_GR_EQ(val1, val2, msg) \
+        { xNA }
+    #define xTEST_MSG_PTR(ptr, msg) \
+        { xNA }
+    #define xTEST_MSG_PTR_FAIL(ptr, msg) \
+        { xNA }
+    #define xTEST_MSG_FAIL(msg) \
+        { xNA }
+    #define xTEST_MSG(expr, msg) \
+        { xNA }
 #endif
 
 #define xTEST_NA(var) \
