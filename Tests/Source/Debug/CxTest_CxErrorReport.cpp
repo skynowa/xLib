bea161ea70fdb1c9b0b5f95bf6cdb6067828b087
skynowa <skynowa@gmail.com>
2013-11-26 20:16:53 +0200

    CxTest_CxErrorReport - review

--- Tests/Source/Debug/CxTest_CxErrorReport.cpp
+++ Tests/Source/Debug/CxTest_CxErrorReport.cpp
@@ -16,14 +16,14 @@ CxTest_CxErrorReport::unit(
 {
     xTEST_CASE("CxErrorReport::CxErrorReport", a_caseLoops)
     {
-        ulong_t ulLastError = CxLastError::get();
+        ulong_t lastError = CxLastError::get();
 
-        ulong_t val1 = 10;
-        ulong_t val2 = 20;
+        ulong_t val1 = 10UL;
+        ulong_t val2 = 20UL;
 
-        CxErrorReport rpReport(CxErrorReport::rtMsgboxPlain, xT("val1"), xT("val2"), val1, val2, xT("=="),
-                               ulLastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME,
-                               CxStackTrace().toString(), xT(""));
+        CxErrorReport rpReport(CxErrorReport::rtMsgboxPlain, xT("val1"), xT("val2"), val1, val2,
+            xT("=="), lastError, xFILE, xLINE, xFUNCTION, xDATE, xTIME, CxStackTrace().toString(),
+            xT(""));
     }
 }
 //------------------------------------------------------------------------------
