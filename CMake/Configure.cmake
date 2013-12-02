eb83fbd0f552eba4ba705e3c0009a45cc9371e7e
skynowa <skynowa@gmail.com>
2013-12-02 16:47:55 +0200

    CMake: fix code style

--- CMake/Configure.cmake
+++ CMake/Configure.cmake
@@ -10,6 +10,7 @@ include(FindMySQL)
 
 
 #--------------------------------------------------------------------------------------------------
+# find packages
 find_package(OpenSSL REQUIRED)
 find_package(MySQL REQUIRED)
 
@@ -19,8 +20,9 @@ if (MYSQL_FOUND)
     message("MySQL libraries:   ${MYSQL_LIBRARIES}")
 else()
     message("STATUS MySQL found: ${MYSQL_FOUND}")
-endif(MYSQL_FOUND)
-
+endif()
+#--------------------------------------------------------------------------------------------------
+# configure
 configure_file(
     ${CMAKE_CURRENT_SOURCE_DIR}/Include/xLib/Core/xConfig.h.in
     ${CMAKE_CURRENT_SOURCE_DIR}/Include/xLib/Core/xConfig.h)
@@ -28,15 +30,18 @@ configure_file(
 CHECK_LIBRARY_EXISTS(crypto BF_cfb64_encrypt "" xHAVE_OPENSSL_CRYPTO)
 
 if (UNIX)
+    # xHAVE_PR_SET_DUMPABLE
     check_cxx_source_compiles(
         "#include <sys/prctl.h>
 
-        int main() {
+        int main()
+        {
             ::prctl(PR_SET_DUMPABLE, 0);
             return 0;
         }"
         xHAVE_PR_SET_DUMPABLE)
 
+    # xHAVE_RLIMIT_CORE
     check_cxx_source_compiles(
         "#include <sys/resource.h>
 
@@ -48,6 +53,7 @@ if (UNIX)
         }"
         xHAVE_RLIMIT_CORE)
 
+    # xHAVE_PT_DENY_ATTACH
     if (APPLE)
         check_cxx_source_compiles(
             "#include <sys/types.h>
