f37ad72e3cd32896dbe4385d95a2e89952e25496
skynowa <skynowa@gmail.com>
2013-12-12 10:38:06 +0200

    CxVolume: fix code style

--- Include/xLib/Filesystem/CxVolume.h
+++ Include/xLib/Filesystem/CxVolume.h
@@ -20,32 +20,32 @@ public:
     enum ExType
         /// file system type
     {
-        #if xOS_ENV_WIN
-            dtUnknown   = DRIVE_UNKNOWN,
-            dtNoRoot    = DRIVE_NO_ROOT_DIR,
-            dtRemovable = DRIVE_REMOVABLE,
-            dtFixed     = DRIVE_FIXED,
-            dtRemote    = DRIVE_REMOTE,
-            dtCdRom     = DRIVE_CDROM,
-            dtRam       = DRIVE_RAMDISK
-        #else
-            dtUnknown   = 0,
-            dtNoRoot    = 1,
-            dtRemovable = 2,
-            dtFixed     = 3,
-            dtRemote    = 4,
-            dtCdRom     = 5,
-            dtRam       = 6,
-            dtOther     = 7    // TODO: CxVolume::dtGetType
-        #endif
+    #if xOS_ENV_WIN
+        dtUnknown   = DRIVE_UNKNOWN,
+        dtNoRoot    = DRIVE_NO_ROOT_DIR,
+        dtRemovable = DRIVE_REMOVABLE,
+        dtFixed     = DRIVE_FIXED,
+        dtRemote    = DRIVE_REMOTE,
+        dtCdRom     = DRIVE_CDROM,
+        dtRam       = DRIVE_RAMDISK
+    #else
+        dtUnknown   = 0,
+        dtNoRoot    = 1,
+        dtRemovable = 2,
+        dtFixed     = 3,
+        dtRemote    = 4,
+        dtCdRom     = 5,
+        dtRam       = 6,
+        dtOther     = 7    // TODO: CxVolume::type
+    #endif
     };
 
-    explicit          CxVolume(std::ctstring_t &volumePath);
+    explicit          CxVolume(std::ctstring_t &path);
         ///< constructor
     virtual          ~CxVolume() {}
         ///< destructor
 
-    std::ctstring_t & volumePath() const xWARN_UNUSED_RV;
+    std::ctstring_t & path() const xWARN_UNUSED_RV;
         ///< volume path
     ExType            type() const xWARN_UNUSED_RV;
         ///< drive type
@@ -72,9 +72,9 @@ public:
         ///< get paths
 
 private:
-    std::ctstring_t   _volumePath;  ///< volume path
+    std::ctstring_t   _path;  ///< volume path
 
-xNO_COPY_ASSIGN(CxVolume)
+    xNO_COPY_ASSIGN(CxVolume)
 };
 
 xNAMESPACE_END(NxLib)
