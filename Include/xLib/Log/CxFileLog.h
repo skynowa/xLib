cd738446e27c127769a070297555c6da111ae4bf
skynowa <skynowa@gmail.com>
2013-12-26 18:00:17 +0200

    CxSystemLog: fix enum utils for impl converting

--- Include/xLib/Log/CxFileLog.h
+++ Include/xLib/Log/CxFileLog.h
@@ -44,7 +44,7 @@ public:
 
     virtual void_t    write(ctchar_t *format, ...) const xOVERRIDE;
         ///< write to log
-    virtual void_t    write(const ExLevel &level, ctchar_t *format, ...) const xOVERRIDE;
+    virtual void_t    write(cExLevel &level, ctchar_t *format, ...) const xOVERRIDE;
         ///< write to log
     void_t            clear() const;
         ///< clear content
