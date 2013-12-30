e7d9cf146aa22acebbf8bfda7319aebe5addb253
skynowa <skynowa@gmail.com>
2013-12-30 18:38:55 +0200

    xUnit: fix datime

--- Include/xLib/Core/xUnits.h
+++ Include/xLib/Core/xUnits.h
@@ -11,36 +11,36 @@
 xNAMESPACE_BEGIN(NxLib)
 
 // bytes
-#define xBYTES(byte) \
-    ( static_cast<culonglong_t>(byte) )
-#define xKB(byte) \
-    ( static_cast<culonglong_t>(byte) << 10 )
-#define xMB(byte) \
-    ( static_cast<culonglong_t>(byte) << 20 )
-#define xGB(byte) \
-    ( static_cast<culonglong_t>(byte) << 30 )
-#define xTB(byte) \
-    ( static_cast<culonglong_t>(byte) << 40 )
-#define xPB(byte) \
-    ( static_cast<culonglong_t>(byte) << 50 )
-#define xEB(byte) \
-    ( static_cast<culonglong_t>(byte) << 60 )
+#define xBYTES(x) \
+    ( static_cast<culonglong_t>(x) )
+#define xKB(x) \
+    ( static_cast<culonglong_t>(x) << 10 )
+#define xMB(x) \
+    ( static_cast<culonglong_t>(x) << 20 )
+#define xGB(x) \
+    ( static_cast<culonglong_t>(x) << 30 )
+#define xTB(x) \
+    ( static_cast<culonglong_t>(x) << 40 )
+#define xPB(x) \
+    ( static_cast<culonglong_t>(x) << 50 )
+#define xEB(x) \
+    ( static_cast<culonglong_t>(x) << 60 )
 
 // datetime
-#define xMSEC(msec) \
-    ( static_cast<culonglong_t>(msec) )
-#define xSECOND(msec) \
-    ( static_cast<culonglong_t>(msec) / 1000ULL )
-#define xMINUTE(msec) \
-    ( static_cast<culonglong_t>(msec) / 1000ULL * 60 )
-#define xHOUR(msec) \
-    ( static_cast<culonglong_t>(msec) / 1000ULL * 60 * 60 )
-#define xDAY(msec) \
-    ( static_cast<culonglong_t>(msec) / 1000ULL * 60 * 60 * 24 )
-#define xMONTH(msec) \
-    ( static_cast<culonglong_t>(msec) / 1000ULL * 60 * 60 * 24 * 30 )
-#define xYEAR(msec) \
-    ( static_cast<culonglong_t>(msec) / 1000ULL * 60 * 60 * 24 * 30 * 12 )
+#define xMSEC(x) \
+    ( static_cast<culonglong_t>(x) )
+#define xSECOND(x) \
+    ( static_cast<culonglong_t>(x) * 1000ULL )
+#define xMINUTE(x) \
+    ( static_cast<culonglong_t>(x) * 1000ULL * 60 )
+#define xHOUR(x) \
+    ( static_cast<culonglong_t>(x) * 1000ULL * 60 * 60 )
+#define xDAY(x) \
+    ( static_cast<culonglong_t>(x) * 1000ULL * 60 * 60 * 24 )
+#define xMONTH(x) \
+    ( static_cast<culonglong_t>(x) * 1000ULL * 60 * 60 * 24 * 30 )
+#define xYEAR(x) \
+    ( static_cast<culonglong_t>(x) * 1000ULL * 60 * 60 * 24 * 30 * 12 )
 
 xNAMESPACE_END(NxLib)
 //---------------------------------------------------------------------------
