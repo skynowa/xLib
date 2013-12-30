6732edf0a7fd41bd12a6a374201e4504c3465320
skynowa <skynowa@gmail.com>
2013-12-30 17:41:05 +0200

    CxSystemLog: remove depr code

--- Include/xLib/Log/CxSystemLog.h
+++ Include/xLib/Log/CxSystemLog.h
@@ -19,37 +19,6 @@ class CxSystemLog :
     /// logging to system event log
 {
 public:
-#if xTEMP_DISABLED
-    enum ExLevel
-        /// log level
-    {
-    #if   xOS_ENV_WIN
-        lvEmerg    = EVENTLOG_ERROR_TYPE,
-        lvAlert    = EVENTLOG_ERROR_TYPE,
-        lvCritical = EVENTLOG_ERROR_TYPE,
-        lvError    = EVENTLOG_ERROR_TYPE,
-        lvWarning  = EVENTLOG_WARNING_TYPE,
-        lvNotice   = EVENTLOG_SUCCESS,
-        lvInfo     = EVENTLOG_INFORMATION_TYPE,
-        lvDebug    = EVENTLOG_INFORMATION_TYPE
-    #elif xOS_ENV_UNIX
-        lvEmerg    = 0,
-        lvAlert    = 1,
-        lvCritical = 2,
-        lvError    = 3,
-        lvWarning  = 4,
-        lvNotice   = 5,
-        lvInfo     = 6,
-        lvDebug    = 7
-    #endif
-    };
-#endif
-
-    #if xTEMP_DISABLED
-        EVENTLOG_AUDIT_FAILURE  // Failure Audit event
-        EVENTLOG_AUDIT_SUCCESS  // Success Audit event
-    #endif
-
              CxSystemLog();
         ///< constructor
     explicit CxSystemLog(std::ctstring_t &logName);
