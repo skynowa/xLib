ee66222bee88fe5a21a3c4f68fa77d5727b7ae3e
skynowa <skynowa@gmail.com>
2013-12-31 11:01:59 +0200

    Build: fix scripts

--- Build/eclipse_gcc.sh
+++ Build/eclipse_gcc.sh
@@ -6,12 +6,11 @@
 
 
 # vars
-DIR_BUILD="./xLib_`basename -s .sh $0`"
+DIR_BUILD="../../xLib_`basename -s .sh $0`"
 
 
 # build
-cd ../..
 mkdir -p $DIR_BUILD
 cd $DIR_BUILD
-cmake VERBOSE=1 -G"Eclipse CDT4 - Unix Makefiles" "../xLib"
-make
+cmake -G"Eclipse CDT4 - Unix Makefiles" "../xLib"
+make -j4 VERBOSE=1
