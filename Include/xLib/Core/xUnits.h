208988ed57072bebe20a28ee9e6e2dddadd5c346
skynowa <skynowa@gmail.com>
2013-12-31 10:18:17 +0200

    xUnits: review

--- Include/xLib/Core/xUnits.h
+++ Include/xLib/Core/xUnits.h
@@ -14,33 +14,33 @@ xNAMESPACE_BEGIN(NxLib)
 #define xBYTES(x) \
     ( static_cast<culonglong_t>(x) )
 #define xKB(x) \
-    ( static_cast<culonglong_t>(x) << 10 )
+    ( xBYTES(x) << 10 )
 #define xMB(x) \
-    ( static_cast<culonglong_t>(x) << 20 )
+    ( xBYTES(x) << 20 )
 #define xGB(x) \
-    ( static_cast<culonglong_t>(x) << 30 )
+    ( xBYTES(x) << 30 )
 #define xTB(x) \
-    ( static_cast<culonglong_t>(x) << 40 )
+    ( xBYTES(x) << 40 )
 #define xPB(x) \
-    ( static_cast<culonglong_t>(x) << 50 )
+    ( xBYTES(x) << 50 )
 #define xEB(x) \
-    ( static_cast<culonglong_t>(x) << 60 )
+    ( xBYTES(x) << 60 )
 
 // datetime
 #define xMSEC(x) \
     ( static_cast<culonglong_t>(x) )
 #define xSECOND(x) \
-    ( static_cast<culonglong_t>(x) * 1000ULL )
+    ( xMSEC(x) * 1000ULL )
 #define xMINUTE(x) \
-    ( static_cast<culonglong_t>(x) * 1000ULL * 60 )
+    ( xMSEC(x) * 1000ULL * 60 )
 #define xHOUR(x) \
-    ( static_cast<culonglong_t>(x) * 1000ULL * 60 * 60 )
+    ( xMSEC(x) * 1000ULL * 60 * 60 )
 #define xDAY(x) \
-    ( static_cast<culonglong_t>(x) * 1000ULL * 60 * 60 * 24 )
+    ( xMSEC(x) * 1000ULL * 60 * 60 * 24 )
 #define xMONTH(x) \
-    ( static_cast<culonglong_t>(x) * 1000ULL * 60 * 60 * 24 * 30 )
+    ( xMSEC(x) * 1000ULL * 60 * 60 * 24 * 30 )
 #define xYEAR(x) \
-    ( static_cast<culonglong_t>(x) * 1000ULL * 60 * 60 * 24 * 30 * 12 )
+    ( xMSEC(x) * 1000ULL * 60 * 60 * 24 * 30 * 12 )
 
 xNAMESPACE_END(NxLib)
 //---------------------------------------------------------------------------
