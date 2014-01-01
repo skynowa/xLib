b87d665d1cf6aed16423d204fd97bcf1edc033c4
skynowa <skynowa@gmail.com>
2014-01-01 14:17:05 +0200

    Build: fix clang scripts

--- Build/eclipse_clang.sh
+++ Build/eclipse_clang.sh
@@ -6,6 +6,9 @@
 
 
 # vars
+set CC="clang"
+set CXX="clang++"
+
 DIR_XLIB="../xLib"
 DIR_BUILD="../../xLib_`basename -s .sh $0`"
 
@@ -13,5 +16,5 @@ DIR_BUILD="../../xLib_`basename -s .sh $0`"
 # build
 mkdir -p $DIR_BUILD
 cd $DIR_BUILD
-cmake -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++" -G"Eclipse CDT4 - Unix Makefiles" $DIR_XLIB
+cmake -G"Eclipse CDT4 - Unix Makefiles" $DIR_XLIB
 make -j4 VERBOSE=1
