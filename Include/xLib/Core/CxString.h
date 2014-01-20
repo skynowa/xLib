4562874c0b04b4acabb2574f356c7e85adfe4c33
skynowa <skynowa@gmail.com>
2014-01-20 15:41:44 +0200

    CxStringCI (no case string): add

--- Include/xLib/Core/CxString.h
+++ Include/xLib/Core/CxString.h
@@ -134,10 +134,11 @@ public:
     *******************************************************************************/
 
     static std::size_t    findNoCase(std::ctstring_t &str, std::ctstring_t &target,
-                              std::csize_t &pos = 0) xWARN_UNUSED_RV;
-        ///< no case search
-    static bool_t         compareNoCase(std::ctstring_t &str1, std::ctstring_t &str2)
+                              std::csize_t &pos = 0, const std::locale &a_locale = std::locale())
                               xWARN_UNUSED_RV;
+        ///< no case search
+    static bool_t         compareNoCase(std::ctstring_t &str1, std::ctstring_t &str2,
+                              const std::locale &a_locale = std::locale()) xWARN_UNUSED_RV;
         ///< no case compare
 
 
@@ -176,6 +177,35 @@ private:
         ///< destructor
 
     xNO_COPY_ASSIGN(CxString)
+};
+
+xNAMESPACE_END(xlib)
+//-------------------------------------------------------------------------------------------------
+xNAMESPACE_BEGIN(xlib)
+
+class CxStringCI
+    /// strings utils (case insensitive)
+{
+public:
+    /*******************************************************************************
+    *   search, compare
+    *
+    *******************************************************************************/
+
+    static std::size_t    find(std::ctstring_t &str, std::ctstring_t &target, std::csize_t &pos = 0,
+                              const std::locale &a_locale = std::locale()) xWARN_UNUSED_RV;
+        ///< no case search
+    static bool_t         compare(std::ctstring_t &str1, std::ctstring_t &str2,
+                              const std::locale &a_locale = std::locale()) xWARN_UNUSED_RV;
+        ///< no case compare
+
+private:
+                          CxStringCI();
+        ///< constructor
+                         ~CxStringCI();
+        ///< destructor
+
+    xNO_COPY_ASSIGN(CxStringCI)
 };
 
 xNAMESPACE_END(xlib)
