c3250008b11941dc66d4fbeb7683af553097c843
skynowa <skynowa@gmail.com>
2014-01-21 15:25:41 +0200

    CxTest_CxArray: review

--- Tests/Source/Core/CxTest_CxArray.cpp
+++ Tests/Source/Core/CxTest_CxArray.cpp
@@ -7,42 +7,41 @@
 #include <Test/Core/CxTest_CxArray.h>
 
 
-//------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 /* virtual */
 void_t
 CxTest_CxArray::unit(
     culonglong_t &a_caseLoops
 )
 {
-    std::csize_t cuiArraySize = 5;
+    std::csize_t arraySize = 5;
 
 
-    CxArray<size_t, cuiArraySize> aszArray = {{0, 1, 2, 3, 4}};
+    CxArray<size_t, arraySize> array = {{0, 1, 2, 3, 4}};
 
     xTEST_CASE("CxArray::operator[]", a_caseLoops)
     {
-        for (size_t i = 0; i < aszArray.size(); ++ i) {
-            xTEST_EQ(i, aszArray[i]);
+        for (size_t i = 0; i < array.size(); ++ i) {
+            xTEST_EQ(i, array[i]);
         }
     }
 
     xTEST_CASE("CxArray::at", a_caseLoops)
     {
-        for (size_t i = 0; i < aszArray.size(); ++ i) {
-            xTEST_EQ(i, aszArray.at(i));
+        for (size_t i = 0; i < array.size(); ++ i) {
+            xTEST_EQ(i, array.at(i));
         }
     }
 
-    xTEST_CASE("CxArray::uiGetSize", a_caseLoops)
+    xTEST_CASE("CxArray::size", a_caseLoops)
     {
-        m_stRv = aszArray.size();
-        xTEST_EQ(cuiArraySize, m_stRv);
+        m_stRv = array.size();
+        xTEST_EQ(arraySize, m_stRv);
     }
 
     xTEST_CASE("CxArray::clear", a_caseLoops)
     {
-        /*m_bRv =*/ aszArray.clear();
-        ////xTEST_EQ(true, m_bRv);
+        array.clear();
     }
 
     xTEST_CASE("NxArray::make_array", a_caseLoops) {
@@ -50,4 +49,4 @@ CxTest_CxArray::unit(
         NxArray::make_array<std::tstring_t>( xT("0"),  xT("1"),  xT("2") );
     }
 }
-//------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
