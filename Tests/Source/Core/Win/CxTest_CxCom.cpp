da9cb58a8af995c3e521fa12948df512f425f88b
skynowa <skynowa@gmail.com>
2014-01-21 13:52:28 +0200

    CxTest_CxCom: review

--- Tests/Source/Core/Win/CxTest_CxCom.cpp
+++ Tests/Source/Core/Win/CxTest_CxCom.cpp
@@ -15,20 +15,19 @@ CxTest_CxCom::unit(
 )
 {
 #if xOS_ENV_WIN
-
     xTEST_CASE("CxCom::CxCom", a_caseLoops)
     {
-        const COINIT ccmModel[] = {
+        const COINIT model[] = {
             COINIT_APARTMENTTHREADED,
             COINIT_MULTITHREADED,
             COINIT_DISABLE_OLE1DDE,
             COINIT_SPEED_OVER_MEMORY
         };
 
-        for (size_t i = 0; i < xARRAY_SIZE(ccmModel) - 1; ++ i) {
-            CxCom cmCom1(ccmModel[i]);
-            CxCom cmCom2(ccmModel[i]);
-            CxCom cmCom3(ccmModel[i]);
+        for (size_t i = 0; i < xARRAY_SIZE(model) - 1; ++ i) {
+            CxCom com1(model[i]);
+            CxCom com2(model[i]);
+            CxCom com3(model[i]);
         }
     }
 #else
