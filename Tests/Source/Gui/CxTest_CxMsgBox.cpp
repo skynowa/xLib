3f645eb1a855b746b2de7f599906d3a1aad0c3b6
skynowa <skynowa@gmail.com>
2014-03-12 11:37:58 +0200

    Gui: CxMsgBox - fix

--- Tests/Source/Gui/CxTest_CxMsgBox.cpp
+++ Tests/Source/Gui/CxTest_CxMsgBox.cpp
@@ -16,12 +16,12 @@ CxTest_CxMsgBox::unit(
 {
     xUNUSED(a_caseLoops);
 
-#if xTEST_IGNORE
+#if xTEST_IGNORE || 1
     xTEST_CASE("CxMsgBox::show", a_caseLoops)
     {
         std::ctstring_t  text  = xT("\n::: Message box :::\n\n\n\nSimple test 1\n\nSimple test 2\n");
         std::ctstring_t  title = xT("Simple title");
-        CxMsgBox::ExType type = CxMsgBox::tpOk;
+        CxMsgBox::ExType type  = CxMsgBox::tpOk;
 
         CxMsgBox::ExModalResult mrRes = CxMsgBox().show(text, title, type);
         xUNUSED(mrRes);
