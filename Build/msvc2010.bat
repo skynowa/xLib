d5ffd4995421462a69ce7fbbfb503bf0ad803688
skynowa <skynowa@gmail.com>
2014-04-11 16:09:51 +0300

    Build: review windows scripts

--- Build/msvc2010.bat
+++ Build/msvc2010.bat
@@ -1,15 +1,15 @@
 @echo off
-rem \file  msvc2010.bat
-rem \brief build tests
-rem
+:: \file  msvc2010.bat
+:: \brief build tests
+::
 
 
-rem vars
+:: vars
 set DIR_XLIB="../xLib"
 set DIR_BUILD="../../xLib_msvc2010"
 
 
-rem build
+:: build
 if not exist %DIR_BUILD% mkdir %DIR_BUILD%
 cd %DIR_BUILD%
 cmake -G"Visual Studio 10" %DIR_XLIB%
--- Build/msvc2012.bat
+++ Build/msvc2012.bat
@@ -1,15 +1,15 @@
 @echo off
-rem \file  msvc2012.bat
-rem \brief build tests
-rem
+:: \file  msvc2012.bat
+:: \brief build tests
+::
 
 
-rem vars
+:: vars
 set DIR_XLIB="../xLib"
 set DIR_BUILD="../../xLib_msvc2012"
 
 
-rem build
+:: build
 if not exist %DIR_BUILD% mkdir %DIR_BUILD%
 cd %DIR_BUILD%
 cmake -G"Visual Studio 11" %DIR_XLIB%
