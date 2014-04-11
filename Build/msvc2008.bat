e024b6caba32bf36ff75578dca2c67d58e850935
skynowa <skynowa@gmail.com>
2014-04-11 16:09:51 +0300

    Build: review windows scripts

--- Build/msvc2008.bat
+++ Build/msvc2008.bat
@@ -1,15 +1,15 @@
 @echo off
-rem \file  msvc2008.bat
-rem \brief build tests
-rem
+:: \file  msvc2008.bat
+:: \brief build tests
+::
 
 
-rem vars
+:: vars
 set DIR_XLIB="../xLib"
 set DIR_BUILD="../../xLib_msvc2008"
 
 
-rem build
+:: build
 if not exist %DIR_BUILD% mkdir %DIR_BUILD%
 cd %DIR_BUILD%
 cmake -G"Visual Studio 9 2008" %DIR_XLIB%
