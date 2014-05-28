1c8501a9a38f7b63e8533f9a937c7651ba85744a
skynowa <skynowa@gmail.com>
2014-05-28 18:15:36 +0300

    Log: CxTrace - fix platforms

--- Include/xLib/Log/CxTrace.h
+++ Include/xLib/Log/CxTrace.h
@@ -33,6 +33,9 @@ public:
         ///< tracing to debugger, std::cout
     virtual void_t write(cExLevel &level, ctchar_t *format, ...) const xOVERRIDE;
         ///< tracing to debugger, std::cout
+
+xPLATFORM:
+    void_t         _write_impl(std::ctstring_t &msg) const;
 };
 
 xNAMESPACE_END2(xlib, log)
