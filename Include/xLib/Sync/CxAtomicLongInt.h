349f1c6c877dedd3765e64058f1ef02adb35b96f
skynowa <skynowa@gmail.com>
2014-06-03 15:23:04 +0300

    Sync: CxAtomicLongInt - add xWARN_UNUSED_RV

--- Include/xLib/Sync/CxAtomicLongInt.h
+++ Include/xLib/Sync/CxAtomicLongInt.h
@@ -17,35 +17,35 @@ public:
                       CxAtomicLongInt();
     virtual          ~CxAtomicLongInt() {}
 
-    CxAtomicLongInt & operator += (const CxAtomicLongInt &value);
-    CxAtomicLongInt & operator += (clong_t &right);
+    CxAtomicLongInt & operator += (const CxAtomicLongInt &value) xWARN_UNUSED_RV;
+    CxAtomicLongInt & operator += (clong_t &right) xWARN_UNUSED_RV;
 
-    CxAtomicLongInt & operator -= (const CxAtomicLongInt &value);
-    CxAtomicLongInt & operator -= (clong_t &right);
+    CxAtomicLongInt & operator -= (const CxAtomicLongInt &value) xWARN_UNUSED_RV;
+    CxAtomicLongInt & operator -= (clong_t &right) xWARN_UNUSED_RV;
 
-    CxAtomicLongInt & operator = (const CxAtomicLongInt &value);
-    CxAtomicLongInt & operator = (clong_t &right);
+    CxAtomicLongInt & operator = (const CxAtomicLongInt &value) xWARN_UNUSED_RV;
+    CxAtomicLongInt & operator = (clong_t &right) xWARN_UNUSED_RV;
 
-    bool_t            operator == (const CxAtomicLongInt &value) const;
-    bool_t            operator == (clong_t &right) const ;
+    bool_t            operator == (const CxAtomicLongInt &value) const xWARN_UNUSED_RV;
+    bool_t            operator == (clong_t &right) const xWARN_UNUSED_RV;
 
-    bool_t            operator != (const CxAtomicLongInt &value) const;
-    bool_t            operator != (clong_t &right) const ;
+    bool_t            operator != (const CxAtomicLongInt &value) const xWARN_UNUSED_RV;
+    bool_t            operator != (clong_t &right) const xWARN_UNUSED_RV;
 
-    bool_t            operator < (clong_t &value) const;
-    bool_t            operator < (const CxAtomicLongInt &value) const;
+    bool_t            operator < (clong_t &value) const xWARN_UNUSED_RV;
+    bool_t            operator < (const CxAtomicLongInt &value) const xWARN_UNUSED_RV;
 
-    bool_t            operator <= (clong_t &value) const;
-    bool_t            operator <= (const CxAtomicLongInt &value) const;
+    bool_t            operator <= (clong_t &value) const xWARN_UNUSED_RV;
+    bool_t            operator <= (const CxAtomicLongInt &value) const xWARN_UNUSED_RV;
 
-    bool_t            operator > (clong_t &value) const;
-    bool_t            operator > (const CxAtomicLongInt &value) const;
+    bool_t            operator > (clong_t &value) const xWARN_UNUSED_RV;
+    bool_t            operator > (const CxAtomicLongInt &value) const xWARN_UNUSED_RV;
 
-    bool_t            operator >= (clong_t &value) const;
-    bool_t            operator >= (const CxAtomicLongInt &value) const;
+    bool_t            operator >= (clong_t &value) const xWARN_UNUSED_RV;
+    bool_t            operator >= (const CxAtomicLongInt &value) const xWARN_UNUSED_RV;
 
-    CxAtomicLongInt & operator ++ (cint_t value);
-    CxAtomicLongInt & operator -- (cint_t value);
+    CxAtomicLongInt & operator ++ (cint_t value) xWARN_UNUSED_RV;
+    CxAtomicLongInt & operator -- (cint_t value) xWARN_UNUSED_RV;
 
     long_t            value() const xWARN_UNUSED_RV;
 
@@ -53,14 +53,14 @@ private:
     volatile long_t   _value;
 
 xPLATFORM:
-    CxAtomicLongInt & _addAssign_impl(const CxAtomicLongInt &value);
-    CxAtomicLongInt & _addAssign_impl(clong_t &right);
-    CxAtomicLongInt & _subtractAssign_impl(const CxAtomicLongInt &value);
-    CxAtomicLongInt & _subtractAssign_impl(clong_t &right);
-    CxAtomicLongInt & _assign_impl(const CxAtomicLongInt &value);
-    CxAtomicLongInt & _assign_impl(clong_t &right);
-    CxAtomicLongInt & _inc_impl(cint_t value);
-    CxAtomicLongInt & _decr_impl(cint_t value);
+    CxAtomicLongInt & _addAssign_impl(const CxAtomicLongInt &value) xWARN_UNUSED_RV;
+    CxAtomicLongInt & _addAssign_impl(clong_t &right) xWARN_UNUSED_RV;
+    CxAtomicLongInt & _subtractAssign_impl(const CxAtomicLongInt &value) xWARN_UNUSED_RV;
+    CxAtomicLongInt & _subtractAssign_impl(clong_t &right) xWARN_UNUSED_RV;
+    CxAtomicLongInt & _assign_impl(const CxAtomicLongInt &value) xWARN_UNUSED_RV;
+    CxAtomicLongInt & _assign_impl(clong_t &right) xWARN_UNUSED_RV;
+    CxAtomicLongInt & _inc_impl(cint_t value) xWARN_UNUSED_RV;
+    CxAtomicLongInt & _decr_impl(cint_t value) xWARN_UNUSED_RV;
     long_t            _value_impl() const xWARN_UNUSED_RV;
 };
 
