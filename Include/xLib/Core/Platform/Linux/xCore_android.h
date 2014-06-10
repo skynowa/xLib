9f3c913d120f143586c62b7da14af797d944c0ce
skynowa <skynowa@gmail.com>
2014-06-10 18:48:00 +0300

    Core: xCore_android - add

---
+++ Include/xLib/Core/Platform/Linux/xCore_android.h
@@ -0,0 +1,29 @@
+/**
+ * \file  xCore_unix.h
+ * \brief Unix core header for xLib
+ */
+
+
+#pragma once
+
+
+#include <linux/version.h>
+// #include <gnu/libc-version.h>
+#include <sys/vfs.h>
+#include <sys/prctl.h>
+#include <sys/sysinfo.h>
+#include <mntent.h>
+
+// #if xHAVE_GETCPU
+//     #include <linux/getcpu.h>
+// #endif
+
+// POSIX
+// #include <cpio.h>
+// #include <tar.h>
+// #include <sys/msg.h>
+
+// thread
+// #include <sys/sem.h>
+
+// GUI
