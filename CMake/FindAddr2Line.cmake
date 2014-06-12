736615fec5bbcc7d4810d4d8009f46c2b13548a5
skynowa <skynowa@gmail.com>
2014-06-13 00:22:18 +0300

    CMake: FindAddr2Line - add

---
+++ CMake/FindAddr2Line.cmake
@@ -0,0 +1,23 @@
+#--------------------------------------------------------------------------------------------------
+# \file  FindAddr2Line.cmake
+# \brief Find addr2Line
+#
+# ADDR2LINE_FOUND     - true if found
+# ADDR2LINE_FILE_PATH - where to find includes
+#--------------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------------
+find_file(ADDR2LINE_FILE_PATH "addr2line")
+
+if (NOT ADDR2LINE_FILE_PATH STREQUAL "ADDR2LINE_FILE_PATH-NOTFOUND")
+    set(ADDR2LINE_FOUND TRUE)
+endif()
+
+#--------------------------------------------------------------------------------------------------
+# trace
+if (NOT ADDR2LINE_FOUND AND ADDR2LINE_FIND_REQUIRED)
+    message(FATAL_ERROR "ADDR2LINE_FOUND: ${ADDR2LINE_FOUND}")
+else()
+    message(STATUS "ADDR2LINE_FOUND: ${ADDR2LINE_FOUND}")
+endif()
