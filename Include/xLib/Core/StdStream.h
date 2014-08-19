86ea492b88e5d35616ed4a6b43a7a3bd201e9c4d
skynowa <skynowa@gmail.com>
2014-08-19 09:56:50 +0300

    Core: StdStream - add todos

--- Include/xLib/Core/StdStream.h
+++ Include/xLib/Core/StdStream.h
@@ -9,21 +9,36 @@
 #include <xLib/Core/Defines.h>
 //-------------------------------------------------------------------------------------------------
 xNAMESPACE_BEGIN2(xlib, core)
-
-template<class Traits>
-inline std::basic_ostream<tchar_t, Traits> &
-operator << (std::basic_ostream<tchar_t, Traits> &os, std::custring_t &value);
-    ///< for std::ustring_t
-
+//-------------------------------------------------------------------------------------------------
+///@name Sequence containers
+///@{
 template<class Traits, class T>
 inline std::basic_ostream<tchar_t, Traits> &
 operator << (std::basic_ostream<tchar_t, Traits> &os, const std::vector<T> &value);
     ///< for std::vector
 
+// TODO: for std::deque
+
 template<class Traits, class T>
 inline std::basic_ostream<tchar_t, Traits> &
 operator << (std::basic_ostream<tchar_t, Traits> &os, const std::list<T> &value);
     ///< for std::list
+///@}
+//-------------------------------------------------------------------------------------------------
+///@name Container adaptors
+///@{
+// TODO: for std::stack
+
+// TODO: for std::queue
+
+// TODO: for std::priority_queue
+///@}
+//-------------------------------------------------------------------------------------------------
+///@name Associative containers
+///@{
+// TODO: for std::set
+
+// TODO: for std::multiset
 
 template<class Traits, class T1, class T2>
 inline std::basic_ostream<tchar_t, Traits> &
@@ -34,7 +49,21 @@ template<class Traits, class T1, class T2>
 inline std::basic_ostream<tchar_t, Traits> &
 operator << (std::basic_ostream<tchar_t, Traits> &os, const std::multimap<T1, T2> &value);
     ///< for std::multimap
+///@}
+//-------------------------------------------------------------------------------------------------
+///@name Unordered associative containers
+///@{
 
+///@}
+//-------------------------------------------------------------------------------------------------
+///@name Etc
+///@{
+template<class Traits>
+inline std::basic_ostream<tchar_t, Traits> &
+operator << (std::basic_ostream<tchar_t, Traits> &os, std::custring_t &value);
+    ///< for std::ustring_t
+///@}
+//-------------------------------------------------------------------------------------------------
 xNAMESPACE_END2(xlib, core)
 //-------------------------------------------------------------------------------------------------
 #include "StdStream.inl"
