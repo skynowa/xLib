63de20c32bba81df9e8dc6e19c5dae07ba5ce43f
skynowa <skynowa@gmail.com>
2014-10-03 16:18:52 +0300

    Sync: Signal - update Application

--- Include/xLib/Sync/Signal.cpp
+++ Include/xLib/Sync/Signal.cpp
@@ -31,7 +31,55 @@ xNAMESPACE_BEGIN2(xlib, sync)
 **************************************************************************************************/
 
 //-------------------------------------------------------------------------------------------------
+inline void_t
+Signal::setOnSignals(
+    const std::vector<int_t> &a_signalNums,
+    sighandler_t              a_callback
+) const
+{
+   /**
+    * FAQ: set handlers
+    *
+    * https://gist.github.com/jvranish/4441299
+    */
 
+    xFOREACH_CONST(std::vector<int_t>, it, a_signalNums) {
+        sighandler_t shRv = std::signal(*it, a_callback);
+        xTEST(shRv != SIG_ERR);
+    }
+}
+//-------------------------------------------------------------------------------------------------
+inline void_t
+Signal::setOnExit(
+    void_t (*a_callback)()
+) const
+{
+    xCHECK_DO(a_callback == xPTR_NULL, return);
+
+    int_t iRv = std::atexit(a_callback);
+    xTEST(iRv == 0);
+}
+//-------------------------------------------------------------------------------------------------
+inline void_t
+Signal::setOnTerminate(
+    void_t (*a_callback)()
+) const
+{
+    xTEST_PTR(a_callback);
+
+    void_t (*callback_old)() = xPTR_NULL;
+
+    callback_old = std::set_terminate(a_callback);
+    xUNUSED(callback_old);
+}
+//-------------------------------------------------------------------------------------------------
+inline int_t
+Signal::raise(cint_t &a_signal) const
+{
+
+    int iRv = std::raise(a_signal);
+    xTEST(iRv == 0);
+}
 //-------------------------------------------------------------------------------------------------
 
 xNAMESPACE_END2(xlib, sync)
--- Include/xLib/Sync/Signal.h
+++ Include/xLib/Sync/Signal.h
@@ -19,6 +19,16 @@ public:
     virtual ~Signal() {}
         ///< destructor
 
+    void_t   setOnSignals(const std::vector<int_t> &signalNums, sighandler_t callback) const;
+        ///< set signal handle
+    void_t   setOnExit(void_t (*callback)()) const;
+        ///< set exit handle
+    void_t   setOnTerminate(void_t (*callback)()) const;
+        ///< set terminate handle (by default, the terminate handler calls abort)
+
+    int_t    raise(cint_t &signal) const;
+        ///< sends signal to the current executing program
+
 private:
     xNO_COPY_ASSIGN(Signal)
 };
