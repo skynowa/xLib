bbc0e8286f156afdfb29514f8d03769d5fd132b0
skynowa <skynowa@gmail.com>
2014-10-24 20:00:49 +0300

    Core: StdStream - add todos

--- Include/xLib/Core/StdStream.h
+++ Include/xLib/Core/StdStream.h
@@ -12,12 +12,16 @@ xNAMESPACE_BEGIN2(xlib, core)
 //-------------------------------------------------------------------------------------------------
 ///@name Sequence containers
 ///@{
+
+// TODO: for std::array (C++11)
+
 template<class Traits, class T>
 inline std::basic_ostream<tchar_t, Traits> &
 operator << (std::basic_ostream<tchar_t, Traits> &os, const std::vector<T> &value);
     ///< for std::vector
 
 // TODO: for std::deque
+// TODO: for std::forward_list (C++11)
 
 template<class Traits, class T>
 inline std::basic_ostream<tchar_t, Traits> &
@@ -27,17 +31,17 @@ operator << (std::basic_ostream<tchar_t, Traits> &os, const std::list<T> &value)
 //-------------------------------------------------------------------------------------------------
 ///@name Container adaptors
 ///@{
+
 // TODO: for std::stack
-
 // TODO: for std::queue
-
 // TODO: for std::priority_queue
+
 ///@}
 //-------------------------------------------------------------------------------------------------
 ///@name Associative containers
 ///@{
-// TODO: for std::set
 
+// TODO: for std::set
 // TODO: for std::multiset
 
 template<class Traits, class T1, class T2>
@@ -53,6 +57,11 @@ operator << (std::basic_ostream<tchar_t, Traits> &os, const std::multimap<T1, T2
 //-------------------------------------------------------------------------------------------------
 ///@name Unordered associative containers
 ///@{
+
+// TODO: for std::unordered_set (C++11)
+// TODO: for std::unordered_multiset (C++11)
+// TODO: for std::unordered_map (C++11)
+// TODO: for std::unordered_multimap (C++11)
 
 ///@}
 //-------------------------------------------------------------------------------------------------
