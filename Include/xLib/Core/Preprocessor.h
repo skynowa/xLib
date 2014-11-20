70d8c2f3888a64511a2e00463bf3610f2a4cab3a
skynowa <skynowa@gmail.com>
2014-11-20 14:07:28 +0200

    Core: Preprocessor - add comments

--- Include/xLib/Core/Preprocessor.h
+++ Include/xLib/Core/Preprocessor.h
@@ -7,7 +7,8 @@
 #pragma once
 
 //-------------------------------------------------------------------------------------------------
-// lexeme utils
+///@name Lexeme utils
+///@{
 #define xLEX_TO_STR(a) \
     xT(#a)
     ///< lexeme to string
@@ -39,6 +40,7 @@
 #define xLEX_CAT9(x1, x2, x3, x4, x5, x6, x7, x8, x9) \
     xLEX_CAT(xLEX_CAT8(x1, x2, x3, x4, x5, x6, x7, x8), x9)
     ///< concatenate 9 strings
+///@}
 //-------------------------------------------------------------------------------------------------
 #if   xENV_WIN
     #define xPRAGMA(x) __pragma(x)
