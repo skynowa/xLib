d9de83398a6489e900bfb8b76c14d563e557075c
skynowa <skynowa@gmail.com>
2014-11-21 10:39:58 +0200

    Tests: Test_Application - update

--- Tests/Source/Core/Test_Application.cpp
+++ Tests/Source/Core/Test_Application.cpp
@@ -82,6 +82,60 @@ Test_Application::unit(
 {
     xTEST_CASE("actions", a_caseLoops)
     {
+    }
+
+    xTEST_CASE("handles (signal)", a_caseLoops)
+    {
+        Application application(xT("[app_name]_guid"), xT(""));
+
+        std::vector<int_t> signalNums;
+        signalNums.push_back(SIGHUP);
+        signalNums.push_back(SIGINT);
+        signalNums.push_back(SIGQUIT);
+        signalNums.push_back(SIGILL);
+        signalNums.push_back(SIGTRAP);
+        signalNums.push_back(SIGABRT);
+        signalNums.push_back(SIGIOT);
+        signalNums.push_back(SIGBUS);
+        signalNums.push_back(SIGFPE);
+        signalNums.push_back(SIGKILL);
+        signalNums.push_back(SIGUSR1);
+        signalNums.push_back(SIGSEGV);
+        signalNums.push_back(SIGUSR2);
+        signalNums.push_back(SIGPIPE);
+        signalNums.push_back(SIGALRM);
+        signalNums.push_back(SIGTERM);
+        signalNums.push_back(SIGSTKFLT);
+        signalNums.push_back(SIGCLD);
+        signalNums.push_back(SIGCHLD);
+        signalNums.push_back(SIGCONT);
+        signalNums.push_back(SIGSTOP);
+        signalNums.push_back(SIGTSTP);
+        signalNums.push_back(SIGTTIN);
+        signalNums.push_back(SIGTTOU);
+        signalNums.push_back(SIGURG);
+        signalNums.push_back(SIGXCPU);
+        signalNums.push_back(SIGXFSZ);
+        signalNums.push_back(SIGVTALRM);
+        signalNums.push_back(SIGPROF);
+        signalNums.push_back(SIGWINCH);
+        signalNums.push_back(SIGPOLL);
+        signalNums.push_back(SIGIO);
+        signalNums.push_back(SIGPWR);
+        signalNums.push_back(SIGSYS);
+
+        application.signal().connect(signalNums, SignalHandlers::onSignals);
+        application.signal().connectExit(SignalHandlers::onExit);
+        application.signal().connectTerminate(SignalHandlers::onTerminate);
+        application.signal().connectUnexpected(SignalHandlers::onUnexpected);
+
+        // test error
+        TestFail testFail;
+        testFail.foo3();
+    }
+
+    xTEST_CASE("info", a_caseLoops)
+    {
         std::ctstring_t name            = xT("[app_name]");
         std::ctstring_t decription      = xT("[decription]");
         std::ctstring_t usage           = xT("[usage]");
@@ -145,62 +199,7 @@ Test_Application::unit(
         xTEST_EQ(Application::vendorEmail(), vendorEmail);
 
         Application::setVendorSkype(vendorSkype);
-        xTEST_EQ(Application::vendorSkype(), vendorSkype);;
-    }
-
-    xTEST_CASE("handles (signal)", a_caseLoops)
-    {
-        Application application(xT("[app_name]_guid"), xT(""));
-
-        std::vector<int_t> signalNums;
-        signalNums.push_back(SIGHUP);
-        signalNums.push_back(SIGINT);
-        signalNums.push_back(SIGQUIT);
-        signalNums.push_back(SIGILL);
-        signalNums.push_back(SIGTRAP);
-        signalNums.push_back(SIGABRT);
-        signalNums.push_back(SIGIOT);
-        signalNums.push_back(SIGBUS);
-        signalNums.push_back(SIGFPE);
-        signalNums.push_back(SIGKILL);
-        signalNums.push_back(SIGUSR1);
-        signalNums.push_back(SIGSEGV);
-        signalNums.push_back(SIGUSR2);
-        signalNums.push_back(SIGPIPE);
-        signalNums.push_back(SIGALRM);
-        signalNums.push_back(SIGTERM);
-        signalNums.push_back(SIGSTKFLT);
-        signalNums.push_back(SIGCLD);
-        signalNums.push_back(SIGCHLD);
-        signalNums.push_back(SIGCONT);
-        signalNums.push_back(SIGSTOP);
-        signalNums.push_back(SIGTSTP);
-        signalNums.push_back(SIGTTIN);
-        signalNums.push_back(SIGTTOU);
-        signalNums.push_back(SIGURG);
-        signalNums.push_back(SIGXCPU);
-        signalNums.push_back(SIGXFSZ);
-        signalNums.push_back(SIGVTALRM);
-        signalNums.push_back(SIGPROF);
-        signalNums.push_back(SIGWINCH);
-        signalNums.push_back(SIGPOLL);
-        signalNums.push_back(SIGIO);
-        signalNums.push_back(SIGPWR);
-        signalNums.push_back(SIGSYS);
-
-        application.signal().connect(signalNums, SignalHandlers::onSignals);
-        application.signal().connectExit(SignalHandlers::onExit);
-        application.signal().connectTerminate(SignalHandlers::onTerminate);
-        application.signal().connectUnexpected(SignalHandlers::onUnexpected);
-
-        // test error
-        TestFail testFail;
-        testFail.foo3();
-    }
-
-    xTEST_CASE("info", a_caseLoops)
-    {
-
+        xTEST_EQ(Application::vendorSkype(), vendorSkype);
     }
 
     xTEST_CASE("files", a_caseLoops)
