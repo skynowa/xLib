6409ddccbae40b3a5cda2e811740a75c8f4ec9f0
skynowa <skynowa@gmail.com>
2014-11-21 14:55:03 +0200

    Tests: Test_Sinal - add decription test

--- Tests/Source/Sync/Test_Signal.cpp
+++ Tests/Source/Sync/Test_Signal.cpp
@@ -14,9 +14,49 @@ Test_Signal::unit(
     culonglong_t &a_caseLoops
 )
 {
-    xTEST_CASE("[TEST_CASE_1]", a_caseLoops)
+    xTEST_CASE("decription", a_caseLoops)
     {
+        std::vector<int_t> signalNums;
+        signalNums.push_back(SIGHUP);
+        signalNums.push_back(SIGINT);
+        signalNums.push_back(SIGQUIT);
+        signalNums.push_back(SIGILL);
+        signalNums.push_back(SIGTRAP);
+        signalNums.push_back(SIGABRT);
+        signalNums.push_back(SIGIOT);
+        signalNums.push_back(SIGBUS);
+        signalNums.push_back(SIGFPE);
+        signalNums.push_back(SIGKILL);
+        signalNums.push_back(SIGUSR1);
+        signalNums.push_back(SIGSEGV);
+        signalNums.push_back(SIGUSR2);
+        signalNums.push_back(SIGPIPE);
+        signalNums.push_back(SIGALRM);
+        signalNums.push_back(SIGTERM);
+        signalNums.push_back(SIGSTKFLT);
+        signalNums.push_back(SIGCLD);
+        signalNums.push_back(SIGCHLD);
+        signalNums.push_back(SIGCONT);
+        signalNums.push_back(SIGSTOP);
+        signalNums.push_back(SIGTSTP);
+        signalNums.push_back(SIGTTIN);
+        signalNums.push_back(SIGTTOU);
+        signalNums.push_back(SIGURG);
+        signalNums.push_back(SIGXCPU);
+        signalNums.push_back(SIGXFSZ);
+        signalNums.push_back(SIGVTALRM);
+        signalNums.push_back(SIGPROF);
+        signalNums.push_back(SIGWINCH);
+        signalNums.push_back(SIGPOLL);
+        signalNums.push_back(SIGIO);
+        signalNums.push_back(SIGPWR);
+        signalNums.push_back(SIGSYS);
 
+        xFOREACH_CONST(std::vector<int_t>, it, signalNums) {
+            m_sRv = Signal::decription(*it);
+            // std::cout << xTRACE_VAR2(*it, m_sRv) << std::endl;
+            xTEST_EQ(m_sRv.empty(), false);
+        }
     }
 }
 //-------------------------------------------------------------------------------------------------
