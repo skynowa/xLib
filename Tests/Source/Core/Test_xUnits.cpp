4ec63e46973b958f13586e79cf04722f77291963
skynowa <skynowa@gmail.com>
2014-11-26 23:23:46 +0200

    Tests: xformat - update

--- Tests/Source/Core/Test_xUnits.cpp
+++ Tests/Source/Core/Test_xUnits.cpp
@@ -9,13 +9,32 @@
 #include <xLib/Core/String.h>
 
 
+#define xARGTYPES_1  class T1
+#define xARGTYPES_2  class T1, class T2
+#define xARGTYPES_3  class T1, class T2, class T3
+#define xARGTYPES_4  class T1, class T2, class T3, class T4
+#define xARGTYPES_5  class T1, class T2, class T3, class T4, class T5
+#define xARGTYPES_6  class T1, class T2, class T3, class T4, class T5, class T6
+#define xARGTYPES_7  class T1, class T2, class T3, class T4, class T5, class T6, class T7
+#define xARGTYPES_8  class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8
+#define xARGTYPES_9  class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9
+#define xARGTYPES_10 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10
+
+#define xVARARGS_1  const T1& a_t1
+#define xVARARGS_2  const T1& a_t1, const T2& a_t2
+#define xVARARGS_3  const T1& a_t1, const T2& a_t2, const T3& a_t3
+#define xVARARGS_4  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4
+#define xVARARGS_5  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5
+#define xVARARGS_6  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5, const T6& a_t6
+#define xVARARGS_7  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5, const T6& a_t6, const T7& a_t7
+#define xVARARGS_8  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5, const T6& a_t6, const T7& a_t7, const T8& a_t8
+#define xVARARGS_9  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5, const T6& a_t6, const T7& a_t7, const T8& a_t8, const T9& a_t9
+#define xVARARGS_10 const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5, const T6& a_t6, const T7& a_t7, const T8& a_t8, const T9& a_t9, const T10& a_t10
 //-------------------------------------------------------------------------------------------------
-template<class T1, class T2>
+template<xARGTYPES_3>
 std::tstring_t
 xformat(
-    std::ctstring_t &a_format,
-    const T1        &a_t1,
-    const T2        &a_t2
+    std::ctstring_t &a_format, xVARARGS_3
 )
 {
     std::tstring_t sRv;
@@ -35,6 +54,9 @@ xformat(
             ss << a_t2;
             break;
         case 2:
+            ss << a_t3;
+            break;
+        case 3:
         default:
             ;
             break;
@@ -82,9 +104,10 @@ Test_Units::unit(
     {
         std::size_t    v1 = 777;
         std::tstring_t v2 = "sss";
+        cint_t         v3 = 3;
 
-        m_sRv = ::xformat("***[{}]=[{}]***", v1, v2);
-        xTEST_EQ(m_sRv, std::tstring_t(xT("***[777]=[sss]***")));
+        m_sRv = ::xformat("***[{}]=[{}]=[{}]***", v1, v2, v3);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("***[777]=[sss]=[3]***")));
 
         Trace() << xTRACE_VAR(m_sRv);
 
