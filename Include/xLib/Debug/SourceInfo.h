deb45d25d2683407c6984157e450f983b8ed1c45
skynowa <skynowa@gmail.com>
2014-12-02 18:48:08 +0200

    Debug: SourceInfo - fix

--- Include/xLib/Debug/SourceInfo.h
+++ Include/xLib/Debug/SourceInfo.h
@@ -14,23 +14,23 @@ class SourceInfo
     ///<
 {
 public:
-                     SourceInfo(std::ctstring_t &filePath, culong_t &lineNum,
-                         std::ctstring_t &funcName);
+                      SourceInfo(std::ctstring_t &filePath, culong_t &lineNum,
+                          std::ctstring_t &funcName);
         ///< constructor
-    virtual         ~SourceInfo() {};
+    virtual          ~SourceInfo() {};
         ///< destructor
 
-    std::tstring_t   filePath() const;
+    std::ctstring_t & filePath() const;
         ///< file path
-    ulong_t          lineNum() const;
+    culong_t        & lineNum() const;
         ///< line number
-    std::tstring_t   funcName() const;
+    std::ctstring_t & funcName() const;
         ///< function name
 
 private:
-    std::ctstring_t &_filePath; ///< file path
-    culong_t        &_lineNum;  ///< line number
-    std::ctstring_t &_funcName; ///< function name
+    std::ctstring_t  &_filePath; ///< file path
+    culong_t         &_lineNum;  ///< line number
+    std::ctstring_t  &_funcName; ///< function name
 
     xNO_COPY_ASSIGN(SourceInfo)
 };
