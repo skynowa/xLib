737636f6a94dca6c9a817c054a2464dc2e79cb75
skynowa <skynowa@gmail.com>
2014-12-07 18:13:43 +0200

    Core: Array - fix code style

--- Include/xLib/Core/Array.h
+++ Include/xLib/Core/Array.h
@@ -15,7 +15,7 @@ class Array
     /// array
 {
 public:
-    T elems[N];
+    T data[N];
 
 public:
     // type definitions
@@ -28,39 +28,76 @@ public:
     typedef std::ptrdiff_t difference_type;
 
     // iterator support
-    iterator        begin()        { return elems; }
-    const_iterator  begin()  const { return elems; }
-    const_iterator  cbegin() const { return elems; }
+    iterator
+    begin()
+    {
+        return data;
+    }
 
-    iterator        end()          { return elems + N; }
-    const_iterator  end()    const { return elems + N; }
-    const_iterator  cend()   const { return elems + N; }
+    const_iterator
+    begin() const
+    {
+        return data;
+    }
+
+    const_iterator
+    cbegin() const
+    {
+        return data;
+    }
+
+    iterator
+    end()
+    {
+        return data + N;
+    }
+
+    const_iterator
+    end() const
+    {
+        return data + N;
+    }
+    const_iterator
+    cend() const
+    {
+        return data + N;
+    }
 
     typedef std::reverse_iterator<iterator>       reverse_iterator;
     typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 
-    reverse_iterator rbegin()
+    reverse_iterator
+    rbegin()
     {
         return reverse_iterator(end());
     }
-    const_reverse_iterator rbegin() const
-    {
-        return const_reverse_iterator(end());
-    }
-    const_reverse_iterator crbegin() const
+
+    const_reverse_iterator
+    rbegin() const
     {
         return const_reverse_iterator(end());
     }
 
-    reverse_iterator rend()
+    const_reverse_iterator
+    crbegin() const
+    {
+        return const_reverse_iterator(end());
+    }
+
+    reverse_iterator
+    rend()
     {
         return reverse_iterator(begin());
     }
-    const_reverse_iterator rend() const
+
+    const_reverse_iterator
+    rend() const
     {
         return const_reverse_iterator(begin());
     }
-    const_reverse_iterator crend() const
+
+    const_reverse_iterator
+    crend() const
     {
         return const_reverse_iterator(begin());
     }
@@ -68,140 +105,193 @@ public:
     // operator[]
     reference operator[](size_type i)
     {
-        // ASSERT( i < N && "out of range" );
-        return elems[i];
+        // ASSERT(i < N && "out of range");
+        return data[i];
     }
 
     const_reference operator[](size_type i) const
     {
-        // ASSERT( i < N && "out of range" );
-        return elems[i];
+        // ASSERT(i < N && "out of range");
+        return data[i];
     }
 
     // at() with range check
-    reference at(size_type i) {
-        rangecheck(i); return elems[i];
-    }
-    const_reference at(size_type i) const
+    reference
+    at(size_type i)
     {
-        rangecheck(i); return elems[i];
+        rangecheck(i);
+        return data[i];
+    }
+
+    const_reference
+    at(size_type i) const
+    {
+        rangecheck(i);
+        return data[i];
     }
 
     // front() and back()
-    reference front()
+    reference
+    front()
     {
-        return elems[0];
+        return data[0];
     }
-    const_reference front() const
+
+    const_reference
+    front() const
     {
-        return elems[0];
+        return data[0];
     }
-    reference back()
+
+    reference
+    back()
     {
-        return elems[N - 1];
+        return data[N - 1];
     }
-    const_reference back() const
+
+    const_reference
+    back() const
     {
-        return elems[N - 1];
+        return data[N - 1];
     }
 
     // size is constant
-    static size_type size    () { return N; }
-    static bool_t    empty   () { return false; }
-    static size_type max_size() { return N; }
-    enum { static_size = N };
+    static size_type
+    size()
+    {
+        return N;
+    }
+
+    static bool_t
+    empty()
+    {
+        return false;
+    }
+
+    static size_type
+    max_size()
+    {
+        return N;
+    }
+
+    enum {
+        static_size = N
+    };
 
     // swap (note: linear complexity)
-    void_t swap (Array<T, N> &y)
+    void_t
+    swap(Array<T, N> &y)
     {
         for (size_type i = 0; i < N; ++ i) {
-            /*boost*/std::swap(elems[i], y.elems[i]);
+            std::swap(data[i], y.data[i]);
         }
     }
 
     // direct access to data (read-only)
-    const T* data() const
+    const T*
+    data() const
     {
-        return elems;
+        return data;
     }
-    T* data() {
-        return elems;
+
+    T*
+    data()
+    {
+        return data;
     }
 
     // use array as C array (direct read/write access to data)
-    T* c_array()
+    T*
+    c_array()
     {
-        return elems;
+        return data;
     }
 
     // assignment with type conversion
     template <class T2>
-    Array<T, N>& operator= (const Array<T2, N> &rhs)
+    Array<T, N>&
+    operator= (const Array<T2, N> &rhs)
     {
         std::copy(rhs.begin(), rhs.end(), begin());
         return *this;
     }
 
     // assign one value to all elements
-    void_t assign(const T &value)
+    void_t
+    assign(const T &value)
     {
         fill(value);
     }
-    void_t fill(const T &value)
+
+    void_t
+    fill(const T &value)
     {
         std::fill_n(begin(),size(),value);
     }
 
-    void_t clear()
+    void_t
+    clear()
     {
         fill(0);
     }
 
     // check range (may be private because it is static)
-    static void_t rangecheck(size_type i)
+    static void_t
+    rangecheck(size_type i)
     {
         if (i >= size()) {
-            std::out_of_range e("array<>: index out of range");
-            ////boost::throw_exception(e);
+            std::out_of_range e("xlib::core::Array<>: index out of range");
         }
     }
 };
 //-------------------------------------------------------------------------------------------------
 // comparisons
 template<class T, std::size_t N>
-bool_t operator == (const Array<T, N> &x, const Array<T, N> &y)
+bool_t
+operator == (const Array<T, N> &x, const Array<T, N> &y)
 {
     return std::equal(x.begin(), x.end(), y.begin());
 }
+
 template<class T, std::size_t N>
-bool_t operator< (const Array<T, N> &x, const Array<T, N> &y)
+bool_t
+operator< (const Array<T, N> &x, const Array<T, N> &y)
 {
     return std::lexicographical_compare(x.begin(),x.end(),y.begin(),y.end());
 }
+
 template<class T, std::size_t N>
-bool_t operator != (const Array<T, N> &x, const Array<T, N> &y)
+bool_t
+operator != (const Array<T, N> &x, const Array<T, N> &y)
 {
     return !(x == y);
 }
+
 template<class T, std::size_t N>
-bool_t operator > (const Array<T, N> &x, const Array<T, N> &y)
+bool_t
+operator > (const Array<T, N> &x, const Array<T, N> &y)
 {
     return (y < x);
 }
+
 template<class T, std::size_t N>
-bool_t operator <= (const Array<T, N> &x, const Array<T, N> &y)
+bool_t
+operator <= (const Array<T, N> &x, const Array<T, N> &y)
 {
     return !(y < x);
 }
+
 template<class T, std::size_t N>
-bool_t operator >= (const Array<T, N> &x, const Array<T, N> &y)
+bool_t
+operator >= (const Array<T, N> &x, const Array<T, N> &y)
 {
     return !(x < y);
 }
 
 // swap()
 template<class T, std::size_t N>
-inline void_t swap(Array<T, N> &x, Array<T, N> &y)
+inline void_t
+swap(Array<T, N> &x, Array<T, N> &y)
 {
     x.swap(y);
 }
@@ -211,9 +301,7 @@ template<class T>
 Array<T, 2>
 make_array(const T &t1, const T &t2)
 {
-    Array<T, 2> aArrayT = {{t1, t2}};
-
-    return aArrayT;
+    return {{t1, t2}};
 }
 
 // make_array (3 elements)
@@ -221,9 +309,7 @@ template<class T>
 Array<T, 3>
 make_array(const T &t1, const T &t2, const T &t3)
 {
-    Array<T, 3> aArrayT = {{t1, t2, t3}};
-
-    return aArrayT;
+    return {{t1, t2, t3}};
 }
 
 xNAMESPACE_END2(xlib, core)
