b40dada022195a925294d18902adaca10e7cf433
skynowa <skynowa@gmail.com>
2014-12-08 18:55:58 +0200

    Core: Format - update

--- Include/xLib/Core/Format.h
+++ Include/xLib/Core/Format.h
@@ -22,119 +22,130 @@ xNAMESPACE_BEGIN2(xlib, core)
 #define xARG_TYPES_9  class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9
 #define xARG_TYPES_10 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10
 
-#define xARG_VARS_1  const T1& a_t1
-#define xARG_VARS_2  const T1& a_t1, const T2& a_t2
-#define xARG_VARS_3  const T1& a_t1, const T2& a_t2, const T3& a_t3
-#define xARG_VARS_4  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4
-#define xARG_VARS_5  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5
-#define xARG_VARS_6  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5, const T6& a_t6
-#define xARG_VARS_7  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5, const T6& a_t6, const T7& a_t7
-#define xARG_VARS_8  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5, const T6& a_t6, const T7& a_t7, const T8& a_t8
-#define xARG_VARS_9  const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5, const T6& a_t6, const T7& a_t7, const T8& a_t8, const T9& a_t9
-#define xARG_VARS_10 const T1& a_t1, const T2& a_t2, const T3& a_t3, const T4& a_t4, const T5& a_t5, const T6& a_t6, const T7& a_t7, const T8& a_t8, const T9& a_t9, const T10& a_t10
+#define xARG_VARS_1  const T1& a_v1
+#define xARG_VARS_2  const T1& a_v1, const T2& a_v2
+#define xARG_VARS_3  const T1& a_v1, const T2& a_v2, const T3& a_v3
+#define xARG_VARS_4  const T1& a_v1, const T2& a_v2, const T3& a_v3, const T4& a_v4
+#define xARG_VARS_5  const T1& a_v1, const T2& a_v2, const T3& a_v3, const T4& a_v4, const T5& a_v5
+#define xARG_VARS_6  const T1& a_v1, const T2& a_v2, const T3& a_v3, const T4& a_v4, const T5& a_v5, const T6& a_v6
+#define xARG_VARS_7  const T1& a_v1, const T2& a_v2, const T3& a_v3, const T4& a_v4, const T5& a_v5, const T6& a_v6, const T7& a_v7
+#define xARG_VARS_8  const T1& a_v1, const T2& a_v2, const T3& a_v3, const T4& a_v4, const T5& a_v5, const T6& a_v6, const T7& a_v7, const T8& a_v8
+#define xARG_VARS_9  const T1& a_v1, const T2& a_v2, const T3& a_v3, const T4& a_v4, const T5& a_v5, const T6& a_v6, const T7& a_v7, const T8& a_v8, const T9& a_v9
+#define xARG_VARS_10 const T1& a_v1, const T2& a_v2, const T3& a_v3, const T4& a_v4, const T5& a_v5, const T6& a_v6, const T7& a_v7, const T8& a_v8, const T9& a_v9, const T10& a_v10
+
+#define xARGS_1 v1
+#define xARGS_2 v1, v2
+#define xARGS_3 v1, v2, v3
+#define xARGS_4 v1, v2, v3, v4
+#define xARGS_5 v1, v2, v3, v4, v5
+#define xARGS_6 v1, v2, v3, v4, v5, v6
+#define xARGS_7 v1, v2, v3, v4, v5, v6, v7
+#define xARGS_8 v1, v2, v3, v4, v5, v6, v7, v8
+#define xARGS_9 v1, v2, v3, v4, v5, v6, v7, v8, v9
+#define xARGS_10 v1, v2, v3, v4, v5, v6, v7, v8, v9, v10
 
 #define xSWITCH_1(v) \
             switch (v) { \
-            case 0: ss << a_t1; break; \
+            case 0: ss << a_v1; break; \
             default:            break; \
             }
 
 #define xSWITCH_2(v) \
             switch (v) { \
-            case 0: ss << a_t1; break; \
-            case 1: ss << a_t2; break; \
+            case 0: ss << a_v1; break; \
+            case 1: ss << a_v2; break; \
             default:            break; \
             }
 
 #define xSWITCH_3(v) \
             switch (v) { \
-            case 0: ss << a_t1; break; \
-            case 1: ss << a_t2; break; \
-            case 2: ss << a_t3; break; \
+            case 0: ss << a_v1; break; \
+            case 1: ss << a_v2; break; \
+            case 2: ss << a_v3; break; \
             default:            break; \
             }
 
 #define xSWITCH_4(v) \
             switch (v) { \
-            case 0: ss << a_t1; break; \
-            case 1: ss << a_t2; break; \
-            case 2: ss << a_t3; break; \
-            case 3: ss << a_t4; break; \
+            case 0: ss << a_v1; break; \
+            case 1: ss << a_v2; break; \
+            case 2: ss << a_v3; break; \
+            case 3: ss << a_v4; break; \
             default:            break; \
             }
 
 #define xSWITCH_5(v) \
             switch (v) { \
-            case 0: ss << a_t1; break; \
-            case 1: ss << a_t2; break; \
-            case 2: ss << a_t3; break; \
-            case 3: ss << a_t4; break; \
-            case 4: ss << a_t5; break; \
+            case 0: ss << a_v1; break; \
+            case 1: ss << a_v2; break; \
+            case 2: ss << a_v3; break; \
+            case 3: ss << a_v4; break; \
+            case 4: ss << a_v5; break; \
             default:            break; \
             }
 
 #define xSWITCH_6(v) \
             switch (v) { \
-            case 0: ss << a_t1; break; \
-            case 1: ss << a_t2; break; \
-            case 2: ss << a_t3; break; \
-            case 3: ss << a_t4; break; \
-            case 4: ss << a_t5; break; \
-            case 5: ss << a_t6; break; \
+            case 0: ss << a_v1; break; \
+            case 1: ss << a_v2; break; \
+            case 2: ss << a_v3; break; \
+            case 3: ss << a_v4; break; \
+            case 4: ss << a_v5; break; \
+            case 5: ss << a_v6; break; \
             default:            break; \
             }
 
 #define xSWITCH_7(v) \
             switch (v) { \
-            case 0: ss << a_t1; break; \
-            case 1: ss << a_t2; break; \
-            case 2: ss << a_t3; break; \
-            case 3: ss << a_t4; break; \
-            case 4: ss << a_t5; break; \
-            case 5: ss << a_t6; break; \
-            case 6: ss << a_t7; break; \
+            case 0: ss << a_v1; break; \
+            case 1: ss << a_v2; break; \
+            case 2: ss << a_v3; break; \
+            case 3: ss << a_v4; break; \
+            case 4: ss << a_v5; break; \
+            case 5: ss << a_v6; break; \
+            case 6: ss << a_v7; break; \
             default:            break; \
             }
 
 #define xSWITCH_8(v) \
             switch (v) { \
-            case 0: ss << a_t1; break; \
-            case 1: ss << a_t2; break; \
-            case 2: ss << a_t3; break; \
-            case 3: ss << a_t4; break; \
-            case 4: ss << a_t5; break; \
-            case 5: ss << a_t6; break; \
-            case 6: ss << a_t7; break; \
-            case 7: ss << a_t8; break; \
+            case 0: ss << a_v1; break; \
+            case 1: ss << a_v2; break; \
+            case 2: ss << a_v3; break; \
+            case 3: ss << a_v4; break; \
+            case 4: ss << a_v5; break; \
+            case 5: ss << a_v6; break; \
+            case 6: ss << a_v7; break; \
+            case 7: ss << a_v8; break; \
             default:            break; \
             }
 
 #define xSWITCH_9(v) \
             switch (v) { \
-            case 0: ss << a_t1; break; \
-            case 1: ss << a_t2; break; \
-            case 2: ss << a_t3; break; \
-            case 3: ss << a_t4; break; \
-            case 4: ss << a_t5; break; \
-            case 5: ss << a_t6; break; \
-            case 6: ss << a_t7; break; \
-            case 7: ss << a_t8; break; \
-            case 8: ss << a_t9; break; \
+            case 0: ss << a_v1; break; \
+            case 1: ss << a_v2; break; \
+            case 2: ss << a_v3; break; \
+            case 3: ss << a_v4; break; \
+            case 4: ss << a_v5; break; \
+            case 5: ss << a_v6; break; \
+            case 6: ss << a_v7; break; \
+            case 7: ss << a_v8; break; \
+            case 8: ss << a_v9; break; \
             default:            break; \
             }
 
 #define xSWITCH_10(v) \
             switch (v) { \
-            case 0: ss << a_t1; break; \
-            case 1: ss << a_t2; break; \
-            case 2: ss << a_t3; break; \
-            case 3: ss << a_t4; break; \
-            case 4: ss << a_t5; break; \
-            case 5: ss << a_t6; break; \
-            case 6: ss << a_t7; break; \
-            case 7: ss << a_t8; break; \
-            case 8: ss << a_t9; break; \
-            case 9: ss << a_t10; break; \
+            case 0: ss << a_v1; break; \
+            case 1: ss << a_v2; break; \
+            case 2: ss << a_v3; break; \
+            case 3: ss << a_v4; break; \
+            case 4: ss << a_v5; break; \
+            case 5: ss << a_v6; break; \
+            case 6: ss << a_v7; break; \
+            case 7: ss << a_v8; break; \
+            case 8: ss << a_v9; break; \
+            case 9: ss << a_v10; break; \
             default:            break; \
             }
 //-------------------------------------------------------------------------------------------------
@@ -265,12 +276,12 @@ Format(
         ss.clear();
 
         switch (i) {
-        case 0: ss << a_t1; break;
-        case 1: ss << a_t2; break;
-        case 2: ss << a_t3; break;
-        case 3: ss << a_t4; break;
-        case 4: ss << a_t5; break;
-        case 5: ss << a_t6; break;
+        case 0: ss << a_v1; break;
+        case 1: ss << a_v2; break;
+        case 2: ss << a_v3; break;
+        case 3: ss << a_v4; break;
+        case 4: ss << a_v5; break;
+        case 5: ss << a_v6; break;
         default:            break;
         }
 
