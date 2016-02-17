959b848a4739d1a0f32aff8f84f3a8ebc0631af3
skynowa <skynowa@gmail.com>
2016-02-17 21:14:52 +0200

    Package: Ssh2User - remove

--- Include/xLib/Package/Ssh2Client.h
+++ Include/xLib/Package/Ssh2Client.h
@@ -8,7 +8,6 @@
 
 #include <xLib/Core/Core.h>
 #include <libssh2.h>
-#include "Ssh2User.h"
 //-------------------------------------------------------------------------------------------------
 xNAMESPACE_BEGIN2(xlib, package)
 
@@ -27,11 +26,11 @@ public:
     virtual       ~Ssh2Client();
 
     void           construct(std::ctstring_t &hostName, cushort_t &port, std::ctstring_t &userName,
-                        std::ctstring_t &password, cbool_t &isUseKey, std::ctstring_t &keyDirPath);
+                       std::ctstring_t &password);
 
     bool           connect();
     void           authPassword(cUserAuth userAuth);
-    void           authPublicKey();
+    void           authPublicKey(std::ctstring_t &keyDirPath);
     bool           executeCmd(std::ctstring_t &cmd, std::tstring_t *stdOut, std::tstring_t *stdErr);
     void           disconnect();
 
@@ -42,15 +41,10 @@ private:
     LIBSSH2_SESSION * _session;
     int            _socket;
 
-    Ssh2User       _user;
-
     std::tstring_t _hostName;
     ushort_t       _port;
     std::tstring_t _userName;
     std::tstring_t _password;
-    bool           _isUseKey;
-    std::tstring_t _keyDirPath;
-
 };
 
 xNAMESPACE_END2(xlib, package)
