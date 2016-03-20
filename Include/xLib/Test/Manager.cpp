8fce6f390913abb945e39d3b5ccf154d6cc902cb
skynowa <skynowa@gmail.com>
2016-03-20 17:01:54 +0200

    Test: Manager - fix parsing cmdline params

--- Include/xLib/Test/Manager.cpp
+++ Include/xLib/Test/Manager.cpp
@@ -31,78 +31,21 @@ Manager::Manager(
     cint_t   &a_argsNum,
     ctchar_t *a_args[]
 ) :
-    _data (),
-    _units()
+    _data         (),
+    _units        (),
+    _isConstructed(false)
 {
-    // checks
-    {
-    #if xENV_UNIX
-        SystemInfo info;
-        xCHECK_MSG_DO(info.isUserAdmin(), xT("xLib_test: Can't run as root"), return);
-    #endif
-    }
-
-    // parse params
-    {
-        std::vec_tstring_t args;
-
-        ProcessInfo info;
-        info.setProcessId(Process::currentId());
-        info.commandLine(&args);
-
-        Trace() << xTRACE_VAR(a_argsNum);
-        Trace() << xTRACE_VAR(args.size());
-        Trace() << xTRACE_VAR(args);
-
-        if (a_argsNum == 1) {
-            // OK, run tests with default params
-        }
-        else if (a_argsNum == 2) {
-            // usage
-            bool_t bRv = StringCI::compare(xT("-h"),     args.at(1)) ||
-                         StringCI::compare(xT("--help"), args.at(1));
-            if (!bRv) {
-                std::tcout << xT("\nxLib_test: unknown switches\n") << std::endl;
-            } else {
-                std::tcout << xT("\nUsage: ./xLib_test [is_tracing] [all_loops] [unit_loops]\n")
-                              xT("  - xLib_test  (binary file path)\n")
-                              xT("  - is_tracing (is tracing)\n")
-                              xT("  - all_loops  (loops for all tests)\n")
-                              xT("  - unit_loops (loops for unit test)\n")
-                              xT("  - case_loops (loops for case test)\n") << std::endl;
-            }
-
-            return;
-        }
-        else if (a_argsNum == 5) {
-            // params
-            _data.isUseTracing = String::cast<bool_t>     ( args.at(1) );
-            _data.allLoops     = String::cast<std::size_t>( args.at(2) );
-            _data.unitLoops    = String::cast<std::size_t>( args.at(3) );
-            _data.caseLoops    = String::cast<std::size_t>( args.at(4) );
-        }
-        else {
-            // fail
-            std::tcout << xT("\nxLib_test: unknown switches\n") << std::endl;
-            return;
-        }
-    }
-
-    xCHECK_DO(_data.isUseTracing,
-        Trace()
-            << xT("\n\nManager: *** ") << xLIB_NAME
-            << xT(" v.")               << xLIB_VERSION << xT(" ") << xLIB_VERSION_SUFFIX
-            << xT(" author: ")         << xLIB_AUTHOR
-            << xT(" date: ")           << xLIB_DATE
-            << xT(" ***"));
+    _construct(a_argsNum, a_args);
 }
 //-------------------------------------------------------------------------------------------------
 Manager::Manager(
     cManagerData &a_data
 ) :
-    _data (a_data),
-    _units()
+    _data         (a_data),
+    _units        (),
+    _isConstructed(false)
 {
+    _construct(0, xPTR_NULL);
 }
 //-------------------------------------------------------------------------------------------------
 /* virtual */
@@ -129,8 +72,13 @@ Manager::add(
 xINLINE void_t
 Manager::run()
 {
+    xCHECK_DO(!_isConstructed,
+        Trace() << xT("\nManager: not constructed. Stopped.");
+        return);
+
     xCHECK_DO(_data.isUseTracing,
-        Trace() << xT("\nTestManager: start all...");
+        Trace() << xT("\n");
+        Trace() << xT("Manager: start all...");
         Trace() << xT("Manager: module path: ") << Path::exe();
         Trace() << xT("Manager: ")
                 << xT("all loops: ")     << _data.allLoops
@@ -147,6 +95,91 @@ Manager::run()
     }
 
     xCHECK_DO(_data.isUseTracing, Trace() << xT("Manager: all successful done."));
+}
+//-------------------------------------------------------------------------------------------------
+
+
+/**************************************************************************************************
+*   private
+*
+**************************************************************************************************/
+
+//-------------------------------------------------------------------------------------------------
+xINLINE void_t
+Manager::_construct(
+    cint_t   &a_argsNum,
+    ctchar_t *a_args[]
+)
+{
+    bool_t bRv = false;
+
+    // checks
+    {
+    #if xENV_UNIX
+        SystemInfo info;
+        xCHECK_MSG_DO(info.isUserAdmin(), xT("xLib_test: Can't run as root"), return);
+    #endif
+    }
+
+    // parse params
+    if (a_args == xPTR_NULL) {
+        _isConstructed = true;
+    } else {
+        std::vec_tstring_t args;
+        ProcessInfo::commandLine(a_argsNum, a_args, &args);
+        xTEST_EQ(a_argsNum, (int_t)args.size());
+
+        switch ( args.size() ) {
+        case 1:
+            // OK - default params
+            _isConstructed = true;
+
+            break;
+        case 2:
+            // usage
+            bRv = StringCI::compare(xT("-h"),     args.at(1)) ||
+                  StringCI::compare(xT("--help"), args.at(1));
+            if (!bRv) {
+                std::tcout << xT("\nxLib_test: unknown switches\n") << std::endl;
+            } else {
+                std::tcout << xT("\nUsage: ./xLib_test [is_tracing] [all_loops] [unit_loops]\n")
+                              xT("  - xLib_test  (binary file path)\n")
+                              xT("  - is_tracing (is tracing)\n")
+                              xT("  - all_loops  (loops for all tests)\n")
+                              xT("  - unit_loops (loops for unit test)\n")
+                              xT("  - case_loops (loops for case test)\n") << std::endl;
+            }
+
+            _isConstructed = false;
+
+            break;
+        case 5:
+            // params
+            _data.isUseTracing = String::cast<bool_t>     ( args.at(1) );
+            _data.allLoops     = String::cast<std::size_t>( args.at(2) );
+            _data.unitLoops    = String::cast<std::size_t>( args.at(3) );
+            _data.caseLoops    = String::cast<std::size_t>( args.at(4) );
+
+            _isConstructed = true;
+
+            break;
+        default:
+            // fail
+            std::tcout << xT("\nxLib_test: unknown switches\n") << std::endl;
+
+            _isConstructed = false;
+
+            break;
+        }
+    }
+
+    xCHECK_DO(_data.isUseTracing,
+        Trace()
+            << xT("\n\nManager: *** ") << xLIB_NAME
+            << xT(" v.")               << xLIB_VERSION << xT(" ") << xLIB_VERSION_SUFFIX
+            << xT(" author: ")         << xLIB_AUTHOR
+            << xT(" date: ")           << xLIB_DATE
+            << xT(" ***"));
 }
 //-------------------------------------------------------------------------------------------------
 
