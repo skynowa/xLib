09a94d544795e3c0446a8428ac11bd60003db1b8
skynowa <skynowa@gmail.com>
2016-03-20 20:55:25 +0200

    Core: CoreApplication - temp fix

--- Include/xLib/Core/CoreApplication.cpp
+++ Include/xLib/Core/CoreApplication.cpp
@@ -29,7 +29,7 @@ CoreApplication::CoreApplication(
 {
     // Trace() << xFUNCTION;
 
-#if 1
+#if 0
     std::vector<int_t> signalNums;
     signalNums.push_back(SIGHUP);
     signalNums.push_back(SIGINT);
