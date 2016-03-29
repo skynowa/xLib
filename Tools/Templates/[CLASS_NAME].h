1205bb3d40df516b7592af76c235c3b4827090c8
skynowa <skynowa@gmail.com>
2016-03-29 18:46:36 +0300

    Tools: Templates - update

--- Tools/Templates/[CLASS_NAME].h
+++ Tools/Templates/[CLASS_NAME].h
@@ -8,7 +8,7 @@
 
 #include <xLib/Core/Core.h>
 //-------------------------------------------------------------------------------------------------
-xNAMESPACE_BEGIN2(xlib, [NAMESPACE])
+xNAMESPACE_BEGIN2(xlib, [XLIB_NS])
 
 class [CLASS_NAME]
     ///<
@@ -21,9 +21,13 @@ public:
 
 private:
     xNO_COPY_ASSIGN([CLASS_NAME])
+
+xPLATFORM_IMPL:
+    void_t   _construct_impl();
+    void_t   _destruct_impl();
 };
 
-xNAMESPACE_END2(xlib, [NAMESPACE])
+xNAMESPACE_END2(xlib, [XLIB_NS])
 //-------------------------------------------------------------------------------------------------
 #if xOPTION_PROJECT_HEADER_ONLY
     #include "[CLASS_NAME].cpp"
