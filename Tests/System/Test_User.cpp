942f569ec709196362fc7be211602f10d674739d
skynowa <skynowa@gmail.com>
2016-03-30 00:03:30 +0300

    System: User - update

--- Tests/System/Test_User.cpp
+++ Tests/System/Test_User.cpp
@@ -22,65 +22,65 @@ Test_User::unit()
     xTEST_CASE("isAdmin")
     {
         m_bRv = User().isAdmin();
-        #if xTEST_IGNORE
-            xTRACEV(xT("\tUser::isUserAdmin(): %s"), String::castBool(m_bRv).c_str());
-        #endif
+        Trace() << xT("\t\t") << xTRACE_VAR(m_bRv);
     }
 
     xTEST_CASE("name")
     {
         m_sRv = User().name();
-        #if xTEST_IGNORE
-            Trace() << xTRACE_VAR(m_sRv);
-        #endif
         xTEST_EQ(m_sRv.empty(), false);
+
+        Trace() << xT("\t\t") << xTRACE_VAR(m_sRv);
     }
 
     xTEST_CASE("loginName")
     {
         m_sRv = User().loginName();
-        xTEST_EQ(false, m_sRv.empty());
-        #if xTEST_IGNORE
-            xTRACEV(xT("\tUser::loginUserName(): %s"), m_sRv.c_str());
-        #endif
+        Trace() << xT("\t\t") << xTRACE_VAR(m_sRv);
+
+        xTEST_EQ(m_sRv.empty(), false);
+    }
+
+    xTEST_CASE("name")
+    {
+        m_sRv = User().name();
+        Trace() << xT("\t\t") << xTRACE_VAR(m_sRv);
+
+        xTEST_EQ(m_sRv.empty(), false);
     }
 
     xTEST_CASE("userId")
     {
         m_uiRv = User().userId();
-        #if xTEST_IGNORE
-            Trace() << xTRACE_VAR(m_uiRv);
-        #endif
         xTEST_DIFF(m_uiRv, 0U);
+
+        Trace() << xT("\t\t") << xTRACE_VAR(m_uiRv);
     }
 
     xTEST_CASE("groupId")
     {
         m_uiRv = User().groupId();
-        #if xTEST_IGNORE
-            Trace() << xTRACE_VAR(m_uiRv);
-        #endif
         xTEST_DIFF(m_uiRv, 0U);
+
+        Trace() << xT("\t\t") << xTRACE_VAR(m_uiRv);
     }
 
     xTEST_CASE("homeDir")
     {
         m_sRv = User().homeDir();
-        xTEST_EQ(false, m_sRv.empty());
-        xTEST_EQ(true,  Dir(m_sRv).isExists());
-        #if xTEST_IGNORE
-            xTRACEV(xT("\tUser::homeDir(): %s"), m_sRv.c_str());
-        #endif
+        xTEST_EQ(m_sRv.empty(), false);
+        xTEST_EQ(Dir(m_sRv).isExists(), true);
+
+        Trace() << xT("\t\t") << xTRACE_VAR(m_sRv);
     }
 
     xTEST_CASE("shellPath")
     {
         m_sRv = User().shellPath();
-        xTEST_EQ(false, m_sRv.empty());
-        xTEST_EQ(true,  File::isExists(m_sRv));
-        #if xTEST_IGNORE
-            xTRACEV(xT("\tUser::userShellPath(): %s"), m_sRv.c_str());
-        #endif
+        xTEST_EQ(m_sRv.empty(), false);
+        xTEST_EQ(File::isExists(m_sRv), true);
+
+        Trace() << xT("\t\t") << xT("shellPath(): ") << m_sRv;
     }
 }
 //-------------------------------------------------------------------------------------------------
