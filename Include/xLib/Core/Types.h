53f7af8cfeca2cd6204f0145d532b48dcecb556b
skynowa <skynowa@gmail.com>
2016-03-31 10:05:52 +0300

    Core: Types - review

--- Include/xLib/Core/Types.h
+++ Include/xLib/Core/Types.h
@@ -9,192 +9,187 @@
 #include <xLib/Core/SysDefines.h>
 
 xNAMESPACE_BEGIN(xlib)
-    typedef void                   void_t;
-    typedef const void_t           cvoid_t;
 
-    typedef bool                   bool_t;
-    typedef const bool_t           cbool_t;
+    typedef void void_t;
+    xTYPEDEF_CONST(void_t);
+
+    typedef bool bool_t;
+    xTYPEDEF_CONST(bool_t);
 
     // tchar_t
 #if xUNICODE
-    typedef wchar_t                tchar_t;
+    typedef wchar_t tchar_t;
 #else
-    typedef char                   tchar_t;
+    typedef char    tchar_t;
 #endif
-    typedef const tchar_t          ctchar_t;
+    xTYPEDEF_CONST(tchar_t);
         ///< tchar_t
 
-    typedef unsigned char          uchar_t;
-    typedef const uchar_t          cuchar_t;
+    typedef unsigned char uchar_t;
+    xTYPEDEF_CONST(uchar_t);
 
-    typedef int                    int_t;
-    typedef const int_t            cint_t;
+    typedef int int_t;
+    xTYPEDEF_CONST(int_t);
 
-    typedef signed short int       short_t;
-    typedef const short_t          cshort_t;
+    typedef signed short int short_t;
+    xTYPEDEF_CONST(short_t);
 
-    typedef signed long int        long_t;
-    typedef const long_t           clong_t;
+    typedef signed long int long_t;
+    xTYPEDEF_CONST(long_t);
 
     // longlong_t
 #if xCOMPILER_GNUC
     __extension__
 #endif
-    typedef signed long long int   longlong_t;
-    typedef const longlong_t       clonglong_t;
+    typedef signed long long int longlong_t;
+    xTYPEDEF_CONST(longlong_t);
 
-    typedef unsigned int           uint_t;
-    typedef const uint_t           cuint_t;
+    typedef unsigned int uint_t;
+    xTYPEDEF_CONST(uint_t);
 
-    typedef unsigned short int     ushort_t;
-    typedef const ushort_t         cushort_t;
+    typedef unsigned short int ushort_t;
+    xTYPEDEF_CONST(ushort_t);
 
-    typedef unsigned long int      ulong_t;
-    typedef const ulong_t          culong_t;
+    typedef unsigned long int ulong_t;
+    xTYPEDEF_CONST(ulong_t);
 
     // ulonglong_t
 #if xCOMPILER_GNUC
     __extension__
 #endif
     typedef unsigned long long int ulonglong_t;
-    typedef const ulonglong_t      culonglong_t;
+    xTYPEDEF_CONST(ulonglong_t);
 
-    typedef float                  float_t;
-    typedef const float_t          cfloat_t;
+    typedef float float_t;
+    xTYPEDEF_CONST(float_t);
 
-    // TODO: typedef double                 double_t;
-    typedef const double           cdouble_t;
+    typedef double double_t;
+    xTYPEDEF_CONST(double_t);
 
-    typedef long double            longdouble_t;
-    typedef const longdouble_t     clongdouble_t;
+    typedef long double longdouble_t;
+    xTYPEDEF_CONST(longdouble_t);
 
     // addrinfo_t
 #if   xENV_WIN
     #if   xCOMPILER_MINGW
-        typedef addrinfo           addrinfo_t;
+        typedef addrinfo  addrinfo_t;
     #elif xCOMPILER_CODEGEAR
-        typedef addrinfo           addrinfo_t;
+        typedef addrinfo  addrinfo_t;
     #else
-        typedef ADDRINFOT          addrinfo_t;
+        typedef ADDRINFOT addrinfo_t;
     #endif
 #elif xENV_UNIX
-    typedef addrinfo               addrinfo_t;
+    typedef addrinfo      addrinfo_t;
 #endif
-    typedef const addrinfo_t       caddrinfo_t;
+    xTYPEDEF_CONST(addrinfo_t);
         ///< hold host address information
 
     // native_handle_t
 #if   xENV_WIN
-    typedef HANDLE                 native_handle_t;
+    typedef HANDLE native_handle_t;
 #elif xENV_UNIX
-    typedef int                    native_handle_t;
+    typedef int    native_handle_t;
 #endif
-    typedef const native_handle_t  cnative_handle_t;
+    xTYPEDEF_CONST(native_handle_t);
         ///< native handle
 
     // socket_t
 #if   xENV_WIN
-    typedef SOCKET                 socket_t;
+    typedef SOCKET socket_t;
 #elif xENV_UNIX
-    typedef int                    socket_t;
+    typedef int    socket_t;
 #endif
-    typedef const socket_t         csocket_t;
+    xTYPEDEF_CONST(socket_t);
         ///< socket native handle
 
     // socklen_t
 #if   xENV_WIN
-    typedef int_t                  socket_length_t;
+    typedef int_t     socket_length_t;
 #elif xENV_UNIX
-    typedef socklen_t              socket_length_t;
+    typedef socklen_t socket_length_t;
 #endif
-    typedef const socket_length_t  csocket_length_t;
+    xTYPEDEF_CONST(socket_length_t);
         ///< socket address length
 
     // twint_t
 #if xUNICODE
-    typedef wint_t                 twint_t;
+    typedef wint_t twint_t;
 #else
-    typedef int                    twint_t;
+    typedef int    twint_t;
 #endif
-    typedef const twint_t          ctwint_t;
+    xTYPEDEF_CONST(twint_t);
         ///< twint_t
 
 xNAMESPACE_END(xlib)
 
 xNAMESPACE_BEGIN(std)
+    // std::size_t
 #if xOS_WIN
-    typedef SSIZE_T                ::ssize_t;
-    typedef const ssize_t          cssize_t;
+    typedef SSIZE_T ::ssize_t;
+    xTYPEDEF_CONST(ssize_t);
 #endif
 
-    typedef const size_t           csize_t;
-    typedef const ssize_t          cssize_t;
-    typedef const string           cstring_t;
-    typedef const wstring          cwstring_t;
+    xTYPEDEF_CONST(size_t);
+    xTYPEDEF_CONST(ssize_t);
 
-    typedef basic_string<xlib::tchar_t, char_traits<xlib::tchar_t>, allocator<xlib::tchar_t> >
-                                   tstring_t;
-    typedef const tstring_t        ctstring_t;
+    // std::string
+    typedef const string cstring_t;
+    typedef const wstring cwstring_t;
 
-    typedef basic_string<xlib::uchar_t, char_traits<xlib::uchar_t>, allocator<xlib::uchar_t> >
-                                   ustring_t;
-    typedef const ustring_t        custring_t;
+    typedef basic_string<xlib::tchar_t, char_traits<xlib::tchar_t>, allocator<xlib::tchar_t> > tstring_t;
+    xTYPEDEF_CONST(tstring_t);
 
-    typedef basic_istream<xlib::tchar_t, char_traits<xlib::tchar_t> >
-                                   tistream_t;
-    typedef const tistream_t       ctistream_t;
+    typedef basic_string<xlib::uchar_t, char_traits<xlib::uchar_t>, allocator<xlib::uchar_t> > ustring_t;
+    xTYPEDEF_CONST(ustring_t);
 
-    typedef basic_ostream<xlib::tchar_t, char_traits<xlib::tchar_t> >
-                                   tostream_t;
-    typedef const tostream_t       ctostream_t;
+    // std::stream
+    typedef basic_istream<xlib::tchar_t, char_traits<xlib::tchar_t> > tistream_t;
+    xTYPEDEF_CONST(tistream_t);
 
-    typedef basic_iostream<xlib::tchar_t, char_traits<xlib::tchar_t> >
-                                   tiostream_t;
-    typedef const tiostream_t      ctiostream_t;
+    typedef basic_ostream<xlib::tchar_t, char_traits<xlib::tchar_t> > tostream_t;
+    xTYPEDEF_CONST(tostream_t);
 
-    typedef basic_istringstream<xlib::tchar_t, char_traits<xlib::tchar_t>, allocator<xlib::tchar_t> >
-                                   tistringstream_t;
-    typedef const tistringstream_t ctistringstream_t;
+    typedef basic_iostream<xlib::tchar_t, char_traits<xlib::tchar_t> > tiostream_t;
+    xTYPEDEF_CONST(tiostream_t);
 
-    typedef basic_ostringstream<xlib::tchar_t, char_traits<xlib::tchar_t>, allocator<xlib::tchar_t> >
-                                   tostringstream_t;
-    typedef const tostringstream_t ctostringstream_t;
+    // std::stringstream
+    typedef basic_istringstream<xlib::tchar_t, char_traits<xlib::tchar_t>, allocator<xlib::tchar_t> > tistringstream_t;
+    xTYPEDEF_CONST(tistringstream_t);
 
-    typedef basic_stringstream<xlib::tchar_t, char_traits<xlib::tchar_t>, allocator<xlib::tchar_t> >
-                                   tstringstream_t;
-    typedef const tstringstream_t  ctstringstream_t;
+    typedef basic_ostringstream<xlib::tchar_t, char_traits<xlib::tchar_t>, allocator<xlib::tchar_t> > tostringstream_t;
+    xTYPEDEF_CONST(tostringstream_t);
 
-    typedef basic_ifstream< xlib::tchar_t, char_traits<xlib::tchar_t> >
-                                   tifstream_t;
-    typedef const tifstream_t      ctifstream_t;
+    typedef basic_stringstream<xlib::tchar_t, char_traits<xlib::tchar_t>, allocator<xlib::tchar_t> > tstringstream_t;
+    xTYPEDEF_CONST(tstringstream_t);
 
-    typedef basic_ofstream<xlib::tchar_t, char_traits<xlib::tchar_t> >
-                                   tofstream_t;
-    typedef const tofstream_t      ctofstream_t;
+    // std::fstream
+    typedef basic_ifstream< xlib::tchar_t, char_traits<xlib::tchar_t> > tifstream_t;
+    xTYPEDEF_CONST(tifstream_t);
 
-    typedef basic_fstream<xlib::tchar_t, char_traits<xlib::tchar_t> >
-                                   tfstream_t;
-    typedef const tfstream_t       ctfstream_t;
+    typedef basic_ofstream<xlib::tchar_t, char_traits<xlib::tchar_t> > tofstream_t;
+    xTYPEDEF_CONST(tofstream_t);
 
-    // it's useful
-    typedef vector<xlib::tchar_t>  vec_tchar_t;
-    typedef const vec_tchar_t      cvec_tchar_t;
+    typedef basic_fstream<xlib::tchar_t, char_traits<xlib::tchar_t> > tfstream_t;
+    xTYPEDEF_CONST(tfstream_t);
 
-    typedef vector<xlib::uchar_t>  vec_uchar_t;
-    typedef const vec_uchar_t      cvec_uchar_t;
+    // std::vector
+    typedef vector<xlib::tchar_t> vec_tchar_t;
+    xTYPEDEF_CONST(vec_tchar_t);
 
-    typedef vector<tstring_t>      vec_tstring_t;
-    typedef const vec_tstring_t    cvec_tstring_t;
+    typedef vector<xlib::uchar_t> vec_uchar_t;
+    xTYPEDEF_CONST(vec_uchar_t);
 
-    typedef vector<ustring_t>      vec_ustring_t;
-    typedef const vec_ustring_t    cvec_ustring_t;
+    typedef vector<tstring_t> vec_tstring_t;
+    xTYPEDEF_CONST(vec_tstring_t);
 
-    typedef map<tstring_t, tstring_t>
-                                   map_tstring_t;
-    typedef const map_tstring_t    cmap_tstring_t;
+    typedef vector<ustring_t> vec_ustring_t;
+    xTYPEDEF_CONST(vec_ustring_t);
 
-    typedef multimap<tstring_t, tstring_t>
-                                   mmap_tstring_t;
-    typedef const mmap_tstring_t   cmmap_tstring_t;
+    // std::map
+    typedef map<tstring_t, tstring_t> map_tstring_t;
+    xTYPEDEF_CONST(map_tstring_t);
+
+    typedef multimap<tstring_t, tstring_t> mmap_tstring_t;
+    xTYPEDEF_CONST(mmap_tstring_t);
 
 xNAMESPACE_END(std)
