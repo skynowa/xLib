5065eefc3f6736fb3d6aefc2001459f2d735ef8a
skynowa <skynowa@gmail.com>
2016-04-01 16:50:16 +0300

    Core: StdStream - tests, review

--- Tests/Core/Test_StdStream.cpp
+++ Tests/Core/Test_StdStream.cpp
@@ -19,18 +19,18 @@ Test_StdStream::unit()
     xTEST_CASE("operator << for std::ustring_t")
     {
         for (size_t i = 0; i < 10; ++ i) {
-            std::ustring_t usStr;
-            StdRandom    random;
+            std::ustring_t str;
+            StdRandom      random;
 
-            usStr.resize( random.nextInt(size_t(1), size_t(64)) );
-            std::fill_n(usStr.begin(), usStr.size(), static_cast<std::ustring_t::value_type>( random.nextInt(1, 255) ));
+            str.resize( random.nextInt(size_t(1), size_t(64)) );
+            std::fill_n(str.begin(), str.size(), static_cast<std::ustring_t::value_type>( random.nextInt(1, 255) ));
 
-            std::tstring_t sVal1 = std::tstring_t(usStr.begin(), usStr.end());
+            std::tstring_t val1 = std::tstring_t(str.begin(), str.end());
 
-            std::tostringstream_t osOut;   osOut << usStr;
-            std::tstring_t sVal2 = osOut.str();
-
-            xTEST_EQ(sVal1, sVal2);
+            std::tostringstream_t oss;
+            oss << str;
+            std::tstring_t val2 = oss.str();
+            xTEST_EQ(val1, val2);
         }
     }
 
@@ -40,7 +40,6 @@ Test_StdStream::unit()
 
         std::tostringstream_t oss;
         oss << data;
-
         xTEST_EQ(oss.str().empty(), false);
     }
 
@@ -54,8 +53,9 @@ Test_StdStream::unit()
         m_vsRv.push_back(xT("Value3"));
         m_vsRv.push_back(xT("Value4"));
 
-        std::tostringstream_t osOut;   osOut << m_vsRv;
-        xTEST_EQ(false, osOut.str().empty());
+        std::tostringstream_t oss;
+        oss << m_vsRv;
+        xTEST_EQ(oss.str().empty(), false);
 
         m_vsRv.clear();
     }
@@ -70,8 +70,9 @@ Test_StdStream::unit()
         m_msRv.insert( std::pair<std::tstring_t, std::tstring_t>(xT("Key3"), xT("Value3")) );
         m_msRv.insert( std::pair<std::tstring_t, std::tstring_t>(xT("Key4"), xT("Value4")) );
 
-        std::tostringstream_t osOut;   osOut << m_msRv;
-        xTEST_EQ(false, osOut.str().empty());
+        std::tostringstream_t oss;
+        oss << m_msRv;
+        xTEST_EQ(oss.str().empty(), false);
 
         m_msRv.clear();
     }
@@ -86,8 +87,9 @@ Test_StdStream::unit()
         m_mmsRv.insert( std::pair<std::tstring_t, std::tstring_t>(xT("Key0"), xT("Value0")) );
         m_mmsRv.insert( std::pair<std::tstring_t, std::tstring_t>(xT("Key1"), xT("Value1")) );
 
-        std::tostringstream_t osOut;   osOut << m_mmsRv;
-        xTEST_EQ(false, osOut.str().empty());
+        std::tostringstream_t oss;
+        oss << m_mmsRv;
+        xTEST_EQ(oss.str().empty(), false);
 
         m_mmsRv.clear();
     }
