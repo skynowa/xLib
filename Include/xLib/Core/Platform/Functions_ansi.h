7ef7bd33f4bb09ef2f634ea9b4d8f149baaca464
skynowa <skynowa@gmail.com>
2016-05-06 16:38:29 +0300

    Core: Functions - split ansi, unicode

---
+++ Include/xLib/Core/Platform/Functions_ansi.h
@@ -0,0 +1,113 @@
+/**
+ * \file  Functions_ansi.h
+ * \brief function macroses
+ */
+
+
+#pragma once
+
+    #define tcin            cin
+    #define tcout           cout
+    #define tcerr           cerr
+    #define tclog           clog
+    #define xTEOF           EOF
+
+    #define xTMAIN          main
+    #define xTFPRINTF       std::fprintf
+    #define xTASCTIME       std::asctime
+    #define xLOCALE         std::locale
+    #define xTSETLOCALE     std::setlocale
+    #define xTFOPEN         std::fopen
+    #define xTFREOPEN       std::freopen
+    #define xTFDOPEN        ::fdopen
+    #define xTVSNPRINTF     ::vsnprintf
+    #define xTPERROR        std::perror
+    #define xTVSPRINTF      std::vsprintf
+    #define xTVPRINTF       std::vprintf
+    #define xTVFPRINTF      std::vfprintf
+    #define xTFGETC         std::getc
+    #define xTPUTS          std::puts
+    #define xTPUTCHAR       std::putchar
+    #define xTACCESS        ::access
+    #define xTGETS          std::gets
+    #define xTGETCHAR       std::getchar
+    #define xTTMPNAM        std::tmpnam
+    #define xSTRFTIME       std::strftime
+
+    // xTMKSTEMP
+#if   xCOMPILER_MINGW
+    #define xTMKSTEMP       ::_mktemp
+#elif xCOMPILER_MS
+    #define xTMKSTEMP       ::_tmktemp_s
+#elif xCOMPILER_CODEGEAR
+    #define xTMKSTEMP       ::_mktemp
+#elif xCOMPILER_GNUC
+    #define xTMKSTEMP       ::mkstemp
+#else
+    #define xTMKSTEMP       std::mktemp
+#endif
+
+    #define xTRENAME        std::rename
+    #define xTUNLINK        ::unlink
+    #define xTREMOVE        std::remove
+    #define xTFPUTS         std::fputs
+    #define xTUNGETC        std::ungetc
+    #define xTFPUTC         std::fputc
+    #define xTFGETS         std::fgets
+    #define xTSTRLEN        std::strlen
+    #define xTCHMOD         ::chmod
+    #define xTSYSTEM        std::system
+
+// xTSTAT_STRUCT
+#if   xCOMPILER_MINGW
+    #define xTSTAT_STRUCT   struct stat
+#elif xCOMPILER_MS
+    #define xTSTAT_STRUCT   struct _tstat64
+#elif xCOMPILER_CODEGEAR
+    #define xTSTAT_STRUCT   struct _stat
+#elif xCOMPILER_GNUC
+    #define xTSTAT_STRUCT   struct stat
+#else
+    #define xTSTAT_STRUCT   struct stat
+#endif
+
+// xTSTAT
+#if   xCOMPILER_MINGW
+    #define xTSTAT          ::stat
+#elif xCOMPILER_MS
+    #define xTSTAT          ::_tstat64
+#elif xCOMPILER_CODEGEAR
+    #define xTSTAT          ::_tstat
+#elif xCOMPILER_GNUC
+    #define xTSTAT          ::lstat
+#else
+    #define xTSTAT          ::stat
+#endif
+
+// xSTRERROR
+#if   xCOMPILER_MINGW
+    #define xSTRERROR       ::_tcserror
+#elif xCOMPILER_MS
+    #define xSTRERROR       ::_tcserror_s
+#elif xCOMPILER_GNUC
+    #define xSTRERROR       ::strerror_r
+#elif xCOMPILER_CODEGEAR
+    #define xSTRERROR       ::_tcserror
+#else
+    #define xSTRERROR       std::strerror
+#endif
+
+    // chars
+    #define xTISALNUM       std::isalnum
+    #define xTISALPHA       std::isalpha
+    #define xTISCNTRL       std::iscntrl
+    #define xTISDIGIT       std::isdigit
+    #define xTISXDIGIT      std::isxdigit
+    #define xTISGRAPH       std::isgraph
+    #define xTISPRINT       std::isprint
+    #define xTISPUNCT       std::ispunct
+    #define xTISSPACE       std::isspace
+    #define xTISLOWER       std::islower
+    #define xTISUPPER       std::isupper
+    #define xTTOLOWER       std::tolower
+    #define xTTOUPPER       std::toupper
---
+++ Include/xLib/Core/Platform/Functions_unicode.h
@@ -0,0 +1,113 @@
+/**
+ * \file  Functions_unicode.h
+ * \brief function macroses
+ */
+
+
+#pragma once
+
+    #define tcin            wcin
+    #define tcout           wcout
+    #define tcerr           wcerr
+    #define tclog           wclog
+    #define xTEOF           WEOF
+
+    #define xTMAIN          wmain
+    #define xTFPRINTF       std::fwprintf
+    #define xTASCTIME       std::_wasctime
+    #define xLOCALE         ::locale_t
+    #define xTSETLOCALE     std::_wsetlocale
+    #define xTFOPEN         std::_wfopen
+    #define xTFREOPEN       std::_wfreopen
+    #define xTFDOPEN        ::_wfdopen
+    #define xTVSNPRINTF     ::_vsnwprintf
+    #define xTPERROR        std::_wperror
+    #define xTVSPRINTF      std::_vswprintf
+    #define xTVPRINTF       std::vwprintf
+    #define xTVFPRINTF      std::vfwprintf
+    #define xTFGETC         std::getwc
+    #define xTPUTS          std::_putws
+    #define xTPUTCHAR       std::putwchar
+    #define xTACCESS        ::_waccess
+    #define xTGETS          std::_getws
+    #define xTGETCHAR       std::getwchar
+    #define xTTMPNAM        std::_wtmpnam
+    #define xSTRFTIME       std::wcsftime
+
+    // xTMKSTEMP
+#if   xCOMPILER_MINGW
+    #define xTMKSTEMP       ::_wmktemp
+#elif xCOMPILER_MS
+    #define xTMKSTEMP       ::_tmktemp_s
+#elif xCOMPILER_CODEGEAR
+    #define xTMKSTEMP       ::_wmktemp
+#elif xCOMPILER_GNUC
+    #define xTMKSTEMP       ::mkstemp
+#else
+    #define xTMKSTEMP       std::mkstemp
+#endif
+
+    #define xTRENAME        std::_wrename
+    #define xTUNLINK        ::_wunlink
+    #define xTREMOVE        std::_wremove
+    #define xTFPUTS         std::fputws
+    #define xTUNGETC        std::ungetwc
+    #define xTFPUTC         std::fputwc
+    #define xTFGETS         std::fgetws
+    #define xTSTRLEN        std::wcslen
+    #define xTCHMOD         ::_wchmod
+    #define xTSYSTEM        std::_wsystem
+
+// xTSTAT_STRUCT
+#if   xCOMPILER_MINGW
+    #define xTSTAT_STRUCT   struct stat
+#elif xCOMPILER_MS
+    #define xTSTAT_STRUCT   struct _tstat64
+#elif xCOMPILER_CODEGEAR
+    #define xTSTAT_STRUCT   struct _stat
+#elif xCOMPILER_GNUC
+    #define xTSTAT_STRUCT   struct stat
+#else
+    #define xTSTAT_STRUCT   struct stat
+#endif
+
+// xTSTAT
+#if   xCOMPILER_MINGW
+    #define xTSTAT          ::_wstat
+#elif xCOMPILER_MS
+    #define xTSTAT          ::_tstat64
+#elif xCOMPILER_CODEGEAR
+    #define xTSTAT          ::_tstat
+#elif xCOMPILER_GNUC
+    #define xTSTAT          ::_wlstat
+#else
+    #define xTSTAT          ::_wstat
+#endif
+
+// xSTRERROR
+#if   xCOMPILER_MINGW
+    #define xSTRERROR       ::_tcserror
+#elif xCOMPILER_MS
+    #define xSTRERROR       ::_tcserror_s
+#elif xCOMPILER_GNUC
+    #define xSTRERROR       ::strerror_r
+#elif xCOMPILER_CODEGEAR
+    #define xSTRERROR       ::_tcserror
+#else
+    #define xSTRERROR       ::_wstrerror
+#endif
+
+    // chars
+    #define xTISALNUM       ::iswalnum_l
+    #define xTISALPHA       ::iswalpha_l
+    #define xTISCNTRL       ::iswcntrl_l
+    #define xTISDIGIT       ::iswdigit_l
+    #define xTISXDIGIT      ::iswxdigit_l
+    #define xTISGRAPH       ::iswgraph_l
+    #define xTISPRINT       ::iswprint_l
+    #define xTISPUNCT       ::iswpunct_l
+    #define xTISSPACE       ::iswspace_l
+    #define xTISLOWER       ::iswlower_l
+    #define xTISUPPER       ::iswupper_l
+    #define xTTOLOWER       ::towlower_l
+    #define xTTOUPPER       ::towupper_l
