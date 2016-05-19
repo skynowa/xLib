70ef7dbfc4612d309102b108c7dbeba55137bfc1
skynowa <skynowa@gmail.com>
2016-05-19 15:50:47 +0300

    Debug: Debug - xTRACE_VAR, fix

--- Include/xLib/Debug/Debug.h
+++ Include/xLib/Debug/Debug.h
@@ -55,26 +55,51 @@
 //-------------------------------------------------------------------------------------------------
 ///@name Trace variables (variable with value)
 ///@{
-#define xTRACE_VAR(xVA_VALUES_1) \
-    #xVA_VALUES_1 xT(": ") << (xVA_VALUES_1)
-#define xTRACE_VAR_2(xVA_VALUES_2) \
-    xTRACE_VAR(xVA_VALUES_1)  << xT(", ") << xTRACE_VAR(v2)
-#define xTRACE_VAR_3(xVA_VALUES_3) \
-    xTRACE_VAR_2(xVA_VALUES_2) << xT(", ") << xTRACE_VAR(v3)
-#define xTRACE_VAR_4(xVA_VALUES_4) \
-    xTRACE_VAR_3(xVA_VALUES_3) << xT(", ") << xTRACE_VAR(v4)
-#define xTRACE_VAR_5(xVA_VALUES_5) \
-    xTRACE_VAR_4(xVA_VALUES_4) << xT(", ") << xTRACE_VAR(v5)
-#define xTRACE_VAR_6(xVA_VALUES_6) \
-    xTRACE_VAR_5(xVA_VALUES_5) << xT(", ") << xTRACE_VAR(v6)
-#define xTRACE_VAR_7(xVA_VALUES_7) \
-    xTRACE_VAR_6(xVA_VALUES_6) << xT(", ") << xTRACE_VAR(v7)
-#define xTRACE_VAR_8(xVA_VALUES_8) \
-    xTRACE_VAR_7(xVA_VALUES_7) << xT(", ") << xTRACE_VAR(v8)
-#define xTRACE_VAR_9(xVA_VALUES_9) \
-    xTRACE_VAR_8(xVA_VALUES_8) << xT(", ") << xTRACE_VAR(v9)
-#define xTRACE_VAR_10(xVA_VALUES_10) \
-    xTRACE_VAR_8(xVA_VALUES_9) << xT(", ") << xTRACE_VAR(v10)
+#if xDEPRECIATE
+    #define xTRACE_VAR(xVA_VALUES_1) \
+        #xVA_VALUES_1 xT(": ") << (xVA_VALUES_1)
+    #define xTRACE_VAR_2(xVA_VALUES_2) \
+        xTRACE_VAR(xVA_VALUES_1)  << xT(", ") << xTRACE_VAR(v2)
+    #define xTRACE_VAR_3(xVA_VALUES_3) \
+        xTRACE_VAR_2(xVA_VALUES_2) << xT(", ") << xTRACE_VAR(v3)
+    #define xTRACE_VAR_4(xVA_VALUES_4) \
+        xTRACE_VAR_3(xVA_VALUES_3) << xT(", ") << xTRACE_VAR(v4)
+    #define xTRACE_VAR_5(xVA_VALUES_5) \
+        xTRACE_VAR_4(xVA_VALUES_4) << xT(", ") << xTRACE_VAR(v5)
+    #define xTRACE_VAR_6(xVA_VALUES_6) \
+        xTRACE_VAR_5(xVA_VALUES_5) << xT(", ") << xTRACE_VAR(v6)
+    #define xTRACE_VAR_7(xVA_VALUES_7) \
+        xTRACE_VAR_6(xVA_VALUES_6) << xT(", ") << xTRACE_VAR(v7)
+    #define xTRACE_VAR_8(xVA_VALUES_8) \
+        xTRACE_VAR_7(xVA_VALUES_7) << xT(", ") << xTRACE_VAR(v8)
+    #define xTRACE_VAR_9(xVA_VALUES_9) \
+        xTRACE_VAR_8(xVA_VALUES_8) << xT(", ") << xTRACE_VAR(v9)
+    #define xTRACE_VAR_10(xVA_VALUES_10) \
+        xTRACE_VAR_8(xVA_VALUES_9) << xT(", ") << xTRACE_VAR(v10)
+#else
+    #define xTRACE_VAR(v1) \
+        #v1 ": " << (v1)
+    #define xTRACE_VAR_2(v1, v2) \
+        xTRACE_VAR(v1) << ", " << xTRACE_VAR(v2)
+    #define xTRACE_VAR_3(v1, v2, v3) \
+        xTRACE_VAR2(v1, v2) << ", " << xTRACE_VAR(v3)
+    #define xTRACE_VAR_4(v1, v2, v3, v4) \
+        xTRACE_VAR3(v1, v2, v3) << ", " << xTRACE_VAR(v4)
+    #define xTRACE_VAR_5(v1, v2, v3, v4, v5) \
+        xTRACE_VAR4(v1, v2, v3, v4) << ", " << xTRACE_VAR(v5)
+    #define xTRACE_VAR_6(v1, v2, v3, v4, v5, v6) \
+        xTRACE_VAR5(v1, v2, v3, v4, v5) << ", " << xTRACE_VAR(v6)
+    #define xTRACE_VAR_7(v1, v2, v3, v4, v5, v6, v7) \
+        xTRACE_VAR6(v1, v2, v3, v4, v5, v6) << ", " << xTRACE_VAR(v7)
+    #define xTRACE_VAR_8(v1, v2, v3, v4, v5, v6, v7, v8) \
+        xTRACE_VAR7(v1, v2, v3, v4, v5, v6, v7) << ", " << xTRACE_VAR(v8)
+    #define xTRACE_VAR_9(v1, v2, v3, v4, v5, v6, v7, v8, v9) \
+        xTRACE_VAR8(v1, v2, v3, v4, v5, v6, v7, v8) << ", " << xTRACE_VAR(v9)
+    #define xTRACE_VAR_10(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10) \
+        xTRACE_VAR9(v1, v2, v3, v4, v5, v6, v7, v8, v9) << ", " << xTRACE_VAR(v10)
+    #define xTRACE_VAR_11(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11) \
+        xTRACE_VAR9(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10) << ", " << xTRACE_VAR(v11)
+#endif
 ///@}
 //-------------------------------------------------------------------------------------------------
 #define xTRACE_POINT \
