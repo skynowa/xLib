aea5365ce52bcbad3da5de144f4375f67e48ce7e
skynowa <skynowa@gmail.com>
2016-05-24 17:21:42 +0300

    Format: std::array - add

--- Tests/Core/Test_Format.cpp
+++ Tests/Core/Test_Format.cpp
@@ -375,7 +375,10 @@ Test_Format::unit()
     xTEST_CASE("std::array")
     {
     #if xOPTION_CPP11
-        TODO: add test
+        std::array<int_t, 5> value {2, 16, 77, 34, 50};
+
+        m_sRv = Format::str(xT("{}"), value);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("{2, 16, 77, 34, 50}")));
     #else
         Trace() << xT("[skip]");
     #endif
