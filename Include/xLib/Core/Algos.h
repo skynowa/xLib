ed1ea66efa2860a9574453f0aa70c1315025a413
skynowa <skynowa@gmail.com>
2016-05-24 16:04:47 +0300

    Core: Algos - add

---
+++ Include/xLib/Core/Algos.h
@@ -0,0 +1,29 @@
+/**
+ * \file   Algos.h
+ * \brief  [CLASS_COMMENTS]
+ */
+
+
+#pragma once
+
+#include <xLib/Core/Core.h>
+//-------------------------------------------------------------------------------------------------
+xNAMESPACE_BEGIN2(xlib, core)
+
+class Algos
+    ///< Algorithms
+{
+public:
+
+private:
+             Algos() {}
+        ///< constructor
+
+    xNO_COPY_ASSIGN(Algos)
+};
+
+xNAMESPACE_END2(xlib, core)
+//-------------------------------------------------------------------------------------------------
+#if xOPTION_PROJECT_HEADER_ONLY
+    #include "Algos.cpp"
+#endif
