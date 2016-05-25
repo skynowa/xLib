17a34234798fdfe66932d67d859a9195112c2b98
skynowa <skynowa@gmail.com>
2016-05-25 10:59:32 +0300

    Core: Algos - add

--- Include/xLib/Core/Algos.h
+++ Include/xLib/Core/Algos.h
@@ -16,13 +16,26 @@ class Algos
 public:
     template <typename T>
     static
-    bool isInBounds(const T &value, const T &low, const T &high);
-        //< check value bounds
-
-    template <typename T, typename R, typename Comparator>
+    bool_t isInBounds(const T &value, const T &low, const T &high);
+        ///< check value bounds
+    template <typename T, typename R, typename ComparatorT>
     static
-    bool isInBounds(const T &value, const R &low, const R &high, Comparator comp);
-        //< check value bounds (with custom comparator)
+    bool_t isInBounds(const T &value, const R &low, const R &high, ComparatorT comp);
+        ///< check value bounds (with custom comparator)
+    template <typename T>
+    static
+    void_t deleteAll(T &container);
+        ///< delete all values
+    static
+    bool_t startsWith(std::ctstring_t &value, std::ctstring_t &prefix);
+        ///< starts with
+    static
+    bool_t endsWith(std::ctstring_t &value, std::ctstring_t &suffix);
+        ///< ends with
+    static
+    bool_t contains(std::ctstring_t &value, std::ctstring_t &infix);
+        ///< contains
+
 private:
     Algos() {}
         ///< constructor
@@ -32,6 +45,8 @@ private:
 
 xNAMESPACE_END2(xlib, core)
 //-------------------------------------------------------------------------------------------------
+#include "Algos.inl"
+
 #if xOPTION_PROJECT_HEADER_ONLY
     #include "Algos.cpp"
 #endif
