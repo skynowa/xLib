621610fb283d820990eb3215ba3da3c8ecf116df
skynowa <skynowa@gmail.com>
2016-05-27 15:20:17 +0300

    Core: Flags - fix code style

--- Include/xLib/Core/Flags.h
+++ Include/xLib/Core/Flags.h
@@ -15,38 +15,38 @@ class Flags
     /// bit mask flags
 {
 public:
-              Flags();
+             Flags();
         ///< constructor
-    explicit  Flags(const Flags &values);
+    explicit Flags(const Flags &values);
         ///< constructor
-    explicit  Flags(const T &values);
+    explicit Flags(const T &values);
         ///< constructor
-    virtual  ~Flags();
+    virtual ~Flags();
         ///< destructor
 
-    Flags & operator = (const Flags &values);
+    Flags &  operator = (const Flags &values);
         ///< operator =
 
-    size_t  & get() const;
+    size_t & get() const;
         ///< get all flags
-    void_t    set(const T &values);
+    void_t   set(const T &values);
         ///< set all flags
-    void_t    clear();
+    void_t   clear();
         ///< reset all flags
 
-    bool_t    isSetFlag(const T &value) const;
+    bool_t   isSetFlag(const T &value) const;
         ///< flag is set
-    bool_t    isSetAnyFlag(const T &value) const;
+    bool_t   isSetAnyFlag(const T &value) const;
         ///< is any flag set
-    void_t    setFlag(const T &value);
+    void_t   setFlag(const T &value);
         ///< set flag
-    void_t    unsetFlag(const T &value);
+    void_t   unsetFlag(const T &value);
         ///< unset flag
-    void_t    toggleFlag(const T &value);
+    void_t   toggleFlag(const T &value);
         ///< toggle flag
 
 private:
-    size_t    _flags;
+    size_t   _flags;
 };
 
 xNAMESPACE_END2(xlib, core)
