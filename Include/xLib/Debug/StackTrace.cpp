a3065800732c986512d9d5f6b96573a6eaae4fbf
skynowa <skynowa@gmail.com>
2016-06-08 11:25:29 +0300

    StackTrace: review formatting

--- Include/xLib/Debug/StackTrace.cpp
+++ Include/xLib/Debug/StackTrace.cpp
@@ -61,6 +61,7 @@ StackTrace::toString()
     std::vector<std::vec_tstring_t> stack;
 
     _get_impl(&stack);
+    std::reverse(stack.begin(), stack.end());
     _format(stack, &sRv);
     xCHECK_RET(sRv.empty(), Const::strUnknown());
 
@@ -93,8 +94,8 @@ StackTrace::_get(
 //-------------------------------------------------------------------------------------------------
 xINLINE void_t
 StackTrace::_format(
-    const std::vector<std::vec_tstring_t> &a_stack,     ///< stack as std::vector
-    std::tstring_t                        *a_stackStr   ///< [out] stack as formatted string
+    std::vector<std::vec_tstring_t> &a_stack,     ///< stack as std::vector
+    std::tstring_t                  *a_stackStr   ///< [out] stack as formatted string
 ) const
 {
     xCHECK_DO(a_stack.empty(),         return);
@@ -102,6 +103,18 @@ StackTrace::_format(
 
     std::tstring_t     sRv;
     std::vector<int_t> maxs(::elementsNum, 0);
+
+	// add header
+	{
+		std::vec_tstring_t stackHeader;
+		stackHeader.push_back(xT("Module"));
+		stackHeader.push_back(xT("File"));
+		stackHeader.push_back(xT("Line"));
+		stackHeader.push_back(xT("Offset"));
+		stackHeader.push_back(xT("Function"));
+
+		a_stack.insert(a_stack.begin(), stackHeader);
+	}
 
     // get elements max sizes
     for (size_t i = 0; i < ::elementsNum; ++ i) {
@@ -115,15 +128,19 @@ StackTrace::_format(
     std::size_t lineNumber = 0;
 
     xFOR_EACH_CONST(std::vector<std::vec_tstring_t>, it, a_stack) {
-        ++ lineNumber;
+        if (it != a_stack.begin()) {
+            ++ lineNumber;
+        }
+
+        std::tstring_t lineNumberStr = (lineNumber == 0) ? xT("# ") : (String::cast(lineNumber) + xT("."));
 
         std::tstringstream_t stackLine;
         stackLine
-            << std::setw(0)       << std::right << lineNumber << xT(". ")
-            << std::setw(maxs[0]) << std::left  << it->at(0)  << xT("  ")
-            << std::setw(maxs[1]) << std::left  << it->at(1)  << xT("  ")
-            << std::setw(maxs[2]) << std::right << it->at(2)  << xT("  ")
-            << std::setw(maxs[3]) << std::left  << it->at(3)  << xT("  ")
+            << std::setw(0)       << std::right << lineNumberStr << xT(" ")
+            << std::setw(maxs[0]) << std::left  << it->at(0)     << xT("  ")
+            << std::setw(maxs[1]) << std::left  << it->at(1)     << xT("  ")
+            << std::setw(maxs[2]) << std::right << it->at(2)     << xT("  ")
+            << std::setw(maxs[3]) << std::left  << it->at(3)     << xT("  ")
             << std::setw(maxs[4]) << std::left  << it->at(4);
 
         if (it + 1 != a_stack.end()) {
