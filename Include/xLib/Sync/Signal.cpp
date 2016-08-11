6fa7ea9b6a24472e656febfe26d18eb5a90c7df8
skynowa <skynowa@gmail.com>
2016-08-11 15:50:26 +0300

    Application: connectAll - add

--- Include/xLib/Sync/Signal.cpp
+++ Include/xLib/Sync/Signal.cpp
@@ -102,6 +102,50 @@ Signal::connect(
 }
 //-------------------------------------------------------------------------------------------------
 xINLINE void_t
+Signal::connectAll(
+    const sighandler_t a_onSignals
+) const
+{
+	std::vector<int_t> signalNums;
+	signalNums.push_back(SIGHUP);
+	signalNums.push_back(SIGINT);
+	signalNums.push_back(SIGQUIT);
+	signalNums.push_back(SIGILL);
+	signalNums.push_back(SIGTRAP);
+	signalNums.push_back(SIGABRT);
+	signalNums.push_back(SIGIOT);
+	signalNums.push_back(SIGBUS);
+	signalNums.push_back(SIGFPE);
+	signalNums.push_back(SIGKILL);
+	signalNums.push_back(SIGUSR1);
+	signalNums.push_back(SIGSEGV);
+	signalNums.push_back(SIGUSR2);
+	signalNums.push_back(SIGPIPE);
+	signalNums.push_back(SIGALRM);
+	signalNums.push_back(SIGTERM);
+	signalNums.push_back(SIGSTKFLT);
+	signalNums.push_back(SIGCLD);
+	signalNums.push_back(SIGCHLD);
+	signalNums.push_back(SIGCONT);
+	signalNums.push_back(SIGSTOP);
+	signalNums.push_back(SIGTSTP);
+	signalNums.push_back(SIGTTIN);
+	signalNums.push_back(SIGTTOU);
+	signalNums.push_back(SIGURG);
+	signalNums.push_back(SIGXCPU);
+	signalNums.push_back(SIGXFSZ);
+	signalNums.push_back(SIGVTALRM);
+	signalNums.push_back(SIGPROF);
+	signalNums.push_back(SIGWINCH);
+	signalNums.push_back(SIGPOLL);
+	signalNums.push_back(SIGIO);
+	signalNums.push_back(SIGPWR);
+	signalNums.push_back(SIGSYS);
+
+	connect(signalNums, a_onSignals);
+}
+//-------------------------------------------------------------------------------------------------
+xINLINE void_t
 Signal::connectExit(
     const exit_handler_t a_onExit
 ) const
