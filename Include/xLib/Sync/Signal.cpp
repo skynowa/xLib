4be672eb467668eb2b8c52b72086478deae68959
skynowa <skynowa@gmail.com>
2016-08-23 15:40:46 +0300

    Signal: codeDescription - add

--- Include/xLib/Sync/Signal.cpp
+++ Include/xLib/Sync/Signal.cpp
@@ -246,6 +246,107 @@ Signal::isValid(
 	return (::sigaction(a_signalNum, xPTR_NULL, xPTR_NULL) == 0);
 }
 //-------------------------------------------------------------------------------------------------
+struct _Signal
+{
+	int_t          num;
+	int_t          code;
+	std::tstring_t codeStr;
+	std::tstring_t description;
+};
+
+/* static */
+xINLINE std::tstring_t
+Signal::codeDecription(
+    cint_t          &a_signalNum, ///< signal number
+    const siginfo_t &a_info       ///<  signal info struct
+)
+{
+	std::tstring_t sRv;
+	cint_t         _KERNEL = - 1;
+
+	const _Signal signals[] =
+	{
+		// _KERNEL
+		{_KERNEL, SI_ASYNCNL,    xLEX_TO_STR(SI_ASYNCNL),    xT("Sent by asynch name lookup completion")},
+		{_KERNEL, SI_TKILL,      xLEX_TO_STR(SI_TKILL),      xT("Sent by tkill")},
+		{_KERNEL, SI_SIGIO,      xLEX_TO_STR(SI_SIGIO),      xT("Sent by queued SIGIO")},
+		{_KERNEL, SI_ASYNCIO,    xLEX_TO_STR(SI_ASYNCIO),    xT("Sent by AIO completion")},
+		{_KERNEL, SI_MESGQ,      xLEX_TO_STR(SI_MESGQ),      xT("Sent by real time mesq state change")},
+		{_KERNEL, SI_TIMER,      xLEX_TO_STR(SI_TIMER),      xT("Sent by timer expiration")},
+		{_KERNEL, SI_QUEUE,      xLEX_TO_STR(SI_QUEUE),      xT("Sent by sigqueue")},
+		{_KERNEL, SI_USER,       xLEX_TO_STR(SI_USER),       xT("Sent by kill, sigsend")},
+		{_KERNEL, SI_KERNEL,     xLEX_TO_STR(SI_KERNEL),     xT("Send by kernel")},
+
+		// SIGILL
+		{SIGILL,  ILL_ILLOPC,    xLEX_TO_STR(ILL_ILLOPC),    xT("Illegal opcode")},
+		{SIGILL,  ILL_ILLOPN,    xLEX_TO_STR(ILL_ILLOPN),    xT("Illegal operand")},
+		{SIGILL,  ILL_ILLADR,    xLEX_TO_STR(ILL_ILLADR),    xT("Illegal addressing mode")},
+		{SIGILL,  ILL_ILLTRP,    xLEX_TO_STR(ILL_ILLTRP),    xT("Illegal trap")},
+		{SIGILL,  ILL_PRVOPC,    xLEX_TO_STR(ILL_PRVOPC),    xT("Privileged opcode")},
+		{SIGILL,  ILL_PRVREG,    xLEX_TO_STR(ILL_PRVREG),    xT("Privileged register")},
+		{SIGILL,  ILL_COPROC,    xLEX_TO_STR(ILL_COPROC),    xT("Coprocessor error")},
+		{SIGILL,  ILL_BADSTK,    xLEX_TO_STR(ILL_BADSTK),    xT("Internal stack error")},
+
+		// SIGFPE
+		{SIGFPE,  FPE_INTDIV,    xLEX_TO_STR(FPE_INTDIV),    xT("Integer divide by zero")},
+		{SIGFPE,  FPE_INTOVF,    xLEX_TO_STR(FPE_INTOVF),    xT("Integer overflow")},
+		{SIGFPE,  FPE_FLTDIV,    xLEX_TO_STR(FPE_FLTDIV),    xT("Floating point divide by zero")},
+		{SIGFPE,  FPE_FLTOVF,    xLEX_TO_STR(FPE_FLTOVF),    xT("Floating point overflow")},
+		{SIGFPE,  FPE_FLTUND,    xLEX_TO_STR(FPE_FLTUND),    xT("Floating point underflow")},
+		{SIGFPE,  FPE_FLTRES,    xLEX_TO_STR(FPE_FLTRES),    xT("Floating point inexact result")},
+		{SIGFPE,  FPE_FLTINV,    xLEX_TO_STR(FPE_FLTINV),    xT("Floating point invalid operation")},
+		{SIGFPE,  FPE_FLTSUB,    xLEX_TO_STR(FPE_FLTSUB),    xT("Subscript out of range")},
+
+		// SIGSEGV
+		{SIGSEGV, SEGV_MAPERR,   xLEX_TO_STR(SEGV_MAPERR),   xT("Address not mapped to object")},
+		{SIGSEGV, SEGV_ACCERR,   xLEX_TO_STR(SEGV_ACCERR),   xT("Invalid permissions for mapped object")},
+
+		// SIGBUS
+		{SIGBUS,  BUS_ADRALN,    xLEX_TO_STR(BUS_ADRALN),    xT("Invalid address alignment")},
+		{SIGBUS,  BUS_ADRERR,    xLEX_TO_STR(BUS_ADRERR),    xT("Non-existant physical address")},
+		{SIGBUS,  BUS_OBJERR,    xLEX_TO_STR(BUS_OBJERR),    xT("Object specific hardware error")},
+		{SIGBUS,  BUS_MCEERR_AR, xLEX_TO_STR(BUS_MCEERR_AR), xT("Hardware memory error: action required")},
+		{SIGBUS,  BUS_MCEERR_AO, xLEX_TO_STR(BUS_MCEERR_AO), xT("Hardware memory error: action optional")},
+
+		// SIGTRAP
+		{SIGTRAP, TRAP_BRKPT,    xLEX_TO_STR(TRAP_BRKPT),    xT("Process breakpoint")},
+		{SIGTRAP, TRAP_TRACE,    xLEX_TO_STR(TRAP_TRACE),    xT("Process trace trap")},
+
+		// SIGCHLD
+		{SIGCHLD, CLD_EXITED,    xLEX_TO_STR(CLD_EXITED),    xT("Child has exited")},
+		{SIGCHLD, CLD_KILLED,    xLEX_TO_STR(CLD_KILLED),    xT("Child was killed")},
+		{SIGCHLD, CLD_DUMPED,    xLEX_TO_STR(CLD_DUMPED),    xT("Child terminated abnormally")},
+		{SIGCHLD, CLD_TRAPPED,   xLEX_TO_STR(CLD_TRAPPED),   xT("Traced child has trapped")},
+		{SIGCHLD, CLD_STOPPED,   xLEX_TO_STR(CLD_STOPPED),   xT("Child has stopped")},
+		{SIGCHLD, CLD_CONTINUED, xLEX_TO_STR(CLD_CONTINUED), xT("Stopped child has continued")},
+
+		// SIGPOLL
+		{SIGPOLL, POLL_IN,       xLEX_TO_STR(POLL_IN),       xT("Data input available")},
+		{SIGPOLL, POLL_OUT,      xLEX_TO_STR(POLL_OUT),      xT("Output buffers available")},
+		{SIGPOLL, POLL_MSG,      xLEX_TO_STR(POLL_MSG),      xT("Input message available")},
+		{SIGPOLL, POLL_ERR,      xLEX_TO_STR(POLL_ERR),      xT("I/O error")},
+		{SIGPOLL, POLL_PRI,      xLEX_TO_STR(POLL_PRI),      xT("High priority input available")},
+		{SIGPOLL, POLL_HUP,      xLEX_TO_STR(POLL_HUP),      xT("Device disconnected")}
+	};
+
+	xFOR_ARRAY(i, signals) {
+		const _Signal &sig = signals[i];
+
+		xCHECK_DO(sig.num != a_signalNum,     continue);
+		xCHECK_DO(sig.code != a_info.si_code, continue);
+
+		sRv = Format::str(xT("{}: {} ({}) - {}"),
+				decription(sig.num),
+				sig.code,
+				sig.codeStr,
+				sig.description);
+
+		break;
+	}
+
+	return sRv;
+}
+//-------------------------------------------------------------------------------------------------
 /* static */
 xINLINE std::tstring_t
 Signal::decription(
