73bb0920a70161a3d207c40357d4fad1216e58e0
skynowa <skynowa@gmail.com>
2017-01-27 17:30:39 +0200

    System: CmdOptions - add

---
+++ Include/xLib/System/CmdOptions.h
@@ -0,0 +1,56 @@
+/**
+ * \file   CmdOptions.h
+ * \brief  Command-line options parser
+ */
+
+
+#pragma once
+
+#include <xLib/Core/Core.h>
+//-------------------------------------------------------------------------------------------------
+xNAMESPACE_BEGIN2(xlib, system)
+
+struct CmdOptionsUsage
+    /// CmdOptions usage
+{
+	enum EnableType
+	{
+		Disabled = 0,
+		Enabled  = 1
+	};
+
+	enum MandatoryType
+	{
+		Optional  = 0,
+		Mandatory = 1
+	};
+
+	EnableType     enableType;
+	MandatoryType  mandatoryType;
+	std::tstring_t optShort;
+	std::tstring_t optLong;
+	std::tstring_t args;
+	std::tstring_t help;
+};
+xTYPEDEF_CONST(CmdOptionsUsage);
+
+class CmdOptions
+    ///< Command-line options parser
+{
+public:
+             CmdOptions();
+        ///< constructor
+    virtual ~CmdOptions();
+        ///< destructor
+
+    void_t   parse(cint_t argc, cptr_ctchar_t argv[], const std::vector<CmdOptionsUsage> &usage);
+
+private:
+    xNO_COPY_ASSIGN(CmdOptions)
+};
+
+xNAMESPACE_END2(xlib, system)
+//-------------------------------------------------------------------------------------------------
+#if cmOPTION_PROJECT_HEADER_ONLY
+    #include "CmdOptions.cpp"
+#endif
