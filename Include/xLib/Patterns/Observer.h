9dbc4a8e87a05809fc1bd9d477b3705aeb1545a8
skynowa <skynowa@gmail.com>
2017-02-02 14:49:31 +0200

    Observer: review

--- Include/xLib/Patterns/Observer.h
+++ Include/xLib/Patterns/Observer.h
@@ -12,12 +12,12 @@
 xNAMESPACE_BEGIN2(xlib, patterns)
 
 template<typename T>
-class xNO_VTABLE IxObserver
+class xNO_VTABLE IObserver
     /// observer interface
 {
 public:
-                    IxObserver() {}
-    virtual        ~IxObserver() {}
+                    IObserver() {}
+    virtual        ~IObserver() {}
 
     virtual void_t  update(cint_t &type, const T &message) = 0;
 };
@@ -37,14 +37,14 @@ public:
     }
         ///< destructor
 
-    void_t attach(IxObserver<T> *a_observer)
+    void_t attach(IObserver<T> *a_observer)
     {
         _observers.push_back(a_observer);
     }
         ///< attach object
-    void_t detach(IxObserver<T> *a_observer)
+    void_t detach(IObserver<T> *a_observer)
     {
-        xFOR_EACH(typename std::vector<IxObserver<T> *>, it, _observers) {
+        xFOR_EACH(typename std::vector<IObserver<T> *>, it, _observers) {
             if (*it == a_observer) {
                 _observers.erase(it);
             }
@@ -53,14 +53,14 @@ public:
         ///< dettach object
     void_t updateAll(cint_t &a_type, const T &a_message)
     {
-        xFOR_EACH(typename std::vector<IxObserver<T> *>, it, _observers) {
+        xFOR_EACH(typename std::vector<IObserver<T> *>, it, _observers) {
             (*it)->update(a_type, a_message);
         }
     }
         ///< update all objects
 
 private:
-    std::vector<IxObserver<T> *> _observers;    ///< objects container
+    std::vector<IObserver<T> *> _observers;    ///< objects container
 
     xNO_COPY_ASSIGN(Observer)
 };
