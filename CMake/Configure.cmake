f05a8587fc98709cb495654e747b63d98c6faf35
skynowa <skynowa@gmail.com>
2017-02-27 16:12:04 +0200

    CMake: ClangTidy - add

--- CMake/Configure.cmake
+++ CMake/Configure.cmake
@@ -52,6 +52,7 @@ include(CheckCSourceCompiles)
 include(CheckCXXSourceCompiles)
 #--------------------------------------------------------------------------------------------------
 # find packages
+find_package(ClangTidy QUIET REQUIRED)
 find_package(GitRevision QUIET REQUIRED)
 find_package(OS QUIET REQUIRED)
 find_package(OpenSSL QUIET REQUIRED)
