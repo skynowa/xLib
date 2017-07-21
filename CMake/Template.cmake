be9fa6a577b2869c5b393f4259c8ea5bc237eb81
skynowa <skynowa@gmail.com>
2017-07-21 17:49:35 +0300

    Core: Enum - add

--- CMake/Template.cmake
+++ CMake/Template.cmake
@@ -11,10 +11,10 @@ unset(cmXLIB_TEMPLATE_COMMENTS CACHE)
 unset(cmXLIB_TEMPLATE_SUBDIR   CACHE)
 unset(cmXLIB_TEMPLATE_NS       CACHE)
 
-set(cmXLIB_TEMPLATE          "IData")
-set(cmXLIB_TEMPLATE_COMMENTS "Data struct")
-set(cmXLIB_TEMPLATE_SUBDIR   "Package")
-set(cmXLIB_TEMPLATE_NS       "package")
+set(cmXLIB_TEMPLATE          "Enum")
+set(cmXLIB_TEMPLATE_COMMENTS "Enumeration")
+set(cmXLIB_TEMPLATE_SUBDIR   "Core")
+set(cmXLIB_TEMPLATE_NS       "core")
 
 if (NOT cmXLIB_TEMPLATE OR
     NOT cmXLIB_TEMPLATE_COMMENTS OR
