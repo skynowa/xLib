3b633c647a82956813baec0e227e3aba5c38dfa4
skynowa <skynowa@gmail.com>
2017-09-15 12:44:13 +0300

    Enum: fromString - add

--- Include/xLib/Core/Enum.h
+++ Include/xLib/Core/Enum.h
@@ -23,6 +23,8 @@ public:
     static
     std::tstring_t toString(const T value);
     static
+    T              fromString(const std::tstring_t &value);
+    static
     std::tstring_t asString(const T value);
     bool_t         isValid(const ssize_t value) const;
 
