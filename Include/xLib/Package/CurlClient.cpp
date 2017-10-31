fe3570ef825963cbc3cd5cfff1a710f01d645248
skynowa <skynowa@gmail.com>
2017-10-31 22:51:26 +0200

    CurlClient: add methods

--- Include/xLib/Package/CurlClient.cpp
+++ Include/xLib/Package/CurlClient.cpp
@@ -8,6 +8,16 @@
     #include "CurlClient.h"
 #endif
 
+#include <xLib/Core/Const.h>
+#include <xLib/Core/Utils.h>
+#include <xLib/Core/String.h>
+#include <xLib/Core/Format.h>
+#include <xLib/Log/Trace.h>
+#include <xLib/Log/FileLog.h>
+#include <xLib/Debug/NativeError.h>
+#include <xLib/Debug/ErrorReport.h>
+#include <xLib/Test/Test.h>
+
 
 xNAMESPACE_BEGIN2(xl, Package)
 
@@ -18,13 +28,211 @@ xNAMESPACE_BEGIN2(xl, Package)
 
 //-------------------------------------------------------------------------------------------------
 xINLINE
-CurlClient::CurlClient()
+CurlClient::CurlClient() :
+    _handle(xPTR_NULL)
 {
+    _handle = ::curl_easy_init();
+    xTEST_PTR(_handle);
 }
 //-------------------------------------------------------------------------------------------------
 xINLINE
 CurlClient::~CurlClient()
 {
+    (void_t)::curl_easy_cleanup( get() );
+}
+//-------------------------------------------------------------------------------------------------
+std::tstring_t
+CurlClient::version()
+{
+    return ::curl_version();
+}
+//-------------------------------------------------------------------------------------------------
+std::tstring_t
+CurlClient::versionInfo(
+    const CURLversion a_version
+)
+{
+    std::tstring_t sRv;
+
+    curl_version_info_data *infoData = ::curl_version_info(a_version);
+    xTEST_PTR(infoData);
+
+    Format::str(
+        "age: {}\n"
+        "version: {}\n"
+        "version_num: {}\n"
+        "host: {}\n"
+        "features: {}\n"
+        "ssl_version: {}\n"
+        "ssl_version_num: {}\n"
+        "libz_version: {}\n"
+        "protocols: {}\n"
+        "ares: {}\n"
+        "ares_num: {}\n"
+        "libidn: {}\n"
+        "iconv_ver_num: {}\n"
+        "libssh_version: {}",
+        infoData->age,
+        infoData->version,
+        infoData->version_num,
+        infoData->host,
+        infoData->features,
+        infoData->ssl_version,
+        infoData->ssl_version_num,
+        infoData->libz_version,
+        infoData->protocols,
+        infoData->ares,
+        infoData->ares_num,
+        infoData->libidn,
+        infoData->iconv_ver_num,
+        infoData->libssh_version);
+
+    return sRv;
+}
+//-------------------------------------------------------------------------------------------------
+bool_t
+CurlClient::isValid() const
+{
+    return (_handle != xPTR_NULL);
+}
+//-------------------------------------------------------------------------------------------------
+CURL *
+CurlClient::get()
+{
+    xTEST_PTR(_handle);
+
+    return _handle;
+}
+//-------------------------------------------------------------------------------------------------
+CURL *
+CurlClient::dup()
+{
+    return ::curl_easy_duphandle( get() );
+}
+//-------------------------------------------------------------------------------------------------
+void
+CurlClient::reset()
+{
+    ::curl_easy_reset( get() );
+}
+//-------------------------------------------------------------------------------------------------
+void
+CurlClient::setOption(
+    const CURLoption a_option, ...
+)
+{
+    va_list args;
+    xVA_START(args, a_option);
+    CURLcode iRv = ::curl_easy_setopt(get(), a_option, args);
+    xVA_END(args);
+
+    xTEST_EQ(iRv, CURLE_OK);
+}
+//-------------------------------------------------------------------------------------------------
+void
+CurlClient::perform()
+{
+    CURLcode iRv = ::curl_easy_perform( get() );
+    xTEST_EQ(iRv, CURLE_OK);
+}
+//-------------------------------------------------------------------------------------------------
+void
+CurlClient::pause(
+    cint_t a_bitmask
+)
+{
+    CURLcode iRv = ::curl_easy_pause(get(), a_bitmask);
+    xTEST_EQ(iRv, CURLE_OK);
+}
+//-------------------------------------------------------------------------------------------------
+void
+CurlClient::info(
+    const CURLINFO a_info, ...
+)
+{
+    va_list args;
+    xVA_START(args, a_info);
+    CURLcode iRv = ::curl_easy_getinfo(get(), a_info, args);
+    xVA_END(args);
+
+    xTEST_EQ(iRv, CURLE_OK);
+}
+//-------------------------------------------------------------------------------------------------
+void
+CurlClient::recv(
+    void         *a_buffer,
+    const size_t  a_buflen,
+    size_t       *a_n
+)
+{
+    CURLcode iRv = ::curl_easy_recv(get(), a_buffer, a_buflen, a_n);
+    xTEST_EQ(iRv, CURLE_OK);
+}
+//-------------------------------------------------------------------------------------------------
+void
+CurlClient::send(
+    const void   *a_buffer,
+    const size_t  a_buflen,
+    size_t       *a_n
+)
+{
+    CURLcode iRv = ::curl_easy_send(get(), a_buffer, a_buflen, a_n);
+    xTEST_EQ(iRv, CURLE_OK);
+}
+//-------------------------------------------------------------------------------------------------
+std::tstring_t
+CurlClient::escape(
+    std::ctstring_t &a_str
+)
+{
+    std::tstring_t sRv;
+
+    char *szRv = ::curl_easy_escape(get(), a_str.c_str(), static_cast<int>( a_str.size() ));
+    xTEST_PTR(szRv);
+
+    sRv.assign(szRv);
+
+    if (szRv != xPTR_NULL) {
+        ::curl_free(szRv);
+        szRv = xPTR_NULL;
+    }
+
+    return sRv;
+}
+//-------------------------------------------------------------------------------------------------
+std::tstring_t
+CurlClient::unescape(
+    std::ctstring_t &a_str
+)
+{
+    std::tstring_t sRv;
+
+    int size_out = 0;
+    char *szRv = ::curl_easy_unescape(get(), a_str.c_str(), static_cast<int>( a_str.size() ),
+        &size_out);
+    xTEST_PTR(szRv);
+
+    sRv.assign(szRv, static_cast<std::size_t>(size_out));
+
+    if (szRv != xPTR_NULL) {
+        ::curl_free(szRv);
+        szRv = xPTR_NULL;
+    }
+
+    return sRv;
+}
+//-------------------------------------------------------------------------------------------------
+std::tstring_t
+CurlClient::strError(
+   const CURLcode a_code
+)
+{
+    const char *pszRv = ::curl_easy_strerror(a_code);
+    if (pszRv == xPTR_NULL) {
+        return Const::strUnknown();
+    }
+
+    return pszRv;
 }
 //-------------------------------------------------------------------------------------------------
 
--- Include/xLib/Package/CurlClient.h
+++ Include/xLib/Package/CurlClient.h
@@ -16,13 +16,41 @@ class CurlClient
     ///< CURL client
 {
 public:
-             CurlClient();
+                   CurlClient();
         ///< constructor
-    virtual ~CurlClient();
+    virtual       ~CurlClient();
         ///< destructor
+
+    std::tstring_t version();
+    std::tstring_t versionInfo(const CURLversion version);
+
+    // handle
+    bool_t         isValid() const;
+    CURL *         get();
+    CURL *         dup();
+    void           reset();
+
+    void           setOption(const CURLoption option, ...);
+    void           perform();
+    void           pause(cint_t bitmask);
+    void           info(const CURLINFO info, ...);
+
+    void           recv(void *buffer, const size_t buflen, size_t *n);
+    void           send(const void *buffer, const size_t buflen, size_t *n);
+
+
+    std::tstring_t escape(std::ctstring_t &str);
+    std::tstring_t unescape(std::ctstring_t &str);
+
+    /// struct curl_slist *curl_slist_append(struct curl_slist *,  const char *);
+    /// void               curl_slist_free_all(struct curl_slist *);
+
+    std::tstring_t strError(const CURLcode code);
 
 private:
     xNO_COPY_ASSIGN(CurlClient)
+
+    CURL    *_handle;
 };
 
 xNAMESPACE_END2(xl, Package)
