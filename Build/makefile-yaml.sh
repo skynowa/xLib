49b153ff31076588034106f62bdc95d2ab279e9d
skynowa <skynowa@gmail.com>
2017-11-02 10:39:31 +0200

    bitbucket-pipelines: build

--- Build/makefile-yaml.sh
+++ Build/makefile-yaml.sh
@@ -6,18 +6,4 @@
 
 set -ex
 
-# vars
-DIR_XLIB=$BITBUCKET_CLONE_DIR
-DIR_BUILD="../../xLib_makefile"
-
-
-# build
-mkdir -p $DIR_BUILD
-cd $DIR_BUILD
-
-cmake \
-	-G"Unix Makefiles" \
-	$DIR_XLIB
-
-cmake --build .
-ctest
+build.sh
