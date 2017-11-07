db697b015cfcc5a958a71af3a6a3e1211b3496df
skynowa <skynowa@gmail.com>
2017-11-07 21:28:59 +0200

    HandlePolicy: split sources

--- Include/xLib/Core/HandlePolicy.h
+++ Include/xLib/Core/HandlePolicy.h
@@ -26,26 +26,10 @@ template<typename T>
 struct HandlePolicy<T, hvInvalid>
     /// handle error is hvInvalid
 {
-    static T null() xWARN_UNUSED_RV
-    {
-        return xNATIVE_HANDLE_INVALID;
-    }
-
-    static T dup(const T &a_handle) xWARN_UNUSED_RV
-    {
-        return _dup_impl(a_handle);
-    }
-
-    static bool_t isValid(const T &a_handle)
-    {
-        return _isValid_impl(a_handle);
-    }
-
-    static void_t close(T *a_handle)
-    {
-        return _close_impl(a_handle);
-    }
-
+    static T      null() xWARN_UNUSED_RV;
+    static T      dup(const T &a_handle) xWARN_UNUSED_RV;
+    static bool_t isValid(const T &a_handle) xWARN_UNUSED_RV;
+    static void_t close(T *a_handle);
 
 xPLATFORM_IMPL:
     static T      _dup_impl(const T &handle);
@@ -57,26 +41,10 @@ template<typename T>
 struct HandlePolicy<T, hvNull>
     /// handle error is hvNull
 {
-    static
-    T null() xWARN_UNUSED_RV
-    {
-        return xNATIVE_HANDLE_NULL;
-    }
-
-    static T dup(const T &a_handle) xWARN_UNUSED_RV
-    {
-        return _dup_impl(a_handle);
-    }
-
-    static bool_t isValid(const T &a_handle)
-    {
-        return _isValid_impl(a_handle);
-    }
-
-    static void_t close(T *a_handle)
-    {
-       return _close_impl(a_handle);
-    }
+    static T      null() xWARN_UNUSED_RV;
+    static T      dup(const T &a_handle) xWARN_UNUSED_RV;
+    static bool_t isValid(const T &a_handle) xWARN_UNUSED_RV;
+    static void_t close(T *a_handle);
 
 xPLATFORM_IMPL:
     static T      _dup_impl(const T &handle);
@@ -88,35 +56,16 @@ template<typename T>
 struct HandlePolicy<T, hvStd>
     /// handle error is hvStd
 {
-    static
-    T null() xWARN_UNUSED_RV
-    {
-        return xPTR_NULL;
-    }
-
-    static T dup(const T &a_handle) xWARN_UNUSED_RV
-    {
-        return _dup_impl(a_handle);
-    }
-
-    static bool_t isValid(const T &a_handle)
-    {
-        return _isValid_impl(a_handle);
-    }
-
-    static void_t close(T *a_handle)
-    {
-       return _close_impl(a_handle);
-    }
-
-xPLATFORM_IMPL:
-    static T      _dup_impl(const T &handle);
-    static bool_t _isValid_impl(const T &handle);
-    static void_t _close_impl(T *handle);
+    static T      null() xWARN_UNUSED_RV;
+    static T      dup(const T &a_handle) xWARN_UNUSED_RV;
+    static bool_t isValid(const T &a_handle) xWARN_UNUSED_RV;
+    static void_t close(T *a_handle);
 };
 //-------------------------------------------------------------------------------------------------
 xNAMESPACE_END2(xl, core)
 //-------------------------------------------------------------------------------------------------
+#include "HandlePolicy.inl"
+
 #if   xENV_WIN
     #include "Platform/Win/HandlePolicy_win.inl"
 #elif xENV_UNIX
