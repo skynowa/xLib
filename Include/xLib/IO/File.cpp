ee6d1d5b9040326b2708d904337e20a851e1b43a
skynowa <skynowa@gmail.com>
2017-11-10 22:12:21 +0200

    Handle: add logs

--- Include/xLib/IO/File.cpp
+++ Include/xLib/IO/File.cpp
@@ -537,7 +537,7 @@ File::close()
 
     xCHECK_DO(!isValid(), return);
 
-    errorClear();
+    /// errorClear();
 
     twint_t iRv = std::fclose( get() ); _handle = xPTR_NULL;
     xTEST_DIFF(iRv, xTEOF);
