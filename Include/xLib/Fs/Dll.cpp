3660764b7166c2c7f50a23733cabd0e2f9b4b85a
skynowa <skynowa@gmail.com>
2017-11-15 22:36:22 +0200

    IHandle: Dll - implement

--- Include/xLib/Fs/Dll.cpp
+++ Include/xLib/Fs/Dll.cpp
@@ -21,14 +21,6 @@
     #include "Platform/Win/Dll_win.inl"
 #elif xENV_UNIX
     #include "Platform/Unix/Dll_unix.inl"
-
-    #if   xENV_LINUX
-
-    #elif xENV_BSD
-
-    #elif xENV_APPLE
-
-    #endif
 #endif
 
 
