d4f731c475b1362c4d4247301e05ea8ce8d9ac52
skynowa <skynowa@gmail.com>
2017-11-21 00:03:50 +0200

    FsWatcher: Shell::execute()

--- Include/xLib/System/Shell.h
+++ Include/xLib/System/Shell.h
@@ -27,8 +27,10 @@ public:
 
     bool_t     isAvailable() const xWARN_UNUSED_RV;
         ///< is available
+    int_t      execute(std::ctstring_t &filePath) const;
+        ///< execute file (no params)
     int_t      execute(std::ctstring_t &filePath, std::ctstring_t &params) const;
-        ///< execute file
+        ///< execute file (with params)
 
 #if xENV_WIN
     enum Operation
