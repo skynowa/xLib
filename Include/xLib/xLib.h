b246192a9323fecdbc6a1f3aa2ca49c592548ec8
Sergey Shevchenko <skynowa@fabrica.net.ua>
2017-11-23 09:18:46 +0200

    Config.h - as 1-st includes

--- Include/xLib/xLib.h
+++ Include/xLib/xLib.h
@@ -7,9 +7,10 @@
 #pragma once
 
 // Core
+#include <xLib/Config.h>
 #include <xLib/Core/Core.h>
 #include <xLib/Core/SysDefines.h>
-#include <xLib/Config.h>
+#include <xLib/Internal/Warnings.h>
 #include <xLib/Core/Units.h>
 #include <xLib/Core/Defines.h>
 #include <xLib/Core/Limits.h>
