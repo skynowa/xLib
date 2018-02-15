01a8f20289490b76e05e1123f81e1fa71320186e
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-02-15 16:04:45 +0200

    Xml: review

--- Include/xLib/Package/Xml.h
+++ Include/xLib/Package/Xml.h
@@ -65,7 +65,7 @@ public:
 	std::tstring_t getName() const;
 	std::tstring_t getText() const;
 
-	bool           findContent(std::clist_tstring_t &xpaths, XmlNode &value) const;
+	bool           findContents(std::clist_tstring_t &xpaths, std::list<XmlNode> &values) const;
 	int            getContent(std::ctstring_t &xpath, XmlNode &value) const;
 	int            getContents(std::ctstring_t &xpath, std::list_tstring_t &values) const;
 	int            getContents(std::ctstring_t &xpath, std::list<XmlNode> &values) const;
