46e3d4cc625cae864774c425f7f49116eafb766d
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-07-02 15:01:16 +0300

    MySqlConnection: escapeString - test

--- Tests/Db/Test_MySql.cpp
+++ Tests/Db/Test_MySql.cpp
@@ -101,7 +101,7 @@ Test_MySql::unit()
     {
         const std::vector<data2_tstring_t> data
         {
-            {xT(""),                 ssxT("")},
+            {xT(""),                 xT("")},
             {xT("NULL"),             xT("NULL")},
             {xT("value"),            xT("'value'")},
             {xT("\"value\""),        xT("'\\\"value\\\"'")},
