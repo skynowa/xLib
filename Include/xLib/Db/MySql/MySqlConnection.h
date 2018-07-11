fbe18d609d1556af9d6d2e0904b5a650adef3aa4
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-07-11 10:08:26 +0300

    MySqlConnection: add comments

--- Include/xLib/Db/MySql/MySqlConnection.h
+++ Include/xLib/Db/MySql/MySqlConnection.h
@@ -19,16 +19,27 @@ xNAMESPACE_BEGIN2(xl, db)
 struct MySqlConnectionData
     /// MySqlConnection data
 {
-    std::tstring_t host;			///< host
-    std::tstring_t user;			///< user name
-    std::tstring_t password;		///< password
-    std::tstring_t db;				///< DB name
-    uint_t         port {};			///<
-    std::tstring_t unixSocket;		///<
-    std::tstring_t charset; 		///< specifies character name
-    bool_t         isAutoCommit {};	///< sets autocommit mode on
-    bool_t         isCompress {};	///< Use compression in the client/server protocol
-    std::map<mysql_option, cptr_cvoid_t> options;	///< extra options
+    std::tstring_t host;
+        ///< host name or an IP address. If host is empty or the string "localhost",
+        ///< a connection to the local host is assumed
+    std::tstring_t user;
+        ///< MySQL login ID. If user is empty string, the current user is assumed
+    std::tstring_t password;
+        ///< password
+    std::tstring_t db;
+        ///< DB name. If empty -  create Db
+    uint_t         port {};
+        ///< If port is not 0, the value is used as the port number for the TCP/IP connection
+    std::tstring_t unixSocket;
+        ///< If unix_socket is not empty, the string specifies the socket or named pipe to use
+    std::tstring_t charset;
+        ///< specifies character name
+    bool_t         isAutoCommit {};
+        ///< sets autocommit mode on
+    bool_t         isCompress {};
+        ///< Use compression in the client/server protocol
+    std::map<mysql_option, cptr_cvoid_t> options;
+        ///< extra options
 };
 xTYPEDEF_CONST(MySqlConnectionData);
 
