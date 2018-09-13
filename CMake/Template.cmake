683b833fc2bc77402a45a9e77a050a621b37ecfe
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-09-13 12:48:08 +0300

    HttpClient: add

--- CMake/Template.cmake
+++ CMake/Template.cmake
@@ -11,9 +11,9 @@ unset(cmXLIB_TEMPLATE_COMMENTS CACHE)
 unset(cmXLIB_TEMPLATE_SUBDIR   CACHE)
 unset(cmXLIB_TEMPLATE_NS       CACHE)
 
-set(cmXLIB_TEMPLATE          "Iconv")
-set(cmXLIB_TEMPLATE_COMMENTS "Perform character set conversion")
-set(cmXLIB_TEMPLATE_SUBDIR   "Package")
+set(cmXLIB_TEMPLATE          "HttpClient")
+set(cmXLIB_TEMPLATE_COMMENTS "HTTP client")
+set(cmXLIB_TEMPLATE_SUBDIR   "Package/Curl")
 set(cmXLIB_TEMPLATE_NS       "package")
 
 if (NOT cmXLIB_TEMPLATE OR
---
+++ Tests/Package/Curl/Test_HttpClient.cpp
@@ -0,0 +1,27 @@
+/**
+ * \file   Test_HttpClient.cpp
+ * \brief  test HttpClient
+ */
+
+
+#include <xLib/xLib.h>
+
+
+//-------------------------------------------------------------------------------------------------
+using namespace xl;
+
+xTEST_CLASS(Test_HttpClient)
+xTEST_UNIT(Test_HttpClient)
+//-------------------------------------------------------------------------------------------------
+/* virtual */
+bool_t
+Test_HttpClient::unit()
+{
+    xTEST_CASE("HttpClient::[TEST_CASE_1]")
+    {
+
+    }
+
+    return true;
+}
+//-------------------------------------------------------------------------------------------------
