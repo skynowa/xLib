6e1bbc5428689a66b9c8b093f2ae4fb3b4c8d08d
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-09-14 16:56:03 +0300

    Curl: Version - tests

--- Tests/Package/Curl/Test_Version.cpp
+++ Tests/Package/Curl/Test_Version.cpp
@@ -20,12 +20,39 @@ Test_HttpClient::unit()
 {
 	Version version;
 
-    xTEST_CASE("Version::version")
-    {
+	xTEST_CASE("version")
+	{
 		m_sRv = version.version();
 		xTEST_EQ(m_sRv.empty(), false);
-    }
 
-    return true;
+		// std::tcout << (OStream() << xTRACE_VAR(m_sRv)).str() << std::endl;
+	}
+
+	xTEST_CASE("info")
+	{
+		m_sRv = version.info(CURLVERSION_FOURTH);
+		xTEST_EQ(m_sRv.empty(), false);
+
+		// std::tcout << (OStream() << xTRACE_VAR(m_sRv)).str() << std::endl;
+	}
+
+	xTEST_CASE("infoCurrent")
+	{
+		m_sRv = version.infoCurrent();
+		xTEST_EQ(m_sRv.empty(), false);
+
+		// std::tcout << (OStream() << xTRACE_VAR(m_sRv)).str() << std::endl;
+	}
+
+	xTEST_CASE("protocols")
+	{
+		std::vec_tstring_t protos;
+		version.protocols(&protos);
+		xTEST_EQ(protos.empty(), false)
+
+		// std::tcout << (OStream() << xTRACE_VAR(protos)).str() << std::endl;
+	}
+
+	return true;
 }
 //-------------------------------------------------------------------------------------------------
