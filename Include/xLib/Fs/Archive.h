7a9680308807600750c7968fe7399ae3302db3f6
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-01-11 09:10:17 +0200

    Fs: Archive - add

---
+++ Include/xLib/Fs/Archive.h
@@ -0,0 +1,47 @@
+/**
+ * \file   Archive.h
+ * \brief  Pack/unpack archives
+ */
+
+
+#pragma once
+
+#include <xLib/Core/Core.h>
+//-------------------------------------------------------------------------------------------------
+xNAMESPACE_BEGIN2(xl, fs)
+
+class Archive
+    ///< Pack/unpack archives
+{
+public:
+	enum class Type
+		/// archive type
+	{
+		Unknown = 0,
+		Zip     = 1,
+		Rar     = 2,
+		Gz      = 3,
+		TarBz2  = 4
+	};
+
+             Archive() = default;
+        ///< constructor
+    virtual ~Archive() = default;
+        ///< destructor
+
+	bool_t   fileUnarchive(const Type archive_type, std::ctstring_t &archive_path,
+				std::ctstring_t &dest_dir, cbool_t is_remove_archive);
+		///< unarchive file
+	bool_t   dirArchive(const Type archive_type, std::ctstring_t &source_path,
+				std::ctstring_t &dest_archive_path, cbool_t is_remove_source);
+		///< archive dir
+	bool_t   dirUnarchive(const Type archive_type, std::ctstring_t &archive_dir_path,
+				std::ctstring_t &archive_shell_filter, std::ctstring_t &dest_dir,
+				cbool_t is_remove_archives);
+		///< unarchive dir
+private:
+    xNO_COPY_ASSIGN(Archive)
+};
+
+xNAMESPACE_END2(xl, fs)
+//-------------------------------------------------------------------------------------------------
