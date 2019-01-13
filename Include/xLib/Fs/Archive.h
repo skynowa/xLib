be34ea371c7e9afaae11afdc98b32b338b822e6c
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-01-13 22:44:03 +0200

    Archive: code style

--- Include/xLib/Fs/Archive.h
+++ Include/xLib/Fs/Archive.h
@@ -31,27 +31,23 @@ public:
     virtual ~Archive() = default;
         ///< destructor
 
-	bool_t   fileArchive(const Type archive_type, std::ctstring_t &source_path,
-				std::ctstring_t &dest_archive_path, cbool_t is_remove_source);
+	bool_t   fileArchive(const Type type, std::ctstring_t &sourceFilePath,
+				std::ctstring_t &destFilePath, cbool_t isRemoveSourceFile);
 		///< archive file
-	bool_t   dirArchive(const Type archive_type, std::ctstring_t &source_path,
-				std::ctstring_t &dest_archive_path, cbool_t is_remove_source);
+	bool_t   dirArchive(const Type type, std::ctstring_t &sourceDirPath,
+				std::ctstring_t &destFilePath, cbool_t isRemoveSourceDir);
 		///< archive dir
 
-	bool_t   fileUnarchive(const Type archive_type, std::ctstring_t &archive_path,
-				std::ctstring_t &dest_dir, cbool_t is_remove_archive);
+	bool_t   fileUnarchive(const Type type, std::ctstring_t &sourceFilePath,
+				std::ctstring_t &destDirPath, cbool_t isRemoveSourceFile);
 		///< unarchive file
-	bool_t   dirUnarchive(const Type archive_type, std::ctstring_t &archive_dir_path,
-				std::ctstring_t &archive_shell_filter, std::ctstring_t &dest_dir,
-				cbool_t is_remove_archives);
+	bool_t   dirUnarchive(const Type type, std::ctstring_t &sourceDirPath,
+				std::ctstring_t &fileShellFilter, std::ctstring_t &destDirPath,
+				cbool_t isRemoveSourceFiles);
 		///< unarchive dir
 
 private:
     xNO_COPY_ASSIGN(Archive)
-
-	// static
-	static
-	std::tstring_t quoted(std::ctstring_t &value, std::ctstring_t &delimiter = Const::dqm());
 };
 
 xNAMESPACE_END2(xl, fs)
