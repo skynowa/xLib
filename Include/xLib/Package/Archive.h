e03060b8eebc086b2bf1a9687adb9083cf009112
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-01-18 20:16:02 +0200

    Arhive: review

--- Include/xLib/Package/Archive.h
+++ Include/xLib/Package/Archive.h
@@ -29,20 +29,20 @@ public:
 	xUSING_CONST(Type);
 
 	static
-	bool_t   fileCompress(const Type type, std::ctstring_t &sourceFilePath,
+	bool_t fileCompress(const Type type, std::ctstring_t &sourceFilePath,
 				std::ctstring_t &destFilePath, cbool_t isRemoveSourceFile);
 		///< archive file
 	static
-	bool_t   dirCompress(const Type type, std::ctstring_t &sourceDirPath,
+	bool_t dirCompress(const Type type, std::ctstring_t &sourceDirPath,
 				std::ctstring_t &destFilePath, cbool_t isRemoveSourceDir);
 		///< archive dir
 
 	static
-	bool_t   fileUncompress(const Type type, std::ctstring_t &sourceFilePath,
+	bool_t fileUncompress(const Type type, std::ctstring_t &sourceFilePath,
 				std::ctstring_t &destDirPath, cbool_t isRemoveSourceFile);
 		///< unarchive file
 	static
-	bool_t   dirUncompress(const Type type, std::ctstring_t &sourceDirPath,
+	bool_t dirUncompress(const Type type, std::ctstring_t &sourceDirPath,
 				std::ctstring_t &fileShellFilter, std::ctstring_t &destDirPath,
 				cbool_t isRemoveSourceFiles);
 		///< unarchive dir
