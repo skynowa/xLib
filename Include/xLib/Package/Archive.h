5a6127cfdfe7fe604f753804d0e2a40aff89ccdf
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-01-18 20:50:16 +0200

    Archive: auto detect Type from file name

--- Include/xLib/Package/Archive.h
+++ Include/xLib/Package/Archive.h
@@ -1,8 +1,6 @@
 /**
  * \file   Archive.h
- * \brief  Pack/unpack archives
- *
- * TODO: auto detect Type from file name
+ * \brief  Compress/Uncompress archives
  */
 
 
@@ -13,18 +11,18 @@
 xNAMESPACE_BEGIN2(xl, package)
 
 class Archive
-    ///< Pack/unpack archives
+    ///< Compress/unpack archives
 {
 public:
 	enum class Type
 		/// archive type
 	{
 		Unknown = 0,
-		Zip     = 1,
-		zip7    = 2,
-		Rar     = 3,
-		Gz      = 4,
-		TarBz2  = 5
+		Zip     = 2,
+		zip7    = 3,
+		Rar     = 4,
+		Gz      = 5,
+		TarBz2  = 6
 	};
 	xUSING_CONST(Type);
 
@@ -39,17 +37,21 @@ public:
 
 	static
 	bool_t fileUncompress(const Type type, std::ctstring_t &sourceFilePath,
-				std::ctstring_t &destDirPath, cbool_t isRemoveSourceFile);
+				std::ctstring_t &destDirPath, cbool_t isRemoveSourceFile, cbool_t isAutoDetectType);
 		///< unarchive file
 	static
 	bool_t dirUncompress(const Type type, std::ctstring_t &sourceDirPath,
 				std::ctstring_t &fileShellFilter, std::ctstring_t &destDirPath,
-				cbool_t isRemoveSourceFiles);
+				cbool_t isRemoveSourceFiles, cbool_t isAutoDetectType);
 		///< unarchive dir
 
 private:
     xNO_DEFAULT_CONSTRUCT(Archive)
     xNO_COPY_ASSIGN(Archive)
+
+    static
+    Type  	_autoDetectType(std::ctstring_t &sourceFilePath);
+    	///< auto detect archive type by file extention
 };
 
 xNAMESPACE_END2(xl, package)
