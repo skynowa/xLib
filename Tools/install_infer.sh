e72748df7129c79bc1e184c8a669c703d8740e4d
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-03-13 08:57:33 +0200

    Tools: infer - update

--- Tools/install_infer.sh
+++ Tools/install_infer.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
+#
+# \file  run_infer.sh
+# \brief run infer analizator
+#
+
+
 VERSION=0.15.0; \
-curl -sSL "https://github.com/facebook/infer/releases/download/v$VERSION/infer-linux64-v$VERSION.tar.xz" \
-| sudo tar -C /opt -xJ && \
-ln -s "/opt/infer-linux64-v$VERSION/bin/infer" /usr/local/bin/infer
+curl -sSL "https://github.com/facebook/infer/releases/download/v$VERSION/infer-linux64-v$VERSION.tar.xz" | sudo tar -C /opt -xJ && \
+sudo ln -s "/opt/infer-linux64-v$VERSION/bin/infer" /usr/local/bin/infer
--- Tools/run_infer.sh
+++ Tools/run_infer.sh
@@ -1,5 +1,19 @@
+#!/usr/bin/env bash
+#
+# \file  run_infer.sh
+# \brief run infer analizator
+#
+
+
+# vars
+DIR_XLIB="../xLib"
+DIR_BUILD="../../xLib_eclipse"
+
+mkdir -p $DIR_BUILD
+cd $DIR_BUILD
+
 infer run \
 	--compilation-database compile_commands.json
 
-# infer compile -- cmake ../xLib
+# infer compile -- cmake $DIR_XLIB
 # infer run -- make -j 4
