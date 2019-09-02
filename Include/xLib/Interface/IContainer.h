a23c94a2a061cf11423eae3fcd204d2b1e793a07
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-09-02 14:38:28 +0300

    M Config.cmake - add destructor

--- Include/xLib/Interface/IContainer.h
+++ Include/xLib/Interface/IContainer.h
@@ -30,6 +30,11 @@ public:
 	using reverse_iterator       = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+             IContainer() = default;
+        ///< constructor
+    virtual ~IContainer() = default;
+        ///< destructor
+
 	// iterators
 	virtual iterator       begin() = 0;
 	virtual const_iterator cbegin() const = 0;
