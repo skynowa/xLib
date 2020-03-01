26cbdf00d85c672e103732ffc01a3cd2a6b2d649
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-03-02 00:49:29 +0200

    execute, test

--- Include/xLib/Package/Translate.cpp
+++ Include/xLib/Package/Translate.cpp
@@ -163,7 +163,6 @@ Translate::execute(
 		response = manager.post(request, query.toString(QUrl::FullyEncoded).toUtf8());
 		xTEST(!response.empty());
 	#else
-
 		const std::map_tstring_t request
 		{
 			{"h1", _languageCode(a_langFrom)},
@@ -180,7 +179,18 @@ Translate::execute(
 
 		curl::HttpClient http;
 		bRv = http.request(curl::HttpClient::RequestType::Post, baseData, &baseDataOut);
+		xTEST(m_bRv);
+		xTEST_EQ(baseDataOut.headers.empty(), false);
+		xTEST_EQ(baseDataOut.body.empty(), false);
 
+		Cout()
+			<< xTRACE_VAR(baseDataOut.contentType)  << std::endl
+			<< xTRACE_VAR(baseDataOut.effectiveUrl) << std::endl
+			<< xTRACE_VAR(baseDataOut.responseCode) << std::endl
+			<< xTRACE_VAR(baseDataOut.totalTimeSec) << std::endl
+			<< xT("\n")
+			<< xTRACE_VAR(baseDataOut.headers)      << std::endl
+			<< xTRACE_VAR(baseDataOut.body.size())  << std::endl;
 	#endif
      }
 
