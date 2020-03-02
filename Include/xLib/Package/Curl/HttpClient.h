679f6729bf18929c2dd8efc6e8a6be0fa353e52f
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-03-02 17:26:21 +0200

    Base.h - HttpCode, add

--- Include/xLib/Package/Curl/HttpClient.h
+++ Include/xLib/Package/Curl/HttpClient.h
@@ -22,6 +22,43 @@ public:
     virtual ~HttpClient() = default;
         ///< destructor
 
+	enum class HttpCode
+	{
+		Unknown              = 0,
+
+		// 1xx: Informational
+		Continue             = 100,
+		SwitchingProtocols   = 101,
+		Processing           = 102,
+
+		// 2xx: Success
+		OK                   = 200,
+		Created              = 201,
+		Accepted             = 202,
+		NoContent            = 204,
+
+		// 3xx: Redirection
+		MovedPermanently     = 301,
+		Found                = 302,
+		SeeOther             = 303,
+		NotModified          = 304,
+		TemporaryRedirect    = 307,
+
+		// 4xx: Client Error
+		BadRequest           = 400,
+		Unauthorized         = 401,
+		Forbidden            = 403,
+		NotFound             = 404,
+		MethodNotAllowed     = 405,
+		NotAcceptable        = 406,
+		PreconditionFailed   = 412,
+		UnsupportedMediaType = 415,
+
+		// 5xx: Server Error
+		InternalServerError  = 500,
+		NotImplemented       = 501
+	};
+
 	enum class RequestType
 	{
 		Unknown = 0,
