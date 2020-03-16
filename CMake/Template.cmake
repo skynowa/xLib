09334af6fc5d21b50083e99d61aedf40024efe30
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-03-16 10:55:09 +0200

    Template.cmake - cmXLIB_TEMPLATE_PLATFORM_FILES, add

--- CMake/Template.cmake
+++ CMake/Template.cmake
@@ -6,15 +6,17 @@
 
 #--------------------------------------------------------------------------------------------------
 # Config (set manually)
-unset(cmXLIB_TEMPLATE          CACHE)
-unset(cmXLIB_TEMPLATE_COMMENTS CACHE)
-unset(cmXLIB_TEMPLATE_SUBDIR   CACHE)
-unset(cmXLIB_TEMPLATE_NS       CACHE)
+unset(cmXLIB_TEMPLATE                CACHE)
+unset(cmXLIB_TEMPLATE_COMMENTS       CACHE)
+unset(cmXLIB_TEMPLATE_SUBDIR         CACHE)
+unset(cmXLIB_TEMPLATE_NS             CACHE)
+unset(cmXLIB_TEMPLATE_PLATFORM_FILES CACHE)
 
-set(cmXLIB_TEMPLATE          "Translate")
-set(cmXLIB_TEMPLATE_COMMENTS "Translate text")
-set(cmXLIB_TEMPLATE_SUBDIR   "Package")
-set(cmXLIB_TEMPLATE_NS       "package")
+set(cmXLIB_TEMPLATE                "FeatureFlag")           # TODO: set manually
+set(cmXLIB_TEMPLATE_COMMENTS       "Feature flag / toggle") # TODO: set manually
+set(cmXLIB_TEMPLATE_SUBDIR         "Package")               # TODO: set manually
+set(cmXLIB_TEMPLATE_NS             "package")               # TODO: set manually
+set(cmXLIB_TEMPLATE_PLATFORM_FILES OFF)
 
 if (NOT cmXLIB_TEMPLATE OR
     NOT cmXLIB_TEMPLATE_COMMENTS OR
@@ -37,23 +39,25 @@ configure_file(
     ${XLIB_TEMPLATES}/Test_[Class].cpp.in
     ${XLIB_LOCATION}/Tests/${cmXLIB_TEMPLATE_SUBDIR}/Test_${cmXLIB_TEMPLATE}.cpp)
 
-configure_file(
-    ${XLIB_TEMPLATES}/Platform/Win/[Class]_win.inl.in
-    ${XLIB_LOCATION}/Include/xLib/${cmXLIB_TEMPLATE_SUBDIR}/Platform/Win/${cmXLIB_TEMPLATE}_win.inl)
+if (cmXLIB_TEMPLATE_PLATFORM_FILES)
+    configure_file(
+        ${XLIB_TEMPLATES}/Platform/Win/[Class]_win.inl.in
+        ${XLIB_LOCATION}/Include/xLib/${cmXLIB_TEMPLATE_SUBDIR}/Platform/Win/${cmXLIB_TEMPLATE}_win.inl)
 
-configure_file(
-    ${XLIB_TEMPLATES}/Platform/Unix/[Class]_unix.inl.in
-    ${XLIB_LOCATION}/Include/xLib/${cmXLIB_TEMPLATE_SUBDIR}/Platform/Unix/${cmXLIB_TEMPLATE}_unix.inl)
+    configure_file(
+        ${XLIB_TEMPLATES}/Platform/Unix/[Class]_unix.inl.in
+        ${XLIB_LOCATION}/Include/xLib/${cmXLIB_TEMPLATE_SUBDIR}/Platform/Unix/${cmXLIB_TEMPLATE}_unix.inl)
 
-configure_file(
-    ${XLIB_TEMPLATES}/Platform/Linux/[Class]_linux.inl.in
-    ${XLIB_LOCATION}/Include/xLib/${cmXLIB_TEMPLATE_SUBDIR}/Platform/Linux/${cmXLIB_TEMPLATE}_linux.inl)
+    configure_file(
+        ${XLIB_TEMPLATES}/Platform/Linux/[Class]_linux.inl.in
+        ${XLIB_LOCATION}/Include/xLib/${cmXLIB_TEMPLATE_SUBDIR}/Platform/Linux/${cmXLIB_TEMPLATE}_linux.inl)
 
-configure_file(
-    ${XLIB_TEMPLATES}/Platform/Bsd/[Class]_bsd.inl.in
-    ${XLIB_LOCATION}/Include/xLib/${cmXLIB_TEMPLATE_SUBDIR}/Platform/Bsd/${cmXLIB_TEMPLATE}_bsd.inl)
+    configure_file(
+        ${XLIB_TEMPLATES}/Platform/Bsd/[Class]_bsd.inl.in
+        ${XLIB_LOCATION}/Include/xLib/${cmXLIB_TEMPLATE_SUBDIR}/Platform/Bsd/${cmXLIB_TEMPLATE}_bsd.inl)
 
-configure_file(
-    ${XLIB_TEMPLATES}/Platform/Apple/[Class]_apple.inl.in
-    ${XLIB_LOCATION}/Include/xLib/${cmXLIB_TEMPLATE_SUBDIR}/Platform/Apple/${cmXLIB_TEMPLATE}_apple.inl)
+    configure_file(
+        ${XLIB_TEMPLATES}/Platform/Apple/[Class]_apple.inl.in
+        ${XLIB_LOCATION}/Include/xLib/${cmXLIB_TEMPLATE_SUBDIR}/Platform/Apple/${cmXLIB_TEMPLATE}_apple.inl)
+endif(cmXLIB_TEMPLATE_PLATFORM_FILES)
 #--------------------------------------------------------------------------------------------------
