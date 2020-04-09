9ed29cb78fd0b698dfa846713f4fab4f700f5b61
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-04-10 00:19:06 +0300

    PrivaBankApi.cpp - log(s)

--- Include/xLib/Package/PrivaBankApi.cpp
+++ Include/xLib/Package/PrivaBankApi.cpp
@@ -84,25 +84,16 @@ PrivaBankApi::getExchangeRates(
 	bRv = http.request(curl::HttpClient::Request::Get, dataIn, &dataOut);
 	xTEST(bRv);
 	if ( !http.isSuccess(dataOut) ) {
-		Cout()
-			<< xTRACE_VAR(dataIn.request)       << std::endl
-			<< xT("\n")
-			<< xTRACE_VAR(dataOut.contentType)  << std::endl
-			<< xTRACE_VAR(dataOut.effectiveUrl) << std::endl
-			<< xTRACE_VAR(dataOut.responseCode) << std::endl
-			<< xTRACE_VAR(dataOut.totalTimeSec) << std::endl
-			<< xT("\n")
-			<< xTRACE_VAR(dataOut.headers)      << std::endl
-			<< xTRACE_VAR(dataOut.body.size())  << std::endl
-			<< xTRACE_VAR(dataOut.body)         << std::endl;
-
+		std::tcout
+			<< xTRACE_VAR(dataIn)  << "\n"
+			<< xTRACE_VAR(dataOut) << std::endl;
 		return;
 	}
 
 	xTEST(!dataOut.headers.empty());
 	xTEST(!dataOut.body.empty());
 
-#if 1
+#if 0
 	std::tcout
 		<< xTRACE_VAR(dataIn)  << "\n"
 		<< xTRACE_VAR(dataOut) << std::endl;
