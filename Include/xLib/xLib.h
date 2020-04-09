501a73076069f89f9301091bd02cb81c60676d9b
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-04-09 23:10:28 +0300

    xLib.h - fix build

--- Include/xLib/xLib.h
+++ Include/xLib/xLib.h
@@ -162,7 +162,7 @@
 #include <xLib/Package/Curl/Types.h>
 #include <xLib/Package/Curl/Version.h>
 #include <xLib/Package/Curl/Client.h>
-#include <xLib/Package/Curl/Base.h>
+#include <xLib/Package/Curl/BaseClient.h>
 #include <xLib/Package/Curl/HttpClient.h>
 
 #include <xLib/Package/Xml.h>
