ddaa99f5792e135fee9f1a7c6d68049ff3fe551e
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-05-19 14:49:32 +0300

    Test_Units.cpp - test(s)

--- Tests/Core/Test_Units.cpp
+++ Tests/Core/Test_Units.cpp
@@ -18,22 +18,22 @@ Test_Units::unit()
 {
     xTEST_CASE("Bytes")
     {
-        culonglong_t BYTE = 1ULL;
-        culonglong_t KB   = 1024ULL;
-        culonglong_t MB   = 1024ULL * 1024;
-        culonglong_t GB   = 1024ULL * 1024 * 1024;
-        culonglong_t TB   = 1024ULL * 1024 * 1024 * 1024;
-        culonglong_t PB   = 1024ULL * 1024 * 1024 * 1024 * 1024;
-        culonglong_t EB   = 1024ULL * 1024 * 1024 * 1024 * 1024 * 1024;
+        culonglong_t Byte = 1ULL;
+        culonglong_t Kb   = 1024ULL;
+        culonglong_t Mb   = 1024ULL * 1024;
+        culonglong_t Gb   = 1024ULL * 1024 * 1024;
+        culonglong_t Tb   = 1024ULL * 1024 * 1024 * 1024;
+        culonglong_t Pb   = 1024ULL * 1024 * 1024 * 1024 * 1024;
+        culonglong_t Eb   = 1024ULL * 1024 * 1024 * 1024 * 1024 * 1024;
 
         for (size_t i = 0; i < 20; ++ i) {
-            xTEST_EQ(xBYTES(1 * i), BYTE * i);
-            xTEST_EQ(xKB(1 * i),    KB * i);
-            xTEST_EQ(xMB(1 * i),    MB * i);
-            xTEST_EQ(xGB(1 * i),    GB * i);
-            xTEST_EQ(xTB(1 * i),    TB * i);
-            xTEST_EQ(xPB(1 * i),    PB * i);
-            xTEST_EQ(xEB(1 * i),    EB * i);
+            xTEST_EQ(xBYTES(1 * i), Byte * i);
+            xTEST_EQ(xKB(1 * i),    Kb * i);
+            xTEST_EQ(xMB(1 * i),    Mb * i);
+            xTEST_EQ(xGB(1 * i),    Gb * i);
+            xTEST_EQ(xTB(1 * i),    Tb * i);
+            xTEST_EQ(xPB(1 * i),    Pb * i);
+            xTEST_EQ(xEB(1 * i),    Eb * i);
         }
     }
 
