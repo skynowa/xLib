5408b7c1552d198061028debe97230d404b95c40
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-05-19 14:50:53 +0300

    Test_Units.cpp - test(s)

--- Tests/Core/Test_Units.cpp
+++ Tests/Core/Test_Units.cpp
@@ -27,13 +27,13 @@ Test_Units::unit()
         culonglong_t Eb   = 1024ULL * 1024 * 1024 * 1024 * 1024 * 1024;
 
         for (size_t i = 0; i < 20; ++ i) {
-            xTEST_EQ(xBYTES(1 * i), Byte * i);
-            xTEST_EQ(xKB(1 * i),    Kb * i);
-            xTEST_EQ(xMB(1 * i),    Mb * i);
-            xTEST_EQ(xGB(1 * i),    Gb * i);
-            xTEST_EQ(xTB(1 * i),    Tb * i);
-            xTEST_EQ(xPB(1 * i),    Pb * i);
-            xTEST_EQ(xEB(1 * i),    Eb * i);
+            xTEST_EQ(xBYTES(i), Byte * i);
+            xTEST_EQ(xKB(i),    Kb * i);
+            xTEST_EQ(xMB(i),    Mb * i);
+            xTEST_EQ(xGB(i),    Gb * i);
+            xTEST_EQ(xTB(i),    Tb * i);
+            xTEST_EQ(xPB(i),    Pb * i);
+            xTEST_EQ(xEB(i),    Eb * i);
         }
     }
 
