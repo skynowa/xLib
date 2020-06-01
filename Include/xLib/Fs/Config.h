5d670a03b7ca937e56ea3d78312fd04293e7baea
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-06-01 20:02:12 +0300

    Config.cpp - remove dead code

--- Include/xLib/Fs/Config.h
+++ Include/xLib/Fs/Config.h
@@ -120,10 +120,9 @@ Config::value(std::ctstring_t &a_key, const T a_defaultValue)
     	return String::cast<T>(_value);
     }
     else if constexpr (std::is_same_v<T, std::ustring_t>) {
-		// hex string
-		std::ctstring_t sRv = String::cast(_value, 16);
+		std::ctstring_t hex = String::cast(_value, 16);
 
-		return {sRv.begin(), sRv.end()};
+		return {hex.begin(), hex.end()};
     }
     else {
     	return String::cast<T>(_value);
