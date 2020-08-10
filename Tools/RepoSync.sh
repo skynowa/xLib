42eda9ddc3ab9a0f517acfbc221f52af7c5bf5a0
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-08-10 14:49:36 +0300

    RepoSync.sh - code.google.com, rm

--- Tools/RepoSync.sh
+++ Tools/RepoSync.sh
@@ -6,6 +6,5 @@
 
 cd ..
 
-git push --tags https://skynowa@code.google.com/p/xlib-library/ master:master
 git push --tags https://skynowa@github.com/skynowa/xLib.git master:master
 git push --tags https://skynowa@git.code.sf.net/p/x-lib/code master:master
