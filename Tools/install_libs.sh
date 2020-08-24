895e7db2a02c90359e325c7b298a2cb7e3f850ca
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-08-24 04:55:53 +0300

    install_libs.sh - fix build

--- Tools/install_libs.sh
+++ Tools/install_libs.sh
@@ -4,7 +4,11 @@ sudo apt-get install \
 	libssh2-1-dev \
 	default-libmysqlclient-dev \
 
-sudo dpkg -i http://security.ubuntu.com/ubuntu/pool/main/c/curl/libcurl4-openssl-dev_7.68.0-1ubuntu2.2_amd64.deb
+TEMP_DEB="$(mktemp)" &&
+wget -O "$TEMP_DEB" 'http://security.ubuntu.com/ubuntu/pool/main/c/curl/libcurl4-openssl-dev_7.68.0-1ubuntu2.2_amd64.deb' &&
+sudo dpkg -i "$TEMP_DEB"
+rm -f "$TEMP_DEB"
+
 
 	# libcurl4-openssl-dev --fix-missing
 	# libcurl4-nss-dev
