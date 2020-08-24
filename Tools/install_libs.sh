67414b0d4f4075b0d870eef7c7770f2268c61620
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-08-24 05:05:53 +0300

    main.yml - fix build

--- Tools/install_libs.sh
+++ Tools/install_libs.sh
@@ -3,13 +3,11 @@ sudo apt-get install \
 	libssl-dev \
 	libssh2-1-dev \
 	default-libmysqlclient-dev \
-
-TEMP_DEB="$(mktemp)" &&
-wget -O "$TEMP_DEB" 'http://security.ubuntu.com/ubuntu/pool/main/c/curl/libcurl4-openssl-dev_7.68.0-1ubuntu2.2_amd64.deb' &&
-sudo dpkg -i "$TEMP_DEB"
-rm -f "$TEMP_DEB"
-
-
-	# libcurl4-openssl-dev --fix-missing
+	# libcurl4-openssl-dev
 	# libcurl4-nss-dev
 	# libcurl4-gnutls-dev
+
+# TEMP_DEB="$(mktemp)" &&
+# wget -O "$TEMP_DEB" 'http://security.ubuntu.com/ubuntu/pool/main/c/curl/libcurl4-openssl-dev_7.68.0-1ubuntu2.2_amd64.deb' &&
+# sudo dpkg -i "$TEMP_DEB"
+# rm -f "$TEMP_DEB"
