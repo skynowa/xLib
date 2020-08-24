2e89ec3f617b8c2f8a69bd86cc46b8ba0ef601a9
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-08-24 04:46:26 +0300

    install_libs.sh - fix build

--- Tools/install_libs.sh
+++ Tools/install_libs.sh
@@ -1,8 +1,8 @@
-sudo apt-get install \
+sudo apt-get install --fix-missing \
 	libx11-xcb-dev \
 	libssl-dev \
 	libssh2-1-dev \
 	default-libmysqlclient-dev \
-	libcurl4-nss-dev
+	libcurl4-openssl-dev
+	# libcurl4-nss-dev
 	# libcurl4-gnutls-dev
-	# libcurl4-openssl-dev
