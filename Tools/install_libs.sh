353a3612854450b3b757a1bb2402d1e0b9c83a35
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-09-01 16:36:59 +0300

    main.yml - install libs

--- Tools/install_libs.sh
+++ Tools/install_libs.sh
@@ -3,11 +3,7 @@ sudo apt-get install \
 	libssl-dev \
 	libssh2-1-dev \
 	default-libmysqlclient-dev \
+	# libcurl - from xLib-data
 	# libcurl4-openssl-dev
 	# libcurl4-nss-dev
 	# libcurl4-gnutls-dev
-
-# libcurl
-cd ../../xLib-data/Libs/ubuntu-20.04
-
-sudo dpkg -i libcurl4-openssl-dev_7.68.0-1ubuntu2.2_amd64.deb
