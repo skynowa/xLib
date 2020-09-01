84269ddac29ef874d53f409f3ccd51708076d36e
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-09-01 16:42:05 +0300

    install_libs.sh - review

--- Tools/install_libs.sh
+++ Tools/install_libs.sh
@@ -2,7 +2,8 @@ sudo apt-get install \
 	libx11-xcb-dev \
 	libssl-dev \
 	libssh2-1-dev \
-	default-libmysqlclient-dev \
+	default-libmysqlclient-dev
+
 	# libcurl - from xLib-data
 	# libcurl4-openssl-dev
 	# libcurl4-nss-dev
