d3dbaac87fe01d8347a9ed8149c4580fd186a894
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-09-02 17:53:21 +0300

    Bitset.h - review

--- Include/xLib/Core/Bitset.h
+++ Include/xLib/Core/Bitset.h
@@ -18,10 +18,14 @@ class Bitset
     /// bit flags
 {
 public:
-             Bitset() = default;
-    explicit Bitset(const Bitset &values);
-    explicit Bitset(const T &values);
-    virtual ~Bitset() = default;
+///@name ctors, dtor
+///@{
+			 Bitset() = default;
+	explicit Bitset(const Bitset &values);
+	explicit Bitset(const T values);
+	virtual ~Bitset() = default;
+///@}
+
 
     Bitset & operator = (const Bitset &values);
 	T &      operator [] (const std::size_t index);
@@ -30,22 +34,22 @@ public:
     // TODO: https://github.com/oliora/bitmask
     T &      get() const;
         ///< get all bits
-    void_t   set(const T &values);
+    void_t   set(const T values);
         ///< set all bits
     void_t   clear();
         ///< reset all bits
 
     bool_t   test(const std::size_t index) const;
         ///< check bit at position is set
-    bool_t   isSetBit(const T &value) const;
+    bool_t   isSetBit(const T value) const;
         ///< flag is set
-    bool_t   isSetAnyBit(const T &value) const;
+    bool_t   isSetAnyBit(const T value) const;
         ///< is any flag set
-    void_t   setBit(const T &value);
+    void_t   setBit(const T value);
         ///< set flag
-    void_t   unsetBit(const T &value);
+    void_t   unsetBit(const T value);
         ///< unset flag
-    void_t   flipBit(const T &value);
+    void_t   flipBit(const T value);
         ///< flip flag
 
 	constexpr
