184273d3161d618f8b0225ed7bd7fc1cd8d1419c
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-09-03 14:49:37 +0300

    Test_IpcSemaphore.cpp - review

--- Tests/Sync/Test_IpcSemaphore.cpp
+++ Tests/Sync/Test_IpcSemaphore.cpp
@@ -25,31 +25,25 @@ Test_IpcSemaphore::unit()
         #elif xENV_UNIX
             static void_t * xSTDCALL
         #endif
-            worker(void_t *pArguments)
+            worker(void_t *param)
             {
-                // Tracer() << xT("Start");
-
-                IpcSemaphore *psemSem = static_cast<IpcSemaphore *>(pArguments);
-                xTEST_PTR(psemSem);
+                auto *semaphore = static_cast<IpcSemaphore *>(param);
+                xTEST_PTR(semaphore);
 
                 for (int_t i = 0; i < 50; i ++) {
-                    psemSem->wait(xTIMEOUT_INFINITE);
-
-                    // Tracer() << xTRACE_VAR(i);
+                    semaphore->wait(xTIMEOUT_INFINITE);
                 }
-
-                // Tracer() << xT("Stop");
 
                 return 0;
             }
         };
 
         // create
-        IpcSemaphore semSemaphore;
-        semSemaphore.create(4, xT("sema_name"));
+        IpcSemaphore semaphore;
+        semaphore.create(4, xT("sema_name"));
 
     #if   xENV_WIN
-        uintptr_t puiRv = ::_beginthreadex(nullptr, 0U, &_Functor::worker, &semSemaphore, 0U, nullptr);
+        uintptr_t puiRv = ::_beginthreadex(nullptr, 0U, &_Functor::worker, &semaphore, 0U, nullptr);
         #if xARCH_BITS_32
             xTEST_DIFF(uintptr_t(0), puiRv);
         #else
@@ -57,7 +51,7 @@ Test_IpcSemaphore::unit()
         #endif
     #elif xENV_UNIX
         pthread_t id = 0UL;
-        int_t iRv = ::pthread_create(&id, nullptr, &_Functor::worker, &semSemaphore);
+        int_t iRv = ::pthread_create(&id, nullptr, &_Functor::worker, &semaphore);
         xTEST_EQ(iRv, 0);
     #endif
 
@@ -65,7 +59,7 @@ Test_IpcSemaphore::unit()
         for (size_t i = 0; i < 50; ++ i) {
             Thread::currentSleep(1);
             for (int_t x = 0; x < 2; x ++) {
-                semSemaphore.post();
+                semaphore.post();
             }
         }
 
