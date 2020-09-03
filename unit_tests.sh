e23647417467e08570c39b14c755447bbebbc294
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-09-03 19:34:34 +0300

    unit_tests.sh - review

--- unit_tests.sh
+++ unit_tests.sh
@@ -10,10 +10,16 @@ if [[ -z "$UNIT_TEST_NAME" ]]; then
 	exit 1
 fi
 
+# vars
+DIR_BUILD="../../xLib_eclipse"
+JOBS_NUM=$(($(nproc) * 2))
+
+echo "Jobs: $JOBS_NUM"
+
 cd ../xLib_eclipse
 
 if [[ "$UNIT_TEST_NAME" == "all" ]]; then
-    make all -j$(($(nproc) * 2)) && ctest -j1
+    make all -j$JOBS_NUM && ctest -j$JOBS_NUM
 else
     make Test_$UNIT_TEST_NAME && ./Tests/Test_$UNIT_TEST_NAME
 fi
