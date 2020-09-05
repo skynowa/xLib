a63460aafe95c12bab876d3951bc98650fef3213
skynowa <skynowa@gmail.com>
2020-09-06 02:49:33 +0300

    DirTemp-impl (#376)
    * DirTemp-impl: DirTemp.cpp - add
    
    * DirTemp-impl: DirTemp.h - review
    
    * DirTemp-impl: DirTemp.cpp - implementation
    
    * DirTemp-impl: DirTemp.cpp - implementation
    
    * DirTemp-impl: DirTemp_apple.inl - remove
    
    * DirTemp-impl: DirTemp.cpp - implementation
    
    * DirTemp-impl: DirTemp.cpp - implementation
    
    * DirTemp-impl: DirTemp.cpp - implementation
    
    * DirTemp-impl: DirTemp.h - code style
    
    * DirTemp-impl: DirTemp.cpp - implementation
    
    * DirTemp-impl: DirTemp.cpp - implementation
    
    * DirTemp-impl: Dir.h - implementation
    
    * DirTemp-impl: DirTemp.h - comment(s)
    
    * DirTemp-impl: DirTemp.cpp - implementation
    
    * DirTemp-impl: DirTemp.cpp - implementation
    
    * DirTemp-impl: Unit.cpp - implementation
    
    Co-authored-by: Sergey Shevchenko <skynowa@fabrica.net.ua>

---
+++ Include/xLib/Fs/DirTemp.cpp
@@ -0,0 +1,61 @@
+/**
+ * \file  DirTemp.cpp
+ * \brief Temporary directory
+ */
+
+
+#include "DirTemp.h"
+
+#include <xLib/Test/Test.h>
+#include <xLib/Debug/Debug.h>
+#include <xLib/Debug/NativeError.h>
+#include <xLib/Debug/StdError.h>
+#include <xLib/Debug/StackTrace.h>
+#include <xLib/Debug/ErrorReport.h>
+#include <xLib/Debug/Debugger.h>
+#include <xLib/Log/Trace.h>
+#include <xLib/Fs/Path.h>
+#include <xLib/Fs/Dir.h>
+#include <xLib/Crypt/Guid.h>
+
+
+xNAMESPACE_BEGIN2(xl, fs)
+
+/**************************************************************************************************
+*   public
+*
+**************************************************************************************************/
+
+//-------------------------------------------------------------------------------------------------
+DirTemp::DirTemp(
+	std::ctstring_t &a_dirPathPrefix,
+	cbool_t          a_isAutoDelete
+) :
+	_dirPath     (a_dirPathPrefix + xT("_") + Guid(Guid::Type::RandomBased).get()),
+	_isAutoDelete(a_isAutoDelete)
+{
+	xTEST(!a_dirPathPrefix.empty());
+}
+//-------------------------------------------------------------------------------------------------
+/* virtual */
+DirTemp::~DirTemp()
+{
+	if (_isAutoDelete) {
+		Dir(_dirPath).pathDelete();
+	}
+}
+//-------------------------------------------------------------------------------------------------
+std::ctstring_t &
+DirTemp::path() const
+{
+	return _dirPath;
+}
+//-------------------------------------------------------------------------------------------------
+void_t
+DirTemp::create() const
+{
+	Dir(_dirPath).pathCreate();
+}
+//-------------------------------------------------------------------------------------------------
+
+xNAMESPACE_END2(xl, fs)
