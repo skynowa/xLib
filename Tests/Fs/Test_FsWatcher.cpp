2e7e1836866ab85ed94e7141a3279cb13639554e
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-09-06 20:20:53 +0300

    tests-data: Test_FsWatcher.cpp - fix test(s)

--- Tests/Fs/Test_FsWatcher.cpp
+++ Tests/Fs/Test_FsWatcher.cpp
@@ -122,14 +122,9 @@ Test_FsWatcher::unit()
     };
 
     std::vec_tstring_t dbWatchDirPaths
-#if 0
 	{
 		#include "fs_watcher_dirs_suppliers.db"
 	};
-#else
-	;
-	dbWatchDirPaths.push_back("/home/skynowa/Projects/xLib/Include/xLib/Fs");
-#endif
 
     static std::cvec_tstring_t dbWatchDirPathsDisabled
     {
