89a42c21b3fc97507fbb8efc20b5630f920341ae
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-10-25 15:02:34 +0200

    FindxLibData.cmake - 'cm' prefix, add

--- CMake/FindxLibData.cmake
+++ CMake/FindxLibData.cmake
@@ -2,17 +2,17 @@
 # \file  FindxLibData.cmake
 # \brief Find xLib-data
 #
-# XLIB_DATA_FOUND - true if system has library
-# cmXLIB_DATA_DIR - directory path
+# cmXLIB_DATA_FOUND - true if system has library
+# cmXLIB_DATA_DIR   - directory path
 #--------------------------------------------------------------------------------------------------
 
 
 #--------------------------------------------------------------------------------------------------
-unset(XLIB_DATA_FOUND CACHE)
-unset(cmXLIB_DATA_DIR CACHE)
+unset(cmXLIB_DATA_FOUND CACHE)
+unset(cmXLIB_DATA_DIR   CACHE)
 
 if (NOT XLIB_LOCATION)
-    set(XLIB_DATA_FOUND 0)
+    set(cmXLIB_DATA_FOUND 0)
 
     message(FATAL_ERROR "XLIB_LOCATION: ${XLIB_LOCATION}")
 endif()
@@ -27,17 +27,17 @@ find_path(cmXLIB_DATA_DIR
         "")
 
 if (NOT cmXLIB_DATA_DIR)
-    set(XLIB_DATA_FOUND 0)
+    set(cmXLIB_DATA_FOUND 0)
 else()
     set(cmXLIB_DATA_DIR "${cmXLIB_DATA_DIR}/xLib-data")
-    set(XLIB_DATA_FOUND 1)
+    set(cmXLIB_DATA_FOUND 1)
 endif()
 #--------------------------------------------------------------------------------------------------
 # trace
-if (NOT XLIB_DATA_FOUND AND xLibData_FIND_REQUIRED)
-    message(FATAL_ERROR "XLIB_DATA_FOUND: ${XLIB_DATA_FOUND}")
+if (NOT cmXLIB_DATA_FOUND AND xLibData_FIND_REQUIRED)
+    message(FATAL_ERROR "cmXLIB_DATA_FOUND: ${cmXLIB_DATA_FOUND}")
 else()
-    message(STATUS "XLIB_DATA_FOUND: ${XLIB_DATA_FOUND}")
+    message(STATUS "cmXLIB_DATA_FOUND: ${cmXLIB_DATA_FOUND}")
     message("   cmXLIB_DATA_DIR: ${cmXLIB_DATA_DIR}")
 endif()
 #--------------------------------------------------------------------------------------------------
