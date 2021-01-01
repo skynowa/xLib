bf9e6d694d8e47081eec5c17c009dfa8a19a9fe2
skynowa <skynowa@gmail.com>
2021-01-01 22:31:33 +0200

    Test_Debugger.cpp - toString() -> str()

--- Tests/Debug/Test_Debugger.cpp
+++ Tests/Debug/Test_Debugger.cpp
@@ -91,16 +91,16 @@ Test_Debugger::unit()
         };
 
         for (size_t i = 0; i < xARRAY_SIZE(type); ++ i) {
-            ulong_t         nativeError    { NativeError::get() {;
-            SourceInfoData  sourceInfoData {xFILE, xLINE, xFUNCTION, xCOUNTER};
-            SourceInfo      sourceInfo(sourceInfoData);
-            std::ctstring  &stackTrace     = StackTrace().str();
+            ulong_t          nativeError    { NativeError::get() };
+            SourceInfoData   sourceInfoData {xFILE, xLINE, xFUNCTION, xCOUNTER};
+            SourceInfo       sourceInfo(sourceInfoData);
+            std::ctstring_t &stackTrace     = StackTrace().str();
 
             culong_t val1 {10};
             culong_t val2 {20};
 
             ErrorReport report(type[i], xLEX_TO_STR(val1), xLEX_TO_STR(val2), val1, val2,
-                xT("=="), nativeError, sourceInfo, , xT(""));
+                xT("=="), nativeError, sourceInfo, stackTrace, xT(""));
 
             Debugger debugger;
             debugger.setEnabled(false);
