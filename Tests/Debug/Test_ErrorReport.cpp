412e2c01d3764b829aa3340b250bd159b792a0bc
skynowa <skynowa@gmail.com>
2021-01-01 22:13:09 +0200

    Test_ErrorReport.cpp - review

--- Tests/Debug/Test_ErrorReport.cpp
+++ Tests/Debug/Test_ErrorReport.cpp
@@ -18,15 +18,17 @@ Test_ErrorReport::unit()
 {
     xTEST_CASE("ErrorReport")
     {
-        ulong_t        nativeError    = NativeError::get();
-        SourceInfoData sourceInfoData = {xFILE, xLINE, xFUNCTION, xCOUNTER};
-        SourceInfo     sourceInfo(sourceInfoData);
+        ulong_t          nativeError    { NativeError::get() };
+        SourceInfoData   sourceInfoData {xFILE, xLINE, xFUNCTION, xCOUNTER};
+        SourceInfo       sourceInfo(sourceInfoData);
+        std::ctstring_t &stackTrace     = StackTrace().toString();
 
-        ulong_t val1 {10};
-        ulong_t val2 {20};
+        culong_t val1 {10};
+        culong_t val2 {20};
 
-        ErrorReport eport(ErrorReport::Type::Msgbox, xT("val1"), xT("val2"), val1, val2,
-            xT("=="), nativeError, sourceInfo, StackTrace().toString(), xT(""));
+        ErrorReport eport(ErrorReport::Type::Msgbox,
+            xLEX_TO_STR(val1), xLEX_TO_STR(val), val1, val2,
+            xT("=="), nativeError, sourceInfo, stackTrace, xT(""));
     }
 
     return true;
