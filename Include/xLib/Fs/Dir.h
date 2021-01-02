792ee45ffc16cf8facd14c21124e0208d8fd3b71
skynowa <skynowa@gmail.com>
2021-01-02 05:13:30 +0200

    Dir.h - review

--- Include/xLib/Fs/Dir.h
+++ Include/xLib/Fs/Dir.h
@@ -38,59 +38,59 @@ public:
     std::ctstring_t & str() const;
         ///< directory path
 
-    bool_t            isExists() const;
+    bool_t isExists() const;
         ///< check for existence
-    bool_t            isEmpty(std::ctstring_t &shellFilter = Const::maskAll()) const;
+    bool_t isEmpty(std::ctstring_t &shellFilter = Const::maskAll()) const;
         ///< is empty
-    bool_t            isRoot() const;
+    bool_t isRoot() const;
         ///< is root
-    bool_t            isDir() const;
+    bool_t isDir() const;
         ///< is dir
-    void_t            create() const;
+    void_t create() const;
         ///< create
-    void_t            pathCreate() const;
+    void_t pathCreate() const;
         ///< creation of all directories that not exists in path
-    void_t            copy(std::ctstring_t &dirPathTo, cbool_t failIfExists) const;
+    void_t copy(std::ctstring_t &dirPathTo, cbool_t failIfExists) const;
         ///< copy
-    void_t            move(std::ctstring_t &dirPathTo, cbool_t failIfExists) const;
+    void_t move(std::ctstring_t &dirPathTo, cbool_t failIfExists) const;
         ///< move
-    void_t            createSymlink(std::ctstring_t &dirPathTo) const;
+    void_t createSymlink(std::ctstring_t &dirPathTo) const;
         ///< create symlink
 
-    void_t            remove() const;
+    void_t remove() const;
         ///< deletion dir which empty
-    void_t            tryRemove(std::csize_t attempts, culong_t timeoutMsec) const;
+    void_t tryRemove(std::csize_t attempts, culong_t timeoutMsec) const;
         ///< try deleting, max 100 attempts
-    void_t            pathClear() const;
+    void_t pathClear() const;
         ///< deletion all content of dir
-    void_t            pathDelete() const;
+    void_t pathDelete() const;
         ///< deletion dir find all content of it
 
 xPUBLIC_STATIC:
     static
-    void_t            setCurrent(std::ctstring_t &dirPath);
+    void_t setCurrent(std::ctstring_t &dirPath);
         ///< set current
 
 private:
-    std::ctstring_t   _dirPath;
+    std::ctstring_t _dirPath;
 
 xPLATFORM_IMPL:
     // static
 	static
-	std::ctstring_t   _dirTmp();
+	std::ctstring_t _dirTmp();
 		///< temporary directory path
     static
-    std::tstring_t    _current_impl();
+    std::tstring_t  _current_impl();
     static
-    void_t            _setCurrent_impl(std::ctstring_t &dirPath);
+    void_t          _setCurrent_impl(std::ctstring_t &dirPath);
     static
-    std::tstring_t    _temp_impl();
+    std::tstring_t  _temp_impl();
 
-    bool_t            _isRoot_impl() const;
-    void_t            _create_impl() const;
-    void_t            _createSymlink_impl(std::ctstring_t &dirPathTo) const;
-    void_t            _remove_impl() const;
-    bool_t            _tryRemove_impl() const;
+    bool_t          _isRoot_impl() const;
+    void_t          _create_impl() const;
+    void_t          _createSymlink_impl(std::ctstring_t &dirPathTo) const;
+    void_t          _remove_impl() const;
+    bool_t          _tryRemove_impl() const;
 };
 
 } // namespace
