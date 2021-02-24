ac8904ebb040a8ccd7720d15e04dcb68e992085c
skynowa <skynowa@gmail.com>
2021-02-24 11:09:23 +0200

    Translate.cpp - review

--- Include/xLib/Package/Translate.cpp
+++ Include/xLib/Package/Translate.cpp
@@ -183,16 +183,19 @@ Translate::execute(
 
 		// baseDataIn.request
 		{
-			std::ctstring_t sl = (a_langFrom == Language::Unknown) ?
+			std::ctstring_t &h_lang      = _langCode(a_langFrom);
+			std::ctstring_t &source_lang = (a_langFrom == Language::Unknown) ?
 				xT("auto") : _langCode(a_langFrom);
+			std::ctstring_t &target_lang = _langCode(a_langTo);
+			std::ctstring_t &query       = a_textFrom;
 
 			const std::map_tstring_t request
 			{
-				{"h1", _langCode(a_langFrom)},
-				{"sl", sl},
-				{"tl", _langCode(a_langTo)},
+				{"h1", h_lang},
+				{"sl", source_lang},
+				{"tl", target_lang},
 				{"ie", "UTF-8"},
-				{"q",  a_textFrom}
+				{"q",  query}
 			};
 
 			for (const auto &[param, value] : request) {
