e7f18fc3999d901a1b50ed966c9b40b6665c5e60
skynowa <skynowa@gmail.com>
2021-02-24 11:45:42 +0200

    Translate.cpp - params, add

--- Include/xLib/Package/Translate.cpp
+++ Include/xLib/Package/Translate.cpp
@@ -183,13 +183,16 @@ Translate::execute(
 
 		// baseDataIn.request
 		{
-			std::ctstring_t &source_lang  = (a_langFrom == Language::Unknown) ?
+			std::ctstring_t  source_lang  = (a_langFrom == Language::Unknown) ?
 				xT("auto") : _langCode(a_langFrom);
-			std::ctstring_t &target_lang  = _langCode(a_langTo);
+			std::ctstring_t  target_lang  = _langCode(a_langTo);
 			std::ctstring_t &h_lang       = source_lang;
 
 			std::csize_t     querySizeMax = 2048;
 			std::ctstring_t &query        = a_textFrom;
+
+			std::ctstring_t  encoding_in  = "UTF-8";
+			std::ctstring_t  encoding_out = "UTF-8";
 
 			xCHECK_DO(query.size() > querySizeMax,
 				Cout() << "Warning: " << xTRACE_VAR_2(querySizeMax, query.size()));
@@ -199,7 +202,8 @@ Translate::execute(
 				{"sl", source_lang},
 				{"tl", target_lang},
 				{"hl", h_lang},
-				{"ie", "UTF-8"},
+				{"ie", encoding_in},
+				{"oe", encoding_out},
 				{"q",  query}
 			};
 
