06dd9dfd669f7f38099905ff85a3340d357f8588
skynowa <skynowa@gmail.com>
2021-02-24 11:30:28 +0200

    Translate.cpp - review

--- Include/xLib/Package/Translate.cpp
+++ Include/xLib/Package/Translate.cpp
@@ -183,11 +183,16 @@ Translate::execute(
 
 		// baseDataIn.request
 		{
-			std::ctstring_t &source_lang = (a_langFrom == Language::Unknown) ?
+			std::ctstring_t &source_lang  = (a_langFrom == Language::Unknown) ?
 				xT("auto") : _langCode(a_langFrom);
-			std::ctstring_t &target_lang = _langCode(a_langTo);
-			std::ctstring_t &h_lang      = source_lang;
-			std::ctstring_t &query       = a_textFrom;
+			std::ctstring_t &target_lang  = _langCode(a_langTo);
+			std::ctstring_t &h_lang       = source_lang;
+
+			std::csize_t     querySizeMax = 2048;
+			std::ctstring_t &query        = a_textFrom;
+
+			xCHECK_DO(query.size() > querySizeMax,
+				Cout() << "Warning: " << xTRACE_VAR_2(querySizeMax, query.size()));
 
 			const std::map_tstring_t request
 			{
