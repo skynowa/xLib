6f7d64ba3340a43dc1ca1106ab7fc10927f59981
skynowa <skynowa@gmail.com>
2021-07-07 12:23:16 +0300

    ILog.cpp - review

--- Include/xLib/Log/ILog.cpp
+++ Include/xLib/Log/ILog.cpp
@@ -48,33 +48,53 @@ ILog::_levelString(
     cLevel a_level
 ) const
 {
-    std::tstring_t sRv;
+#if 1
+	std::tstring_t sRv;
 
-    switch (a_level) {
-    case Level::Off:
-        sRv = xT("Off");
-        break;
-    case Level::Trace:
-        sRv = xT("Trace");
-        break;
-    case Level::Debug:
-        sRv = xT("Debug");
-        break;
-    case Level::Info:
-        sRv = xT("Info");
-        break;
-    case Level::Warning:
-        sRv = xT("Warning");
-        break;
-    case Level::Error:
-        sRv = xT("Error");
-        break;
-    case Level::Fatal:
-        sRv = xT("Fatal");
-        break;
-    }
+	switch (a_level) {
+	case Level::Off:
+		sRv = xT("Off");
+		break;
+	case Level::Trace:
+		sRv = xT("Trace");
+		break;
+	case Level::Debug:
+		sRv = xT("Debug");
+		break;
+	case Level::Info:
+		sRv = xT("Info");
+		break;
+	case Level::Warning:
+		sRv = xT("Warning");
+		break;
+	case Level::Error:
+		sRv = xT("Error");
+		break;
+	case Level::Fatal:
+		sRv = xT("Fatal");
+		break;
+	}
 
-    return sRv;
+	return sRv;
+#else
+	static const std::map<Level, std::tstring_t> levels
+	{
+		{Level::Off,     xT("Off")},
+		{Level::Trace,   xT("Trace")},
+		{Level::Debug,   xT("Debug")},
+		{Level::Info,    xT("Info")},
+		{Level::Warning, xT("Warning")},
+		{Level::Error,   xT("Error")},
+		{Level::Fatal,   xT("Fatal")}
+	};
+
+	const auto &it = levels.find(a_level);
+	if (it == levels.cend()) {
+		return {};
+	}
+
+    return it->second;
+#endif
 }
 //-------------------------------------------------------------------------------------------------
 
