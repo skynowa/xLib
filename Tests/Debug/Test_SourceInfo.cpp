1f54bf2d5ecf681b87b4d355c9c949556a92e245
skynowa <skynowa@gmail.com>
2021-07-07 19:12:44 +0300

    source-info: Test_SourceInfo.cpp - test(s)

--- Tests/Debug/Test_SourceInfo.cpp
+++ Tests/Debug/Test_SourceInfo.cpp
@@ -15,13 +15,14 @@ Test_SourceInfo::unit()
 {
     xTEST_CASE("all")
     {
-        std::csize_t var1 {100};
-        std::csize_t var2 {200};
+        std::csize_t    var1 {100};
+        std::csize_t    var2 {200};
+        std::ctstring_t op = xT("<");
 
         cSourceInfoData sourceInfoData
         {
             xFILE, xLINE, xFUNCTION, xCOUNTER,
-            xLEX_TO_STR(var1), xLEX_TO_STR(var2), std::to_string(var1), std::to_string(var2), xT("<")
+            xLEX_TO_STR(var1), xLEX_TO_STR(var2), std::to_string(var1), std::to_string(var2), op
         };
 
         SourceInfo sourceInfo(sourceInfoData);
@@ -31,7 +32,10 @@ Test_SourceInfo::unit()
         xTEST_GR(sourceInfo.data().counter, 0UL);
         xTEST(!sourceInfo.format().empty());
         xTEST(!sourceInfo.at().empty());
-        xTEST_EQ(sourceInfo.expr(), std::tstring_t(xT("var1 (100) < var2 (200)")));
+        xTEST_EQ(sourceInfo.expr(), Format::str(xT("{} ({}) {} {} ({})"),
+             xLEX_TO_STR(var1), std::to_string(var1),
+             op,
+             xLEX_TO_STR(var2), std::to_string(var2)));
 
 		if (0) {
 			Trace() << xTRACE_VAR(sourceInfo.format());
