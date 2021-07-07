384ee3f34e156a2385c0aa1de2bd7c42101ab23d
skynowa <skynowa@gmail.com>
2021-07-07 22:25:10 +0300

    source-info: Test_SourceInfo.cpp - test(s)

--- Tests/Debug/Test_SourceInfo.cpp
+++ Tests/Debug/Test_SourceInfo.cpp
@@ -32,10 +32,10 @@ Test_SourceInfo::unit()
         xTEST_GR(sourceInfo.data().counter, 0UL);
         xTEST(!sourceInfo.format().empty());
         xTEST(!sourceInfo.at().empty());
-        xTEST_EQ(sourceInfo.expr(), Format::str(xT("{} ({}) {} {} ({})"),
-             xLEX_TO_STR(var1), std::to_string(var1),
-             op,
-             xLEX_TO_STR(var2), std::to_string(var2)));
+        xTEST_EQ(sourceInfo.expr(), Format::str(xT("{} {} {}"),
+             xLEX_TO_STR(var1), op, xLEX_TO_STR(var2)));
+        xTEST_EQ(sourceInfo.exprValues(), Format::str(xT("{} {} {}"),
+             std::to_string(var1), op, std::to_string(var2)));
 
 		if (0) {
 			Trace() << xTRACE_VAR(sourceInfo.format());
