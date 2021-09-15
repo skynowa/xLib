58a7d5b224e32fefa40b746424619eb6a296f99d
skynowa <skynowa@gmail.com>
2021-09-15 22:57:45 +0300

    SmtpClient.cpp - comment(s)

--- Include/xLib/Package/Curl/SmtpClient.cpp
+++ Include/xLib/Package/Curl/SmtpClient.cpp
@@ -59,48 +59,55 @@ SmtpClient::request()
 
 	setOption(CURLOPT_PROTOCOLS, CURLPROTO_SMTP);
 
-    /* This is the URL for your mailserver. Note the use of port 587 here,
-     * instead of the normal SMTP port (25). Port 587 is commonly used for
-     * secure mail submission (see RFC4403), but you should use whatever
-     * matches your server configuration. */
+   /**
+	* This is the URL for your mailserver. Note the use of port 587 here,
+	* instead of the normal SMTP port (25). Port 587 is commonly used for
+	* secure mail submission (see RFC4403), but you should use whatever
+	* matches your server configuration.
+	*/
 	setOption(CURLOPT_URL, _url.c_str());
 
-    /* In this example, we'll start with a plain text connection, and upgrade
-     * to Transport Layer Security (TLS) using the STARTTLS command. Be careful
-     * of using CURLUSESSL_TRY here, because if TLS upgrade fails, the transfer
-     * will continue anyway - see the security discussion in the libcurl
-     * tutorial for more details. */
-    setOption(CURLOPT_USE_SSL, static_cast<long_t>(CURLUSESSL_ALL));
+   /**
+	* In this example, we'll start with a plain text connection, and upgrade
+	* to Transport Layer Security (TLS) using the STARTTLS command. Be careful
+	* of using CURLUSESSL_TRY here, because if TLS upgrade fails, the transfer
+	* will continue anyway - see the security discussion in the libcurl
+	* tutorial for more details.
+	*/
+	setOption(CURLOPT_USE_SSL, static_cast<long_t>(CURLUSESSL_ALL));
 
-    /* If your server doesn't have a valid certificate, then you can disable
-     * part of the Transport Layer Security protection by setting the
-     * CURLOPT_SSL_VERIFYPEER and CURLOPT_SSL_VERIFYHOST options to 0 (false).
-     *   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-     *   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-     * That is, in general, a bad idea. It is still better than sending your
-     * authentication details in plain text though.  Instead, you should get
-     * the issuer certificate (or the host certificate if the certificate is
-     * self-signed) and add it to the set of certificates that are known to
-     * libcurl using CURLOPT_CAINFO and/or CURLOPT_CAPATH. See docs/SSLCERTS
-     * for more information. */
-	if (0) {
-		setOption(CURLOPT_CAINFO, _caPath.c_str());
-	}
+   /**
+	* If your server doesn't have a valid certificate, then you can disable
+	* part of the Transport Layer Security protection by setting the
+	* CURLOPT_SSL_VERIFYPEER and CURLOPT_SSL_VERIFYHOST options to 0 (false).
+	*   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+	*   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+	* That is, in general, a bad idea. It is still better than sending your
+	* authentication details in plain text though.  Instead, you should get
+	* the issuer certificate (or the host certificate if the certificate is
+	* self-signed) and add it to the set of certificates that are known to
+	* libcurl using CURLOPT_CAINFO and/or CURLOPT_CAPATH. See docs/SSLCERTS
+	* for more information.
+	*/
+	setOption(CURLOPT_CAINFO, _caPath.c_str());
 
-    /* Set username and password */
-    setOption(CURLOPT_USERNAME, _userName.c_str());
-    setOption(CURLOPT_PASSWORD, _password.c_str());
+   /**
+	* Set username and password
+	*/
+	setOption(CURLOPT_USERNAME, _userName.c_str());
+	setOption(CURLOPT_PASSWORD, _password.c_str());
 
-    /* Note that this option isn't strictly required, omitting it will result
-     * in libcurl sending the MAIL FROM command with empty sender data. All
-     * autoresponses should have an empty reverse-path, and should be directed
-     * to the address in the reverse-path which triggered them. Otherwise,
-     * they could cause an endless loop. See RFC 5321 Section 4.5.5 for more
-     * details.
-     */
-    setOption(CURLOPT_MAIL_FROM, _from.c_str());
+   /**
+	* Note that this option isn't strictly required, omitting it will result
+	* in libcurl sending the MAIL FROM command with empty sender data. All
+	* autoresponses should have an empty reverse-path, and should be directed
+	* to the address in the reverse-path which triggered them. Otherwise,
+	* they could cause an endless loop. See RFC 5321 Section 4.5.5 for more
+	* details.
+	*/
+	setOption(CURLOPT_MAIL_FROM, _from.c_str());
 
-    // _recipients
+	// _recipients
 	{
 	   /**
 		* Add two recipients, in this particular case they correspond to the
