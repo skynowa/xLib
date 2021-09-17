b57336a8ea6b779059ca1f2c1bfaa03f5db4e95d
skynowa <skynowa@gmail.com>
2021-09-18 01:06:36 +0300

    Client.cpp - _lastError - impl

--- Include/xLib/Package/Curl/Client.h
+++ Include/xLib/Package/Curl/Client.h
@@ -74,8 +74,7 @@ public:
     /// struct curl_slist *curl_slist_append(struct curl_slist *,  const char *);
     /// void_t               curl_slist_free_all(struct curl_slist *);
 
-	static
-    std::tstring_t strError(cCURLcode code);
+    std::tstring_t strError() const;
 
 ///@name Callbacks
 ///@{
