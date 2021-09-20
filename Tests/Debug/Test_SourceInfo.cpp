5221d5a208a1b8fc5cd0c0d6ef2101a91481f673
skynowa <skynowa@gmail.com>
2021-09-20 16:36:49 +0300

    Test_SourceInfo.cpp - fix warning(s)

--- Tests/Debug/Test_SourceInfo.cpp
+++ Tests/Debug/Test_SourceInfo.cpp
@@ -19,13 +19,13 @@ Test_SourceInfo::unit()
         std::csize_t    var2 {200};
         std::ctstring_t op = xT("<");
 
-        cSourceInfoData sourceInfoData
+        cSourceInfoData _sourceInfoData
         {
             xFILE, xLINE, xFUNCTION, xCOUNTER,
             xLEX_TO_STR(var1), xLEX_TO_STR(var2), std::to_string(var1), std::to_string(var2), op
         };
 
-        SourceInfo _sourceInfo(sourceInfoData);
+        SourceInfo _sourceInfo(_sourceInfoData);
         xTEST(!_sourceInfo.data().filePath.empty());
         xTEST_GR(_sourceInfo.data().lineNum, 0UL);
         xTEST(!_sourceInfo.data().funcName.empty());
