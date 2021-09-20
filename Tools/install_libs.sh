45941217afdc47ab85c91881f629fe51493472c3
skynowa <skynowa@gmail.com>
2021-09-20 15:24:14 +0300

    install_libs.sh - Clang - impl

--- Tools/install_libs.sh
+++ Tools/install_libs.sh
@@ -17,7 +17,14 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	if [[ "${IS_COMPILER_CLANG}" == "1" ]]; then
 		sudo apt-get install -y --no-install-recommends \
 			libc++-dev \
+			libx11-xcb-dev \
+			libssl-dev \
+			libssh2-1-dev \
+			default-libmysqlclient-dev \
+			\
 			libcurl4-openssl-dev
+			# libcurl4-nss-dev
+			# libcurl4-gnutls-dev
 	else
 		sudo apt-get install -y --no-install-recommends \
 			libx11-xcb-dev \
