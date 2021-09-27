e7a0415e79a16078b2afcd81beab02c0ede288b2
skynowa <skynowa@gmail.com>
2021-09-27 20:44:07 +0300

    curl_install.sh - Win build - impl

---
+++ Tools/curl_install.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+#
+# Install AMP C++
+#
+
+
+CURL_VERSION="7.79.1"
+CURL_ARCHIVE="curl.tar.gz"
+
+echo ""
+echo "[Download...]"
+curl -L https://github.com/curl/curl/releases/download/curl-7_79_1/curl-${CURL_VERSION}.tar.gz -o ${CURL_ARCHIVE}
+
+
+echo ""
+echo "[Unpack...]"
+tar zxpf ${CURL_ARCHIVE}
+rm ${CURL_ARCHIVE}
+
+echo ""
+echo "[Build...]"
+
+mkdir curl-${CURL_VERSION}_build
+cd curl-${CURL_VERSION}_build
+cmake ../curl-${CURL_VERSION}
+
+echo ""
+echo "[Install...]"
+sudo make install
+make clean
