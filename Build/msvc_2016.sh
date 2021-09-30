061470b5fac285b5d2e2876e96dfa918fc33b2d2
skynowa <skynowa@gmail.com>
2021-09-30 10:51:12 +0300

    msvc_2016.sh - Win (MSVC) - build

---
+++ Build/msvc_2016.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# \file  eclipse.sh
+# \brief CMake configure project
+#
+
+
+# vars
+PROJECT_NAME="xLib"
+DIR_PROJECT="../${PROJECT_NAME}"
+DIR_BUILD="../../${PROJECT_NAME}_eclipse"
+GENERATOR="-G 'Visual Studio 16 2019' -A x64"
+
+# prepare
+mkdir -p $DIR_BUILD
+cd $DIR_BUILD
+
+# build
+cmake \
+	$GENERATOR \
+	$DIR_PROJECT
