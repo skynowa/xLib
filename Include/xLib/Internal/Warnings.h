96419f7724ec67f7ae1c26c4f6e4ed5d0d725b6f
skynowa <skynowa@gmail.com>
2021-10-15 00:05:37 +0300

    Warnings.h - fix build

--- Include/xLib/Internal/Warnings.h
+++ Include/xLib/Internal/Warnings.h
@@ -13,6 +13,7 @@
     #pragma warning(disable: 4996) // function or variable may be unsafe (deprecated)
     #pragma warning(disable: 4355) // 'this' : used in base member initializer list
     #pragma warning(disable: 4101) // 'e' : unreferenced local variable
+	#pragma warning(disable: 4312) // 'type cast': conversion from 'xl::int_t' to 'T' of greater size
 #elif xCOMPILER_CLANG
     // off
 #elif xCOMPILER_GNUC
