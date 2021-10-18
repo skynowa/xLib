ef7a9e499b57c71d35049f49e418813d96cb222f
skynowa <skynowa@gmail.com>
2021-10-19 00:32:09 +0300

    Config.cmake (+4) - CMAKE_***_LINKER_FLAGS_STRING - fix

--- Build/makefile.sh
+++ Build/makefile.sh
@@ -6,7 +6,7 @@ set -ex
 # vars
 PROJECT_NAME="xLib"
 DIR_PROJECT="../${PROJECT_NAME}"
-DIR_BUILD="../../${PROJECT_NAME}_makefile"
+DIR_BUILD="../../${PROJECT_NAME}_build"
 ECLIPSE_VERSION="4.17" # 2020-09
 
 # prepare
