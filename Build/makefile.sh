d9bde927366ba2e6239010f81e43b5aa590d1d94
skynowa <skynowa@gmail.com>
2021-10-19 00:41:26 +0300

    eclipse.sh (+3) - review

--- Build/makefile.sh
+++ Build/makefile.sh
@@ -8,6 +8,7 @@ PROJECT_NAME="xLib"
 DIR_PROJECT="../${PROJECT_NAME}"
 DIR_BUILD="../../${PROJECT_NAME}_build"
 ECLIPSE_VERSION="4.17" # 2020-09
+GENERATOR="Unix Makefiles"
 
 # prepare
 mkdir -p $DIR_BUILD
@@ -15,6 +16,6 @@ cd $DIR_BUILD
 
 # build
 cmake \
-	-G"Unix Makefiles" \
+	-G${GENERATOR} \
 	-D_ECLIPSE_VERSION=${ECLIPSE_VERSION} \
 	$DIR_PROJECT
