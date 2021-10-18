e3b08d86d321330dbe9ce913fa72ccfff4fc6beb
skynowa <skynowa@gmail.com>
2021-10-19 01:53:09 +0300

    makefile.sh (+1) [9+/2-] - comment(s)

--- Build/android_eclipse.sh
+++ Build/android_eclipse.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#
+# \file  android_eclipse.sh
+# \brief CMake configure project
+#
 
 
 # vars
@@ -12,6 +16,7 @@ GENERATOR="Eclipse CDT4 - Unix Makefiles"
 mkdir -p ${DIR_BUILD}
 cd ${DIR_BUILD}
 
+# build
 cmake \
 	-G"${GENERATOR}" \
 	-D_ECLIPSE_VERSION=${ECLIPSE_VERSION} \
