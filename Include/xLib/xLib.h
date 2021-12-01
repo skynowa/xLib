8c07eb7dc394c3551f338d56f2f0685025381bdd
skynowa <skynowa@gmail.com>
2021-12-01 14:18:33 +0200

    ICompare.h (+1) [81+] - implementation

--- Include/xLib/xLib.h
+++ Include/xLib/xLib.h
@@ -37,6 +37,7 @@
 #include <xLib/Core/DateTime.h>
 
 // Interface
+#include <xLib/Interface/ICompare.h>
 #include <xLib/Interface/IData.h>
 #include <xLib/Interface/IHandle.h>
 #include <xLib/Interface/ILastError.h>
