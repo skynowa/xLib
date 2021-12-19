bde351ff47cc7c392501083d865c3b1c82927b08
skynowa <skynowa@gmail.com>
2021-12-19 05:50:12 +0200

    Db.cpp [9+/9-] - fix

--- Include/xLib/Db/MySql/Db.cpp
+++ Include/xLib/Db/MySql/Db.cpp
@@ -34,20 +34,19 @@ Db::isExists()
 {
     bool_t bRv {};
 
-    Options options = _options;
-    options.db = {};
-
-    Connection conn(options);
+    Connection conn(_options);
     {
         bRv = conn.get().isValid();
         xCHECK_RET(!bRv, false);
 
         conn.connect();
 
+        std::tstring_t sql = Format::str(
+            xT("SELECT count(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{}'"),
+			_options.db);
+
         Query query(conn);
-        query.exec(xT("SELECT IF (EXISTS(SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA "
-               "WHERE SCHEMA_NAME = '%s'), 'true', 'false')"),
-			   options.db.c_str());
+        query.exec(sql);
     }
 
     {
@@ -60,8 +59,9 @@ Db::isExists()
         std::vector<std::vec_tstring_t> rows;
         result.fetchRows(&rows);
         xTEST_EQ(rows.size(), static_cast<size_t>(1));
-        xCHECK_RET(StringCI::compare(xT("false"), rows[0][0]), false);
-        xTEST_EQ(StringCI::compare(xT("true"), rows[0][0]), true);
+        xCHECK_RET(rows[0][0] == xT("0"), false);
+
+        xTEST_EQ(rows[0][0], "1");
     }
 
     return true;
