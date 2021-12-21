9d3016bfa9cfdee258cdb43ef4c762f6326797a3
skynowa <skynowa@gmail.com>
2021-12-21 07:30:02 +0200

    StoreResult.cpp [6+] - doc(s)

--- Include/xLib/Db/MySql/StoreResult.cpp
+++ Include/xLib/Db/MySql/StoreResult.cpp
@@ -24,6 +24,12 @@ StoreResult::StoreResult(
    /**
 	* Retrieves all the rows immediately (small result)
 	*
+	* \return NULL if:
+    *
+	* - There was a malloc() failure (for example, if the result set was too large).
+	* - The data could not be read (an error occurred on the connection).
+	* - The query returned no data (for example, it was an INSERT, UPDATE, or DELETE).
+	*
 	* \see https://dev.mysql.com/doc/c-api/8.0/en/null-mysql-store-result.html
 	*      https://dev.mysql.com/doc/c-api/8.0/en/mysql-field-count.html
 	*/
