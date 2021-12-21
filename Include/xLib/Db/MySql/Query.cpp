71245eb4a54aaf47b3bbe3f7b7aa22fea4694299
skynowa <skynowa@gmail.com>
2021-12-21 02:12:23 +0200

    Query.cpp (+1) [40+/39-] - review

--- Include/xLib/Db/MySql/Query.cpp
+++ Include/xLib/Db/MySql/Query.cpp
@@ -29,41 +29,6 @@ Query::Query(
     xTEST(_conn.get().isValid());
 }
 //-------------------------------------------------------------------------------------------------
-/**
- * If the ANSI_QUOTES SQL mode is enabled:
- *
- * string literals can be quoted only within single quotation marks because
- * a string quoted within double quotation marks is interpreted as an identifier.
- */
-std::tstring_t
-Query::escape(
-	std::ctstring_t &a_sqlValue	///< SQL string value
-) const
-{
-	if ( a_sqlValue.empty() ) {
-		return Const::sqm() + Const::sqm();
-	}
-
-	if (a_sqlValue == xT("NULL")) {
-	   /**
-		* If the argument is NULL:
-		*
-		* the return value is the word “NULL” without enclosing single quotation marks
-		*/
-		return a_sqlValue;
-	}
-
-	std::tstring_t sRv(a_sqlValue.size() * 2 + 1, xT('\0'));
-
-	culong_t quotedSize = ::mysql_real_escape_string_quote(_conn.get().get(), &sRv[0],
-		a_sqlValue.data(), static_cast<ulong_t>(a_sqlValue.size()), Const::sqmA()[0]);
-	xTEST_GR_MSG(quotedSize, 0UL, Error(_conn).str());
-
-	sRv.resize(quotedSize * sizeof(std::tstring_t::value_type));
-
-	return sRv;
-}
-//-------------------------------------------------------------------------------------------------
 void_t
 Query::exec(
 	std::ctstring_t &a_sql
@@ -97,6 +62,41 @@ Query::exec(
 	}
 
 	exec(sql);
+}
+//-------------------------------------------------------------------------------------------------
+/**
+ * If the ANSI_QUOTES SQL mode is enabled:
+ *
+ * string literals can be quoted only within single quotation marks because
+ * a string quoted within double quotation marks is interpreted as an identifier.
+ */
+std::tstring_t
+Query::escape(
+	std::ctstring_t &a_sqlValue	///< SQL string value
+) const
+{
+	if ( a_sqlValue.empty() ) {
+		return Const::sqm() + Const::sqm();
+	}
+
+	if (a_sqlValue == xT("NULL")) {
+	   /**
+		* If the argument is NULL:
+		*
+		* the return value is the word “NULL” without enclosing single quotation marks
+		*/
+		return a_sqlValue;
+	}
+
+	std::tstring_t sRv(a_sqlValue.size() * 2 + 1, xT('\0'));
+
+	culong_t quotedSize = ::mysql_real_escape_string_quote(_conn.get().get(), &sRv[0],
+		a_sqlValue.data(), static_cast<ulong_t>(a_sqlValue.size()), Const::sqmA()[0]);
+	xTEST_GR_MSG(quotedSize, 0UL, Error(_conn).str());
+
+	sRv.resize(quotedSize * sizeof(std::tstring_t::value_type));
+
+	return sRv;
 }
 //-------------------------------------------------------------------------------------------------
 
