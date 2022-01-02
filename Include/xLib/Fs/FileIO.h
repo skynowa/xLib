e5670dcefed990fe44a35d31ee85403a97add6a1
skynowa <skynowa@gmail.com>
2022-01-03 00:19:45 +0200

    Pipe.h (+2) [2-] - [Win] build - try fix

--- Include/xLib/Fs/FileIO.h
+++ Include/xLib/Fs/FileIO.h
@@ -8,7 +8,6 @@
 
 #include <xLib/Core/Core.h>
 #include <xLib/Core/Handle.h>
-#include <xLib/Core/HandlePolicy.h>
 //-------------------------------------------------------------------------------------------------
 namespace xl::fs
 {
