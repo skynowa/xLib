a6017281a1b9697a4a365c48052c9f5ff8cf2d30
skynowa <skynowa@gmail.com>
2022-01-03 23:59:22 +0200

    Xml.cpp [1+/1-] - [Win] build - try fix

--- Include/xLib/Package/Xml.cpp
+++ Include/xLib/Package/Xml.cpp
@@ -18,7 +18,7 @@
 #include <xLib/Debug/Debugger.h>
 #include <xLib/Test/Test.h>
 
-#pragma comment(lib,"libxml2")
+#pragma comment(lib, "libxml2_a")
 //-------------------------------------------------------------------------------------------------
 namespace
 {
