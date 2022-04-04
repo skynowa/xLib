deea22e6936350c3f5d27cf72aab92c839338922
skynowa <skynowa@gmail.com>
2022-04-04 14:18:24 +0300

    CMakeLists.txt (+2) [58+/5-] - Tests - try fix (split)

--- Build/tests.sh
+++ Build/tests.sh
@@ -3,10 +3,28 @@
 
 # vars
 PROJECT_NAME="xLib"
-DIR_BUILD="../../${PROJECT_NAME}_build"
+DIR_PROJECT="../${PROJECT_NAME}/Tests"
+DIR_BUILD="../../${PROJECT_NAME}_tests"
+ECLIPSE_VERSION="4.17" # 2020-09
+GENERATOR="Eclipse CDT4 - Unix Makefiles"
+
+DIR_BUILD="../../${PROJECT_NAME}_tests"
 JOBS_NUM=$(($(nproc) * 2))
 
 echo "Jobs: $JOBS_NUM"
 
-cd $DIR_BUILD
-ctest -C Debug -j$JOBS_NUM --output-on-failure
+# prepare
+mkdir -p ${DIR_BUILD}
+cd ${DIR_BUILD}
+
+# Configure
+cmake \
+	-G"${GENERATOR}" \
+	-D_ECLIPSE_VERSION=${ECLIPSE_VERSION} \
+	$DIR_PROJECT
+
+# Build
+cmake --build . -- -j${JOBS_NUM}
+
+# Run
+ctest -C Debug -j${JOBS_NUM} --output-on-failure
