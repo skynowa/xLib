5af7bc54d193603a545097242f7d98d09f35ee30
skynowa <skynowa@gmail.com>
2022-04-06 16:28:07 +0300

    main.yml (+5) [12+/10-] - [Win] 32 bit - impl

--- Build/msvc_2022.sh
+++ Build/msvc_2022.sh
@@ -10,6 +10,7 @@ PROJECT_NAME="xLib"
 DIR_PROJECT="../${PROJECT_NAME}"
 DIR_BUILD="../../${PROJECT_NAME}_build"
 GENERATOR="Visual Studio 17 2022"
+ARCH=Win32	### x64
 
 # prepare
 mkdir -p ${DIR_BUILD}
@@ -18,5 +19,5 @@ cd ${DIR_BUILD}
 # build
 cmake \
 	-G"${GENERATOR}" \
-	-A x64 \
+	-A ${ARCH} \
 	${DIR_PROJECT}
