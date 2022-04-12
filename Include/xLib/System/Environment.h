fc1e7129ffc4e0664dcd02bea9ea99d01a400b68
skynowa <skynowa@gmail.com>
2022-04-12 19:26:31 +0300

    Environment.h [18+] - todo(s)

--- Include/xLib/System/Environment.h
+++ Include/xLib/System/Environment.h
@@ -72,3 +72,21 @@ xPLATFORM_IMPL:
 
 } // namespace
 //-------------------------------------------------------------------------------------------------
+/**
+ * TODO:
+ *
+ * [ ] Dll path (LD_LIBRARY_PATH)
+ *
+ * Imports - https://docs.conan.io/en/latest/using_packages/conanfile_txt.html#imports-txt
+ * There are some differences between shared libraries on Linux (*.so), Windows (*.dll) and
+ * MacOS (*.dylib). The shared libraries must be located in a folder where they can be found,
+ * either by the linker, or by the OS runtime.
+ * You can add the libraries’ folders to the path (LD_LIBRARY_PATH environment variable in Linux,
+ * DYLD_LIBRARY_PATH in OSX, or system PATH in Windows), or copy those shared libraries to
+ * some system folder where they can be found by the OS. But these operations are only related
+ * to the deployment or installation of apps; they are not relevant during development.
+ * Conan is intended for developers, so it avoids such manipulation of the OS environment.
+ * In Windows and OSX, the simplest approach is to copy the shared libraries to the executable
+ * folder, so they are found by the executable, without having to modify the path.
+ */
+//-------------------------------------------------------------------------------------------------
