977bbe8f1fb869a280b095ee131aa7f5f2b3ebb6
skynowa <skynowa@gmail.com>
2022-04-12 21:32:04 +0300

    build.sh (+3) [11+/1-] - Jobs num - impl

--- Build/build.sh
+++ Build/build.sh
@@ -5,10 +5,14 @@
 # vars
 PROJECT_NAME="xLib"
 DIR_BUILD="../../${PROJECT_NAME}_build"
+JOBS_NUM=$(($(nproc) * 2))
+
+echo "Jobs: $JOBS_NUM"
 
 # prepare
 mkdir -p ${DIR_BUILD}
 cd ${DIR_BUILD}
 
 # build
-cmake --build . --target ALL_BUILD --config Release
+cmake --build . --target ALL_BUILD --config Release --parallel ${JOBS_NUM}
+# cmake --build . --target xLib_static --config Release -- -j${JOBS_NUM}
