dc38228351db95671656341f15d7d13e7382a1ab
skynowa <skynowa@gmail.com>
2022-04-14 21:33:43 +0300

    gen_eclipse.sh (+2) [4+/2-] - review

--- Build/gen_tests_eclipse.sh
+++ Build/gen_tests_eclipse.sh
@@ -8,6 +8,7 @@ DIR_BUILD="../../${PROJECT_NAME}_build"
 
 GENERATOR="Eclipse CDT4 - Unix Makefiles"
 ECLIPSE_VERSION="4.22.0"
+BUILD_TYPE=Release
 
 # prepare
 mkdir -p ${DIR_BUILD}
@@ -16,5 +17,5 @@ cd ${DIR_BUILD}
 # generate
 cmake \
 	-G "${GENERATOR}" -D CMAKE_ECLIPSE_VERSION=${ECLIPSE_VERSION} \
-	-D CMAKE_BUILD_TYPE=Release \
+	-D CMAKE_BUILD_TYPE=${BUILD_TYPE} \
 	${DIR_PROJECT}
