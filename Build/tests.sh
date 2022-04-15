d05edd9bb81c413c047d375e0ec49aeec851e9f9
skynowa <skynowa@gmail.com>
2022-04-15 13:00:04 +0300

    tests.sh [1+/1-] - test(s)

--- Build/tests.sh
+++ Build/tests.sh
@@ -20,7 +20,7 @@ UNAME=$(uname)
 
 if   [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
 	echo "OS: Windows"
-	cmake --build . --target ALL_BUILD --config Release --parallel ${JOBS_NUM} --verbose
+	cmake --build . --target ALL_BUILD --config Release --parallel ${JOBS_NUM} # --verbose
 elif [ "$UNAME" == "Linux" ] ; then
 	echo "OS: Linux"
 	# cmake --build . --target xLib_tests --config Release -- -j${JOBS_NUM}
