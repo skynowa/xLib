08857da31f3a0c5235a60df3940ff4fad0142a28
skynowa <skynowa@gmail.com>
2022-04-20 01:32:54 +0300

    build.sh (+2) [6+/7-] - tests - fix

--- Build/build.sh
+++ Build/build.sh
@@ -21,12 +21,11 @@ UNAME=$(uname)
 if   [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
 	echo "OS: Windows"
 	## cmake --build . --target ALL_BUILD --config Release --parallel ${JOBS_NUM}
-	cmake --build . --target xLib --config Release --parallel ${JOBS_NUM}
-
+	cmake --build . --target ${PROJECT_NAME} --config Release --parallel ${JOBS_NUM}
 elif [ "$UNAME" == "Linux" ] ; then
 	echo "OS: Linux"
-	cmake --build . --target xLib --config Release -- -j${JOBS_NUM}
+	cmake --build . --target ${PROJECT_NAME} --config Release -- -j${JOBS_NUM}
 elif [ "$UNAME" == "Darwin" ] ; then
 	echo "OS: Darwin"
-	cmake --build . --target xLib --config Release -- -j${JOBS_NUM}
+	cmake --build . --target ${PROJECT_NAME} --config Release -- -j${JOBS_NUM}
 fi
