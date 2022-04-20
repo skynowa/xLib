7718680cb86279db5656d759293a6313d2f59b79
skynowa <skynowa@gmail.com>
2022-04-20 16:03:51 +0300

    conan_install_dev.sh [2+/3-] - impl

--- conan_install_dev.sh
+++ conan_install_dev.sh
@@ -5,12 +5,11 @@
 
 
 # vars
-DIR_INSTALL="/home/skynowa/Projects"
-DIR_BUILD="/home/skynowa/Projects/xLib_conan_build"
+DIR_BUILD="/home/skynowa/Projects/xLib_conan"
 
 conan install \
 	conan_file.txt \
-	--install-folder "${DIR_INSTALL}" \
+	--install-folder "${DIR_BUILD}" \
 	--output-folder  "${DIR_BUILD}" \
 	--profile conan_profile_dev.txt \
 	--build=missing
