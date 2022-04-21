ba8ef562a3cd28104264b3bc2a712e58cc92b52f
skynowa <skynowa@gmail.com>
2022-04-21 13:31:02 +0300

    Unit.cpp (+3) [19+/24-] - Test Unit - review

--- Include/xLib/Test/Unit.h
+++ Include/xLib/Test/Unit.h
@@ -64,16 +64,15 @@ public:
 
 ///\name ctors, dtor
 ///\{
-			 Unit();
+			 Unit(const UnitData &data);
 	virtual ~Unit() = 0;
 
+	xNO_DEFAULT_CONSTRUCT(Unit)
 	xNO_COPY_ASSIGN(Unit)
+
+	UnitData &getData();
 ///\}
 
-    UnitData      &getData();
-        ///< get data
-    void_t         setData(cUnitData &data);
-        ///< set data
 	bool_t         isGithubCI() const;
        ///< check Github CI
     void_t         cfg(std::map_tstring_t *values) const;
