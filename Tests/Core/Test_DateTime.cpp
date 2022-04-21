4a9a77e534e5c3f614bd7bc78e39d891a0381aeb
skynowa <skynowa@gmail.com>
2022-04-21 16:07:38 +0300

    Test_DateTime.cpp (+2) [109+/98-] - test(s)

--- Tests/Core/Test_DateTime.cpp
+++ Tests/Core/Test_DateTime.cpp
@@ -30,7 +30,7 @@ Test_DateTime::unit()
 
         // valid data
         {
-            cushort_t valid[][7] =
+            cushort_t valid[][7]
             {
                 // year, month, day, hour, minute, second, msec
                 {2011, 1,  1,  0,  0,  0,  0},
@@ -61,7 +61,7 @@ Test_DateTime::unit()
 
         // non valid data
         {
-            cushort_t nonValid[][7] =
+            cushort_t nonValid[][7]
             {
                 // year, month, day, hour, minute, second, msec
 
@@ -114,8 +114,7 @@ Test_DateTime::unit()
     {
         DateTime datetime;
         m_sRv = datetime.format(xT("%d-%m-%Y %H:%M:%S"));
-
-        xTEST_EQ(std::tstring_t(xT("00-01-0 00:00:00.000")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("00-01-0 00:00:00.000")));
     }
 
     xTEST_CASE("DateTime(const DateTime &)")
@@ -123,39 +122,39 @@ Test_DateTime::unit()
         DateTime datetime1(2010, 7, 8, 3, 15, 6, 111);
         DateTime datetime2(datetime1);
         m_sRv = datetime2.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("08-07-2010 03:15:06.111")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("08-07-2010 03:15:06.111")));
 
         DateTime datetime(2010, 1, 14, 17, 0, 55, 666);
         m_sRv = datetime.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("14-01-2010 17:00:55.666")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("14-01-2010 17:00:55.666")));
     }
 
     xTEST_CASE("DateTime(culonglong_t)")
     {
         DateTime datetime(1000 * 60 * 60);
         m_sRv = datetime.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("00-00-0 01:00:00.000")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("00-00-0 01:00:00.000")));
     }
 
     xTEST_CASE("DateTime(cushort_t, cushort_t, cushort_t, cushort_t)")
     {
         DateTime datetime(12, 20, 37, 555);
         m_sRv = datetime.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("00-00-0 12:20:37.555")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("00-00-0 12:20:37.555")));
     }
 
     xTEST_CASE("DateTime(cushort_t, cushort_t, cushort_t)")
     {
         DateTime datetime(2010, 7, 8);
         m_sRv = datetime.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("08-07-2010 00:00:00.000")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("08-07-2010 00:00:00.000")));
     }
 
     xTEST_CASE("DateTime(cushort_t, cushort_t, cushort_t, cushort_t, cushort_t, cushort_t, cushort_t)")
     {
         DateTime datetime(2010, 8, 18, 14, 0, 5, 777);
         m_sRv = datetime.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("18-08-2010 14:00:05.777")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("18-08-2010 14:00:05.777")));
     }
 
 
@@ -233,11 +232,12 @@ Test_DateTime::unit()
     xTEST_CASE("operator +=")
     {
         DateTime  datetime(0, 30, 0, 0);
-        ulonglong_t msec = (1000 * 60) * 60; // 1 hour
+
+        culonglong_t msec = (1000 * 60) * 60; // 1 hour
 
         datetime = msec;
         m_sRv = datetime.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("00-00-0 01:00:00.000")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("00-00-0 01:00:00.000")));
     }
 
     xTEST_CASE("operator (=, +, -)")
@@ -246,11 +246,11 @@ Test_DateTime::unit()
 
         datetime = DateTime() + DateTime(1000 * 60 * 60);
         m_sRv = datetime.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("00-01-0 01:00:00.000")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("00-01-0 01:00:00.000")));
 
         datetime = datetime - DateTime(1000 * 60 * 60 / 2);
         m_sRv = datetime.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("00-01-0 00:30:00.000")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("00-01-0 00:30:00.000")));
     }
 
     xTEST_CASE("operator +=")
@@ -260,7 +260,7 @@ Test_DateTime::unit()
 
         datetime1 += datetime2;
         m_sRv = datetime1.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("00-00-0 02:30:00.000")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("00-00-0 02:30:00.000")));
     }
 
     xTEST_CASE("operator -=")
@@ -270,7 +270,7 @@ Test_DateTime::unit()
 
         datetime1 -= datetime2;
         m_sRv = datetime1.format(xT("%d-%m-%Y %H:%M:%S"));
-        xTEST_EQ(std::tstring_t(xT("00-00-0 00:20:00.000")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("00-00-0 00:20:00.000")));
     }
 
 
@@ -308,26 +308,28 @@ Test_DateTime::unit()
         // 1
         {
             m_ullRv = DateTime(2010, 1, 14, 17, 0, 55, 666).toMsec();
-            xTEST_EQ(62522902855666ULL, m_ullRv);
+            xTEST_EQ(m_ullRv, 62522902855666ULL);
         }
 
         // 2
         {
-            culonglong_t msecs[] = {
+            culonglong_t msecs[]
+            {
                 12234567890ULL, 2234567890ULL, 32345657890ULL, 4294567890ULL, 5234567890ULL,
                 63565474567ULL, 7354234523ULL, 83451467547ULL, 9234436577ULL, 5354362463ULL
             };
 
             for (size_t i = 0; i < xARRAY_SIZE(msecs); ++ i) {
                 m_ullRv = DateTime(msecs[i]).toMsec();
-                xTEST_EQ(msecs[i], m_ullRv);
+                xTEST_EQ(m_ullRv, msecs[i]);
             }
         }
     }
 
     xTEST_CASE("set")
     {
-        DateTime  datetime;
+        DateTime datetime;
+
         ulonglong_t datetimeValue = 1000 * 60 * 60; // 1 hour
         int_t       year   = 0;
         int_t       month  = 0;
@@ -352,13 +354,13 @@ Test_DateTime::unit()
     xTEST_CASE("dayOfWeek")
     {
         m_iRv = DateTime(2011, 7, 25, 13, 0, 0, 0).dayOfWeek();
-        xTEST_EQ(1, m_iRv);
+        xTEST_EQ(m_iRv, 1);
 
         m_iRv = DateTime(2011, 7, 31, 0, 0, 0, 0).dayOfWeek();
-        xTEST_EQ(0, m_iRv);
+        xTEST_EQ(m_iRv, 0);
 
         m_iRv = DateTime(2011, 3, 18, 0, 0, 0, 0).dayOfWeek();
-        xTEST_EQ(5, m_iRv);
+        xTEST_EQ(m_iRv, 5);
     }
 
     /*******************************************************************************
@@ -373,7 +375,7 @@ Test_DateTime::unit()
             DateTime datetimeNow;
 
             m_sRv = datetimeNow.current().format(xT("%d-%m-%Y %H:%M:%S"));
-            xTEST_DIFF(std::tstring_t(xT("00-00-0000 0:00:00:000")), m_sRv);
+            xTEST_DIFF(m_sRv, std::tstring_t(xT("00-00-0000 0:00:00:000")));
         }
 
         // 2
@@ -404,9 +406,9 @@ Test_DateTime::unit()
                 m_iRv = DateTime::daysInMonth(y, m);
 
                 if (2 == m && DateTime::isLeapYear(y)) {
-                    xTEST_EQ(29, m_iRv);
+                    xTEST_EQ(m_iRv, 29);
                 } else {
-                    int_t daysOfMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+                    cint_t daysOfMonth[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
                     xTEST_EQ(m_iRv, daysOfMonth[m]);
                 }
@@ -418,9 +420,7 @@ Test_DateTime::unit()
     {
         // valid
         {
-            cint_t valid[] = {
-                1812, 1828, 1904
-            };
+            cint_t valid[] {1812, 1828, 1904};
 
             for (size_t i = 0; i < xARRAY_SIZE(valid); ++ i) {
                 m_bRv = DateTime::isLeapYear(valid[i]);
@@ -430,9 +430,7 @@ Test_DateTime::unit()
 
         // non valid
         {
-            cint_t nonValid[] = {
-                1813, 1829, 1905
-            };
+            cint_t nonValid[] {1813, 1829, 1905};
 
             for (size_t i = 0; i < xARRAY_SIZE(nonValid); ++ i) {
                 m_bRv = DateTime::isLeapYear(nonValid[i]);
@@ -444,18 +442,18 @@ Test_DateTime::unit()
     xTEST_CASE("fileToInt64")
     {
     #if xENV_WIN
-        const FILETIME time = {100, 200};
+        const FILETIME time {100, 200};
 
         m_ullRv = DateTime::fileToInt64(time);
-        xTEST_EQ(m_ullRv, 0);   // TODO: test - fix
+        xTEST_EQ(m_ullRv, 0ULL);   // TODO: test - fix
     #endif
     }
 
     xTEST_CASE("unixToFile")
     {
     #if xENV_WIN
-        const time_t unixTime = 1000;
-        FILETIME     fileTime = {0};
+        const time_t unixTime {1000};
+        FILETIME     fileTime {};
 
         DateTime::unixToFile(unixTime, &fileTime);
     #endif
@@ -464,11 +462,11 @@ Test_DateTime::unit()
     xTEST_CASE("fileToUnix")
     {
     #if xENV_WIN
-        time_t   unixTime = 0;
-        FILETIME fileTime = {0};
+        time_t   unixTime {};
+        FILETIME fileTime {};
 
         unixTime = DateTime::fileToUnix(fileTime);
-        xTEST_EQ(static_cast<time_t>( -11644473600 ), unixTime);
+        xTEST_EQ(unixTime, static_cast<time_t>( -11644473600 ));
     #endif
     }
 
@@ -482,77 +480,77 @@ Test_DateTime::unit()
     {
 		// isShortName = false
 		m_sRv = DateTime::monthStr(1, false);
-		xTEST_EQ(std::tstring_t(xT("January")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("January")));
 
 		m_sRv = DateTime::monthStr(2, false);
-		xTEST_EQ(std::tstring_t(xT("February")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("February")));
 
 		m_sRv = DateTime::monthStr(3, false);
-		xTEST_EQ(std::tstring_t(xT("March")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("March")));
 
 		m_sRv = DateTime::monthStr(4, false);
-		xTEST_EQ(std::tstring_t(xT("April")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("April")));
 
 		m_sRv = DateTime::monthStr(5, false);
-		xTEST_EQ(std::tstring_t(xT("May")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("May")));
 
 		m_sRv = DateTime::monthStr(6, false);
-		xTEST_EQ(std::tstring_t(xT("June")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("June")));
 
 		m_sRv = DateTime::monthStr(7, false);
-		xTEST_EQ(std::tstring_t(xT("July")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("July")));
 
 		m_sRv = DateTime::monthStr(8, false);
-		xTEST_EQ(std::tstring_t(xT("August")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("August")));
 
 		m_sRv = DateTime::monthStr(9, false);
-		xTEST_EQ(std::tstring_t(xT("September")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("September")));
 
 		m_sRv = DateTime::monthStr(10, false);
-		xTEST_EQ(std::tstring_t(xT("October")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("October")));
 
 		m_sRv = DateTime::monthStr(11, false);
-		xTEST_EQ(std::tstring_t(xT("November")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("November")));
 
 		m_sRv = DateTime::monthStr(12, false);
-		xTEST_EQ(std::tstring_t(xT("December")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("December")));
 
 		// isShortName = true
 		m_sRv = DateTime::monthStr(1, true);
-		xTEST_EQ(std::tstring_t(xT("Jan")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Jan")));
 
 		m_sRv = DateTime::monthStr(2, true);
-		xTEST_EQ(std::tstring_t(xT("Feb")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Feb")));
 
 		m_sRv = DateTime::monthStr(3, true);
-		xTEST_EQ(std::tstring_t(xT("Mar")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Mar")));
 
 		m_sRv = DateTime::monthStr(4, true);
-		xTEST_EQ(std::tstring_t(xT("Apr")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Apr")));
 
 		m_sRv = DateTime::monthStr(5, true);
-		xTEST_EQ(std::tstring_t(xT("May")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("May")));
 
 		m_sRv = DateTime::monthStr(6, true);
-		xTEST_EQ(std::tstring_t(xT("Jun")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Jun")));
 
 		m_sRv = DateTime::monthStr(7, true);
-		xTEST_EQ(std::tstring_t(xT("Jul")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Jul")));
 
 		m_sRv = DateTime::monthStr(8, true);
-		xTEST_EQ(std::tstring_t(xT("Aug")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Aug")));
 
 		m_sRv = DateTime::monthStr(9, true);
-		xTEST_EQ(std::tstring_t(xT("Sep")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Sep")));
 
 		m_sRv = DateTime::monthStr(10, true);
-		xTEST_EQ(std::tstring_t(xT("Oct")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Oct")));
 
 		m_sRv = DateTime::monthStr(11, true);
-		xTEST_EQ(std::tstring_t(xT("Nov")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Nov")));
 
 		m_sRv = DateTime::monthStr(12, true);
-		xTEST_EQ(std::tstring_t(xT("Dec")), m_sRv);
+		xTEST_EQ(m_sRv, std::tstring_t(xT("Dec")));
     }
 
     xTEST_CASE("monthNum")
@@ -574,23 +572,23 @@ Test_DateTime::unit()
 
         for (size_t i = 0; i < xARRAY_SIZE(months); ++ i) {
             m_iRv = DateTime::monthNum(months[i][0], false);
-            xTEST_EQ(int_t(i), m_iRv);
+            xTEST_EQ(m_iRv, int_t(i));
 
             m_iRv = DateTime::monthNum(months[i][1], true);
-            xTEST_EQ(int_t(i), m_iRv);
+            xTEST_EQ(m_iRv, int_t(i));
 
             if (xT("May") == months[i][0] || xT("May") == months[i][1]) {
                 m_iRv = DateTime::monthNum(months[i][0], true);
-                xTEST_EQ(int_t(i), m_iRv);
+                xTEST_EQ(m_iRv, int_t(i));
 
                 m_iRv = DateTime::monthNum(months[i][1], false);
-                xTEST_EQ(int_t(i), m_iRv);
+                xTEST_EQ(m_iRv, int_t(i));
             } else {
                 m_iRv = DateTime::monthNum(months[i][0], true);
-                xTEST_EQ(- 1, m_iRv);
+                xTEST_EQ(m_iRv, - 1);
 
                 m_iRv = DateTime::monthNum(months[i][1], false);
-                xTEST_EQ(- 1, m_iRv);
+                xTEST_EQ(m_iRv, - 1);
             }
         }
     }
@@ -599,40 +597,53 @@ Test_DateTime::unit()
     {
         // isShortName = false
         m_sRv = DateTime::weekDayStr(0, false);
-        xTEST_EQ(std::tstring_t(xT("Sunday")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Sunday")));
+
         m_sRv = DateTime::weekDayStr(1, false);
-        xTEST_EQ(std::tstring_t(xT("Monday")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Monday")));
+
         m_sRv = DateTime::weekDayStr(2, false);
-        xTEST_EQ(std::tstring_t(xT("Tuesday")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Tuesday")));
+
         m_sRv = DateTime::weekDayStr(3, false);
-        xTEST_EQ(std::tstring_t(xT("Wednesday")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Wednesday")));
+
         m_sRv = DateTime::weekDayStr(4, false);
-        xTEST_EQ(std::tstring_t(xT("Thursday")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Thursday")));
+
         m_sRv = DateTime::weekDayStr(5, false);
-        xTEST_EQ(std::tstring_t(xT("Friday")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Friday")));
+
         m_sRv = DateTime::weekDayStr(6, false);
-        xTEST_EQ(std::tstring_t(xT("Saturday")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Saturday")));
 
         // isShortName = true
         m_sRv = DateTime::weekDayStr(0, true);
-        xTEST_EQ(std::tstring_t(xT("Sun")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Sun")));
+
         m_sRv = DateTime::weekDayStr(1, true);
-        xTEST_EQ(std::tstring_t(xT("Mon")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Mon")));
+
         m_sRv = DateTime::weekDayStr(2, true);
-        xTEST_EQ(std::tstring_t(xT("Tue")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Tue")));
+
         m_sRv = DateTime::weekDayStr(3, true);
-        xTEST_EQ(std::tstring_t(xT("Wed")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Wed")));
+
         m_sRv = DateTime::weekDayStr(4, true);
-        xTEST_EQ(std::tstring_t(xT("Thu")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Thu")));
+
         m_sRv = DateTime::weekDayStr(5, true);
-        xTEST_EQ(std::tstring_t(xT("Fri")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Fri")));
+
         m_sRv = DateTime::weekDayStr(6, true);
-        xTEST_EQ(std::tstring_t(xT("Sat")), m_sRv);
+        xTEST_EQ(m_sRv, std::tstring_t(xT("Sat")));
     }
 
     xTEST_CASE("weekDayNum")
     {
-        std::ctstring_t days[7][2] = {
+        std::ctstring_t days[7][2]
+        {
             { xT("Monday"),     xT("Mon") },
             { xT("Tuesday"),    xT("Tue") },
             { xT("Wednesday"),  xT("Wed") },
@@ -644,16 +655,16 @@ Test_DateTime::unit()
 
         for (size_t i = 0; i < xARRAY_SIZE(days); ++ i) {
             m_iRv = DateTime::weekDayNum(days[i][0], false);
-            xTEST_EQ((int_t)i, m_iRv);
+            xTEST_EQ(m_iRv, (int_t)i);
 
             m_iRv = DateTime::weekDayNum(days[i][1], true);
-            xTEST_EQ((int_t)i, m_iRv);
+            xTEST_EQ(m_iRv, (int_t)i);
 
             m_iRv = DateTime::weekDayNum(days[i][0], true);
-            xTEST_EQ(- 1, m_iRv);
+            xTEST_EQ(m_iRv, - 1);
 
             m_iRv = DateTime::weekDayNum(days[i][1], false);
-            xTEST_EQ(- 1, m_iRv);
+            xTEST_EQ(m_iRv, - 1);
         }
     }
 
