7e4651d62f40b94e4518aca70aa38f79caf43319
skynowa <skynowa@gmail.com>
2022-12-23 22:36:37 +0200

    Mac - fix

---
+++ conan_install_mac_dev.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Dev: skynowa-pc
+#
+
+
+# vars
+PROJECT_NAME="xLib"
+DIR_BUILD="../${PROJECT_NAME}_conan"
+
+conan install \
+	conan_file.txt \
+	--install-folder="${DIR_BUILD}" \
+	--output-folder="${DIR_BUILD}" \
+	--profile=conan_profile_mac_dev.txt \
+	--build=missing \
+	--update
