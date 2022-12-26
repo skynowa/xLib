b5ce6eb424b818c8143b268eaeee496a9a3d3ea5
skynowa <skynowa@gmail.com>
2022-12-26 22:10:02 +0200

    Functions.h [10+] - Apple - fix

--- Include/xLib/Core/Functions.h
+++ Include/xLib/Core/Functions.h
@@ -104,4 +104,14 @@
 #elif xENV_UNIX
 	#define xFILENO ::fileno
 #endif
+
+// Sem API
+#if xENV_APPLE
+int sem_init(sem_t *psem, int flags, unsigned count);
+int sem_destroy(sem_t *psem);
+int sem_post(sem_t *psem);
+int sem_trywait(sem_t *psem);
+int sem_wait(sem_t *psem);
+int sem_timedwait(sem_t *psem, const struct timespec *abstim);
+#endif
 //-------------------------------------------------------------------------------------------------
