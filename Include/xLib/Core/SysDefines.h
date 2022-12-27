b0023302aa61002cbdb45f82285f342873a4ddbb
skynowa <skynowa@gmail.com>
2022-12-27 13:35:04 +0200

    SysDefines.h [2+/2-] - fix

--- Include/xLib/Core/SysDefines.h
+++ Include/xLib/Core/SysDefines.h
@@ -101,10 +101,10 @@
 ///\see  https://stackoverflow.com/questions/5919996/how-to-detect-reliably-mac-os-x-ios-linux-windows-in-c-preprocessor
 ///\{
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(_WIN64) || \
-         defined(__WINDOWS__) || defined(__NT__) || defined(__TOS_WIN__)
+         defined(__WINDOWS__) || defined(__NT__) || defined(__TOS_WIN__) || defined(__CYGWIN__)
     #define xENV_WIN 1
         ///< Windows environment
-#elif defined(__unix__) || defined(__unix) || defined(__APPLE__)
+#elif defined(unix)  || defined(__unix) || defined(__unix__) || defined(__APPLE__)
     #define xENV_UNIX 1
         ///< Unix environment
 
