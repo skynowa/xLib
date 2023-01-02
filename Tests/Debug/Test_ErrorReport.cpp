affc8ccbb41d17fe493da2ed2b0c90a29cb2becd
skynowa <skynowa@gmail.com>
2023-01-03 01:50:10 +0200

    ui-msgbox: Test_ErrorReport.cpp [2+/2-] - remove

--- Tests/Debug/Test_ErrorReport.cpp
+++ Tests/Debug/Test_ErrorReport.cpp
@@ -18,13 +18,13 @@ Test_ErrorReport::unit()
         culong_t val1 {10};
         culong_t val2 {20};
 
-        ulong_t          nativeError    { NativeError::get() };
+        ulong_t          nativeError_    { NativeError::get() };
         cSourceInfoData  sourceInfoData {xFILE, xLINE, xFUNCTION, xCOUNTER,
             xLEX_TO_STR(val1), xLEX_TO_STR(val), std::to_string(val1), std::to_string(val2), xT("==")};
         SourceInfo       sourceInfo(sourceInfoData);
         std::ctstring_t &stackTrace     = StackTrace().str();
 
-        ErrorReport report(ErrorReport::Type::Stdout, nativeError, sourceInfo, stackTrace, xT(""));
+        ErrorReport report(ErrorReport::Type::Stdout, nativeError_, sourceInfo, stackTrace, {});
         m_sRv = report.str();
         xTEST(!m_sRv.empty());
     }
