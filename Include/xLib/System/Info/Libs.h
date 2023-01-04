cd1b491e33c77f3b3e08a4aa62ea3ba0a55152c2
skynowa <skynowa@gmail.com>
2023-01-04 17:08:52 +0200

    system-info: Libs_unix.inl (+3) [5+/5-] - rename

--- Include/xLib/System/Info/Libs.h
+++ Include/xLib/System/Info/Libs.h
@@ -26,7 +26,7 @@ public:
 #if xENV_UNIX
 	std::tstring_t glibcVersion() const;
 		///< get glibc version available on the system
-	std::tstring_t libPthreadVersion() const;
+	std::tstring_t pthreadVersion() const;
 		///< get POSIX implementation supplied by C library ("NPTL 2.3.4" or "linuxthreads-0.10")
 #endif
 
