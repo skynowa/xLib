d9f344f96d15d6d36e9d215f596ca8357e911ad0
skynowa <skynowa@gmail.com>
2023-01-08 19:16:49 +0200

    handle-policy: HandlePolicyType.h [13+/13-] - revert

--- Include/xLib/Core/HandlePolicyType.h
+++ Include/xLib/Core/HandlePolicyType.h
@@ -10,19 +10,19 @@
 namespace xl::core
 {
 
-enum class HandlePolicyType
-    /// error handle type
-{
-    hvNative = 0,       ///< like "null"
-    hvNativeInvalid,    ///< like "invalid" (-1)
-    hvDll,              ///< DLL
-    hvStdFile,          ///< like nullptr
-    hvMySqlConn,        ///< MySQL connection
-    hvMySqlResult,      ///< MySQL result
-    hvCurl,             ///< CURL
-    hvFindDir,          ///< Dir find
-    hvSocket            ///< Socket
-};
+//enum class HandlePolicyType
+//    /// error handle type
+//{
+//    hvNative = 0,       ///< like "null"
+//    hvNativeInvalid,    ///< like "invalid" (-1)
+//    hvDll,              ///< DLL
+//    hvStdFile,          ///< like nullptr
+//    hvMySqlConn,        ///< MySQL connection
+//    hvMySqlResult,      ///< MySQL result
+//    hvCurl,             ///< CURL
+//    hvFindDir,          ///< Dir find
+//    hvSocket            ///< Socket
+//};
 
 } // namespace
 //-------------------------------------------------------------------------------------------------
