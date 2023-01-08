b2f6d97f03387aa0f0c4d58beb6452205220b141
skynowa <skynowa@gmail.com>
2023-01-09 01:25:25 +0200

    Handle policy review 2 (#388)
    * handle-policy: HandlePolicy.h [67+/57-] - review
    * handle-policy: HandlePolicy_unix.inl (+8) [386+/455-] - review
    * handle-policy: HandlePolicy.inl [7+/1-] - review
    * handle-policy: CMakeLists.txt [155+/129-] - revert
    * handle-policy: CMakeLists.txt [144+/28-] - revert
    * Revert "handle-policy: CMakeLists.txt [144+/28-] - revert"
    This reverts commit 9d85e93ecd4a07f17c880b7e371b20fc6db60a25.
    * Revert "handle-policy: CMakeLists.txt [155+/129-] - revert"
    This reverts commit a868f66e89b827d66f0f553bdb1dffdf6fe6d208.
    * handle-policy: HandlePolicy_unix.inl (+4) [503+/578-] - merge
    * handle-policy: HandlePolicyType.h [13+/13-] - revert
    * handle-policy: CMakeLists.txt [144+/28-] - merge
    * handle-policy: HandlePolicy.h [5+/15-] - code style
    * handle-policy: HandlePolicy.h [5+/1-] - merge
    * handle-policy: HandlePolicy.inl [8+/4-] - fix
    * handle-policy: HandlePolicy.inl [9+/5-] - fix
    * handle-policy: HandlePolicy.inl [3+/3-] - fix
    * handle-policy: Handle.h [4+] - fix
    * handle-policy: Pipe.h [1+/1-] - fix
    * handle-policy: HandlePolicy.h [5+/5-] - fix
    * handle-policy: HandlePolicy.inl [12+/6-] - fix
    * handle-policy: HandlePolicy_unix.inl (+4) [375+/424-] - impl
    * handle-policy: HandlePolicy.inl [3+] - fix
    * handle-policy: HandlePolicy.inl [6+/4-] - fix
    * handle-policy: HandlePolicy.inl [2+/1-] - fix
    * handle-policy: HandlePolicy_win.inl (+2) [2+/80-] - remove
    * handle-policy: HandlePolicy_unix.inl (+2) [243-] - remove
    * handle-policy: HandlePolicyType.h (+2) [1+/28-] - remove
    * handle-policy: Handle.h [1-] - remove
    * handle-policy: Handle.h (+2) [9+/48-] - remove
    * handle-policy: HandlePolicy.h [2+/2-] - review
    * handle-policy: HandlePolicy.inl [1+/1-] - fix
    * handle-policy: HandlePolicy.inl [3+/3-] - test(s)
    * handle-policy: HandlePolicy.inl (+2) [1+/23-] - close - review
    * handle-policy: HandlePolicy.inl [13+/5-] - close - impl
    * handle-policy: HandlePolicy.inl [3+/3-] - close - review
    * handle-policy: HandlePolicy.inl [2+/2-] - close - review
    * handle-policy: HandlePolicy.inl [4+/1-] - close - fix
    * handle-policy: HandlePolicy.inl [3-] - close - todo
    * handle-policy: HandlePolicy.inl [1+/1-] - close - fix

--- Include/xLib/Core/HandlePolicy.h
+++ Include/xLib/Core/HandlePolicy.h
@@ -6,6 +6,9 @@
 
 #pragma once
 
+#include <xLib/Core/Core.h>
+#include <xLib/Core/Format.h>
+
 #if   xENV_WIN
 	#include <mysql.h>
 	#include <errmsg.h>
@@ -21,89 +24,134 @@
 #endif
 
 #include <curl/curl.h>
-
-#include <xLib/Core/Core.h>
-#include <xLib/Core/Format.h>
-#include <xLib/Core/HandlePolicyType.h>
 //-------------------------------------------------------------------------------------------------
 namespace xl::core
 {
 
+enum class HandlePolicyType
+    /// error handle type
+{
+    hvNative = 0,       ///< like "null"
+    hvNativeInvalid,    ///< like "invalid" (-1)
+    hvDll,              ///< DLL
+    hvStdFile,          ///< like nullptr
+    hvMySqlConn,        ///< MySQL connection
+    hvMySqlResult,      ///< MySQL result
+    hvCurl,             ///< CURL
+    hvFindDir,          ///< Dir find
+    hvSocket            ///< Socket
+};
+
 template<typename T, HandlePolicyType valueT>
-class HandlePolicy;
-    /// handle error
+class Handle;
+    /// handle
 //-------------------------------------------------------------------------------------------------
 ///\name Factory
 ///\{
 // TODO: isValid - add const
-#define xHANDLE_POLICY_FACTORY(type, null_value) \
-	template<typename T> \
-	class HandlePolicy<T, type> \
-	{ \
-	public: \
-		static \
-		T \
-		null() \
-		{ \
-			return null_value; \
-		} \
-		\
-		static \
-		std::size_t \
-		openMax() \
-		{ \
-			return _openMax_impl(); \
-		} \
-		\
-		static \
-		T \
-		clone(const T a_handle) \
-		{ \
-			return _clone_impl(a_handle); \
-		} \
-		\
-		static \
-		bool_t \
-		isValid(const T a_handle) \
-		{ \
-			return _isValid_impl(a_handle); \
-		} \
-		\
-		static \
-		void \
-		close(T &a_handle) \
-		{ \
-			_close_impl(a_handle); \
-		} \
-	\
-	xPLATFORM_IMPL: \
-		static std::size_t _openMax_impl(); \
-		static T           _clone_impl(const T handle); \
-		static bool_t      _isValid_impl(const T handle); \
-		static void_t      _close_impl(T &handle); \
+/// #define xHANDLE_POLICY_FACTORY(type, null_value)
+template<typename T, HandlePolicyType type>
+struct HandlePolicy
+{
+	static T null()
+	{
+		if      constexpr (type == HandlePolicyType::hvNative) {
+			return xNATIVE_HANDLE_NULL;
+		}
+		else if constexpr (type == HandlePolicyType::hvNativeInvalid) {
+			return xNATIVE_HANDLE_INVALID;
+		}
+		else if constexpr (type == HandlePolicyType::hvDll) {
+			return nullptr;
+		}
+		else if constexpr (type == HandlePolicyType::hvStdFile) {
+			return nullptr;
+		}
+		else if constexpr (type == HandlePolicyType::hvMySqlConn) {
+			return nullptr;
+		}
+		else if constexpr (type == HandlePolicyType::hvMySqlResult) {
+			return nullptr;
+		}
+		else if constexpr (type == HandlePolicyType::hvCurl) {
+			return nullptr;
+		}
+		else if constexpr (type == HandlePolicyType::hvFindDir) {
+			return xFIND_DIR_HANDLE_NULL;
+		}
+		else if constexpr (type == HandlePolicyType::hvSocket) {
+			return xSOCKET_HANDLE_INVALID;
+		}
+		else {
+			/// static_assert(false);
+		}
 	}
+
+	static std::size_t openMax()
+	{
+		return _openMax_impl();
+	}
+
+	static T clone(const T a_handle)
+	{
+		return _clone_impl(a_handle);
+	}
+
+	static bool_t isValid(const T a_handle)
+	{
+		return _isValid_impl(a_handle);
+	}
+
+	static void close(T &a_handle)
+	{
+		_close_impl(a_handle);
+	}
+
+xPLATFORM_IMPL:
+	static std::size_t _openMax_impl();
+	static T           _clone_impl(const T handle);
+	static bool_t      _isValid_impl(const T handle);
+	static void_t      _close_impl(T &handle);
+};
 ///\}
 //-------------------------------------------------------------------------------------------------
-///\name Impl
+///\name Aliases
 ///\{
-xHANDLE_POLICY_FACTORY(HandlePolicyType::hvNative,        xNATIVE_HANDLE_NULL);
-xHANDLE_POLICY_FACTORY(HandlePolicyType::hvNativeInvalid, xNATIVE_HANDLE_INVALID);
-xHANDLE_POLICY_FACTORY(HandlePolicyType::hvDll,           nullptr);
-xHANDLE_POLICY_FACTORY(HandlePolicyType::hvStdFile,       nullptr);
-xHANDLE_POLICY_FACTORY(HandlePolicyType::hvMySqlConn,     nullptr);
-xHANDLE_POLICY_FACTORY(HandlePolicyType::hvMySqlResult,   nullptr);
-xHANDLE_POLICY_FACTORY(HandlePolicyType::hvCurl,          nullptr);
-xHANDLE_POLICY_FACTORY(HandlePolicyType::hvFindDir,       xFIND_DIR_HANDLE_NULL);
-xHANDLE_POLICY_FACTORY(HandlePolicyType::hvSocket,        xSOCKET_HANDLE_INVALID);
+using file_t      = FILE *;
+using mysql_t     = MYSQL *;
+using mysql_res_t = MYSQL_RES *;
+using curl_t      = CURL *;
+
+using HandleNative        = Handle<native_handle_t,   HandlePolicyType::hvNative>;
+xUSING_CONST(HandleNative);
+
+using HandleNativeInvalid = Handle<native_handle_t,   HandlePolicyType::hvNativeInvalid>;
+xUSING_CONST(HandleNativeInvalid);
+
+using HandleDll           = Handle<dll_handle_t,      HandlePolicyType::hvDll>;
+xUSING_CONST(HandleDll);
+
+using HandleStdFile       = Handle<file_t,            HandlePolicyType::hvStdFile>;
+xUSING_CONST(HandleStdFile);
+
+using HandleMySqlConn     = Handle<mysql_t,           HandlePolicyType::hvMySqlConn>;
+xUSING_CONST(HandleMySqlConn);
+
+using HandleMySqlResult   = Handle<mysql_res_t,       HandlePolicyType::hvMySqlResult>;
+xUSING_CONST(HandleMySqlResult);
+
+using HandleCurl          = Handle<curl_t,            HandlePolicyType::hvCurl>;
+xUSING_CONST(HandleCurl);
+
+using HandleFindDir       = Handle<find_dir_handle_t, HandlePolicyType::hvFindDir>;
+xUSING_CONST(HandleFindDir);
+
+using HandleSocket        = Handle<socket_t,          HandlePolicyType::hvSocket>;
+xUSING_CONST(HandleSocket);
 ///\}
 //-------------------------------------------------------------------------------------------------
 } // namespace
 //-------------------------------------------------------------------------------------------------
 #include "HandlePolicy.inl"
-
-#if   xENV_WIN
-    #include "Platform/Win/HandlePolicy_win.inl"
-#elif xENV_UNIX
-    #include "Platform/Unix/HandlePolicy_unix.inl"
-#endif
 //-------------------------------------------------------------------------------------------------
+
