1b5ae1c76743fb838f524c9fbc0c886c863675dd
skynowa <skynowa@gmail.com>
2023-01-10 00:38:56 +0200

    file-io: FsWatcher.cpp [3+/2-] - remove

--- Include/xLib/Fs/FsWatcher.cpp
+++ Include/xLib/Fs/FsWatcher.cpp
@@ -70,8 +70,9 @@ FsWatcher::openFiles(
         _filePaths.push_back(it_filePath);
         _fileHandles.push_back( file.getNative() );
 
-        HandleStdFile &handle = file.detach();
-        xUNUSED(handle);
+        // FileIO::detach - removed
+        // HandleStdFile &handle = file.detach();
+        // xUNUSED(handle);
     }
 
     if ( _filePaths.empty() ) {
