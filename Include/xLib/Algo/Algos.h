a293b1a0647810362825581a51c6ada07e45c3af
skynowa <skynowa@gmail.com>
2023-01-11 22:44:37 +0200

    Algos.h [14+/28-] - code style

--- Include/xLib/Algo/Algos.h
+++ Include/xLib/Algo/Algos.h
@@ -24,69 +24,55 @@ public:
 ///\name Common
 ///\{
     template<typename ContT, typename ValueT>
-    static
-    bool_t isContains(const ContT &cont, const ValueT &value);
+    static bool_t isContains(const ContT &cont, const ValueT &value);
         ///< check value existence
     template<typename ContT, typename UnaryPredicateT>
-    static
-    bool_t findIfAll(const ContT &cont, ContT &out, UnaryPredicateT pred);
+    static bool_t findIfAll(const ContT &cont, ContT &out, UnaryPredicateT pred);
         ///< Find all elements in container
     template<typename T>
-    static
-    bool_t isInBounds(const T &value, const T &low, const T &high);
+    static bool_t isInBounds(const T &value, const T &low, const T &high);
         ///< check value bounds
     template<typename T, typename R, typename ComparatorT>
-    static
-    bool_t isInBounds(const T &value, const R &low, const R &high, ComparatorT comp);
+    static bool_t isInBounds(const T &value, const R &low, const R &high, ComparatorT comp);
         ///< check value bounds (with custom comparator)
     template<typename ContT>
-    static
-    void_t deleteAll(ContT &cont);
+    static void_t deleteAll(ContT &cont);
         ///< delete all values
-    static
-    bool_t isStartsWith(std::ctstring_t &value, std::ctstring_t &prefix);
+    static bool_t isStartsWith(std::ctstring_t &value, std::ctstring_t &prefix);
         ///< starts with
-    static
-    bool_t isEndsWith(std::ctstring_t &value, std::ctstring_t &suffix);
+    static bool_t isEndsWith(std::ctstring_t &value, std::ctstring_t &suffix);
         ///< ends with
-    static
-    bool_t contains(std::ctstring_t &value, std::ctstring_t &infix);
+    static bool_t contains(std::ctstring_t &value, std::ctstring_t &infix);
         ///< contains
     template<typename T>
-    static
-    bool_t isUnique(T first, T last);
+    static bool_t isUnique(T first, T last);
         ///< determining if sorted container has all unique elements
     template<typename ContT>
-    static
-    bool_t isUniqueAll(const ContT &cont);
+    static bool_t isUniqueAll(const ContT &cont);
         ///< determining if sorted container has all unique elements
 ///\}
 
 ///\name std::vector
 ///\{
 	template<typename T>
-	static
-	void_t vectorUnique(std::vector<T> &cont);
+	static void_t vectorUnique(std::vector<T> &cont);
 		///< unique std::vector
 	template<typename T>
-	static
-	void_t vectorRemove(std::vector<T> &vec, const T &item);
+	static void_t vectorRemove(std::vector<T> &vec, const T &item);
 		///< remove item from std::vector
 ///\}
 
 ///\name std::list
 ///\{
 	template<typename T>
-	static
-	void_t listUnique(std::list<T> &cont);
+	static void_t listUnique(std::list<T> &cont);
 		///< unique std::list
 ///\}
 
 ///\name std::map
 ///\{
 	template<typename T1, typename T2>
-	static
-	T1 mapFindByValue(const std::map<T1, T2> &cont, const T2 &value);
+	static T1 mapFindByValue(const std::map<T1, T2> &cont, const T2 &value);
 		///< find find key by value
 ///\}
 };
