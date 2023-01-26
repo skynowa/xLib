76d6df6c2e7f792d9bb210ab3bdaf85312ffbff4
skynowa <skynowa@gmail.com>
2023-01-27 00:38:05 +0200

    ConsoleUI-split (#397)
    * ConsoleUI-split: Console.cpp (+8) [190+/140-] - impl
    
    * ConsoleUI-split: Console.cpp [2+/1-] - impl
    
    * ConsoleUI-split: Console.cpp [6+/6-] - impl
    
    * ConsoleUI-split: Test_Console.cpp (+3) [4+/4-] - impl
    
    * ConsoleUI-split: Test_Console.cpp (+3) [8+/8-] - impl
    
    * ConsoleUI-split: Console.cpp (+4) [10+/7-] - impl
    
    * ConsoleUI-split: Debugger.cpp [1+/1-] - impl
    
    * ConsoleUI-split: Console.h [2+/2-] - impl
    
    * ConsoleUI-split: Debugger.cpp (+3) [5+/3-] - impl
    
    * ConsoleUI-split: Debugger.cpp [3+/3-] - impl
    
    * ConsoleUI-split: Config.h.in [1-] - impl
    
    * ConsoleUI-split: ConsoleUI.cpp (+12) [455+/265-] - impl
    
    * ConsoleUI-split: ConsoleUI.cpp [2+/2-] - fix
    
    * ConsoleUI-split: ConsoleUI_unix.inl (+3) [12-] - impl
    
    * ConsoleUI-split: ConsoleUI.h (+3) [168+/168-] - impl
    
    * ConsoleUI-split: Console.h (+2) [1+/79-] - impl
    
    * ConsoleUI-split: ConsoleUI.cpp (+2) [4+/4-] - review
    
    * ConsoleUI-split: Test_ConsoleUI.cpp [15+/16-] - impl
    
    * ConsoleUI-split: Test_ConsoleUI.cpp [1+/1-] - impl
    
    * ConsoleUI-split: Test_ConsoleUI.cpp [1+/1-] - impl
    
    * ConsoleUI-split: Test_ConsoleUI.cpp [4+/4-] - impl
    
    * ConsoleUI-split: Test_ConsoleUI.cpp [3+/6-] - impl
    
    * ConsoleUI-split: Test_ConsoleUI.cpp [2+/2-] - impl
    
    * ConsoleUI-split: Path.h (+2) [3+/22-] - revert

---
+++ Tests/System/Test_ConsoleUI.cpp
@@ -0,0 +1,47 @@
+/**
+ * \file   Test_ConsoleUI.cpp
+ * \brief
+ */
+
+
+#include <xLib/xLib.h>
+
+//-------------------------------------------------------------------------------------------------
+xTEST_UNIT(Test_ConsoleUI)
+//-------------------------------------------------------------------------------------------------
+/* virtual */
+bool_t
+Test_ConsoleUI::unit()
+{
+	ConsoleUI ui;
+
+	xTEST_CASE("msgBox")
+	{
+	#if xTEST_IGNORE
+		std::ctstring_t title = xT("iMsgBox_title");
+		std::ctstring_t text  = xT("iMsgBox_text\nddddddddd\nRRRRRRRRRRRRR")
+			xT(" AAAA BBBB CCCC DDDD EEEE FFFF GGGG HHHH JJJJ KKKK LLLL ZZZZ XXXX CCCC VVVV BBBB");
+		ConsoleUI::cResult rRes = ui.msgBox(title, text);
+		xUNUSED(rRes);
+	#else
+		Trace() << xT("[skip]");
+	#endif
+	}
+
+	xTEST_CASE("promptBox")
+	{
+	#if xTEST_IGNORE
+		std::ctstring_t title     = xT("Prompt_title");
+		std::ctstring_t text      = xT("Prompt_simple_prompt");
+		cbool_t         isVisible = false;
+		std::tstring_t  answer    = xT("Answer_bla-bla-bla");
+
+		ui.promptBox(title, text, isVisible, &answer);
+	#else
+		Trace() << xT("[skip]");
+	#endif
+	}
+
+	return true;
+}
+//-------------------------------------------------------------------------------------------------
