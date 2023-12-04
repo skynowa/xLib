f5ae9ed0ab541c4b38e6ccc1e52490fd4b502e33
skynowa <skynowa@gmail.com>
2023-12-04 20:39:45 +0200

    NativeError.h [12+/24-] - code style

--- Include/xLib/Debug/NativeError.h
+++ Include/xLib/Debug/NativeError.h
@@ -21,41 +21,29 @@ public:
 	xNO_COPY_ASSIGN(NativeError)
 ///\}
 
-    static
-    bool_t         isSuccess();
+    static bool_t         isSuccess();
         ///< is error code success
-    static
-    ulong_t        get();
+    static ulong_t        get();
         ///< get the calling thread's last-error code value
-    static
-    void_t         set(culong_t code);
+    static void_t         set(culong_t code);
         ///< set the last error code for the calling thread
-    static
-    void_t         reset();
+    static void_t         reset();
         ///< set last error code to success value (0)
-    static
-    std::tstring_t format();
+    static std::tstring_t format();
         ///< get as string
-    static
-    std::tstring_t format(culong_t code);
+    static std::tstring_t format(culong_t code);
         ///< get last error as string
 
 private:
-    static
-    ulong_t        _nativeCodeSuccess();
+    static ulong_t _nativeCodeSuccess();
         ///< native success code
 
 xPLATFORM_IMPL:
-    static
-    bool_t         _isSuccess_impl();
-    static
-    ulong_t        _get_impl();
-    static
-    void_t         _set_impl(culong_t code);
-    static
-    std::tstring_t _format_impl(culong_t a_code);
-    static
-    ulong_t        _nativeCodeSuccess_impl();
+    static bool_t         _isSuccess_impl();
+    static ulong_t        _get_impl();
+    static void_t         _set_impl(culong_t code);
+    static std::tstring_t _format_impl(culong_t a_code);
+    static ulong_t        _nativeCodeSuccess_impl();
 };
 
 } // namespace
