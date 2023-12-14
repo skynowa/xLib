4a056047ae5e6677d1704481dac4f174fca21984
skynowa <skynowa@gmail.com>
2023-12-14 03:03:16 +0200

    Test_Double.cpp (+2) [15+/14-] - test(s)

--- Build/test_unit.sh
+++ Build/test_unit.sh
@@ -2,7 +2,7 @@
 
 
 # Params
-UNIT_TEST_NAME=Dll
+UNIT_TEST_NAME=Double
 
 ./tests.sh ${UNIT_TEST_NAME} # && ./deploy.sh
 #--------------------------------------------------------------------------------------------------
