057056556fae78f1a4e92fdc8482f9a0183970b8
skynowa <skynowa@gmail.com>
2023-12-17 23:12:17 +0200

    backup-exception: test_unit.sh [1+/1-] - impl

--- Build/test_unit.sh
+++ Build/test_unit.sh
@@ -12,7 +12,7 @@ source bash_colors.sh
 for it_test in ${UNIT_TESTS[*]}
 do
 	echo -e "${BASH_BG_BLACK}+------------------------------+${BASH_RESET}"
-	echo -e "${BASH_BG_BLACK}|${BASH_BG_GREEN}${BASH_FG_BLACK} ${it_test} ${BASH_RESET}"
+	echo -e "${BASH_BG_BLACK}|${BASH_FG_YELLOW} ${it_test} ${BASH_RESET}"
 	echo -e "${BASH_BG_BLACK}+------------------------------+${BASH_RESET}"
 
 	./tests.sh ${it_test} # && ./deploy.sh
