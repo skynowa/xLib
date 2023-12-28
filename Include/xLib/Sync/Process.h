25b7051b012aec7204acec333db56adaf532151a
skynowa <skynowa@gmail.com>
2023-12-29 00:09:01 +0200

    process-shellExecute: Process.cpp (+2) [11+/1-] - impl

--- Include/xLib/Sync/Process.h
+++ Include/xLib/Sync/Process.h
@@ -160,7 +160,9 @@ xPLATFORM_IMPL:
 
     static void_t   _shellExecute_impl(std::ctstring_t &filePathOrURL, std::ctstring_t &params);
         ///< TODO: QProcess::startDetached()
-    static bool_t   _isUrlFull(std::ctstring_t &url);
+
+xPRIVATE_STATIC:
+    static bool_t _isUrlFull(std::ctstring_t &url);
 };
 
 } // namespace
