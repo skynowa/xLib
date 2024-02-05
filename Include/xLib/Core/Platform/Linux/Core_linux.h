5386e8ea3c743c02e5f8608246eb48fd8a5024a5
skynowa <skynowa@gmail.com>
2024-02-05 14:16:50 +0200

    conan-2.0: Core_linux.h [5+/2-] - impl

--- Include/xLib/Core/Platform/Linux/Core_linux.h
+++ Include/xLib/Core/Platform/Linux/Core_linux.h
@@ -14,8 +14,11 @@
 #include <mntent.h>
 
 #if cmHAVE_GETCPU
-    // #include <linux/getcpu.h>
-    #include "/usr/src/linux-hwe-6.5-headers-6.5.0-15/include/linux/getcpu.h"
+	#if __has_include(<linux/getcpu.h>)
+        #include <linux/getcpu.h>
+	#else
+        #include "/usr/src/linux-hwe-6.5-headers-6.5.0-15/include/linux/getcpu.h"
+	#endif
 #endif
 
 // POSIX
