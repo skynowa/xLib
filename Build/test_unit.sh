e821cc33a0c83ca592e478cc8461600d0ad3f3ad
skynowa <skynowa@gmail.com>
2024-09-26 22:30:31 +0300

    demangleName-impl: test_unit.sh [1+/1-] - dead(s), remove

--- Build/test_unit.sh
+++ Build/test_unit.sh
@@ -21,6 +21,6 @@ do
 	echo -e "${BASH_BG_BLACK}|${BASH_FG_YELLOW} ${it_test} ${BASH_RESET}"
 	echo -e "${BASH_BG_BLACK}+------------------------------+${BASH_RESET}"
 
-	./tests.sh ${it_test} # && ./deploy.sh
+	./tests.sh "${it_test}"
 done
 #--------------------------------------------------------------------------------------------------
