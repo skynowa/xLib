4ef55366f7f2bcd95da83e86e662a5f0881a63bc
skynowa <skynowa@gmail.com>
2024-11-27 21:16:03 +0200

    docs.sh (+2) [14+/3-] - doc(s)

---
+++ Docs/docs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# \file  docs.sh
+# \brief Generate Doxygen documentation
+#
+
+# build - UI
+# doxywizard ./Documentation.doxyfile
+
+# build
+doxygen ./Documentation.doxyfile
