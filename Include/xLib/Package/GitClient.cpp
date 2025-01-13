69c5c8c396a75022ba154ddb50f089286a1ef50e
skynowa <skynowa@gmail.com>
2025-01-13 23:19:24 +0200

    GitClient-remoteRepoNames-impl (#438)
    * GitClient-remoteRepoNames: GitClient.cpp (+3) [24+/1-] - impl
    
    * GitClient-remoteRepoNames: GitClient.cpp (+2) [33+/11-] - impl
    
    * GitClient-remoteRepoNames: GitClient.cpp [3+/3-] - doc(s)

--- Include/xLib/Package/GitClient.cpp
+++ Include/xLib/Package/GitClient.cpp
@@ -28,7 +28,7 @@ GitClient::isGitDir() const
 	std::tstring_t      stdOut;
 	std::tstring_t      stdError;
 
-	Process::execute(_gitPath, params, &stdOut, &stdError);
+	_gitExecute(params, &stdOut, &stdError);
 	xCHECK_RET(!stdError.empty(), false);
 	xCHECK_RET((stdOut.find(xT(".git")) == std::tstring_t::npos), false);
 
@@ -65,7 +65,7 @@ GitClient::repoUrl() const
 	std::tstring_t      stdOut;
 	std::tstring_t      stdError;
 
-	Process::execute(_gitPath, params, &stdOut, &stdError);
+	_gitExecute(params, &stdOut, &stdError);
 
 	sRv = String::trimSpace(stdOut);
 
@@ -106,11 +106,31 @@ GitClient::repoName() const
 	std::tstring_t      stdOut;
 	std::tstring_t      stdError;
 
-	Process::execute(_gitPath, params, &stdOut, &stdError);
+	_gitExecute(params, &stdOut, &stdError);
 
 	sRv = String::trimSpace( Path(stdOut).fileBaseName() );
 
 	return sRv;
+}
+//-------------------------------------------------------------------------------------------------
+void_t
+GitClient::remoteRepoNames(
+	std::vec_tstring_t *out_repoNames	///< [out]
+) const
+{
+	xCHECK_DO(out_repoNames == nullptr, return);
+	xCHECK_DO(!isGitDir(), return);
+
+	out_repoNames->clear();
+
+	std::cvec_tstring_t params {"remote"};
+	std::tstring_t      stdOut;
+	std::tstring_t      stdError;
+
+	_gitExecute(params, &stdOut, &stdError);
+
+	// [out]
+	String::split(String::trimSpace(stdOut), Const::nl(), out_repoNames);
 }
 //-------------------------------------------------------------------------------------------------
 std::tstring_t
@@ -186,7 +206,7 @@ GitClient::branchName() const
 	std::tstring_t      stdOut;
 	std::tstring_t      stdError;
 
-	Process::execute(_gitPath, params, &stdOut, &stdError);
+	_gitExecute(params, &stdOut, &stdError);
 
 	if      ( stdOut.empty() ) {
 		sRv = xT("");
@@ -212,7 +232,7 @@ GitClient::localBranchesNum() const
 	std::tstring_t      stdOut;
 	std::tstring_t      stdError;
 
-	Process::execute(_gitPath, params, &stdOut, &stdError);
+	_gitExecute(params, &stdOut, &stdError);
 
 	std::vec_tstring_t values;
 	String::split(String::trimSpace(stdOut), Const::nl(), &values);
@@ -280,7 +300,7 @@ GitClient::filesStatuses() const
 	std::tstring_t      stdOut;
 	std::tstring_t      stdError;
 
-	Process::execute(_gitPath, params, &stdOut, &stdError);
+	_gitExecute(params, &stdOut, &stdError);
 
 	cbool_t isNoCommit  = StringCI::contains(stdOut, xT("nothing to commit"));
 	cbool_t isModified  = StringCI::contains(stdOut, xT("modified:"));
@@ -353,7 +373,7 @@ GitClient::commitsAheadBehind(
 	std::tstring_t      stdOut;
 	std::tstring_t      stdError;
 
-	Process::execute(_gitPath, params, &stdOut, &stdError);
+	_gitExecute(params, &stdOut, &stdError);
 
 	std::vec_tstring_t values;
 	String::split(stdOut, Const::ht(), &values);
@@ -374,7 +394,7 @@ GitClient::stashesNum() const
 	std::tstring_t      stdOut;
 	std::tstring_t      stdError;
 
-	Process::execute(_gitPath, params, &stdOut, &stdError);
+	_gitExecute(params, &stdOut, &stdError);
 
 	std::vec_tstring_t values;
 	String::split(String::trimSpace(stdOut), Const::nl(), &values);
@@ -399,7 +419,7 @@ GitClient::modifiedFiles(
 	std::tstring_t      stdOut;
 	std::tstring_t      stdError;
 
-	Process::execute(_gitPath, params, &stdOut, &stdError);
+	_gitExecute(params, &stdOut, &stdError);
 #if 0
 	Cout() << xTRACE_VAR(stdOut);
 	Cout() << xTRACE_VAR(stdError);
@@ -444,7 +464,7 @@ GitClient::trackedFiles(
 	std::tstring_t      stdOut;
 	std::tstring_t      stdError;
 
-	Process::execute(_gitPath, params_master_diff, &stdOut, &stdError);
+	_gitExecute(params_master_diff, &stdOut, &stdError);
 #if 0
 	Cout() << xTRACE_VAR(stdOut);
 	Cout() << xTRACE_VAR(stdError);
@@ -574,6 +594,25 @@ GitClient::_binPath() const
 	xTEST(!sRv.empty());
 
 	return sRv;
+}
+//-------------------------------------------------------------------------------------------------
+void_t
+GitClient::_gitExecute(
+	std::cvec_tstring_t &a_params,		///< command line params
+	std::tstring_t      *out_stdOut,	///< [out] std::cout (maybe as nullptr)
+	std::tstring_t      *out_stdError	///< [out] std::cerr (maybe as nullptr)
+) const
+{
+	Process::execute(_gitPath, a_params, out_stdOut, out_stdError);
+
+	// Fix
+	if (out_stdOut != nullptr) {
+		*out_stdOut   = String::trimSpace(*out_stdOut);
+	}
+
+	if (out_stdError != nullptr) {
+		*out_stdError = String::trimSpace(*out_stdError);
+	}
 }
 //-------------------------------------------------------------------------------------------------
 
