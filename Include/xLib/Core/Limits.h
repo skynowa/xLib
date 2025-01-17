462a805ecc77c9aaf7293375b84e53f1f2a81e4d
skynowa <skynowa@gmail.com>
2025-01-17 23:32:09 +0200

    Limits.h [78+/83-] - doc(s), fix

--- Include/xLib/Core/Limits.h
+++ Include/xLib/Core/Limits.h
@@ -12,124 +12,119 @@
     #include <winsock2.h>
 #endif
 //-------------------------------------------------------------------------------------------------
-// xHOST_NAME_MAX
+///\def   xHOST_NAME_MAX
+///\brief max host name length
+///\{
 #if   xENV_WIN
-    #if defined(MAX_COMPUTERNAME_LENGTH)
-        #define xHOST_NAME_MAX \
-            ( MAX_COMPUTERNAME_LENGTH )
-    #else
-        #define xHOST_NAME_MAX \
-            ( 15 ) // custom define
-    #endif
-#elif xENV_UNIX
-    #if   defined(HOST_NAME_MAX)
-        #define xHOST_NAME_MAX \
-            ( HOST_NAME_MAX )
-    #elif defined(MAXHOSTNAMELEN)
-        #define xHOST_NAME_MAX \
-            ( MAXHOSTNAMELEN )
-    #else
-        #define xHOST_NAME_MAX \
-            ( 256 ) // custom define
-    #endif
-#endif
-    ///< max host name length
-//-------------------------------------------------------------------------------------------------
-// xUSER_NAME_MAX
-#if   xENV_WIN
-    #define xUSER_NAME_MAX \
-        ( UNLEN )
-#elif xENV_UNIX
-	#if defined(L_cuserid)
-		#define xUSER_NAME_MAX \
-			( L_cuserid )
+	#if defined(MAX_COMPUTERNAME_LENGTH)
+		#define xHOST_NAME_MAX ( MAX_COMPUTERNAME_LENGTH )
 	#else
-		#define xUSER_NAME_MAX \
-			( 9 ) // custom define
+		#define xHOST_NAME_MAX ( 15 )
+			///< custom define
+	#endif
+#elif xENV_UNIX
+	#if   defined(HOST_NAME_MAX)
+		#define xHOST_NAME_MAX ( HOST_NAME_MAX )
+	#elif defined(MAXHOSTNAMELEN)
+		#define xHOST_NAME_MAX ( MAXHOSTNAMELEN )
+	#else
+		#define xHOST_NAME_MAX ( 256 )
+			///< custom define
 	#endif
 #endif
-    ///< max user name length
+///\}
 //-------------------------------------------------------------------------------------------------
-// xLINE_MAX
+///\def   xUSER_NAME_MAX
+///\brief max user name length
+///\{
 #if   xENV_WIN
-    #define xLINE_MAX \
-    ( 2048 ) // custom define
+	#define xUSER_NAME_MAX ( UNLEN )
 #elif xENV_UNIX
-    #if   defined(LINE_MAX)
-        #define xLINE_MAX \
-            ( LINE_MAX )
-    #elif defined(_SC_LINE_MAX)
-        #define xLINE_MAX \
-            ( _SC_LINE_MAX )
-    #else
-        #error xLib: xLINE_MAX not defined
-    #endif
+	#if defined(L_cuserid)
+		#define xUSER_NAME_MAX ( L_cuserid )
+	#else
+		#define xUSER_NAME_MAX ( 9 )
+			///< custom define
+	#endif
 #endif
-    ///< maximum length of a utility's input line, either from standard input or from a file
+///\}
 //-------------------------------------------------------------------------------------------------
-// xPAGE_SIZE
+///\def   xLINE_MAX
+///\brief maximum length of a utility's input line, either from standard input or from a file
+///\{
 #if   xENV_WIN
-    xNA
+	#define xLINE_MAX ( 2048 )
+		///< custom define
 #elif xENV_UNIX
-    #if   defined(_SC_PAGESIZE)
-        #define xPAGE_SIZE \
-            ( _SC_PAGESIZE )
-    #elif defined(_SC_PAGE_SIZE)
-        #define xPAGE_SIZE \
-            ( _SC_PAGE_SIZE )
-    #else
-        #error xLib: xPAGE_SIZE not defined
-    #endif
+	#if   defined(LINE_MAX)
+		#define xLINE_MAX ( LINE_MAX )
+	#elif defined(_SC_LINE_MAX)
+		#define xLINE_MAX ( _SC_LINE_MAX )
+	#else
+		#error xLib: xLINE_MAX not defined
+	#endif
 #endif
-    ///< file system page size
+///\}
 //-------------------------------------------------------------------------------------------------
-// xTIMEOUT_INFINITE
+///\def   xPAGE_SIZE
+///\brief file system page size
+///\{
 #if   xENV_WIN
-    #define xTIMEOUT_INFINITE \
-        ( INFINITE )
+	xNA
 #elif xENV_UNIX
-    #define xTIMEOUT_INFINITE \
-        ( ~(0UL) )
+	#if   defined(_SC_PAGESIZE)
+		#define xPAGE_SIZE ( _SC_PAGESIZE )
+	#elif defined(_SC_PAGE_SIZE)
+		#define xPAGE_SIZE ( _SC_PAGE_SIZE )
+	#else
+		#error xLib: xPAGE_SIZE not defined
+	#endif
 #endif
-    ///< infinite timeout
+///\}
 //-------------------------------------------------------------------------------------------------
-// xSOCKET_LISTEN_CONNECTIONS_MAX
+///\def   xTIMEOUT_INFINITE
+///\brief infinite timeout
+///\{
+#if   xENV_WIN
+	#define xTIMEOUT_INFINITE ( INFINITE )
+#elif xENV_UNIX
+	#define xTIMEOUT_INFINITE ( ~(0UL) )
+#endif
+///\}
+//-------------------------------------------------------------------------------------------------
+///\def   xSOCKET_LISTEN_CONNECTIONS_MAX
+///\brief Maximum queue length specifiable by listen
+///\{
 #if defined(SOMAXCONN)
-    #define xSOCKET_LISTEN_CONNECTIONS_MAX \
-            ( SOMAXCONN )
+	#define xSOCKET_LISTEN_CONNECTIONS_MAX ( SOMAXCONN )
 #else
-    #error xLib: xSOCKET_LISTEN_CONNECTIONS_MAX not defined
+	#error xLib: xSOCKET_LISTEN_CONNECTIONS_MAX not defined
 #endif
-    ///< Maximum queue length specifiable by listen
+///\}
 //-------------------------------------------------------------------------------------------------
-// xSOCKET_PORT_MIN, xSOCKET_PORT_MAX
-#define xSOCKET_PORT_MIN \
-        ( 1 )
-    ///< socket minimum port number
-#define xSOCKET_PORT_MAX \
-        ( 65535 )
-    ///< socket maximum port number
+///\name Socket limits
+///\{
+#define xSOCKET_PORT_MIN ( 1 )
+	///< socket minimum port number
+#define xSOCKET_PORT_MAX ( 65535 )
+	///< socket maximum port number
+///\}
 //-------------------------------------------------------------------------------------------------
 ///\name Custom defines
 ///\{
-#define xCUSTOM_SEMAPHORE_VALUE_MAX_WIN \
-    (LONG_MAX)
+#define xCUSTOM_SEMAPHORE_VALUE_MAX_WIN          (LONG_MAX)
     ///< semaphore maximum value (tested on Win7 x64)
-#define xCUSTOM_SYSCONF_SC_GETPW_R_SIZE_MAX_UNIX \
-    (16384L)
-    ///< \code{.c++}
-    ///< ::sysconf(_SC_GETPW_R_SIZE_MAX)
-    ///< \endcode
+#define xCUSTOM_SYSCONF_SC_GETPW_R_SIZE_MAX_UNIX (16384L)
+    ///< as ::sysconf(_SC_GETPW_R_SIZE_MAX)
 
-// xTHREAD_NAME_LENGTH_MAX
 // TODO: [skynowa] xTHREAD_NAME_LENGTH_MAX - fix unicode version
+///\def   xTHREAD_NAME_LENGTH_MAX
+///\brief thread name max length
+///\{
 #if   xENV_WIN
-    #define xTHREAD_NAME_LENGTH_MAX \
-        ( static_cast<std::size_t>(32) )
+	#define xTHREAD_NAME_LENGTH_MAX ( static_cast<std::size_t>(32) )
 #elif xENV_UNIX
-    #define xTHREAD_NAME_LENGTH_MAX \
-        ( static_cast<std::size_t>(16) )
+	#define xTHREAD_NAME_LENGTH_MAX ( static_cast<std::size_t>(16) )
 #endif
-    ///< thread name max length
 ///\}
 //-------------------------------------------------------------------------------------------------
