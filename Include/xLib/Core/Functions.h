373cb38343f8100b60349686ef057f21bf9f4d9b
skynowa <skynowa@gmail.com>
2025-01-17 14:58:40 +0200

    Functions.h [74+/52-] - doc(s), impl

--- Include/xLib/Core/Functions.h
+++ Include/xLib/Core/Functions.h
@@ -12,58 +12,69 @@
     #include "Platform/Functions_ansi.h"
 #endif
 //-------------------------------------------------------------------------------------------------
-// xDUP2
+///\name xDUP2
+///\{
 #if   xENV_WIN
 	#define xDUP2 ::_dup2
 #elif xENV_UNIX
 	#define xDUP2 ::dup2
 #endif
+///\}
 
-// xLOCKING
+///\name xLOCKING
+///\{
 #if   xCOMPILER_MINGW
-    #define xLOCKING ::_locking
+	#define xLOCKING ::_locking
 #elif xCOMPILER_MS
-    #define xLOCKING ::_locking
+	#define xLOCKING ::_locking
 #elif xCOMPILER_GNUC
-    #define xLOCKING ::lockf
+	#define xLOCKING ::lockf
 #else
-    #define xLOCKING ::locking
+	#define xLOCKING ::locking
 #endif
+///\}
 
-// xCHSIZE
+///\name xCHSIZE
+///\{
 #if   xCOMPILER_MINGW
-    #define xCHSIZE ::chsize
+	#define xCHSIZE ::chsize
 #elif xCOMPILER_MS
-    #define xCHSIZE ::_chsize_s
+	#define xCHSIZE ::_chsize_s
 #elif xCOMPILER_GNUC
-    #define xCHSIZE ::ftruncate
+	#define xCHSIZE ::ftruncate
 #else
-    #define xCHSIZE ::chsize
+	#define xCHSIZE ::chsize
 #endif
+///\}
 
-// xIOCTLSOCKET
+///\name xIOCTLSOCKET
+///\{
 #if   xENV_WIN
-    #define xIOCTLSOCKET ::ioctlsocket
+	#define xIOCTLSOCKET ::ioctlsocket
 #elif xENV_UNIX
-    #define xIOCTLSOCKET ::ioctl
+	#define xIOCTLSOCKET ::ioctl
 #endif
+///\}
 
-// xADDR_INFO_GET
+///\name xADDR_INFO_GET
+///\{
 #if   xCOMPILER_MINGW
-    #define xADDR_INFO_GET     ::getaddrinfo
+	#define xADDR_INFO_GET     ::getaddrinfo
 #elif xCOMPILER_MS
-    #define xADDR_INFO_GET     ::GetAddrInfo
+	#define xADDR_INFO_GET     ::GetAddrInfo
 #elif xCOMPILER_GNUC
-    #if xUNICODE
-        #define xADDR_INFO_GET xl::core::getaddrinfoW
-    #else
-        #define xADDR_INFO_GET ::getaddrinfo
-    #endif
+	#if xUNICODE
+		#define xADDR_INFO_GET xl::core::getaddrinfoW
+	#else
+		#define xADDR_INFO_GET ::getaddrinfo
+	#endif
 #else
-    #define xADDR_INFO_GET     ::getaddrinfo
+	#define xADDR_INFO_GET     ::getaddrinfo
 #endif
+///\}
 
-// xADDR_INFO_FREE
+///\name xADDR_INFO_FREE
+///\{
 #if   xENV_WIN
 	#if xCOMPILER_MS
 		#define xADDR_INFO_FREE ::FreeAddrInfo
@@ -73,64 +84,74 @@
 #elif xENV_UNIX
 	#define xADDR_INFO_FREE ::freeaddrinfo
 #endif
+///\}
 
-// xNAME_INFO_GET
+///\name xNAME_INFO_GET
+///\{
 #if   xCOMPILER_MINGW
-    #define xNAME_INFO_GET     ::getnameinfo
+	#define xNAME_INFO_GET     ::getnameinfo
 #elif xCOMPILER_MS
-    #define xNAME_INFO_GET     ::GetNameInfo
+	#define xNAME_INFO_GET     ::GetNameInfo
 #elif xCOMPILER_GNUC
-    #if xUNICODE
-        #define xNAME_INFO_GET xl::core::getnameinfoW
-    #else
-        #define xNAME_INFO_GET ::getnameinfo
-    #endif
+	#if xUNICODE
+		#define xNAME_INFO_GET xl::core::getnameinfoW
+	#else
+		#define xNAME_INFO_GET ::getnameinfo
+	#endif
 #else
-    #define xNAME_INFO_GET     ::getnameinfo
+	#define xNAME_INFO_GET     ::getnameinfo
 #endif
+///\}
 
-// xSTATVFS (struct and function)
+///\name xSTATVFS (filesystem statfs - struct and function)
+///\{
 #if   xENV_WIN
-    xNA
+	xNA
 #elif xENV_UNIX
-    xNA
+	xNA
 
-    #if   xENV_LINUX
-        #if   xOS_ANDROID
-            #define xSTATVFS ::statfs
-        #elif xOS_LINUX
-            #define xSTATVFS ::statfs64
-        #endif
-    #elif xENV_BSD
-        #define xSTATVFS     ::statvfs
-    #elif xENV_APPLE
-        #define xSTATVFS     statfs	/// TODO: ::statfs64
-    #endif
+	#if   xENV_LINUX
+		#if   xOS_ANDROID
+			#define xSTATVFS ::statfs
+		#elif xOS_LINUX
+			#define xSTATVFS ::statfs64
+		#endif
+	#elif xENV_BSD
+		#define xSTATVFS     ::statvfs
+	#elif xENV_APPLE
+		#define xSTATVFS     statfs	/// TODO: ::statfs64
+	#endif
 #endif
-    ///< filesystem statfs (struct and function)
+///\}
 
-// xISATTY
+///\name xISATTY
+///\{
 #if   xENV_WIN
 	#define xISATTY ::_isatty
 #elif xENV_UNIX
 	#define xISATTY ::isatty
 #endif
+///\}
 
-// xFILENO
+///\name xFILENO
+///\{
 #if   xENV_WIN
 	#define xFILENO ::_fileno
 #elif xENV_UNIX
 	#define xFILENO ::fileno
 #endif
+///\}
 
-// Sem API
 #if xENV_APPLE
+///\name Sem API
+///\{
 int sem_init(sem_t *psem, int flags, unsigned count);
 int sem_destroy(sem_t *psem);
 int sem_post(sem_t *psem);
 int sem_trywait(sem_t *psem);
 int sem_wait(sem_t *psem);
 int sem_timedwait(sem_t *psem, const struct timespec *abstim);
+///\}
 #endif
 //-------------------------------------------------------------------------------------------------
 // Custom
@@ -140,6 +161,7 @@ namespace xl
 {
 
 std::string demangleName(const char *nameRaw);
+	///< Demangling names
 
 }
 //-------------------------------------------------------------------------------------------------
