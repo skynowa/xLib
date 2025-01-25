6852bedea3577b0fc6d56124d6a2aa8c305a1439
skynowa <skynowa@gmail.com>
2025-01-25 02:19:40 +0200

    libs_install.sh (+2) [11+] - impl

---
+++ Tools/libs_install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Pre-requisites
+#
+# - bash-compatible Unix command shell (for Unix)
+# - Doxygen 1.8.2: `sudo apt-get install doxygen`
+# - `sudo apt-get install cmake pkg-config`
+# - `~/Projects/CMakeLib/install.sh`
+
+
+# TODO
