59c7a7a861dbe0c428bdad9747c0e314abcfbf50
skynowa <skynowa@gmail.com>
2025-03-03 14:42:55 +0200

    change-log: ChangeLog.md (+2) [84+/78-] - impl

--- ChangeLog.sh
+++ ChangeLog.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# \file  ChangeLog.sh
+# \brief Generate change log from MRs
+#
+
 
 LOGFILE="ChangeLog.md"
+GIT_REPOSITORY="https://github.com/skynowa/xLib"
 
 # Header
-echo "# Merge Request Change log" > "$LOGFILE"
+echo "# Change log" > "$LOGFILE"
 echo "" >> "$LOGFILE"
 
 # Extract merge commits and format as Markdown
-git log --merges --pretty=format:"## %s%n- **Commit:** [%h](https://your.git.repo/commit/%h)%n- **Author:** %an%n- **Date:** %cd%n" --date=short >> "$LOGFILE"
+git log --merges --pretty=format:"## %s%n- **Commit:** [%h](${GIT_REPOSITORY}/commit/%h)%n- **Author:** %an%n- **Date:** %cd%n" --date=short >> "$LOGFILE"
 
-echo "Merge request change log saved to $LOGFILE"
+echo "$LOGFILE - Saved"
