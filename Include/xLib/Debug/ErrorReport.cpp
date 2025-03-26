be39d221c84457e48d413263c76db54fa8ef94c0
skynowa <skynowa@gmail.com>
2025-03-27 00:07:27 +0200

    ErrorReport.cpp (+2) [1+/1-] - Includes - review

--- Include/xLib/Debug/ErrorReport.cpp
+++ Include/xLib/Debug/ErrorReport.cpp
@@ -13,6 +13,7 @@
 #include <xLib/System/Console.h>
 #include <xLib/Debug/BuildInfo.h>
 #include <xLib/Debug/Exception.h>
+#include <xLib/Debug/SourceInfo.h>
 #include <xLib/Fs/Path.h>
 #include <xLib/Sync/Thread.h>
 
