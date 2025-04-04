59df3c4a1cb78be0704dd9212597073e63aba47c
skynowa <skynowa@gmail.com>
2025-04-04 19:01:34 +0300

    Env.h [1+/1-] - operator[] - impl

--- Include/xLib/System/Env.h
+++ Include/xLib/System/Env.h
@@ -98,8 +98,8 @@ public:
 		///< get all values
 	std::tstring_t     findFirstOf(std::cvec_tstring_t &names) const;
 		///< get first value by names
-
 	std::tstring_t     operator[](std::ctstring_t &a_name) const;
+		///< get value by name
 ///\}
 
 xPUBLIC_STATIC:
