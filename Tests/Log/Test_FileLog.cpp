99fc3680ddde1e90c1604fbe93c403a44f405a12
skynowa <skynowa@gmail.com>
2025-05-18 14:49:40 +0300

    Test_FileLog.cpp [14+/14-] - --amend

--- Tests/Log/Test_FileLog.cpp
+++ Tests/Log/Test_FileLog.cpp
@@ -35,28 +35,28 @@ Test_FileLog::unit()
 
     xTEST_CASE("write(...)")
     {
-        log.write(ILog::Level::Off,      xT("\t%s, %d"), xLEX_TO_STR(ILog::L::::Unknown), 12345);
-        log.write(ILog::Level::Critical, xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Critical), 12345);
-        log.write(ILog::Level::Warning,  xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Warning), 12345);
-        log.write(ILog::Level::Critical, xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Critical), 12345);
-        log.write(ILog::Level::Error,    xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Error), 12345);
-        log.write(ILog::Level::Warning,  xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Warning), 12345);
-        log.write(ILog::Level::Trace,    xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Trace), 12345);
-        log.write(ILog::Level::Info,     xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Info), 12345);
-        log.write(ILog::Level::Debug,    xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Debug), 12345);
-        log.write(ILog::Level::Trace,    xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Trace), 12345);
+        log.write(ILog::Level::Off,      xT("%s, %d"), xLEX_TO_STR(ILog::L::::Unknown), 12345);
+        log.write(ILog::Level::Critical, xT("%s, %d"), xLEX_TO_STR(ILog::Level::Critical), 12345);
+        log.write(ILog::Level::Warning,  xT("%s, %d"), xLEX_TO_STR(ILog::Level::Warning), 12345);
+        log.write(ILog::Level::Critical, xT("%s, %d"), xLEX_TO_STR(ILog::Level::Critical), 12345);
+        log.write(ILog::Level::Error,    xT("%s, %d"), xLEX_TO_STR(ILog::Level::Error), 12345);
+        log.write(ILog::Level::Warning,  xT("%s, %d"), xLEX_TO_STR(ILog::Level::Warning), 12345);
+        log.write(ILog::Level::Trace,    xT("%s, %d"), xLEX_TO_STR(ILog::Level::Trace), 12345);
+        log.write(ILog::Level::Info,     xT("%s, %d"), xLEX_TO_STR(ILog::Level::Info), 12345);
+        log.write(ILog::Level::Debug,    xT("%s, %d"), xLEX_TO_STR(ILog::Level::Debug), 12345);
+        log.write(ILog::Level::Trace,    xT("%s, %d"), xLEX_TO_STR(ILog::Level::Trace), 12345);
     }
 
     xTEST_CASE("clear")
     {
-        log.clear();
-        xTEST_EQ(FileInfo( log.filePath( )).size(), longlong_t(0));
+		log.clear();
+		xTEST_EQ(FileInfo( log.filePath( )).size(), longlong_t(0));
     }
 
     xTEST_CASE("remove")
     {
-        log.remove();
-        xTEST(!FileInfo(log.filePath()).isExists());
+		log.remove();
+		xTEST(!FileInfo(log.filePath()).isExists());
     }
 
     return true;
