77863e93c10b862a7caa050f836ab52c8331ac9f
skynowa <skynowa@gmail.com>
2025-05-18 14:35:47 +0300

    Test_SystemLog.cpp (+3) [22+/21-] - --amend

--- Build/test_unit.sh
+++ Build/test_unit.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 
+UNIT_TESTS=(FileLog)
 # UNIT_TESTS=(MySql)
 # UNIT_TESTS=(Thread)
 # UNIT_TESTS=(Dll)
 # UNIT_TESTS=(Env)
 # UNIT_TESTS=(DateTime)
 # UNIT_TESTS=(Type)
-UNIT_TESTS=(SourceInfo)
+# UNIT_TESTS=(SourceInfo)
 # UNIT_TESTS=(StackTrace)
 # UNIT_TESTS=(BuildInfo)
 # UNIT_TESTS=(FileText FileBin)
--- Tests/Log/Test_SystemLog.cpp
+++ Tests/Log/Test_SystemLog.cpp
@@ -56,16 +56,16 @@ Test_SystemLog::unit()
     xTEST_CASE("write(...)")
     {
     #if xTEST_IGNORE
-        SystemLog().write(ILog::Level::Off,      xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Off), 12345);
-        SystemLog().write(ILog::Level::Critical, xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Critical), 12345);
-        SystemLog().write(ILog::Level::Warning,  xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Warning), 12345);
-        SystemLog().write(ILog::Level::Critical, xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Critical), 12345);
-        SystemLog().write(ILog::Level::Error,    xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Error), 12345);
-        SystemLog().write(ILog::Level::Warning,  xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Warning), 12345);
-        SystemLog().write(ILog::Level::Trace,    xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Trace), 12345);
-        SystemLog().write(ILog::Level::Info,     xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Info), 12345);
-        SystemLog().write(ILog::Level::Debug,    xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Debug), 12345);
-        SystemLog().write(ILog::Level::Trace,    xT("\t%s, %d"), xLEX_TO_STR(ILog::Level::Trace), 12345);
+        SystemLog().write(ILog::Level::Off,      xT("%s, %d"), xLEX_TO_STR(ILog::Level::Off), 12345);
+        SystemLog().write(ILog::Level::Critical, xT("%s, %d"), xLEX_TO_STR(ILog::Level::Critical), 12345);
+        SystemLog().write(ILog::Level::Warning,  xT("%s, %d"), xLEX_TO_STR(ILog::Level::Warning), 12345);
+        SystemLog().write(ILog::Level::Critical, xT("%s, %d"), xLEX_TO_STR(ILog::Level::Critical), 12345);
+        SystemLog().write(ILog::Level::Error,    xT("%s, %d"), xLEX_TO_STR(ILog::Level::Error), 12345);
+        SystemLog().write(ILog::Level::Warning,  xT("%s, %d"), xLEX_TO_STR(ILog::Level::Warning), 12345);
+        SystemLog().write(ILog::Level::Trace,    xT("%s, %d"), xLEX_TO_STR(ILog::Level::Trace), 12345);
+        SystemLog().write(ILog::Level::Info,     xT("%s, %d"), xLEX_TO_STR(ILog::Level::Info), 12345);
+        SystemLog().write(ILog::Level::Debug,    xT("%s, %d"), xLEX_TO_STR(ILog::Level::Debug), 12345);
+        SystemLog().write(ILog::Level::Trace,    xT("%s, %d"), xLEX_TO_STR(ILog::Level::Trace), 12345);
     #endif
     }
 
