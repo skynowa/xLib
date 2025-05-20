d9cd4727ff1769db6e4d757f1b12d818e8fa0fd0
skynowa <skynowa@gmail.com>
2025-05-20 23:05:59 +0300

    ilog-review: Trace.h [2+/2-] - revert

--- Include/xLib/Log/Trace.h
+++ Include/xLib/Log/Trace.h
@@ -26,8 +26,8 @@ class Trace final :
 public:
 ///\name ctors, dtor
 ///\{
-             Trace() = default;
-    virtual ~Trace();
+			 Trace() = default;
+	virtual ~Trace();
 ///\}
 
 ///\name Overrides
