012eb7cc16afa31278350e1d01322ec955b1168b
skynowa <skynowa@gmail.com>
2025-06-06 20:48:50 +0300

    TraceStream-impl: Test_Trace.cpp [1+/13-] - impl

--- Tests/Log/Test_Trace.cpp
+++ Tests/Log/Test_Trace.cpp
@@ -29,25 +29,13 @@ Test_Trace::unit()
     #endif
     }
 
-    xTEST_CASE("operator <<")
+    xTEST_CASE("TraceStream")
     {
-	#if 0
-		std::ctstring_t str     = xT("test");
-		int_t           integer = 777;
-
-		Trace() << xTRACE_VAR(str) << ", " << xTRACE_VAR(integer);
-	#endif
-    }
-
-    xTEST_CASE("operator <<")
-    {
-	#if 1
 		std::ctstring_t str     = xT("test");
 		int_t           integer = 777;
 
 		TraceStream() << xTRACE_VAR(str) << ", " << xTRACE_VAR(integer);
 		TraceStream(ILog::Level::Error) << xTRACE_VAR(str) << ", " << xTRACE_VAR(integer);
-	#endif
     }
 
     return true;
