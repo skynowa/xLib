d36266bd164a681e51702ae51eb476f4a3e0a31f
skynowa <skynowa@gmail.com>
2025-06-20 21:06:51 +0300

    log-cout: Cout.cpp (+3) [29+/8-] - impl

--- Include/xLib/Log/Cout.cpp
+++ Include/xLib/Log/Cout.cpp
@@ -46,7 +46,28 @@ Cout::write(
         if (a_level == ILog::Level::Trace) {
             msg = a_msg;
         } else {
-            msg = _levelString(a_level) + xT(": ") + a_msg;
+            std::tstring_t levelStr = _levelString(a_level);
+
+		#if 0
+			case Level::Debug:   oss << "\033[30;42m " << level_str << " \033[0m "; break; // black on green
+			case Level::Info:    oss << "\033[37;44m " << level_str << " \033[0m "; break; // white on blue
+			case Level::Warning: oss << "\033[30;43m " << level_str << " \033[0m "; break; // black on yellow
+			case Level::Error:   oss << "\033[37;41m " << level_str << " \033[0m "; break; // white on red
+			case Level::Fatal:   oss << "\033[37;45m " << level_str << " \033[0m "; break; // white on magenta
+		#endif
+			if (1) {
+				switch (a_level) {
+				case Level::Trace:
+					break;
+				case Level::Debug:    std::tcout << "\033[30;42m " << levelStr << " \033[0m "; break; // black on green
+				case Level::Info:     std::tcout << "\033[37;44m " << levelStr << " \033[0m "; break; // white on blue
+				case Level::Warning:  std::tcout << "\033[30;43m " << levelStr << " \033[0m "; break; // black on yellow
+				case Level::Error:    std::tcout << "\033[37;41m " << levelStr << " \033[0m "; break; // white on red
+				case Level::Critical: std::tcout << "\033[37;45m " << levelStr << " \033[0m "; break; // white on magenta
+				}
+			}
+
+            msg = /* levelStr + */ xT(": ") + a_msg;
         }
     }
 
