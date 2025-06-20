b0666bc9873f28f37c0edd5c9828d499b85cdfab
skynowa <skynowa@gmail.com>
2025-06-20 19:40:10 +0300

    console-attrs: Test_Console.cpp [43+/43-] - code style

--- Tests/System/Test_Console.cpp
+++ Tests/System/Test_Console.cpp
@@ -20,99 +20,99 @@ Test_Console::unit()
         const auto fg = Color::FG::Green;
         const auto bg = Color::BG::Default;
 
-		const auto attrs = static_cast<Color::Attr>(
-				static_cast<int_t>(Color::Attr::Bold) |
-				static_cast<int_t>(Color::Attr::Underline) |
-				static_cast<int_t>(Color::Attr::Dim));
+        const auto attrs = static_cast<Color::Attr>(
+                static_cast<int_t>(Color::Attr::Bold) |
+                static_cast<int_t>(Color::Attr::Underline) |
+                static_cast<int_t>(Color::Attr::Dim));
 
-		Color color(true, false, fg, bg, attrs);
+        Color color(true, false, fg, bg, attrs);
 
         Console console;
-		console.write( color.set() );
-		console.write(text);
-		console.writeLine( color.clear() );
+        console.write( color.set() );
+        console.write(text);
+        console.writeLine( color.clear() );
 
-		console.writeLine(color, text);
+        console.writeLine(color, text);
     }
 
     xTEST_CASE("read")
     {
-	#if xTEST_IGNORE
-		Console console;
-		m_sRv = console.read();
-		xTEST_NA(m_sRv);
+    #if xTEST_IGNORE
+        Console console;
+        m_sRv = console.read();
+        xTEST_NA(m_sRv);
     #else
         LogCout() << xT("[skip]");
-	#endif
+    #endif
     }
 
     xTEST_CASE("write")
     {
-	#if xTEST_IGNORE
-		std::ctstring_t str = xT("\tConsole_test_string");
+    #if xTEST_IGNORE
+        std::ctstring_t str = xT("\tConsole_test_string");
 
-		Console console;
-		console.write(str);
+        Console console;
+        console.write(str);
     #else
         LogCout() << xT("[skip]");
-	#endif
+    #endif
     }
 
     xTEST_CASE("writeLine")
     {
-	#if xTEST_IGNORE
-		std::ctstring_t str = xT("\tConsole_test_line");
+    #if xTEST_IGNORE
+        std::ctstring_t str = xT("\tConsole_test_line");
 
-		Console console;
-		console.writeLine(str);
+        Console console;
+        console.writeLine(str);
     #else
         LogCout() << xT("[skip]");
-	#endif
+    #endif
     }
 
     xTEST_CASE("writeLine")
     {
-	#if xTEST_IGNORE
-		std::ctstring_t str = xT("\tConsole_test_error");
+    #if xTEST_IGNORE
+        std::ctstring_t str = xT("\tConsole_test_error");
 
-		Console console;
-		console.writeLine(str);
+        Console console;
+        console.writeLine(str);
     #else
         LogCout() << xT("[skip]");
-	#endif
+    #endif
     }
 
     xTEST_CASE("pause")
     {
-	#if xTEST_IGNORE
-		Console console;
-		console.pause(3 * 1000UL);
-		console.pause(xTIMEOUT_INFINITE);
+    #if xTEST_IGNORE
+        Console console;
+        console.pause(3 * 1000UL);
+        console.pause(xTIMEOUT_INFINITE);
     #else
         LogCout() << xT("[skip]");
-	#endif
+    #endif
     }
 
     xTEST_CASE("clear")
     {
-	#if xTEST_IGNORE
-		Console console;
-		console.clear();
+    #if xTEST_IGNORE
+        Console console;
+        console.clear();
     #else
         LogCout() << xT("[skip]");
-	#endif
+    #endif
     }
 
     xTEST_CASE("setTitle")
     {
-	#if xTEST_IGNORE
-		std::ctstring_t title = xT("Title1");
+    #if xTEST_IGNORE
+        std::ctstring_t title = xT("Title1");
 
-		Console console;
-		console.setTitle(title);
+        Console console;
+        console.setTitle(title);
     #else
         LogCout() << xT("[skip]");
-	#endif
+    #endif
     }
 
     return true;
